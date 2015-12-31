495e87cd4da330d4d7c357ebd6939267775520aa
Lev Israel <eliezer.israel@gmail.com>
2015-12-31 17:30:30 +0200

    NLI import

---
+++ sources/NLI/create_notes.py
@@ -0,0 +1,46 @@
+# coding=utf-8
+from sefaria.model import *
+from sefaria.helper.nli import manuscript_data
+
+NoteSet({"generated_by": "NLI_manuscript_linker"}).delete()
+
+indxs = library.get_indexes_in_category("Tosefta") + library.get_indexes_in_category("Mishnah")
+for title in indxs:
+    if "Tosefta Keilim" in title:
+        continue
+    i = library.get_index(title)
+    assert isinstance(i, Index)
+    top_ref = Ref(i.title)
+    section_refs = top_ref.all_subrefs()
+    for section_ref in section_refs:
+        print " - " + section_ref.normal()
+        for manu in manuscript_data(section_ref):
+            print " . " + section_ref.subref(manu["mi_code"]).normal()
+            Note({
+                "owner": 28,
+                "public": True,
+                "text": '<a href="' + manu["app_url"] + '"><img src="' + manu["img_url"] + '"></a>',
+                "type": "note",
+                "ref": section_ref.subref(manu["mi_code"]).normal(),
+                "title": manu["name_en"]
+            }).save()
+
+indxs = library.get_indexes_in_category("Bavli")
+for title in indxs:
+    i = library.get_index(title)
+    assert isinstance(i, Index)
+    top_ref = Ref(i.title)
+    section_refs = top_ref.all_subrefs()
+    for section_ref in section_refs:
+        print " - " + section_ref.normal()
+        for manu in manuscript_data(section_ref):
+            print " . "
+            Note({
+                "owner": 28,
+                "public": True,
+                "text": '<a href="' + manu["app_url"] + '"><img src="' + manu["img_url"] + '"></a>',
+                "type": "note",
+                "ref": section_ref.subref(1).normal(),
+                "title": manu["name_en"],
+                "generated_by": "NLI_manuscript_linker"
+            }).save()
