2b192cea145af4c3634c91f603838f307483d2b7
jonMosenkis <jonathan@sefaria.org>
2016-04-18 12:43:20 +0300

    Parses footnotes and generates links

--- sources/JPS1985/JPS1985.py
+++ sources/JPS1985/JPS1985.py
@@ -27,8 +27,10 @@ Copyright JPS
 """
 
 from sources import functions
-import re
+import regex as re
 import codecs
+from sefaria.model import *
+import sys
 
 
 def process_verses(chap_string, expression):
@@ -174,103 +176,249 @@ def set_flags(books, upload=True):
 
         # set flags
         flags = {
-            "priority": 2
+            "priority": 2,
+            "status": 'locked'
         }
         print book
         functions.post_flags(version, flags)
 
 
-# declare variables
-Books, Chapters, Verses = {}, [], []
-previous_line = u''
-book_name = u''
-errors = []
+def parse():
 
-# regular expressions
-chapter_reg = re.compile(u'\d{1,3}\s')
-verse_reg = re.compile(u'\d{1,3}[a-zA-Z\-"“\[‘(—]')
-footnote_reg = re.compile(u'\[[a-z]\]')
+    # declare variables
+    Books, Chapters, Verses = {}, [], []
+    previous_line = u''
+    book_name = u''
+    errors = []
 
-input_file = codecs.open('JPSTanakhMaster.txt', 'r', 'utf-8')
+    # regular expressions
+    chapter_reg = re.compile(u'\d{1,3}\s')
+    verse_reg = re.compile(u'\d{1,3}[a-zA-Z\-"“\[‘(—]')
+    footnote_reg = re.compile(u'\[[a-z]\]')
 
-# define replacement dictionary
-replacements = {
-        u'H%': u'\u1e24',
-        u'h%': u'\u1e25',
-        u'\n': u'',
-        u'\r': u'',
-        u'-': u'',
-}
+    input_file = codecs.open('JPSTanakhMaster.txt', 'r', 'utf-8')
 
-# loop through file
-for line in input_file:
+    # define replacement dictionary
+    replacements = {
+            u'H%': u'\u1e24',
+            u'h%': u'\u1e25',
+            u'\n': u'',
+            u'\r': u'',
+    }
 
-    # if this line is a parsha name - do nothing
-    if line == line.upper():
-        continue
+    # loop through file
+    for line in input_file:
 
-    # make necessary replacements and strip footnotes
-    line = functions.multiple_replace(line, replacements)
-    footnotes = footnote_reg.findall(line)
-    for case in footnotes:
-        line = line.replace(case, u'')
+        # if this line is a parsha name - do nothing
+        if line == line.upper():
+            continue
 
-    # check if line is beginning of new chapter
-    new_chap = chapter_reg.match(line)
-    if new_chap:
+        # make necessary replacements and strip footnotes
+        line = functions.multiple_replace(line, replacements)
+        '''footnotes = footnote_reg.findall(line)
+        for case in footnotes:
+            line = line.replace(case, u'')'''
 
-        # get chapter num
-        chap_number = int(new_chap.group())
-        if chap_number == 1:
+        # check if line is beginning of new chapter
+        new_chap = chapter_reg.match(line)
+        if new_chap:
 
-            # save previous book
-            if book_name != u'':
-                Chapters.append((process_verses(u''.join(Verses), verse_reg)))
-                check_verses(Chapters[len(Chapters)-1], (book_name.replace(u'\r', u';'), len(Chapters)), errors)
-                Books[book_name] = Chapters
-                Chapters = []
+            # get chapter num
+            chap_number = int(new_chap.group())
+            if chap_number == 1:
+
+                # save previous book
+                if book_name != u'':
+                    Chapters.append((process_verses(u''.join(Verses), verse_reg)))
+                    check_verses(Chapters[len(Chapters)-1], (book_name.replace(u'\r', u';'), len(Chapters)), errors)
+                    Books[book_name] = Chapters
+                    Chapters = []
+                    Verses = []
+
+                book_name = previous_line
+
+            else:
+                Verses.append(previous_line)
+                Chapters.append(process_verses(u''.join(Verses), verse_reg))
+                check_verses(Chapters[len(Chapters) - 1], (book_name.replace(u'\r', u';'), chap_number - 1), errors)
                 Verses = []
 
-            book_name = previous_line
+            # check that chapters are incrementing correctly
+            if chap_number - len(Chapters) != 1:
+                errors.append((book_name.replace(u'\r', u';'), chap_number-1))
+                Chapters.append([u'error'])
+
+            # copy line into previous_line placeholder, excluding the chapter number itself
+
+            # if chapter number is 1, this is a new book
+            previous_line = line[new_chap.end():]
 
         else:
+
+            # Add previous line to verses, and save current line
             Verses.append(previous_line)
-            Chapters.append(process_verses(u''.join(Verses), verse_reg))
-            check_verses(Chapters[len(Chapters) - 1], (book_name.replace(u'\r', u';'), chap_number - 1), errors)
-            Verses = []
+            previous_line = line
 
-        # check that chapters are incrementing correctly
-        if chap_number - len(Chapters) != 1:
-            errors.append((book_name.replace(u'\r', u';'), chap_number-1))
-            Chapters.append([u'error'])
+    # add last book
+    Chapters.append((process_verses(u''.join(Verses), verse_reg)))
+    check_verses(Chapters[len(Chapters) - 1], (book_name.replace(u'\r', u';'), len(Chapters)), errors)
+    Books[book_name] = Chapters
 
-        # copy line into previous_line placeholder, excluding the chapter number itself
+    input_file.close()
+    out = codecs.open('output.txt', 'w', 'utf-8')
+    write_to_file(Books, out)
+    out.close()
 
-        # if chapter number is 1, this is a new book
-        previous_line = line[new_chap.end():]
+    # write errors
+    out = codecs.open('errors.txt', 'w', 'utf-8')
+    for error in errors:
+        out.write('Book: {0} Chapter: {1}\n'.format(*error))
+    out.close()
+    print len(errors)
 
-    else:
+    return Books
 
-        # Add previous line to verses, and save current line
-        Verses.append(previous_line)
-        previous_line = line
 
-# add last book
-Chapters.append((process_verses(u''.join(Verses), verse_reg)))
-check_verses(Chapters[len(Chapters) - 1], (book_name.replace(u'\r', u';'), len(Chapters)), errors)
-Books[book_name] = Chapters
+def count_foot(books, regexes):
+    """
+    loop through books and count number of times regex appears
 
-input_file.close()
-out = codecs.open('output.txt', 'w', 'utf-8')
-write_to_file(Books, out)
-out.close()
+    :param books: dictionary containing entire jps 1985 translation
+    :param regexes: list of regular expressions to compare
+    :return: number of appearances of the regular expression
+    """
 
-# write errors
-out = codecs.open('errors.txt', 'w', 'utf-8')
-for error in errors:
-    out.write('Book: {0} Chapter: {1}\n'.format(*error))
-out.close()
-print len(errors)
-upload_all(Books, False)
-set_flags(Books)
+    all_books = library.get_indexes_in_category('Tanach')
+    total = 0
 
+    # define replacement dictionary
+    replacements = {
+        u'\n': u'',
+        u'\r': u'',
+        u'\n\r': u'',
+    }
+
+    # loop through books, and combine each chapter into one string
+    for book in all_books:
+        for num, chapter in enumerate(books[book]):
+
+            # make chapter into single string (to account for footnotes spanning multiple lines and verses)
+            text = []
+            for verse in chapter:
+                text.append(functions.multiple_replace(verse, replacements))
+
+            text = ' '.join(text)
+            counts = []
+            for reg in regexes:
+                counts.append(len(re.findall(reg, text)))
+
+            cannon = counts[0]
+            for index, count in enumerate(counts):
+                if count != cannon:
+                    print '{}: {}; {}'.format(book, num+1, index)
+                    total += 1
+                    break
+
+    return total
+
+
+def align_footnotes(books):
+    """
+    The footnotes need to be structured by book and chapter. Each footnote may refer to multiple verses.
+
+    :param books: Dictionary, containing the entire JPS 1985 translation
+    return: Dictionary, with books as keys and chapters as values. Each chapter is a list of dictionaries,
+    with the key "footnote" set to the footnote and the key "links" being a list of verses where the
+    footnote appears.
+    """
+
+    jps_footnotes = {}
+
+    # define replacement dictionary
+    replacements = {
+        u'@': u'\u1e63',
+        u'h%': u'\u1e25',
+        u'H%': u'\u1e24',
+        u'\n': u'',
+        u'\r': u'',
+        u'\n\r': u'',
+    }
+
+    # get list of book in tanach
+    all_books = library.get_indexes_in_category('Tanach')
+
+    # open footnote document and retrieve first footnote
+    input_file = codecs.open('JPS1985_footnotes.txt', 'r', 'utf-8')
+    note = {'footnote': functions.multiple_replace(input_file.readline(), replacements)}
+
+    # iterate through Tanach
+    for book in all_books:
+
+        # set dictionary to with keys set to chapter numbers
+        footnote_chaps = {}
+
+        for chap_num, chapter in enumerate(books[book]):
+
+            # set flag to indicate if any footnote markers have been found
+            found_note = False
+
+            # declare array to hold all note in chapter
+            chap_notes = []
+
+            # repeatedly loop through chapter, searching for cases where footnote appears
+            while True:
+
+                # account for the case where a footnote is tagged "aa"
+                if note['footnote'].find(u'aa') == 0:
+                    tag = u'aa'
+                else:
+                    tag = note['footnote'][0]
+
+                found = []
+                for verse_num, verse in enumerate(chapter):
+                    if verse.find(u'[{}]'.format(tag)) != -1:
+                        found.append(verse_num+1)
+                        found_note = True
+                    note['links'] = found
+
+                # if footnote markers were found, get the next footnote
+                if found_note:
+                    chap_notes.append(note)
+                    note = {'footnote': functions.multiple_replace(input_file.readline(), replacements)}
+
+                    if note['footnote'] == u'':
+                        footnote_chaps[chap_num] = chap_notes
+                        break
+
+                # if footnote begins with "a", this is a new chapter
+                try:
+                    if note['footnote'][0] == u'a' and note['footnote'][1] != u'a':
+                        footnote_chaps[chap_num] = chap_notes
+                        break
+
+                except IndexError:
+                    print 'error'
+                    print note['footnote']
+                    print u'{}, chapter {}'.format(book, chap_num+1)
+                    input_file.close()
+                    sys.exit(1)
+
+        jps_footnotes[book] = functions.convertDictToArray(footnote_chaps)
+
+    input_file.close()
+    return jps_footnotes
+
+test = codecs.open('test.txt', 'w', 'utf-8')
+jps = parse()
+footnotes = align_footnotes(jps)
+books = library.get_indexes_in_category('Tanach')
+for book in books:
+    test.write(u'{}:\n'.format(book))
+    for index, chapter in enumerate(footnotes[book]):
+        if chapter != []:
+            test.write(u'  chapter {}:\n'.format(index+1))
+            for note in chapter:
+                test.write(u'    {}\n'.format(note['footnote']))
+                test.write(u'    links:{}\n'.format(note['links']))
+
+test.close()
