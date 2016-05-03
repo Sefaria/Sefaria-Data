31c7141974ceda125d51491d78b082b9288bf723
stevekaplan123 <skaplan@brandeis.edu>
2016-05-03 11:30:08 +0300

    mishnah

---
+++ sources/Mishnah/sanity_check.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+import urllib
+import urllib2
+from urllib2 import URLError, HTTPError
+import json
+import pdb
+import os
+import sys
+from bs4 import BeautifulSoup
+import re
+p = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.insert(0, p)
+sys.path.insert(0, '../Match/')
+from match import Match
+os.environ['DJANGO_SETTINGS_MODULE'] = "sefaria.settings"
+from local_settings import *
+from functions import *
+import glob
+
+sys.path.insert(0, SEFARIA_PROJECT_PATH)
+from sefaria.model import *
+from sefaria.model.schema import AddressTalmud
+
+
+#checked Mishnayot for Pereks being in order and within each perek each comment is in order
+#checked Boaz for Pereks being in order but DID NOT check that each comment within each perek is in order
+
+#dont_count=['פ"', 'פרק ', 'בבא','פ', 'מעשר', 'פתח']
+
+count=0
+for file in glob.glob(u"*.txt"):
+    file = file.replace(u"\u200f", u"")
+    if file.split(" ")[0] == u"בועז":
+        count += 1
+        print file
+        in_order(file, tag="@00", dont_count=['פ"', 'פרק ', 'בבא','פ', 'מעשר', 'פתח'])
