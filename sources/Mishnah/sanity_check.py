2c2fa1db593d231910d7dbe205bacd91648f03a0
jonMosenkis <jonathan@sefaria.org>
2016-05-04 12:50:28 +0300

    merged my test scripts with Steve's

--- sources/Mishnah/sanity_check.py
+++ sources/Mishnah/sanity_check.py
@@ -1,13 +1,9 @@
 # -*- coding: utf-8 -*-
-import urllib
-import urllib2
-from urllib2 import URLError, HTTPError
-import json
 import pdb
 import os
 import sys
-from bs4 import BeautifulSoup
 import re
+import codecs
 p = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 sys.path.insert(0, p)
 sys.path.insert(0, '../Match/')
@@ -34,3 +30,26 @@ for file in glob.glob(u"*.txt"):
         count += 1
         print file
         in_order(file, tag="@00", dont_count=['פ"', 'פרק ', 'בבא','פ', 'מעשר', 'פתח'])
+
+
+def files_exist():
+    """
+    Looks through the directory to ensure all files that should be there do exist. It will also take care
+    of naming conventions - i.e. it will mark a file as missing if it's named in an unpredictable manner.
+    """
+
+    # get a list of all tractates
+    tractates = library.get_indexes_in_category('Mishnah')
+    missing = codecs.open('missing_tractates.txt', 'w', 'utf-8')
+
+    for tractate in tractates:
+        ref = Ref(tractate)
+        name = ref.he_book()
+        terms = [u'משניות', u'יכין', u'בועז']
+
+        for term in terms:
+            file_name = u'{}.txt'.format(name.replace(u'משנה', term))
+            if not os.path.isfile(file_name):
+                missing.write(u'{}\n'.format(file_name))
+
+    missing.close()
