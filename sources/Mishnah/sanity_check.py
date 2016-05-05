5077c961b6018c857191d2fa61904bed744e410f
jonMosenkis <jonathan@sefaria.org>
2016-05-05 11:34:44 +0300

    moved code into a function

--- sources/Mishnah/sanity_check.py
+++ sources/Mishnah/sanity_check.py
@@ -18,18 +18,19 @@ from sefaria.model import *
 from sefaria.model.schema import AddressTalmud
 
 
-#checked Mishnayot for Pereks being in order and within each perek each comment is in order
-#checked Boaz for Pereks being in order but DID NOT check that each comment within each perek is in order
+def check_mishna_oreder:
+    #checked Mishnayot for Pereks being in order and within each perek each comment is in order
+    #checked Boaz for Pereks being in order but DID NOT check that each comment within each perek is in order
 
-# dont_count=['פ"', 'פרק ', 'בבא','פ', 'מעשר', 'פתח']
+    # dont_count=['פ"', 'פרק ', 'בבא','פ', 'מעשר', 'פתח']
 
-count=0
-for file in glob.glob(u"*.txt"):
-    file = file.replace(u"\u200f", u"")
-    if file.split(" ")[0] == u"משניות":
-        count += 1
-        print file
-        in_order(file, tag="@22", reset_tag="@00", increment_by=1)
+    count=0
+    for file in glob.glob(u"*.txt"):
+        file = file.replace(u"\u200f", u"")
+        if file.split(" ")[0] == u"משניות":
+            count += 1
+            print file
+            in_order(file, tag="@22", reset_tag="@00", increment_by=1)
 
 
 def files_exist():
