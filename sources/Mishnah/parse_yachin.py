ef6a3701f70ad7d5933e916d93c3103d8b586f47
jonMosenkis <jonathan@sefaria.org>
2016-05-29 16:31:33 +0300

    added text aligner function

--- sources/Mishnah/parse_yachin.py
+++ sources/Mishnah/parse_yachin.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
+import codecs
 from sefaria.datatype import jagged_array
 import re
+from data_utilities.util import jagged_array_to_file as j_to_file
 
 
 def file_to_ja(structure, infile, expressions, cleaner):
     """
-    Designed to be the first stage of a reusable parsing tool. Adds lines of text to the Jagged Array
-    in the desired structure (Chapter, verse, etc.)
+    Designed to be the first stage of a reusable parsing tool. Adds lines of text to the Jagged
+    Array in the desired structure (Chapter, verse, etc.)
     :param structure: A nested list one level lower than the final result. Example: for a depth 2
     text, structure should be [[]].
     :param infile: Text file to read from
@@ -40,19 +42,54 @@ def file_to_ja(structure, infile, expressions, cleaner):
         # check for matches to the regexes
         for i, reg in enumerate(regexes):
 
-            if reg:
+            if reg.search(line):
                 # check that we've hit the first chapter and verse
                 if indices.count(-1) == 0:
                     ja.set_element(indices, cleaner(temp))
+                    temp = []
 
                 # increment index that's been hit, reset all subsequent indices
                 indices[i] += 1
-                indices[i+1:] = [0 for x in indices[i+1]]
+                indices[i+1:] = [0 for x in indices[i+1:]]
                 break
 
         else:
-            temp.append(line)
+            if indices.count(-1) == 0:
+                temp.append(line)
     else:
         ja.set_element(indices, cleaner(temp))
 
     return ja
+
+
+def align_comments(text_array):
+    # strip out unnecessary lines
+    remove = re.compile(u'@99')
+    for index, line in enumerate(text_array):
+        if remove.search(line):
+            del text_array[index]
+
+    result, tmp = [], []
+    t = u''.join(text_array)
+    t = t.replace(u'\n', u'')
+    t = t.replace(u'\r', u'')
+    t = t.split(u' ')
+    for word in t:
+        if re.search(u'@11[\u05d0-\u05ea"]{1,4}\)', word):
+            if tmp:
+                result.append(re.sub(u'@[0-9]{2}', u'', u' '.join(tmp)))
+            tmp = []
+        else:
+            tmp.append(word)
+
+    else:
+        result.append(re.sub(u'@[0-9]{2}', u'', u' '.join(tmp)))
+    return result
+
+
+infile = codecs.open('יכין עדיות.txt', 'r', 'utf-8')
+j = file_to_ja([[]], infile, [u'@00פרק'], align_comments)
+infile.close()
+outfile = codecs.open('yachin_test.txt', 'w', 'utf-8')
+j_to_file(outfile, j.array(), ['perek', 'comment'])
+outfile.close()
