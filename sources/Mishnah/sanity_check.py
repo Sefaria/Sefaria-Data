e5dde2690b27982f95495aa9d332f9bccfff73f9
jonMosenkis <jonathan@sefaria.org>
2016-06-02 12:35:26 +0300

    set tester to run on dev server

--- sources/Mishnah/sanity_check.py
+++ sources/Mishnah/sanity_check.py
@@ -415,11 +415,11 @@ def find_weird_stuff(searchlist):
     results.close()
 
 
-check_tags_on_category(u'משניות', u'@22', u'@22([\u05d0-\u05ea"]{1,3})', he_tags_in_order)
+'''check_tags_on_category(u'משניות', u'@22', u'@22([\u05d0-\u05ea"]{1,3})', he_tags_in_order)
 check_tags_on_category(u'יכין', u'@11', u'@11([\u05d0-\u05ea"]{1,3})\)', he_tags_in_order)
 check_tags_on_category(u'בועז', u'@22', u'@22(\([\u05d0-\u05ea"]{1,3})\)', he_tags_in_order)
 compare_mishna_to_yachin(library.get_indexes_in_category('Mishnah'))
-check_chapters(u'משניות', u'@00(?:פרק |פ)([א-ת,"]{1,3})')
+check_chapters(u'משניות', u'@00(?:פרק |פ)([א-ת,"]{1,3})')'''
 
 find_weird_stuff(u'([#\*\?]|\([\u05d0-\u05ea]\)|\[[\u05d0-\u05ea]\])')
 
