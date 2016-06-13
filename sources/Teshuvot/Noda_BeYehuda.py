3df74b44a4206aa302073e605a3073373f98cbf1
jonMosenkis <jonathan@sefaria.org>
2016-06-13 16:31:19 +0300

    check that chapters run in order

---
+++ sources/Teshuvot/Noda_BeYehuda.py
@@ -0,0 +1,36 @@
+# encoding=utf-8
+
+from data_utilities import sanity_checks as tests
+from sources import functions
+import codecs
+from sefaria.model import *
+
+noda_file = codecs.open('Noda_BeYehuda.txt', 'r', 'utf-8')
+
+
+def chapter_in_order(infile, tag, tag_reg, group=0):
+    """
+    Check that the chapters run in order
+    :param infile: input file to examine
+    :param tag: Exact form of tag
+    :param tag_reg: A regular expression to use to find chapters
+    :param group: Capture group for regex if necessary
+    :return: A list of lines where order is broken
+    """
+
+    # grab all chapter headers and convert to numbers
+    tester = tests.TagTester(tag, infile, tag_reg)
+    tester.skip_to_next_segment(u'@00')
+    all_chapters = []
+    while not tester.eof:
+        titles = tester.grab_each_header(u'@00', group)
+        chap_numbers = [functions.getGematria(txt) for txt in titles]
+        all_chapters.append(chap_numbers)
+
+    # check that chapters match index
+    for book_num, book in enumerate(all_chapters):
+        for index, chapter in enumerate(book):
+            if chapter - index != 1:
+                print 'error in {} chapter {}'.format(book_num+1, chapter)
+
+noda_file.close()
