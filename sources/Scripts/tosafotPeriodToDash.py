d6ed143cbfa3076d43ff0d00cab45045efe11503
Joshua Goldmeier <joshuaegoldmeier@gmail.com>
2016-06-16 17:16:42 +0300

    prevents periods and colons that indicated the amud to stop being changed

--- sources/Scripts/tosafotPeriodToDash.py
+++ sources/Scripts/tosafotPeriodToDash.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 import codecs
+
+import regex
+
 from sefaria.model import *
 from sources.functions import post_text
 
@@ -21,61 +24,70 @@ then, run the remaining Tosafot through a changer
 """
 
 
-def standardizeTosafotDivreiHamtchilToDash():
-    tosafotReferences = getCommentatorReferenceCollection("Tosafot")
-    listOfFixedTosafot = standardizeDashes(tosafotReferences)
-    replaceTheTextsViaAPI(listOfFixedTosafot)
+def standardize_tosafot_divrei_hamatchil_to_dash():
+    tosafot_references = get_commentator_reference_collection("Tosafot")
+    after_the_first_trim = remove_comments_every_period_and_colon_is_amud_marker(tosafot_references)
+    the_second_coming = make_the_switches(after_the_first_trim)
+    replace_the_texts_via_api(the_second_coming)
 
 
-def getCommentatorReferenceCollection(commentator):
-        allRefs = []
-    #for mesechet in library.get_indexes_in_category('Bavli'):
-        mesechet = 'Shevuot'
-        allRefs.append(library.get_index(getReferenceName(commentator,mesechet)).all_segment_refs())
-        return allRefs
+def get_commentator_reference_collection(commentator):
+    all_refs = []
+    # for mesechet in library.get_indexes_in_category('Bavli'):
+    mesechet = 'Shevuot'
+    all_refs.append(library.get_index(get_reference_name(commentator, mesechet)).all_segment_refs())
+    return all_refs
 
 
-def getReferenceName(commentator, mesechet):
+def get_reference_name(commentator, mesechet):
     return "{} on {}".format(commentator, mesechet)
 
 
-def standardizeDashes(tosafotReferences):
-    listOfRefsAndTexts =[]
-    for mesechet in tosafotReferences:
+def remove_comments_every_period_and_colon_is_amud_marker(tosafot_references):
+    periods = regex.compile('\(.{,8}\..{,8}\)')
+    colons = regex.compile('\(.{,8}:.{,8}\)')
+    after_the_first_trim = []
+    for mesechet in tosafot_references:
         for eachComment in mesechet:
-            fixedTosafot = False
-            commentary = TextChunk(eachComment,'he').as_string()
-            if (commentary.find(u'\u2013') or commentary.find(u'\u2014')):
-                commentary = commentary.replace(u'\u2013','-').replace(u'\u2014','-')
-                fixedTosafot = True
-
-            theFirstDash = commentary.find('-')
-            theFirstPeriod = commentary.find('.')
-            if (theFirstPeriod!=-1 and theFirstPeriod < 100 and (theFirstPeriod<theFirstDash or theFirstDash == -1)):
-                commentary = commentary.replace('.',' -',1)
-                fixedTosafot = True
-            elif(commentary.count(':')>1 and commentary.find(':')<100):
-                commentary = commentary.replace(':',' -',1)
-                fixedTosafot = True
-            if(fixedTosafot):
-                listOfRefsAndTexts.append([eachComment.uid(),createTexts(commentary, eachComment)])
-
-    return listOfRefsAndTexts
+            commentary = TextChunk(eachComment, 'he').as_string()
+            number_of_periods_in_parentheses = len(periods.findall(commentary))
+            number_of_colons_in_parentheses = len(colons.findall(commentary))
+            if commentary.count('.') > number_of_periods_in_parentheses or commentary.count(':') > (
+                        number_of_colons_in_parentheses + 1):
+                commentary = commentary.replace(u'\u2013', '-').replace(u'\u2014', '-')
+                after_the_first_trim.append({'ref': eachComment, 'comment': commentary})
+    return after_the_first_trim
 
 
-def createTexts(commentary, reference):
+def make_the_switches(list_of_dicts):
+    changed_tosafots = []
+    for comment in list_of_dicts:
+        commentary = comment['comment']
+        the_first_dash = commentary.find('-')
+        the_first_period = commentary.find('.')
+        if the_first_period != -1 and the_first_period < 150 and commentary[the_first_period+1] != ')' and \
+                (the_first_period < the_first_dash or the_first_dash == -1):
+            commentary = commentary.replace('.', ' -', 1)
+            changed_tosafots.append({'ref': comment['ref'].uid(), 'comment': create_texts(commentary, comment['ref'])})
+        elif (commentary.find(':')+1) != len(commentary) and commentary.find(':') < 150 \
+                and (commentary.find(':')+1) != commentary.find('('):
+            commentary = commentary.replace(':', ' -', 1)
+            changed_tosafots.append({'ref': comment['ref'].uid(), 'comment': create_texts(commentary, comment['ref'])})
+    return changed_tosafots
+
+
+def create_texts(commentary, reference):
     return {
-    "versionTitle": reference.version_list()[0]["versionTitle"],
-    "versionSource": reference.version_list()[0]["versionSource"],
-    "language": "he",
-    "text": commentary
+        "versionTitle": reference.version_list()[0]["versionTitle"],
+        "versionSource": reference.version_list()[0]["versionSource"],
+        "language": "he",
+        "text": commentary
     }
 
 
+def replace_the_texts_via_api(list_of_fixed_tosafot):
+    for eachTosafot in list_of_fixed_tosafot:
+        post_text(eachTosafot['ref'], eachTosafot['comment'])
 
-def replaceTheTextsViaAPI(listOfFixedTosafot):
-    for eachTosafot in listOfFixedTosafot:
-        post_text(eachTosafot[0],eachTosafot[1])
 
-
-standardizeTosafotDivreiHamtchilToDash()
+standardize_tosafot_divrei_hamatchil_to_dash()
