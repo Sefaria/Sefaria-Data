341b32ee4e50f58a8b34fcbaa96e6163a9d6708f
Joshua Goldmeier <joshuaegoldmeier@gmail.com>
2016-06-20 15:22:23 +0300

    sadded a another file which stores tosafot that are empty.  Either the text was erased or the tosafot reference doesnt exist

--- sources/Scripts/collectTheBadTosafot.py
+++ sources/Scripts/collectTheBadTosafot.py
@@ -27,15 +27,21 @@ def get_reference_name(commentator, mesechet):
 def put_bad_tosafot_in_file(tosafot_references):
     bad_tosafot = open('badTosafot.txt', 'w')
     two_dash_tosafot = open('twoDashTosafot.txt', 'w')
+    tosafot_that_dont_exist = open('TosafotThatDontExist.txt', 'w')
     for mesechet in tosafot_references:
         for eachComment in mesechet:
+            print(eachComment.uid())
             commentary = TextChunk(eachComment, 'he').as_string()
             trimmed_commentary = commentary.strip()
-            if trimmed_commentary[-1] != ':':
-                bad_tosafot.write(eachComment.uid() + '\r\n')
-            if trimmed_commentary.count('-') > 1:
-                two_dash_tosafot.write(eachComment.uid() + '\r\n')
+            if len(trimmed_commentary)>0:
+                if trimmed_commentary[-1] != ':':
+                    bad_tosafot.write(eachComment.uid() + '\r\n')
+                if trimmed_commentary.count('-') > 1:
+                    two_dash_tosafot.write(eachComment.uid() + '\r\n')
+            else:
+                tosafot_that_dont_exist.write(eachComment.uid() + '\r\n')
 
+    tosafot_that_dont_exist.close()
     bad_tosafot.close()
     two_dash_tosafot.close()
 
