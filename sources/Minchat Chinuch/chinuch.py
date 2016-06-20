d6c7345706fe389882580ca87af5db475e0f03e6
Jonathan Mosenkis <jonathan@sefaria.org>
2016-06-20 15:40:22 +0300

    tested segments to ensure they run in order

--- sources/Minchat Chinuch/chinuch.py
+++ sources/Minchat Chinuch/chinuch.py
@@ -4,6 +4,7 @@ import codecs
 from data_utilities.sanity_checks import TagTester
 from data_utilities import util
 
+filename = 'Minchat_Chinuch.txt'
 """
 מקרא:
 
@@ -29,7 +30,40 @@ def check_chapters():
             count = util.getGematria(header)
 
             if count != index:
-                print header
+                print util.numToHeb(index)
                 index = count
             index += 1
 
+
+def check_segments():
+
+    segments = []
+
+    infile = codecs.open(filename, 'r', 'utf-8')
+
+    headers = TagTester(u'@30', infile, u'@30מצוה ([\u05d0-\u05ea"]{1,5})').grab_each_header()
+    tester = TagTester(u'@44', infile, u'@44\(([\u05d0-\u05ea]{1,2})\)')
+
+    while not tester.eof:
+
+        segments.append(tester.grab_each_header(u'@30מצוה ([\u05d0-\u05ea"]{1,5})', 1))
+
+    infile.close()
+
+    for sec_number, section in enumerate(segments):
+
+        index = 1
+
+        for title in section:
+
+            title = title.replace(u'"', u'')
+            count = util.getGematria(title)
+
+            if count != index:
+
+                print headers[sec_number-1]
+                print util.numToHeb(index)
+                index = count
+            index += 1
+
+check_segments()
