d539f28c03e1f5319b194d5ff4ab589d109bd130
Jonathan Mosenkis <jonathan@sefaria.org>
2016-06-22 11:47:21 +0300

    sefer hachinuch alt struct upload final

--- sources/alternate_structs/sefer_haChinuch.py
+++ sources/alternate_structs/sefer_haChinuch.py
@@ -2,9 +2,10 @@
 
 import csv
 import codecs
+import json
+import urllib
 import urllib2
-from urllib2 import HTTPError, URLError
-from sefaria.model import *
+from urllib2 import HTTPError
 from sources.local_settings import *
 
 
@@ -25,3 +26,47 @@ def get_data(filename):
     return data
 
 
+def construct_alt_struct(data_file):
+
+    struct = {'nodes': []}
+
+    for title in get_data(data_file):
+
+        node = {
+            'depth': 0,
+            'sharedTitle': title['Parsha'],
+            'nodeType': 'ArrayMapNode',
+            'wholeRef': 'Sefer HaChinukh.{}'.format(title['ref']),
+            'includeSections': True
+        }
+
+        struct['nodes'].append(node)
+
+    return struct
+
+
+def post_index(index):
+    url = SEFARIA_SERVER+'/api/v2/raw/index/Sefer_HaChinukh'
+    indexJSON = json.dumps(index)
+    values = {
+        'json': indexJSON,
+        'apikey': API_KEY
+    }
+    data = urllib.urlencode(values)
+    req = urllib2.Request(url, data)
+    try:
+        response = urllib2.urlopen(req)
+        print response.read()
+    except HTTPError as e:
+        print "error"
+
+s = construct_alt_struct('Chinuch_by_Parsha.csv')
+data = {
+        'Parsha': s
+}
+
+raw = urllib2.urlopen('http://www.sefaria.org/api/v2/raw/index/Sefer_HaChinukh')
+index = json.load(raw)
+index['alt_structs'] = data
+
+post_index(index)
