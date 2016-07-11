5b7e62eb4a41378f702df2b8c865fabf29db012c
Joshua Goldmeier <joshuaegoldmeier@gmail.com>
2016-07-11 15:52:58 +0300

    Fixed the index record to include proper names.  Added default nodes.  Fixed most of the parse accordingly.

--- sources/Rabbeinu_Bahya/function.py
+++ sources/Rabbeinu_Bahya/function.py
@@ -4,8 +4,8 @@ import regex
 from sefaria.model import *
 from sources import functions
 from data_utilities import util
-from sources.Match import match_new
-from sources.Match.match import Match
+# from sources.Match import match_new
+# from sources.Match.match import Match
 from sefaria.model.schema import AddressTalmud, SchemaNode, JaggedArrayNode
 import urllib
 import urllib2
@@ -30,16 +30,18 @@ devarim = [u'דברים', u'ואתחנן', u'עקב', u'ראה', u'שפטים', 
            u'וזאת הברכה']
 hebrew_names = [bereishit, shemot, vayikra, bamidbar, devarim]
 
-bereishit_english = ['Bereishit', 'Noah', 'Lech Lecha', 'Vayeira', 'Chaya Sarah', 'Toldos', 'Vayetze', 'Vayishlach',
-                     'Vayeishev', 'Miketz', 'Vayigash', 'Vayihi']
-shemot_english = ['Shemot', 'Varah', 'Bo', 'Beshalah', 'Yitro', 'Mishpatim', 'Terumah', 'Tizaveh', 'Ki Tisa', 'Vayekel',
-                  'Pikudei']
-vayikra_english = ['Vayikra', 'Tzav', 'Shimini', 'Tazria', 'Metzora', 'Ahrei Mot', 'Kedoshim', 'Emor', 'Behar',
-                   'Behukotai']
-bamidbar_english = ['Bamidbar', 'Naso', 'Behaloteha', 'Shelah', 'Korah', 'Hukat', 'Balak', 'Pinheas', 'Matos', 'Masai']
-devarim_english = ['Devarim', 'Veethanan', 'Ekev', 'Reah', 'Shoftim', 'Ki Taza', 'Ki Tavo', 'Netzavim', 'Vayaleh',
-                   'Hazinu', 'Vzot Habraha']
+bereishit_english = [u'Bereshit', u'Noach', u'Lech Lecha', u'Vayera', u'Chayei Sara', u'Toldot', u'Vayetzei',
+                     u'Vayishlach', u'Vayeshev', u'Miketz', u'Vayigash', u'Vayechi']
+shemot_english = [u'Shemot', u'Vaera', u'Bo', u'Beshalach', u'Yitro', u'Mishpatim', u'Terumah', u'Tetzaveh',
+                  u'Ki Tisa', u'Vayakhel', u'Pekudei']
+vayikra_english = [u'Vayikra', u'Tzav', u'Shmini', u'Tazria', u'Metzora', u'Achrei Mot', u'Kedoshim', u'Emor',
+                   u'Behar', u'Bechukotai']
+bamidbar_english = [u'Bamidbar', u'Nasso', u"Beha'alotcha", u"Sh'lach", u'Korach', u'Chukat', u'Balak',
+                    u'Pinchas', u'Matot', u'Masei']
+devarim_english = [u'Devarim', u'Vaetchanan', u'Eikev', u"Re'eh", u'Shoftim', u'Ki Teitzei', u'Ki Tavo',
+                   u'Nitzavim', u'Vayeilech', u"Ha'Azinu", u"V'Zot HaBerachah"]
 english_names = [bereishit_english, shemot_english, vayikra_english, bamidbar_english, devarim_english]
+
 
 introduction_en = 'Introduction '
 introduction_he = u'הקדמה '
@@ -54,8 +56,8 @@ def create_indices():
     rabbeinu_bahya_book = rabbeinu_bahya_index()
     rabbeinu_bahya_book.validate()
     index = {
-        "title": "Rabbeinu Bahya on Torah",
-        "titleVariants": ["Rabbeinu Bechaye on Torah", "Rabbeinu Bahya ben Asher on Torah"],
+        "title": "Rabbeinu Bahya",
+        "titleVariants": ["Rabbeinu Bechaye", "Rabbeinu Bahya ben Asher"],
         "categories": ["Torah", "Commentary2"],
         "schema": rabbeinu_bahya_book.serialize()
     }
@@ -64,10 +66,10 @@ def create_indices():
 
 def rabbeinu_bahya_index():
     rb_on_humash = SchemaNode()
-    rb_on_humash.add_title('Rabbeinu Bahya on Torah', 'en', primary=True)
-    rb_on_humash.add_title(u'רבינו בחיי על תורה', 'he', primary=True)
-    rb_on_humash.key = 'Rabbeinu Bahya on Torah'
-    rb_on_humash.append(create_unique_intro_nodes('Torah', u'תורה'))
+    rb_on_humash.add_title('Rabbeinu Bahya', 'en', primary=True)
+    rb_on_humash.add_title(u'רבינו בחיי', 'he', primary=True)
+    rb_on_humash.key = 'Rabbeinu Bahya'
+    rb_on_humash.append(create_intro_nodes())
     for en_names, he_names in zip(english_names, hebrew_names):
         rb_on_humash.append(create_book_node(en_names, he_names))
     return rb_on_humash
@@ -75,11 +77,11 @@ def rabbeinu_bahya_index():
 
 def create_book_node(en_dict, he_dict):
     book = SchemaNode()
-    book.key = 'Rabbeinu Bahya on Sefer {}'.format(en_dict[0])
-    book.add_title('Rabbeinu Bahya on Sefer {}'.format(en_dict[0]), 'en', primary=True)
-    book.add_title(u'רבינו בחיי על ספר {}'.format(he_dict[0]), 'he', primary=True)
+    book.key = 'Sefer {}'.format(en_dict[0])
+    book.add_title('Sefer {}'.format(en_dict[0]), 'en', primary=True)
+    book.add_title(u'ספר {}'.format(he_dict[0]), 'he', primary=True)
     if en_dict[0] == 'Bereishit':
-        book.append(create_unique_intro_nodes('Sefer Bereishit', u'ספר בראשית'))
+        book.append(create_intro_nodes())
     for en_name, he_name in zip(en_dict, he_dict):
         book.append(create_parsha_node(en_name, he_name))
     return book
@@ -87,33 +89,31 @@ def create_book_node(en_dict, he_dict):
 
 def create_parsha_node(parhsa_name_en, parsha_name_he):
     parsha = SchemaNode()
-    parsha.add_title('Rabbeinu Bahya on Parsha {}'.format(parhsa_name_en), 'en', primary=True)
-    parsha.add_title(u'רבינו בחיי על פרשה {}'.format(parsha_name_he), 'he', primary=True)
-    parsha.key = 'Rabbeinu Bahya on Parsha {}'.format(parhsa_name_en)
+    parsha.key = parhsa_name_en
+    parsha.add_shared_term(parhsa_name_en)
     if parhsa_name_en is not "Bereishit":
-        parsha_one_intro_node = create_jagged_array_node(parhsa_name_en, parsha_name_he, introduction_en, introduction_he)
+        parsha_one_intro_node = create_intro_nodes()
         parsha.append(parsha_one_intro_node)
-    parsha_one_content_node = create_jagged_array_node(parhsa_name_en, parsha_name_he)
+    parsha_one_content_node = create_jagged_array_node()
     parsha.append(parsha_one_content_node)
     return parsha
 
 
-def create_jagged_array_node(parhsa_name_en, parsha_name_he, intro_en='', intro_he=''):
-    intro_node = JaggedArrayNode()
-    intro_node.add_title('Rabbeinu Bahya {}on Parsha {}'.format(intro_en, parhsa_name_en), "en", primary=True)
-    intro_node.add_title(u'רבינו בחיי {}על פרשה {}'.format(intro_he, parsha_name_he), "he", primary=True)
-    intro_node.key = 'Rabbeinu Bahya {}on Parsha {}'.format(intro_en, parhsa_name_en)
-    intro_node.depth = 1
-    intro_node.addressTypes = ["Integer"]
-    intro_node.sectionNames = ["Comment"]
-    return intro_node
+def create_jagged_array_node():
+    jagged_array_node = JaggedArrayNode()
+    jagged_array_node.key = 'default'
+    jagged_array_node.default = True
+    jagged_array_node.depth = 1
+    jagged_array_node.addressTypes = ["Integer"]
+    jagged_array_node.sectionNames = ["Comment"]
+    return jagged_array_node
 
 
-def create_unique_intro_nodes(en_name, he_name):
+def create_intro_nodes():
     intro_node = JaggedArrayNode()
-    intro_node.add_title('Rabbeinu Bahya Introduction on {}'.format(en_name), "en", primary=True)
-    intro_node.add_title(u'רבינו בחיי הקדמה על {}'.format(he_name), "he", primary=True)
-    intro_node.key = 'Rabbeinu Bahya Introduction on {}'.format(en_name)
+    intro_node.add_title('Introduction', "en", primary=True)
+    intro_node.add_title(u'הקדמה', "he", primary=True)
+    intro_node.key = 'Introduction'
     intro_node.depth = 1
     intro_node.addressTypes = ["Integer"]
     intro_node.sectionNames = ["Comment"]
@@ -122,34 +122,33 @@ def create_unique_intro_nodes(en_name, he_name):
 
 def parse_and_post(rabbeinu_bahya_text_file):
     array_of_comments = []
-    book_counter = 0
+    book_counter = -1
     parsha_counter = -1
     intro_counter = 0
     parsha_was_changed = False
-    work_intro = "Rabbeinu Bahya Introduction on Torah"
-    introduction = "Rabbeinu Bahya Introduction on"
-    comment = "Rabbeinu Bahya on"
+    work_intro = "Rabbeinu Bahya, Introduction"
 
+    #Rabbeinu Bahya, Sefer ---, Parsha, (Intro)
 
     with codecs.open(rabbeinu_bahya_text_file, 'r', 'utf-8') as the_file:
         for each_line in the_file:
             if "@00" in each_line:
-                if book_counter == 0 and parsha_counter == -1:
-                    #functions.post_text(work_intro, array_of_comments)
-                    print 'first {} {}'.format(book_counter, parsha_counter)
-                else:
-                    post_the_text(array_of_comments, comment, parsha_sefer_dictionary, book_counter, parsha_counter)
+                post_the_text(array_of_comments, parsha_sefer_dictionary, book_counter, parsha_counter)
                 array_of_comments = []
                 parsha_counter += 1
                 parsha_was_changed = True
 
             elif "@22" in each_line and parsha_was_changed:
-                post_the_text(array_of_comments, introduction, parsha_sefer_dictionary, book_counter, parsha_counter)
+                post_the_text(array_of_comments, parsha_sefer_dictionary, book_counter, parsha_counter, intro=True)
                 array_of_comments = []
                 parsha_was_changed = False
 
             elif "@99" in each_line:
-                post_the_text(array_of_comments, comment, parsha_sefer_dictionary, book_counter, parsha_counter)
+                if book_counter == -1 and parsha_counter == -1:
+                    #functions.post_text(work_intro, array_of_comments)
+                    print work_intro
+                else:
+                    post_the_text(array_of_comments, parsha_sefer_dictionary, book_counter, parsha_counter)
                 book_counter += 1
                 parsha_counter = 0
                 parsha_was_changed = True
@@ -159,14 +158,16 @@ def parse_and_post(rabbeinu_bahya_text_file):
             else:
                 array_of_comments.append(each_line)
 
-    post_the_text(array_of_comments, comment, parsha_sefer_dictionary, book_counter, parsha_counter)
+    post_the_text(array_of_comments, parsha_sefer_dictionary, book_counter, parsha_counter)
 
 
-def post_the_text(jagged_array, reference, dictionary, book_number, parsha_number):
+def post_the_text(jagged_array, dictionary, book_number, parsha_number, intro=False):
     if book_number == 0 and parsha_number == 0:
-        ref = '{} {} {}'.format(reference, dictionary['Sefer'], english_names[book_number][parsha_number])
+        ref = 'Rabbeinu Bahya, Sefer Bereshit, Introduction'
+    elif intro:
+        ref = 'Rabbeinu Bahya, Sefer {}, {}, Introduction'.format(english_names[book_number][0], english_names[book_number][parsha_number])
     else:
-        ref = '{} {} {}'.format(reference, dictionary['Parsha'], english_names[book_number][parsha_number])
+        ref = 'Rabbeinu Bahya, Sefer {}, {}'.format(english_names[book_number][0], english_names[book_number][parsha_number])
 
     text = {
         "versionTitle": "Midrash Rabbeinu Bachya [ben Asher]. Warsaw, 1878",
@@ -174,5 +175,5 @@ def post_the_text(jagged_array, reference, dictionary, book_number, parsha_numbe
         "language": "he",
         "text": jagged_array
     }
-    print '{} {} {}'.format(ref, book_number, parsha_number)
+    print ref
     #functions.post_text(ref, text)
