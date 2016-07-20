1446f9204757c2dd2d43ee0b41d9c0de91b58762
Jonathan Mosenkis <jonathan@sefaria.org>
2016-07-20 11:10:15 +0300

    function to search for lines without clear tagging

--- sources/Mishnah/parse_boaz.py
+++ sources/Mishnah/parse_boaz.py
@@ -9,7 +9,10 @@ from sefaria.model import *
 Parsing strategy: Use @00 and @22 for structure in the auto-parser.
 
 Two places have a repeated numbered comment (i.e. a and a*). I think dropping to depth 3 is silly, I'll add a line
-break instead.
+break instead. I've marked these cases with the @23 tag.
+
+I've skipped the text labeled as פירוש הקצר in בועז שבת, as I'm not sure this is actually a part of Boaz. This is
+tagged with @10.
 """
 
 
@@ -41,5 +44,25 @@ def find_strange_stuff():
                 weird_chars.append(re.findall(u'[^\u05d0-\u05ea \.0-9\(\):@\[\"]', line))
     weird_chars = [thing for char_list in weird_chars for thing in char_list]
     return set(weird_chars)
+
+
+def unclear_lines(expected_tags):
+
+    problems = 0
+    for book in get_file_names():
+        with codecs.open(book, 'r', 'utf-8') as data:
+            for linenum, line in enumerate(data):
+                if not any(re.match(pattern, line) for pattern in expected_tags):
+                    print u'{}: {}'.format(book, linenum)
+                    print line
+                    problems += 1
+    print '{} issues found'.format(problems)
+
+# for book in get_file_names():
+#     util.restructure_file(book, lambda x: x.replace(u'\ufeff', u''))
+
+unclear_lines([u'@00פרק', u'@22', u'@23', u'@44', u'\n', u'@99'])
+
+
 
 
