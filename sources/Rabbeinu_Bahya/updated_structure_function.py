e04e3be0bf4132dfceed7c92404841e5ae8ad994
Joshua Goldmeier <joshuaegoldmeier@gmail.com>
2016-07-26 11:54:58 +0300

    Changed link type from Commentary to commentary.  The uppercase C was placing the links in the wrong area

--- sources/Rabbeinu_Bahya/updated_structure_function.py
+++ sources/Rabbeinu_Bahya/updated_structure_function.py
@@ -263,7 +263,7 @@ def create_links(rb_ja, title_counter):
                         pasuk_chumash = reduce_it_to_letters(pasuk_chumash)
                         if divrei_hamatchil in pasuk_chumash:
                             list_of_links.append(create_link_dictionary(rb_dictionary))
-
+    print list_of_links
     return list_of_links
 
 
@@ -307,7 +307,7 @@ def create_mishlei_links(rb_dict, proverbs_perek, proverb_pasuk):
                         "Rabbeinu Bahya, {}.{}.{}.{}".format(rb_dict['sefer'], rb_dict['perek'], rb_dict['pasuk'], rb_dict['comment']),
                         "Proverbs.{}.{}".format(proverbs_perek, proverb_pasuk)
                     ],
-                "type": "Commentary",
+                "type": "commentary",
         }
 
 
@@ -322,7 +322,7 @@ def create_link_dictionary(rb_dict):
                         "Rabbeinu Bahya, {}.{}.{}.{}".format(rb_dict['sefer'], rb_dict['perek'], rb_dict['pasuk'], rb_dict['comment']),
                         "{}.{}.{}".format(rb_dict['sefer'], rb_dict['perek'], rb_dict['pasuk'])
                     ],
-                "type": "Commentary",
+                "type": "commentary",
         }
 
 
