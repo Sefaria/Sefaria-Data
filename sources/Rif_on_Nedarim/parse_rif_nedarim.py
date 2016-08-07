efd1ab7add76f1e5ce4abf5ada1a8bbcfdcfb1a4
Joshua Goldmeier <joshuaegoldmeier@gmail.com>
2016-08-07 17:25:46 +0300

    Finished parse rough draft.  Corrected the source file. Created index record.

---
+++ sources/Rif_on_Nedarim/parse_rif_nedarim.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import codecs
+from sefaria.model import *
+import regex
+from sources import functions
+from data_utilities import util
+from sources.Rif_on_Nedarim import rif_nedarim_functions
+
+"""
+index record
+parse text
+text record
+link
+clean
+"""
+
+index = rif_nedarim_functions.create_index()
+functions.post_index(index)
+
+rif_nedarim = rif_nedarim_functions.parse()
+
+ref = 'Rif_Nedarim'
+text = rif_nedarim_functions.create_text(rif_nedarim)
+functions.post_text(ref, text)
+
+testing_file = codecs.open("testing_file.txt", 'w', 'utf-8')
+util.jagged_array_to_file(testing_file, rif_nedarim, ['Daf', 'Line'])
+testing_file.close()
