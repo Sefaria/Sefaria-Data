34a1ab236741878205a9b565eec952b39a24dd48
Jonathan Mosenkis <jonathan@sefaria.org>
2016-08-14 15:39:06 +0300

    final upload script

--- sources/Baal_HaTurim_on_Torah/turim.py
+++ sources/Baal_HaTurim_on_Torah/turim.py
@@ -4,6 +4,9 @@ from bs4 import BeautifulSoup
 from data_utilities.util import ToratEmetData
 from data_utilities import util
 import re
+from sefaria.utils.hebrew import hebrew_term
+from sources import functions
+from sefaria.model import *
 
 
 class TurimData(ToratEmetData):
@@ -44,3 +47,81 @@ class TurimData(ToratEmetData):
                 })
         return good_lines
 
+
+def parse_multiple():
+
+    turim = {}
+    for book in library.get_indexes_in_category('Torah'):
+        parser = TurimData('{}.html'.format(book))
+        turim[book] = parser.parsed_text
+    return turim
+
+
+def linker(dict_of_ja):
+    links = []
+
+    for book in library.get_indexes_in_category('Torah'):
+        for segment in util.traverse_ja(dict_of_ja[book]):
+
+            refs = [u'{}.{}.{}'.format(book, *[x+1 for x in segment['indices'][:-1]]),
+                    u'Baal HaTurim, {}.{}.{}.{}'.format(book, *[x+1 for x in segment['indices']])]
+
+            links.append(
+                {
+                    'refs': refs,
+                    'type': 'commentary',
+                    'auto': False,
+                    'generated_by': 'Baal HaTurim parse script'
+                }
+            )
+    return links
+
+
+def build_index():
+
+    books = library.get_indexes_in_category('Torah')
+
+    # create index record
+    record = SchemaNode()
+    record.add_title('Baal HaTurim', 'en', primary=True, )
+    record.add_title(u'בעל הטורים', 'he', primary=True, )
+    record.key = 'Baal HaTurim'
+
+    # add nodes
+    for book in books:
+        node = JaggedArrayNode()
+        node.add_title(book, 'en', primary=True)
+        node.add_title(hebrew_term(book), 'he', primary=True)
+        node.key = book
+        node.depth = 3
+        node.addressTypes = ['Integer', 'Integer', 'Integer']
+        node.sectionNames = ['Chapter', 'Verse', 'Comment']
+        node.toc_zoom = 2
+        record.append(node)
+    record.validate()
+
+    index = {
+        "title": "Baal HaTurim",
+        "categories": ["Commentary2", "Torah", "Baal HaTurim"],
+        "schema": record.serialize()
+    }
+    return index
+
+
+def post_text(parsed_data):
+
+    for book in library.get_indexes_in_category('Torah'):
+        version = {
+            'versionTitle': 'Baal HaTurim',
+            'versionSource': 'http://www.toratemetfreeware.com/',
+            'language': 'he',
+            'text': parsed_data[book]
+        }
+        functions.post_text('Baal HaTurim, {}'.format(book), version)
+
+parsed = parse_multiple()
+links = linker(parsed)
+index = build_index()
+functions.post_index(index)
+post_text(parsed)
+functions.post_link(links)
