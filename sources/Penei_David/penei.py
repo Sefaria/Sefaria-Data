2f7556fad92b8e0cae93cad43143b3272e207fa2
Jonathan Mosenkis <jonathan@sefaria.org>
2016-08-16 16:03:56 +0300

    finished parser class

--- sources/Penei_David/penei.py
+++ sources/Penei_David/penei.py
@@ -9,7 +9,7 @@ from sefaria.model import *
 """
 parsed from this url: http://www.toratemetfreeware.com/online/f_01917.html
 """
-
+url = 'http://www.toratemetfreeware.com/online/f_01917.html'
 
 class PeneiDavid(ToratEmetData):
 
@@ -17,8 +17,12 @@ class PeneiDavid(ToratEmetData):
 
         ToratEmetData.__init__(self, path, from_url, codec)
         self.book_names = library.get_indexes_in_category('Torah')
-        r = lambda x: re.sub(u'פרשת ', u'', x)
-        self.parsha_names = [r(pname) for pname in self._important_lines['parsha names']]
+        self.he_parsha_names = [re.sub(u'פרשת ', u'', x) for x in self._important_lines['parsha names']]
+        self.parsha_names = []
+        self.parsha_map = {}
+        self._build_parsha_map()
+        self.parsha_by_book = self._get_parsha_by_book()
+        self.parsed_as_dict = self._dict_parse()
 
     def _extract_important_data(self):
 
@@ -118,5 +122,43 @@ class PeneiDavid(ToratEmetData):
         }
 
     def _parse(self):
-        return None
+        return self._important_lines['full_text']
+
+    def _build_parsha_map(self):
+        with open('parsha.csv') as csvfile:
+
+            he_to_en = {}
+
+            reader = csv.reader(csvfile)
+            for row in reader:
+
+                # construct a dictionary mapping hebrew parsha names to english
+                he_to_en[row[1]] = row[0]
+                self.parsha_map[row[0]] = row[2]
+
+            for name in self.he_parsha_names:
+                self.parsha_names.append(he_to_en[name])
+
+    def _get_parsha_by_book(self):
+
+        parsha_map = {}
+        start = 0
+        for book_num, book in enumerate(self._important_lines['full_text']):
+            end = start + len(book)
+            parsha_map[self.book_names[book_num]] = self.parsha_names[start:end]
+            start = end
+        return parsha_map
+
+    def _dict_parse(self):
+
+        books = {}
+        for book_num, book in enumerate(self.parsed_text):
+            book_name = self.book_names[book_num]
+            parashot = {}
+
+            for parsha_num, parsha in enumerate(book):
+                parashot[self.parsha_by_book[book_name][parsha_num]] = parsha
+
+            books[book_name] = parashot
+        return books
 
