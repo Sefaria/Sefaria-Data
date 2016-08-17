c6a7493aa0697ed079ddaf5825c29a9216ea17d3
Joshua Goldmeier <joshuaegoldmeier@gmail.com>
2016-08-17 15:12:26 +0300

    Finished parsing the first two sections

--- sources/Rasag_commentary/parse_rasag_commentary.py
+++ sources/Rasag_commentary/parse_rasag_commentary.py
@@ -5,6 +5,7 @@ import regex
 from sources import functions
 from data_utilities import util
 from sources.Rasag_commentary import rasag_commentary_index_record
+from sources.Rasag_commentary import positive_and_negative_parse
 
 """
 index record
@@ -14,21 +15,28 @@ link
 clean
 """
 
-index = rasag_commentary_index_record.create_index()
-functions.post_index(index)
+# index = rasag_commentary_index_record.create_index()
+# functions.post_index(index)
+#
+# rasag_commentary = rasag_commentary_index_record.parse()
+#
+# for key in rasag_commentary:
+#     ref = 'Rasag Commentary,_{}'.format(key)
+#     if key == 'Commentary':
+#         ref = 'Ralbag Esther'
+#     text = rasag_commentary_index_record.create_text(rasag_commentary[key])
+#     functions.post_text(ref, text)
+#
+# list_of_links = rasag_commentary_index_record.create_links(rasag_commentary['Commentary'])
+# functions.post_link(list_of_links)
+#
+# rasag_commetary_ja = [rasag_commentary['Introduction'], rasag_commentary['Commentary'], rasag_commentary['Benefits']]
 
-rasag_commentary = rasag_commentary_index_record.parse()
+positive_mitzvah_number = regex.compile(u'@00\u05e2\u05e9\u05d4((?:\s[\u05d0-\u05ea]{1,4}){1,9})')
+positive_commandments = positive_and_negative_parse.parse('text_positive_commandments.txt', positive_mitzvah_number)
 
-for key in rasag_commentary:
-    ref = 'Rasag Commentary,_{}'.format(key)
-    if key == 'Commentary':
-        ref = 'Ralbag Esther'
-    text = rasag_commentary_index_record.create_text(rasag_commentary[key])
-    functions.post_text(ref, text)
 
-list_of_links = rasag_commentary_index_record.create_links(rasag_commentary['Commentary'])
-functions.post_link(list_of_links)
+# negative_mitzvah_number = regex.compile(u'@00\u05dc[”"]\u05ea((?:\s[\u05d0-\u05ea]{1,4}){1,9})')
+# negative_commandments = positive_and_negative_parse.parse('text_negative_commandments.txt', negative_mitzvah_number)
 
-rasag_commetary_ja = [rasag_commentary['Introduction'], rasag_commentary['Commentary'], rasag_commentary['Benefits']]
-
-util.ja_to_xml(rasag_commetary_ja, ['FIRST', 'SECOND', 'THIRD', 'FOURTH'])
+util.ja_to_xml(positive_commandments, ['FIRST', 'SECOND'])
