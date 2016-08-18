c300bf73b9a49ba7d46d669a9b10b199890c3efa
Jonathan Mosenkis <jonathan@sefaria.org>
2016-08-18 16:31:57 +0300

    mishna sequencial test

---
+++ sources/Sefer_Yitzirah/yitzira.py
@@ -0,0 +1,14 @@
+# encoding=utf-8
+
+from data_utilities.sanity_checks import TagTester
+import codecs
+
+
+def check_order(tag, section_pattern):
+
+    assert isinstance(tag, TagTester)
+
+    # jump to the second appearance of section_pattern, as the first is the legend
+    [tag.skip_to_next_segment(section_pattern) for _ in range(2)]
+
+    return tag.in_order_many_sections(u'00', 1)
