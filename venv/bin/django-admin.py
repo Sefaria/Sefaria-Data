a651d73306e9d5665781f4783ab61841d3e85ceb
Noah Santacruz <noahssantacruz@gmail.com>
2016-08-28 15:18:13 +0300

    added json for shabbat cal matcher. removed venv

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/nss/Documents/Sefaria-Project/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- venv/lib/python2.7/site-packages/geojson/_version.py
+++
@@ -1,2 +0,0 @@
-__version__ = "1.3.3"
-__version_info__ = tuple(map(int, __version__.split(".")))
