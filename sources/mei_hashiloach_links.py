a79b1a07494854784b8347af46b1fbf870646a79
Jonathan Mosenkis <jonathan@sefaria.org>
2017-01-01 15:39:06 +0200

    Mei HaShiloach Linking script

--- sources/mei_hashiloach_links.py
+++ sources/mei_hashiloach_links.py
@@ -31,12 +31,29 @@ class MeiLinks:  # excuse the pun
 
     @staticmethod
     def _dh_extract_method(some_string):
-        return re.search(ur'<b>(.*?)</b>', some_string).group(1)
+        raw_match = re.search(ur'<b>(.*?)</b>', some_string).group(1)
+        raw_match = re.sub(u'[.,]', u'', raw_match)
+        # raw_match = re.sub(ur'(u05d0\u05dc?\u05d5)\u05e7(\u05d9)(\u05da|\u05dd|\u05db)', ur'\1\u05d4\2\3',
+        #                    raw_match)
+
+        # handle cases where multiple texts are quoted
+        mulitple = re.split(ur"\u05d5(\u05db|\u05d2)\u05d5'", raw_match)
+        if len(mulitple) > 1:
+            return reduce(lambda x, y: x if len(x) > len(y) else y, mulitple)  # finds the longest match
+        else:
+            return raw_match
 
     @staticmethod
     def _base_tokenizer(some_string):
         some_string = re.sub(u'\u05be', u' ', some_string)
         return some_string.split()
+
+    @staticmethod
+    def _filter(some_string):
+        if re.search(ur'<b>(.*?)</b>', some_string) is None:
+            return False
+        else:
+            return True
 
     def get_refs(self, node):
         assert isinstance(node, SchemaNode)
@@ -74,15 +91,27 @@ class MeiLinks:  # excuse the pun
         if self.version_map.get(base_ref.book) is None:
             self.set_version_by_category(base_ref.book)  # Books that can't be linked by category need to be set manually
         base_version, mei_version = self.version_map[base_ref.book], self.version_map[comment_ref.index.title]
+        word_count = base_ref.text('he', base_version).word_count()
 
         return match_ref(base_ref.text('he', base_version), comment_ref.text('he', mei_version),
-                         self._base_tokenizer, dh_extract_method=self._dh_extract_method, verbose=verbose)
+                         self._base_tokenizer, dh_extract_method=self._dh_extract_method, verbose=verbose,
+                         rashi_filter=self._filter, char_threshold=0.4, boundaryFlexibility=word_count)
 
-    def match_all(self):
+    def match_all(self, verbose=False):
+        missed = []
         for base, mei in zip(self.base_refs, self.mei_refs):
-            matching = self.match(base, mei)
+            matching = self.match(base, mei, verbose=verbose)
+            if matching.get('matches') is None or matching.get('comment_refs') is None:
+                continue
             for base_ref, mei_ref in zip(matching['matches'], matching['comment_refs']):
-                self.stored_links.append((base_ref.normal(), mei_ref.normal()))
+                if base_ref is None or mei_ref is None:
+                    missed.append(mei_ref)
+                    continue
+                else:
+                    self.stored_links.append((base_ref.normal(), mei_ref.normal()))
+        print 'stored {} links, missed {}'.format(len(self.stored_links), len(missed))
+        for i in missed:
+            print i
 
     def post_links(self):
         links = [{
@@ -95,9 +124,11 @@ class MeiLinks:  # excuse the pun
 
 
 mei = library.get_index('Mei HaShiloach')
-test_node = mei.nodes.children[0].children[1]
+nodes_to_link = mei.nodes.children[0].children[1:9]
+nodes_to_link.extend(mei.nodes.children[1].children[1:9])
+nodes_to_link.extend(mei.nodes.children[2].children[0:8])
 linker = MeiLinks()
-linker.get_refs(test_node)
-linker.match_all()
-for i, j in linker.stored_links:
-    print '{} : {}'.format(i, j)
+for i in nodes_to_link:
+    linker.get_refs(i)
+linker.match_all(verbose=False)
+linker.post_links()
