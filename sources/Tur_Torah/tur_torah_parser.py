16ab2dac5158a23b71d0b1cd47bf0bc2bb18bb9e
Shanee <shanee@sefaria.org>
2017-01-03 12:55:10 +0200

    finished tur_torah_parser some fixes on tur txt file

--- sources/Tur_Torah/tur_torah_parser.py
+++ sources/Tur_Torah/tur_torah_parser.py
@@ -8,55 +8,88 @@ from sefaria.model import *
 from sefaria.datatype.jagged_array import JaggedArray
 
 def parse_he(filename):
-    replace_dict = {u'@(11|44|99)':u'<b>',u'@(33|55)' : u'</b>', ur'@22\(([\u05d0-\u05ea]{1,3})\)':u'',
-                    ur'@22': u''}
+    replace_dict = {u'@(11|44|99)': u'<b>', u'@(33|55)': u'</b>', ur'@22\(([\u05d0-\u05ea]{1,3})\)': u'',
+                    ur'@(22|77)': u''}
     def cleaner(my_text):
         new = []
         for line in my_text:
             line = multiple_replace(line,replace_dict,using_regex=True)
             new.append(line)
 
-        return ''.join(new) # join is reduces it to depth 2 without comments so it is like the en, return new for depth 3.
+        return new #''.join(new) # join is reduces it to depth 2 without comments so it is like the en, return new for depth 3.
 
-    regs = [ur'@00(?P<gim>)',ur'@02(?P<gim>[\u05d0-\u05ea]{1,3})', ur'@22\((?P<gim>[\u05d0-\u05ea]{1,3})\)']
+    regs = [ur'@00(?P<gim>)',ur'@02(?P<gim>[\u05d0-\u05ea]{1,3})', ur'@22\((?P<gim>[\u05d0-\u05ea]{1,3})\)']  # ,ur'@77'
     with codecs.open(filename, 'r', 'utf-8') as fp:
         lines = fp.readlines()
     starting = None
     # check if we got to the end of the legend and change to started
     # clean all lines of Parasha start with @01
     cleaned = []
+    dh_list = []
     for line_num, line in enumerate(lines):
         if line == u'\n':
             starting = line_num + 1
         if starting and not re.search(u'@01', line) and not line.isspace():
-            cleaned.append(line)
+            dh_recognize = re.compile(ur'@11(.*?)@33')
+            if dh_recognize.search(line):
+                dh_list.append(dh_recognize.search(line).group(1))
+            line = re.sub(dh_recognize, ur'#<b>\1</b>', line)
+            line = re.split(ur'#',line)
+            if isinstance(line, basestring):
+                cleaned.append(line)
+            else:
+                cleaned.extend(line)
 
-    tt_ja = file_to_ja_g(4, cleaned, regs, cleaner, gimatria = True, group_name = 'gim', grab_all=[False,False,True]).array()
+    tt_ja = file_to_ja_g(4, cleaned, regs, cleaner, gimatria = True, group_name = 'gim', grab_all=[False, False, False]).array()
     Pentateuch = ['Genesis','Exodus', 'Leviticus', 'Numbers', 'Deuteronomy']
     parsed_texts = dict({book:ja for book, ja in zip(Pentateuch,tt_ja)})
 
     for book,ja in zip(Pentateuch,tt_ja):
         ja_to_xml(ja, ['perek', 'pasuk', 'comment'], 'tur_{}.xml'.format(book))
+
+    # for str in  dh_list:
+    #     print str
     return parsed_texts
 
-def parse_en(filename, parsed_he = False):
+
+def parse_en(filename):
     with codecs.open(filename, 'r', 'utf-8') as fp:
         lines = fp.readlines()
-    ja = JaggedArray([[[]]])
+    ja = JaggedArray([[[[]]]])
+    placing = u'(\s*[0-9]{1,2}),([0-9]{1,2})-?[0-9]*\.'  # the regex to find the indexing on Monk
+    # q1, q2 = ur'“', ur'”' # Rabbi Monk uses these to enclose translation of a pasuk
+    # dh_reg = ur'([\u05d0 - \u05ea]*), *({}.*?{})'.format(q1, q2)
+    replace_dict = {placing: u'', u'@': ''}
     temp = []
-    indices = [-1]*2
+    indices = [0]*3
     for line in lines:
-        dh = re.match(u'(\s*[0-9]{1,2}),([0-9]{1,2})-?[0-9]*\.', line)
-        if dh:
+        pasuk_dh = re.match(placing, line)
+        reg_dh = re.search(ur'@([\u05d0-\u05ea|\\s]*)',line) #  reg_dh = re.search(ur'([\u05d0-\u05ea]+, *“.*?”)',line)
+        line = multiple_replace(line, replace_dict, using_regex=True)
+        if pasuk_dh or reg_dh:
             temp = ' '.join(temp)
             ja.set_element(indices, temp, [])
-            temp = [line]
-            indices = [int(dh.group(1))-1, int(dh.group(2))-1]
-        if not line.isspace():
+            temp = []
+            if pasuk_dh:
+                indices = [int(pasuk_dh.group(1))-1, int(pasuk_dh.group(2))-1, indices[2]]
+                indices[2] = 0
+            elif reg_dh:
+                # print reg_dh.group(1)
+                indices[2] += 1
+        if not line.isspace() and not re.match(ur' *Parshat *(\S+) *(\S+)? *', line):  # don't put into array names of Parasha or empty lines
             temp.append(line)
 
-    ja_to_xml(ja.array(), ['perek', 'pasuk'], '{}.xml'.format(filename))
+    ja_to_xml(ja.array(), ['perek', 'pasuk', 'comment'], '{}.xml'.format(re.match('(.*)\.',filename).group(1)))
     return ja
+
+
+def parse_all_en():
+    en_texts = {}
+    pentateuch = ['Genesis', 'Exodus', 'Leviticus', 'Numbers', 'Deuteronomy']
+    for book in pentateuch:
+        parsed_book = parse_en('en_tur_{}.txt'.format(book.lower()))
+        en_texts[book] = parsed_book
+    return en_texts
 
 
 def tt_schema():
@@ -72,7 +105,12 @@ def tt_schema():
         book_node = JaggedArrayNode()
         book_node.add_primary_titles('{}'.format(Pentateuch[i]),u'{}'.format(Chumshai[i]),True)
         book_node.add_structure(['Chapter', 'Verse', 'Comment'])
+        book_node.toc_zoom = 2
         record_root.append(book_node)
+    # intro_node = JaggedArrayNode()
+    # intro_node.depth = 1
+    # intro_node.add_primary_titles(u'Introduction', u'הקדמה', True)
+    # record_root.append(intro_node)
 
     record_root.validate()
 
@@ -98,38 +136,49 @@ def tt_text_post(text_dict):
         }
         post_text('Tur HaAroch, {}'.format(key), version_dict)
 
-def boolean_ja(ja):
-    bool = JaggedArray([[]])
-    if not isinstance(ja,list):
-        ja = ja.array()
-    for dh in traverse_ja(ja):
-        if dh['data']:
-            bool.set_element(dh['indices'][:2], True, pad=False)
-            #testing where there is more then one comment on a pasuk (in the he)
-            if len(dh['indices']) > 2 and dh['indices'][2] > 0:
-                print dh['indices']
-        else:
-            bool.set_element(dh['indices'][:2], False, pad=False)
-    print bool.array()
-    return bool.array()
+
+def en_tt_text_post(en_text):
+    for key in en_text.keys():
+        version_dict = {
+            'versionTitle': 'Tur on the Torah, trans. Eliyahu Munk',
+            'versionSource': 'http://www.urimpublications.com/Merchant2/merchant.mv?Screen=PROD&Store_Code=UP&Product_Code=TUR&Category_Code=bde',
+            'language': 'en',
+            'text': en_text[key].array()
+        }
+        post_text('Tur HaAroch, {}'.format(key), version_dict)
+
+
+def links(text_dict):
+    links = []
+    for book in text_dict.keys():
+        for dh in traverse_ja(text_dict[book]):
+            perek = (dh['indices'][0] + 1)
+            pasuk = (dh['indices'][1] + 1)
+            comment = (dh['indices'][2] + 1)
+            link = (
+                {
+                    "refs": [
+                        'Tur HaAroch, {} {}:{}:{}'.format(book, perek, pasuk, comment),
+                        '{} {}:{}'.format(book,perek, pasuk),
+                    ],
+                    "type": "commentary",
+                    "auto": True,
+                    "generated_by": "tur_torah_parser"
+                })
+            # append to links list
+            links.append(link)
+    return links
 
 
 if __name__ == "__main__":
-    p_he = parse_he('tur_he.txt')
-    pentateuch = ['Genesis', 'Exodus', 'Leviticus', 'Numbers', 'Deuteronomy']
-    # testing to find where the he vs en differ
-    for book in pentateuch:
-        p_en = parse_en('en_tur_{}.txt'.format(book.lower()))
-        print book
-        b_he = boolean_ja(p_en)
-        b_en = boolean_ja(p_he['{}'.format(book)])
-        i = 0
-        for he, en in zip(b_he, b_en):
-            i += 1
-            if len(he) != len(en):
-                print book, i
-
-    # tt_schema()
-    # tt_index()
-    # tt_text_post(parsed)
-
+    # parse
+    parsed_he = parse_he('tur_he.txt')
+    parsed_en = parse_all_en()
+    # index
+    tt_index()
+    # post
+    tt_text_post(parsed_he)
+    en_tt_text_post(parsed_en)
+    # link
+    links = links(parsed_he)
+    post_link(links)
