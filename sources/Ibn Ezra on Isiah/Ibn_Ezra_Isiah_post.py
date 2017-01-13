8f31744259dd1d51b9a365fd51870aa648bba61b
Eytan <eytansterling@gmail.com>
2017-01-12 20:21:31 -0500

    Started Ibn Ezra Isiah

---
+++ sources/Ibn Ezra on Isiah/Ibn_Ezra_Isiah_post.py
@@ -0,0 +1,17 @@
+import xml.etree.ElementTree as ET
+
+tree = ET.parse('IbnEzra_Isiah.xml')
+root = tree.getroot()
+for chapter in root.iter('chapter'):
+    print chapter.find('title').text
+    for p in chapter.findall('p'):
+        text = "".join(p.itertext())
+        for pitem in p.iter():
+            if pitem.tag == 'italic':
+                text = text.replace(pitem.text,"<b>"+pitem.text+"</b>")
+        """
+        for pitem in p[tag='italic']:
+            text = text.replace(pitem.text,"<b>"+pitem.text+"</b>")
+        print text
+        """
+    print text
