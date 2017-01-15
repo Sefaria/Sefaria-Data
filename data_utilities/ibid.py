7bea472d6f92b528c66c7b9a93efabf7028e97bd
Shanee <shanee@sefaria.org>
2017-01-15 22:02:14 +0200

    first working draft ibid

--- data_utilities/ibid.py
+++ data_utilities/ibid.py
@@ -9,35 +9,88 @@ class BookIbidTracker(object):
     """
     def __init__(self):
         self._table = {}  # Keys are tuples of (index, (sections))
-        self._last_reference = None
+        self._section_length = 0
+        self._last_cit = [None, None]
 
-    def registerRef(self, ref):
-        pass
-
-    def register(self, index_name, sections):
+    def registerRef(self, oref):
         """
         Every time we resolve a reference successfully, `register` is called, in order to record the reference.
         :param index_name:
         :param sections:
         :return:
         """
-        assert library.get_index(index_name)
-        assert isinstance(sections, list)
-        d = {}
-        ref = Ref(_obj=d)
-        self._last_reference = ref
+        assert isinstance(oref, Ref)
+        # d = {}
+        # ref = Ref(_obj=d)
+        self._table[(None,(None, None))] = oref
+        for key_tuple in self.creat_keys(oref):
+            self._table[(oref.book,tuple(key_tuple))] = oref
 
         # register this reference according to all possible future ways it could be referenced
-        
 
     def resolve(self, index_name, sections):
         """
 
         :param index_name:
-        :param sections: If any entry in this list is "None", then we treat that as a "שם" or "ibid", resolve it, and return a refrence
+        :param list sections: If any entry in this list is "None", then we treat that as a "שם" or "ibid", resolve it, and return a refrence
         :return: Ref
         """
-        assert library.get_index(index_name)
-        assert isinstance(sections, list)
-        resolvedRef = Ref()
-        self.register(resolvedRef)
+        #todo: assert if table is empty.
+        #todo: print out an error statment if can't find this sham constilation in table
+
+        # recognize what kind of key we are looking for
+        found_sham = False
+        key = []
+        if not index_name and sections == [None, None]:  # it says only Sham (all were None)
+                resolvedRef = self._table[(None,(None, None))]
+                # notice that self._last_cit doesn't chnge so no need to reasign it
+        else:
+            if not index_name:
+                index_name = self._last_cit[0]
+            for i, sect in enumerate(sections):
+                if found_sham:  # we are after the place that we need info from
+                    key.append(None)
+                else:
+                    key.append(sections[i])
+                if not sect:  # change bool on the first sham you meet
+                    found_sham = True
+            # if self._table.has_key(index_name, tuple(key)):
+            if found_sham:
+                try:
+                    from_table = self._table[(index_name, tuple(key))].sections
+                except:
+                    print "error, couldn't find this key", index_name, tuple(key)
+                    return
+            else:
+                from_table = sections # that is it wasn't found in _table
+            new_sections = []
+            # merge them, shile prefering the sections that were retrived from the citation
+            for i, sect in enumerate(sections):
+                if sect:
+                    new_sections.append(str(sect))
+                else:
+                    new_sections.append(str(from_table[i]))
+            try:
+                resolvedRef = Ref(u'{}.{}'.format(index_name, '.'.join(new_sections)))
+            except:
+                print 'error, problem with the Ref iteslf. ', u'{}.{}'.format(index_name, '.'.join(new_sections))
+                return
+            self._last_cit = [index_name, new_sections]
+        self.registerRef(resolvedRef)
+        return resolvedRef
+
+    def creat_keys(self, oref, i = 1):
+
+        subs = [[None],[oref.sections[0]]]
+        a = len(oref.sections)
+        while i < a:
+            subsi = filter(lambda item: len(item) == i, subs)
+            for sub in subsi:
+                new0 = sub[:]
+                new0.extend([None])
+                subs.append(new0)
+                new1 = sub[:]
+                new1.extend([oref.sections[i]])
+                subs.append(new1)
+            i += 1
+        return filter(lambda item: len(item) == a, subs)
