02a63d72426545d248e5bc75d55d97b71cc5913a
Jonathan Mosenkis <jonathan@sefaria.org>
2017-01-16 10:19:09 +0200

    Singleton meta-class

--- data_utilities/util.py
+++ data_utilities/util.py
@@ -964,3 +964,11 @@ class WeightedLevenshtein:
                 best_dist = dist
                 best_ind = i
         return best_ind, best_dist
+
+class Singleton(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls._instances.get(cls) is None:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
