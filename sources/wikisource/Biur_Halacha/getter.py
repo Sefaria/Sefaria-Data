58ee57be973e1735b7442b245c5609d91f54d850
Kevin <kevwolf@umich.edu>
2017-02-13 17:11:38 +0200

    Original getter

---
+++ sources/wikisource/Biur_Halacha/getter.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import tools
+
+'''
+This method that works by grabbing list of files from a file which was obtained by parsing the index.html file
+index = open("nnn", "r")
+for line in index:
+	line = line.rstrip()
+	title = urllib.unquote(line).decode('utf8')
+	wikiGet("https://he.wikisource.org/w/index.php?title=%s&printable=yes" %(line), title)
+index.close()
+'''
+
+#this method just iterates through the files based on name
+#it's less "correct" but it works where there's no English name to get
+
+for siman in range(1, 697): #696 simanim in O.C.
+	title = "Biur_Halacha." + str(siman)
+	name = "ביאור_הלכה_על_אורח_חיים_" + tools.numToHeb(siman)
+	tools.wikiGet("https://he.wikisource.org/w/index.php?title=%s&printable=yes" %(name), title)
