f789f13699c01133a8812be649e441cb71fd6ffa
Kevin <kevwolf@umich.edu>
2017-02-16 14:11:48 +0200

    Getter for biur halacha

--- sources/wikisource/Biur_Halacha/getter.py
+++ sources/wikisource/Biur_Halacha/getter.py
@@ -15,10 +15,7 @@ index.close()
 #it's less "correct" but it works where there's no English name to get
 
 for siman in range(1, 697): #696 simanim in O.C.
+	if siman == 5 or siman == 6: continue # 5 & 6 are simanim with no text but a page
 	title = "Biur_Halacha." + str(siman)
 	name = "ביאור_הלכה_על_אורח_חיים_" + tools.numToHeb(siman)
 	tools.wikiGet("https://he.wikisource.org/w/index.php?title=%s&printable=yes" %(name), title)
-
-
-title = "Biur_Halacha_index"
-tools.wikiGet("https://he.wikisource.org/w/index.php?title=%D7%91%D7%99%D7%90%D7%95%D7%A8_%D7%94%D7%9C%D7%9B%D7%94&printable=yes", title)
