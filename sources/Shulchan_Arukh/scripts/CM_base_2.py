c9c05d574f5fd4d8a749f7d6225ceef573966a13
Jonathan Mosenkis <jonathan@sefaria.org>
2017-04-03 11:58:18 +0300

    kzot vol.2 with source file corrections

--- sources/Shulchan_Arukh/scripts/CM_base_2.py
+++ sources/Shulchan_Arukh/scripts/CM_base_2.py
@@ -7,6 +7,7 @@ from data_utilities.util import he_ord
 def markup(b_vol):
     commentaries = root.get_commentaries()
     b_vol.mark_references(commentaries.commentary_ids["Me'irat Einayim"], u'@55([\u05d0-\u05ea]{1,3})', group=1)
+    b_vol.mark_references(commentaries.commentary_ids["Ktsot HaHoshen"], u'/([\u05d0-\u05ea]{1,3})\)', group=1)
 
 root = Root('../Choshen_Mishpat.xml')
 base = root.get_base_text()
@@ -48,4 +49,5 @@ for code, pattern in zip(codes, patterns):
 errors = volume.format_text('@33', '@88', 'ramah')
 for i in errors:
     print i
+markup(volume)
 root.export()
