a52f8c8c11dc5cbd4e355d0475fd219a79fe0426
Shanee <shanee@sefaria.org>
2017-04-18 17:30:18 +0300

    Raph dif parsed

--- sources/Semak/semak_parser.py
+++ sources/Semak/semak_parser.py
@@ -14,7 +14,7 @@ def parse_semak_day(filename): # alt struct according to days. (not sure how alt
 def parse_semak(filename):
 
     def cleaner(my_text):
-        replace_dict = {u'@11(.*?)@12': ur'<b>\1</b>', u'@33(.*?)@34': ur'<b>\1</b>', u'@33(.*?)@34': ur'<b>\1</b>', u'@66(.*?)@67': ur'\1'}
+        replace_dict = {u'@11(.*?)@12': ur'<b>\1</b>', u'@33(.*?)@34': ur'<b>\1</b>', u'@33(.*?)@34': ur'<b>\1</b>', u'@66(.*?)@67': ur'\1', u'@55[\u05d0-\u05ea]{1,3}' : u'<i-tags = >'}
         new = []
         for line in my_text:
             line = multiple_replace(line,replace_dict,using_regex=True)
@@ -55,17 +55,16 @@ def parse_semak(filename):
 
 
 def parse_Raph(filename):
-
     def cleaner(my_text):
-        return my_text
-        # replace_dict = {u'@11(.*?)@12': ur'<b>\1</b>'}
-        # new = []
-        # for line in my_text:
-        #     line = multiple_replace(line,replace_dict,using_regex=True)
-        #     new.append(line)
-        # return new
+        replace_dict = {u'@(?:11|77)[\u05d0-\u05ea]{0,3}': u'', u'@(33|22)':u''}#{u'@11(.*?)@12': ur'<b>\1</b>', u'@33(.*?)@34': ur'<b>\1</b>', u'@33(.*?)@34': ur'<b>\1</b>', u'@66(.*?)@67': ur'\1'}
+        new = []
+        for line in my_text:
+            line = multiple_replace(line,replace_dict,using_regex=True)
+            new.append(line)
+        return new
 
-    regs = [ur'@11(?P<gim>[\u05d0-\u05ea]{1,3})'] #find the letter
+
+    regs = [ur'@77(?P<gim>[\u05d0-\u05ea]{0,3})', ur'@11(?P<gim>[\u05d0-\u05ea]{1,3})'] # (?P<gim>[\u05d0-\u05ea]{1,3})
     with codecs.open(filename, 'r', 'utf-8') as fp:
         lines = fp.readlines()
     starting = None
@@ -79,42 +78,27 @@ def parse_Raph(filename):
             break
     for line_num, line in enumerate(lines[starting:]):
         if not re.search(u'@00', line) and not line.isspace():
-            if re.search(u'@11', line):
-                line = re.split(u'(@11[\u05d0-\u05ea]{1,3})', line)
-                if isinstance(line, basestring):
-                    cleaned.append(line)
-                else:
-                    [cleaned.append(st) for st in line if st]
-            else:
+            # if re.search(u'@(77|11)', line):
+            line = re.split(u'(@(?:77|11)[\u05d0-\u05ea]{0,3})', line)
+            if isinstance(line, basestring):
                 cleaned.append(line)
+            else:
+                [cleaned.append(st.strip()) for st in line if st]#(st and not re.search(u'@(77)', st))]
+            # else:
+            #     cleaned.append(line)
     try:
-        ja = file_to_ja_g(2, cleaned, regs, cleaner, gimatria=True,  grab_all=[False, True, True], group_name = 'gim').array() #group_name = 'gim',
+        ja = file_to_ja_g(3, cleaned, regs, cleaner, gimatria=True,  grab_all=[False, False, True], group_name = 'gim').array()
     except AttributeError:
         print 'there are more regs then levels...'
 
-    ja_to_xml(ja, ['letter', 'segments'], 'Raph.xml')
+    ja_to_xml(ja, ['page', 'letter', 'segments'], 'test_raph.xml')
 
     return ja
 
-def count_regex_in_all_db(pattern, lang, text = all):
-    found = 0
-    vtitle = None
-    if text==all:
-        indecies = library.all_index_records()
-    else:
-        indecies = [library.get_index(text)]
-    for index in indecies:
-        # if index == Index().load({'title': 'Divrei Negidim'}):
-        #     continue
-        try:
-            unit_list_temp = index.nodes.traverse_to_list(lambda n, _: TextChunk(n.ref(), lang, vtitle=vtitle).ja().flatten_to_array() if not n.children else [])
-            st = ' '.join(unit_list_temp)
-            found += len(re.findall(pattern,st))
-        except: # sefaria.system.exceptions.NoVersionFoundError:
-            continue
-    return found
+#todo:
+# write a script that will test if the Semaks @55 are roughly correctly placed in the Raph.
+# get it from a tree through the ja of the Raph or of the Semak
 
 if __name__ == "__main__":
-    # ja_smk = parse_semak('Semak.txt')
-    # ja_raph = parse_Raph('Raph_on_Semak.txt')
-    print count_regex_in_all_db(u'(\(|\([^)]* )שם(\)| [^(]*\))', 'he') #, text = 'Ramban on Genesis')
+    ja_smk = parse_semak('Semak.txt')
+    ja_raph = parse_Raph('Raph_on_Semak.txt')
