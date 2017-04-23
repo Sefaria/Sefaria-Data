e38c62d5b6d5b45ad85901bf9d9e5a117f7a6b72
Jonathan Mosenkis <jonathan@sefaria.org>
2017-04-23 11:45:29 +0300

    Parsed Shach vol.2 - Shach Complete

--- sources/Shulchan_Arukh/scripts/CM_base_2.py
+++ sources/Shulchan_Arukh/scripts/CM_base_2.py
@@ -10,6 +10,7 @@ def markup(b_vol):
     b_vol.mark_references(commentaries.commentary_ids["Ktsot HaHoshen"], u'/([\u05d0-\u05ea]{1,3})\)', group=1)
     b_vol.mark_references(commentaries.commentary_ids["Pithei Teshuva"], u'\]([\u05d0-\u05ea]{1,3})\]', group=1)
     b_vol.mark_references(commentaries.commentary_ids["Netivot HaMishpat, Hidushim"], u'@66\(([\u05d0-\u05ea]{1,3})\)', group=1)
+    b_vol.mark_references(commentaries.commentary_ids["Siftei Cohen"], u'@77\(([\u05d0-\u05ea]{1,3})\)', group=1)
 
 root = Root('../Choshen_Mishpat.xml')
 base = root.get_base_text()
