80adc0ef156beb47c1da98a74fa454af77abe280
Jonathan Mosenkis <jonathan@sefaria.org>
2017-05-10 12:14:39 +0300

    Gra vol.3 parsed

--- sources/Shulchan_Arukh/scripts/CM_gra_3.py
+++ sources/Shulchan_Arukh/scripts/CM_gra_3.py
@@ -17,7 +17,7 @@ assert isinstance(volume, Volume)
 
 volume.mark_simanim(u'@00([\u05d0-\u05ea]{1,3})')
 print "Validating Simanim"
-volume.validate_simanim()
+volume.validate_simanim(complete=False)
 
 errors = volume.mark_seifim(u'@22\[([\u05d0-\u05ea]{1,3})\]')
 print "Validating Seifim"
@@ -28,14 +28,14 @@ volume.validate_seifim()
 errors = volume.format_text('@11', '@33', 'dh')
 for i in errors:
     print i
-# base = root.get_base_text()
-# b_vol = base.get_volume(3)
-# assert isinstance(b_vol, Volume)
-# b_vol.mark_references(volume.get_book_id(), u'@69\[([\u05d0-\u05ea]{1,3})\]', group=1)
-# volume.set_rid_on_seifim()
-# root.populate_comment_store()
-# errors = b_vol.validate_all_xrefs_matched(lambda x: x.name=='xref' and re.search(u'@69', x.text) is not None)
-# for i in errors:
-#     print i
+base = root.get_base_text()
+b_vol = base.get_volume(3)
+assert isinstance(b_vol, Volume)
+b_vol.mark_references(volume.get_book_id(), u'@69\[([\u05d0-\u05ea]{1,3})\]', group=1)
+volume.set_rid_on_seifim()
+root.populate_comment_store()
+errors = b_vol.validate_all_xrefs_matched(lambda x: x.name=='xref' and re.search(u'@51', x.text) is not None)
+for i in errors:
+    print i
 # volume.locate_references(ur'\("\)')
-# root.export()
+root.export()
