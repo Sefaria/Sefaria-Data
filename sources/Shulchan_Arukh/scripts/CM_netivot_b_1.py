8057f3d4be066e6274900c0450252cdfb57b34a7
Jonathan Mosenkis <jonathan@sefaria.org>
2017-05-11 14:32:04 +0300

    Netivot Beurim pre-parse

--- sources/Shulchan_Arukh/scripts/CM_netivot_b_1.py
+++ sources/Shulchan_Arukh/scripts/CM_netivot_b_1.py
@@ -28,12 +28,12 @@ volume.validate_seifim()
 errors = volume.format_text('@11', '@33', 'dh')
 for i in errors:
     print i
-# base = root.get_base_text()
-# b_vol = base.get_volume(1)
-# assert isinstance(b_vol, Volume)
-# volume.set_rid_on_seifim()
-# root.populate_comment_store()
-# errors = b_vol.validate_all_xrefs_matched(lambda x: x.name=='xref' and re.search(u'@71', x.text) is not None)
-# for i in errors:
-#     print i
+base = root.get_base_text()
+b_vol = base.get_volume(1)
+assert isinstance(b_vol, Volume)
+volume.set_rid_on_seifim()
+root.populate_comment_store()
+errors = b_vol.validate_all_xrefs_matched(lambda x: x.name=='xref' and re.search(u'@71', x.text) is not None)
+for i in errors:
+    print i
 # root.export()
