c97f14605e6a789c7d6e2d80192c87625e988821
Eytan <eytansterling@gmail.com>
2017-07-03 19:46:19 -0400

    Mordechai Bava Batra update

--- sources/Mordechai/mordechai_parse2.py
+++ sources/Mordechai/mordechai_parse2.py
@@ -72,7 +72,6 @@ class Tractate:
             if remez_segment["Starting Ref"]["Hagahot"]:
                 w_ref+="Amendments, "
             remez_node.wholeRef = w_ref+make_remez_range(remez_segment)
-            print remez_node.wholeRef 
             en_title = "Remez "+str(remez_segment["Starting Ref"]["Remez"])+" Through "+str(remez_segment["Ending Ref"]["Remez"])
             he_title = u"רמז"+u" "+numToHeb(remez_segment["Starting Ref"]["Remez"])+u" עד "+numToHeb(remez_segment["Ending Ref"]["Remez"])
             remez_node.key = en_title
@@ -103,6 +102,7 @@ class Tractate:
         for line in lines:
             if u"הגהות דשייכי" in line:
                 remez_index[-1]["Terminating Ref"]=last_paragraph
+                daf_refs[-1]["Terminating Ref"]=last_paragraph
                 perek_list.append(perek_box)
                 self.text = perek_list
                 perek_box=[]
@@ -115,22 +115,38 @@ class Tractate:
                         if len (remez_index)>0 and "Terminating Ref" not in remez_index[-1]:
                             remez_index[-1]["Terminating Ref"]=last_paragraph
                         remez_index.append({"Remez":getGematria(remez.replace(u"רמז","")),"Perek":len(perek_list),"Paragraph":len(perek_box),"Hagahot":in_hagahot})
+                    for daf_ref in re.findall(ur"@10.*?@01",paragraph):
+                        if u"ע\"א" in daf_ref:
+                            amud="a"
+                        elif u"ע\"ב" in daf_ref:
+                            amud="b"
+                        else:
+                            amud="a-b"
+                        if len(daf_refs)>0:
+                            if "Terminating Ref" not in daf_refs[-1]:
+                                daf_refs[-1]["Terminating Ref"]=last_paragraph
+                        daf_refs.append({"Daf":getGematria(clean_daf_ref(daf_ref)),"Amud":amud,"Perek":len(perek_list),"Paragraph":len(perek_box)})
                     last_paragraph={"Perek":len(perek_list),"Paragraph":len(perek_box)}
+                    if paragraph[-1]==u".":
+                        paragraph=paragraph[:-1]
                     perek_box.append(fix_markers(paragraph)+u":")    
-
+                    
 
             if u"סליק " in line:
+                if "Terminating Ref" not in daf_refs[-1]:
+                    daf_refs[-1]["Terminating Ref"]=last_paragraph
                 perek_list.append(perek_box)
                 #bug with double paragraph
                 if u"סליק פרק ו' וז" in line:
                     perek_list.append([])
                 perek_box = []
+                #last_paragraph={"Perek":len(perek_list),"Paragraph":len(perek_box)}
         perek_list.append(perek_box)
         self.hagahot=perek_list
         self.daf_refs = daf_refs
         remez_index[-1]["Terminating Ref"]=last_paragraph
         self.remez_index = remez_index
-        
+        """
         #to print text:
         for pindex, perek in enumerate(self.text):
             for cindex, comment in enumerate(perek):
@@ -138,7 +154,10 @@ class Tractate:
         for pindex, perek in enumerate(perek_list):
             for cindex, comment in enumerate(perek):
                 print "HAGAHA",pindex, cindex, comment
-        
+        """
+        print "Daf Refs:"
+        for ref in daf_refs:
+            print ref
         segment_remez_refs(remez_index)
     def mord_post_text(self):
         version = {
@@ -155,9 +174,76 @@ class Tractate:
             'language': 'he',
             'text': self.hagahot
         }
-        #post_text_weak_connection('Mordechai on '+self.record_name_en+', Amendments', version)
+        post_text_weak_connection('Mordechai on '+self.record_name_en+', Amendments', version)
     def mord_link_text(self):
-        for link_ref in self.daf_refs:
+        matched_count = 0.00
+        total = 0.00
+        not_matched=[]
+        for ref in self.daf_refs:
+            tractate_chunk = TextChunk(Ref(self.record_name_en+"."+str(ref["Daf"])+ref["Amud"]),"he")
+            m_chunk = TextChunk(Ref('Mordechai on '+self.record_name_en+" "+make_daf_ref_range(ref)),"he")
+            matches = match_ref(tractate_chunk,m_chunk,base_tokenizer,dh_extract_method=dh_extract_method,verbose=True)
+            if "comment_refs" in matches:
+                amud = "a" if ref["Amud"]=="a-b" else ref["Amud"]
+                last_ref = Ref(self.record_name_en+"."+str(ref["Daf"])+amud+" 1")
+                daf_not_matched = []
+                for link_index, (base, comment) in enumerate(zip(matches["matches"],matches["comment_refs"])):
+                    print base,comment
+                    if base:
+                        link = (
+                                {
+                                "refs": [
+                                         base.normal(),
+                                         comment.normal(),
+                                         ],
+                                "type": "commentary",
+                                "auto": True,
+                                "generated_by": "sterling_tos_rid_linker"
+                                })
+                        post_link(link, weak_network=True)
+                        matched_count+=1
+                        while len(daf_not_matched)>0:
+                            link = (
+                                    {
+                                    "refs": [
+                                             last_ref.normal()+"-"+base.ending_ref().normal_last_section(),
+                                             daf_not_matched.pop().normal(),
+                                             ],
+                                    "type": "commentary",
+                                    "auto": True,
+                                    "generated_by": "sterling_tos_rid_linker"
+                                    })
+                            matched_count+=1
+                            post_link(link, weak_network=True)
+                        last_ref = base
+                    else:
+                        daf_not_matched.append(comment)
+                        if link_index==len(matches["matches"])-1:
+                            while len(daf_not_matched)>0:
+                                link = (
+                                        {
+                                        "refs": [
+                                                 last_ref.normal()+"-"+str(len(TextChunk(Ref(last_ref.normal().split(":")[:-1][0])).text)),
+                                                 daf_not_matched.pop().normal(),
+                                                 ],
+                                        "type": "commentary",
+                                        "auto": True,
+                                        "generated_by": "sterling_tos_rid_linker"
+                                        })
+                                matched_count+=1
+                                post_link(link, weak_network=True)
+                    total+=1
+            else:
+                not_matched.append(get_daf_en(amud_index))
+                total+=1
+        if total!=0:
+            print "Ratio: "+str(matched_count/total)
+        else:
+            print "Ratio: No Matches..."
+        print "Not Matched:"
+        for nm in not_matched:
+            print nm
+            """
             print link_ref
             link = (
                     {
@@ -170,8 +256,7 @@ class Tractate:
                     "generated_by": "sterling_tos_rid_linker"
                     })
             post_link(link, weak_network=True)
-            
-        main_tc = TextChunk(Ref('Mordechai on '+self.record_name_en))
+            """
 def make_talmud_array(book):
     tc = TextChunk(Ref(book), "he")
     return_array = []
@@ -189,7 +274,6 @@ def segment_remez_refs(refs):
     return_list = []
     starting_ref = refs[0]
     for index, ref in enumerate(refs[1:]):
-        print "REMEZ", ref["Remez"]
         if ref["Hagahot"]!=refs[index-1]["Hagahot"]:
             last_before_hagahot=refs[index-1]
     for index in range(1,len(refs)):
@@ -204,14 +288,51 @@ def segment_remez_refs(refs):
             remez_count=0
     return_list.append({"Starting Ref":starting_ref, "Ending Ref":refs[-1]})
     return return_list
-
 def make_remez_range(remez_segment):
     return str(remez_segment["Starting Ref"]["Perek"]+1)+":"+str(remez_segment["Starting Ref"]["Paragraph"]+1)+"-"\
         +str(remez_segment["Ending Ref"]["Terminating Ref"]["Perek"]+1)+":"+str(remez_segment["Ending Ref"]["Terminating Ref"]["Paragraph"]+1)
+def make_daf_ref_range(daf_range):
+    return str(daf_range["Perek"]+1)+":"+str(daf_range["Paragraph"]+1)+"-"+str(daf_range["Terminating Ref"]["Perek"]+1)+":"+\
+        str(daf_range["Terminating Ref"]["Paragraph"]+1)
 def not_blank(s):
     while u" " in s:
         s = s.replace(u" ",u"")
     return (len(s.replace(u"\n",u"").replace(u"\r",u"").replace(u"\t",u""))!=0);
+def m_filter(some_string):
+    #asssume every piece of text has a DH
+    return True 
+def remove_extra_space(s):
+    while u"  " in s:
+        s = s.replace(u"  ",u" ")
+    return s
+def dh_extract_method(some_string):
+    some_string=re.sub(ur"[\[\]\(\)\*]",u"",some_string)
+    some_string = re.sub(ur"<small>.*?</small>",u"",remove_extra_space(some_string))
+    splits = {
+        "pi_split":some_string.split(u"פי"+u"'"),
+        "pirush_split": some_string.split(u"פירוש"),
+        "period_split": some_string.split(u"."),
+        "chulei_split": some_string.split(u"כו"+"'")
+        }
+    split_dh=get_smallest(splits)
+    if len(split_dh.split(u" "))<10 and len(split_dh.split(u" "))>1:
+        dh = split_dh
+    else:
+        dh=u' '.join(some_string.split(u" ")[0:4])
+    return dh
+def get_smallest(dic):
+    return_dh = ""
+    keyp = ""
+    smallest_so_far = int
+    for key in dic.keys():
+        if len(dic[key][0])<smallest_so_far:
+            smallest_so_far=len(dic[key][0])
+            return_dh = dic[key][0]
+            keyp=key
+    return return_dh
+def base_tokenizer(some_string):
+    some_string = remove_extra_space(some_string).replace("<b>","").replace("</b>","").replace(":","")
+    return filter(not_blank,some_string.split(" "))
 def fix_markers(s):
     return s.replace(u"@01",u"</small>").replace(u"@10",u"<small>").replace(u'@20',u"<small>").replace(u"\n","")
 def get_perek(ref):
@@ -222,9 +343,9 @@ def get_perek(ref):
 def get_perek_ranges(tractate_name):
     return {node['wholeRef']:int(node['titles'][0]['text'].split()[1]) for node in library.get_index(tractate_name).alt_structs["Chapters"]['nodes']}
 posting_term=True
-posting_index=False
+posting_index=True
 posting_text=True
-linking=False
+linking=True
 
 tractate = Tractate()
 
