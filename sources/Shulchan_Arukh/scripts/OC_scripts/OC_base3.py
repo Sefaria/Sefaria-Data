ce8021647e4032b5884f00c6ab20b670b57abf3a
Jonathan Mosenkis <jonathan@sefaria.org>
2017-08-20 16:53:23 +0300

    parsd OC vol. 3

---
+++ sources/Shulchan_Arukh/scripts/OC_scripts/OC_base3.py
@@ -0,0 +1,23 @@
+# encoding=utf-8
+
+from sources.Shulchan_Arukh.ShulchanArukh import *
+
+root = Root('../../Orach_Chaim.xml')
+base = root.get_base_text()
+filename = u'../../txt_files/Orach_Chaim/part_3/מחבר שולחן ערוך אורח חיים חלק ג.txt'
+
+base.remove_volume(3)
+with codecs.open(filename, 'r', 'utf-8') as infile:
+    volume = base.add_volume(infile.read(), 3)
+assert isinstance(volume, Volume)
+
+volume.mark_simanim(u'@22([\u05d0-\u05ea]{1,4})', specials={u'@00': {'name': u'topic'}})
+print "Validating Simanim"
+volume.validate_simanim()
+
+bad = volume.mark_seifim(u'@11([\u05d0-\u05ea]{1,3})', specials={u'@23': {'name': u'title'}})
+print 'Validating Seifim'
+for i in bad:
+    print i
+volume.validate_seifim()
+root.export()
