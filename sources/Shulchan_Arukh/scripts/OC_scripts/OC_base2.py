7f09efbbe2857edd4a536e08723bed209e472899
Jonathan Mosenkis <jonathan@sefaria.org>
2017-08-20 17:01:54 +0300

    Parsed Orach Chaim (did not yet handle itags)

--- sources/Shulchan_Arukh/scripts/OC_scripts/OC_base2.py
+++ sources/Shulchan_Arukh/scripts/OC_scripts/OC_base2.py
@@ -6,9 +6,9 @@ root = Root('../../Orach_Chaim.xml')
 base = root.get_base_text()
 filename = u'../../txt_files/Orach_Chaim/part_2/שולחן ערוך אורח חיים חלק ב מחבר.txt'
 
-base.remove_volume(1)
+base.remove_volume(2)
 with codecs.open(filename, 'r', 'utf-8') as infile:
-    volume = base.add_volume(infile.read(), 1)
+    volume = base.add_volume(infile.read(), 2)
 assert isinstance(volume, Volume)
 
 volume.mark_simanim(u'@22([\u05d0-\u05ea]{1,3})', specials={u'@00': {'name': u'topic'}})
@@ -20,3 +20,4 @@ print 'Validating Seifim'
 for i in bad:
     print i
 volume.validate_seifim()
+root.export()
