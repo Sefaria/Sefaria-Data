6041e9d0f83192f2d4738f80081d8863008a5d79
Noah Santacruz <noahssantacruz@gmail.com>
2017-12-03 13:08:43 +0200

    adding word2vec training scripts

---
+++ research/word2vec_xprmnts/main.py
@@ -0,0 +1,55 @@
+# encoding=utf-8
+
+import codecs
+import word2vec
+from export_library_as_file import export_library_as_file, export_library_as_docs
+
+def get_training_set():
+    try:
+        with codecs.open("all_of_sefaria.txt", "rb", encoding='utf8') as fin:
+            assert isinstance(fin, file)
+            return fin.readlines()
+    except IOError:
+        print "EXPORTING LIBRARY"
+        export_library_as_file("all_of_sefaria.txt")
+        return get_training_set()
+
+
+def train_word2phrase():
+    word2vec.word2phrase("all_of_sefaria.txt", "word2phrase.bin", verbose=True)
+
+
+def train_word2vec():
+    word2vec.word2vec("word2phrase.bin", "word2vec.bin", verbose=True)
+
+
+def train_clusters():
+    word2vec.word2clusters("word2phrase.bin", "word2vec.bin", 1000, verbose=True)
+
+
+def train_doc2vec():
+    word2vec.doc2vec("all_of_sefaria_docs.txt", "doc2vec.bin", verbose=True, cbow=0)
+
+
+def get_model(filename, clusters=False):
+    if clusters:
+        return word2vec.load_clusters(filename)
+    else:
+        return word2vec.load(filename)
+
+
+
+def get_k_similar(model, word, k=10):
+    indexes, metrics = model.cosine(word, n=k)
+    return model.vocab[indexes]
+
+
+#export_library_as_file("all_of_sefaria.txt")
+#export_library_as_docs("all_of_sefaria_docs.txt")
+# train_word2phrase()
+# train_word2vec()
+#train_clusters()
+train_doc2vec()
+#model = get_model("doc2vec.bin")
+
+#print get_k_similar(model, model.vocab[400], 100)
