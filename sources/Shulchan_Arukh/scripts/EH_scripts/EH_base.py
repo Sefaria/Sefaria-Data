db0881bfcddbf5f7ac9123fb3840d2ae923dbb81
Jonathan Mosenkis <jonathan@sefaria.org>
2017-12-24 17:18:18 +0200

    standardize both ase files and some structural corrections in vol.2

--- sources/Shulchan_Arukh/scripts/EH_scripts/EH_base.py
+++ sources/Shulchan_Arukh/scripts/EH_scripts/EH_base.py
@@ -11,14 +11,14 @@ start_ramah        @33
 end_ramah          @88
 
 Beur HaGra         !.)
-Taz                A[.]
+Taz                A[.]  @91
 Be'er HaGolah      @44.
 Pithei Teshuva     @66(.)
 
 Marks to existing commentaries:
 Beit Shmuel        @55.
 Chelkat Mechokek   @77(.)
-Ba'er Hetev        B.)
+Ba'er Hetev        B.)  @82
 """
 
 
@@ -30,35 +30,44 @@ if __name__ == "__main__":
 
     root = Root(xml_loc)
     base = root.get_base_text()
-    filename = os.path.join(root_dir, u'txt_files/Even_Haezer/part_1/אבן העזר חלק א מחבר.txt')
-    assert os.path.exists(filename)
+    filenames = {
+        1: os.path.join(root_dir, u'txt_files/Even_Haezer/part_1/אבן העזר חלק א מחבר.txt'),
+        2: os.path.join(root_dir, u'txt_files/Even_Haezer/part_2/שולחן ערוך אבן האזל חלק ב מחבר.txt')
+    }
+    for i in [1,2]:
+        filename = filenames[i]
+        assert os.path.exists(filename)
 
-    base.remove_volume(1)
-    with codecs.open(filename, 'r', 'utf-8') as infile:
-        volume = base.add_volume(infile.read(), 1)
-    assert isinstance(volume, Volume)
+        base.remove_volume(i)
+        with codecs.open(filename, 'r', 'utf-8') as infile:
+            volume = base.add_volume(infile.read(), i)
+        assert isinstance(volume, Volume)
 
-    volume.mark_simanim(u'@22([\u05d0-\u05ea]{1,3})', specials={u'@00': {'name': u'topic'}})
-    print "Validating Simanim"
-    volume.validate_simanim()
+        volume.mark_simanim(u'@22([\u05d0-\u05ea]{1,3})', specials={
+            u'@00': {'name': u'topic'},
+            u'@13': {'name': u'Halitza', 'end': u'!end!'},
+            u'@14': {'name': u'Get', 'end': u'!end!'}
+        })
+        print "Validating Simanim"
+        volume.validate_simanim()
 
-    bad = volume.mark_seifim(u'@11([\u05d0-\u05ea]{1,3})', specials={u'@12': {'name': u'title'}})
-    print "Validating Seifim"
-    for i in bad:
-        print i
-    volume.validate_seifim()
+        bad = volume.mark_seifim(u'@11([\u05d0-\u05ea]{1,3})', specials={u'@12': {'name': u'title'}})
+        print "Validating Seifim"
+        for i in bad:
+            print i
+        volume.validate_seifim()
 
-    errors = volume.format_text('@33', '@88', 'ramah')
-    for i in errors:
-        print i
+        errors = volume.format_text('@33', '@88', 'ramah')
+        for i in errors:
+            print i
 
-    codes = [u'!.) -Gra', u'A[.] -Taz', u'@66 -Pithei Teshuva', u'@55 -Beit Shmuel', u'@77 -Chelkat Mechokek', u'B.) -Ba\'er Hetev']
-    patterns = [ur'!{}', ur'A\[{}\]', ur'@66{}', ur'@55{}', ur'@77\({}\)', ur'B{}\)']
-    patterns = [pattern.format(ur'([\u05d0-\u05ea]{1,3})') for pattern in patterns]
+        codes = [u'!.) -Gra', u'@91[.] -Taz', u'@66 -Pithei Teshuva', u'@55 -Beit Shmuel', u'@77 -Chelkat Mechokek', u'@82.) -Ba\'er Hetev']
+        patterns = [ur'!{}', ur'@91\[{}\]', ur'@66{}', ur'@55{}', ur'@77\({}\)', ur'@82{}\)']
+        patterns = [pattern.format(ur'([\u05d0-\u05ea]{1,3})') for pattern in patterns]
 
 
-    volume.validate_references(ur'@44([\u05d0-\u05ea])', u'@44 -Be\'er HaGolah', key_callback=he_ord)
-    for pattern, code in zip(patterns, codes):
-        volume.validate_references(pattern, code)
+        volume.validate_references(ur'@44([\u05d0-\u05ea])', u'@44 -Be\'er HaGolah', key_callback=he_ord)
+        for pattern, code in zip(patterns, codes):
+            volume.validate_references(pattern, code)
 
-    # correct_marks_in_file(filename, u'@22', ur'B([\u05d0-\u05ea]{1,3})\)', overwrite=False)
+        # correct_marks_in_file(filename, u'@22', ur'@82([\u05d0-\u05ea]{1,3})\)', overwrite=False)
