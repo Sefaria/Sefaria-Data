d65180f2e6a8118cd18a2e57f93528263d5c4805
Jonathan Mosenkis <jonathan@sefaria.org>
2018-01-16 11:48:37 +0200

    changed link validation script to read from api instead of locally

--- sources/Shulchan_Arukh/scripts/EH_scripts/validate_existing_commentaries.py
+++ sources/Shulchan_Arukh/scripts/EH_scripts/validate_existing_commentaries.py
@@ -1,6 +1,8 @@
 # coding=utf-8
 
+import requests
 from sefaria.model import *
+from collections import defaultdict
 from sources.Shulchan_Arukh.ShulchanArukh import *
 
 
@@ -42,18 +44,33 @@ def compare_num_comments(xml_book, commentary_name, marker_pattern):
                 no_issues = False
     return no_issues
 
-def check_links_on_seifim(xml_book, commentary_name, marker_pattern):
+
+def compile_linkset(book_name, server='https://www.sefaria.org'):
+    raw_set = requests.get(u'{}/api/links/Shulchan_Arukh,_Even_HaEzer?with_text=0'.format(server)).json()
+    commentary_reg = re.compile(Ref(book_name).regex())
+    filtered_set = [l for l in raw_set if commentary_reg.match(l['ref']) is not None]
+
+    linkset = defaultdict(lambda: [])
+    for l in filtered_set:
+        linkset[Ref(l['anchorRef']).normal()].append(l)
+    return linkset
+
+
+def check_links_on_seifim(xml_book, commentary_name, marker_pattern, server='http://www.sefaria.org'):
     """
     Check each Seif of commentary to ensure the number of links at each seif match the number of references
     :param Record xml_book:
     :param commentary_name:
     :param marker_pattern:
+    :param server
     :return:
     """
     sefaria_simanim = library.get_index(u"Shulchan Arukh, Even HaEzer").all_section_refs()
     xml_simanim = xml_book.get_simanim()
     assert len(sefaria_simanim) == len(xml_simanim)
     issues = 0
+
+    remote_linkset = compile_linkset(commentary_name, server)
 
     for sefaria_siman, xml_siman in zip(sefaria_simanim, xml_simanim):
         sefaria_seifim = sefaria_siman.all_subrefs()
@@ -65,7 +82,7 @@ def check_links_on_seifim(xml_book, commentary_name, marker_pattern):
 
         for sefaria_seif, xml_seif in zip(sefaria_seifim, xml_seifim):
             num_xml_refs = len(xml_seif.grab_references(marker_pattern))
-            num_links = len(LinkSet(sefaria_seif).filter(commentary_name))
+            num_links = len(remote_linkset[sefaria_seif.normal()])
 
             if num_xml_refs != num_links:
                 issues += 1
