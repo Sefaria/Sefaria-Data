a9ca948fb16725f35dd82cfa0feac2f87a4f578f
Jonathan Mosenkis <jonathan@sefaria.org>
2018-02-06 15:46:23 +0200

    gra parse complete

--- sources/Shulchan_Arukh/scripts/EH_scripts/EH_gra.py
+++ sources/Shulchan_Arukh/scripts/EH_scripts/EH_gra.py
@@ -6,7 +6,7 @@ filenames = {
     'part_1': u"/home/jonathan/sefaria/Sefaria-Data/sources/Shulchan_Arukh/txt_files/Even_Haezer/part_1/אבן העזר חלק א ביאור הגר''א.txt",
     'part_2': u"/home/jonathan/sefaria/Sefaria-Data/sources/Shulchan_Arukh/txt_files/Even_Haezer/part_2/שולחן ערוך אבן האזל חלק ב ביאור הגרא.txt"
 }
-root = Root('../../Choshen_Mishpat.xml')
+root = Root('../../Even_HaEzer.xml')
 commentaries = root.get_commentaries()
 gra = commentaries.get_commentary_by_title("Beur HaGra")
 if gra is None:
@@ -29,13 +29,23 @@ for piece in [1,2]:
 
     errors = volume.mark_seifim(u'@11([\u05d0-\u05ea]{1,3})\)')
     print "Validating Seifim"
-    for i in errors:
-        print i
+    for e in errors:
+        print e
     volume.validate_seifim()
 
     if piece == 1:
         errors = volume.format_text(u'@77', u'@33', u'dh')
     else:
         errors = volume.format_text(u'@44', u'@33', u'dh')
-    for i in errors:
-        print i
+    for e in errors:
+        print e
+
+    base = root.get_base_text()
+    b_vol = base.get_volume(piece)
+    assert isinstance(b_vol, Volume)
+    volume.set_rid_on_seifim()
+    root.populate_comment_store(verbose=False)
+    errors = b_vol.validate_all_xrefs_matched(lambda x: x.name=='xref' and re.search(u'!([\u05d0-\u05ea]{1,3})\)', x.text) is not None)
+    for e in errors:
+        print e
+root.export()
