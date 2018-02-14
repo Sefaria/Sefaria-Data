71cd2df5c3523b82b09fe1253eb4439c0df9f8c4
Jonathan Mosenkis <jonathan@sefaria.org>
2018-02-14 14:16:04 +0200

    Parsed Pithei Teshuva Seifim & source file corrections

--- sources/Shulchan_Arukh/scripts/EH_scripts/EH_Pithei_Teshuva.py
+++ sources/Shulchan_Arukh/scripts/EH_scripts/EH_Pithei_Teshuva.py
@@ -8,6 +8,25 @@ filenames = {
 }
 root = Root('../../Even_HaEzer.xml')
 commentaries = root.get_commentaries()
-pithei = commentaries.get_commentary_by_title('Pithei Teshuva', u'פתחי תשובה')
+pithei = commentaries.get_commentary_by_title('Pithei Teshuva')
 if pithei is None:
     pithei = commentaries.add_commentary('Pithei Teshuva', u'פתחי תשובה')
+
+for piece in [1,2]:
+    pithei.remove_volume(piece)
+    with codecs.open(filenames['part_{}'.format(piece)], 'r', 'utf-8') as infile:
+        volume = pithei.add_volume(infile.read(), piece)
+    assert isinstance(volume, Volume)
+
+    volume.mark_simanim(u'@00([\u05d0-\u05ea]{1,3})', specials={
+        u'@13': {'name': u'Halitza', 'end': u'!end!'},
+        u'@14': {'name': u'Get', 'end': u'!end!'}
+    })
+    print "Validating Simanim"
+    volume.validate_simanim(complete=False)
+
+    errors = volume.mark_seifim(u'@22\(([\u05d0-\u05ea]{1,3})\)')
+    print "Validating Seifim"
+    for e in errors:
+        print e
+    volume.validate_seifim()
