7208be4c193dfc0d77795be0dfbdda97814056d6
Kevin <kevwolf@umich.edu>
2018-03-26 23:16:04 -0400

    Do refs for sources

--- lexicon/jastrow_sources.py
+++ lexicon/jastrow_sources.py
@@ -3,13 +3,36 @@
 from sefaria.model import *
 import codecs
 
+
+def find_insert_idx(word):
+    end_insert = 0
+    while (end_insert < len(word)) \
+            and (not any(end_delimiter == word[end_insert]
+                         for end_delimiter in [u'.', u',', u'('])):
+        end_insert += 1
+    return end_insert
+
+
 filepath = '/Users/kevinwolf/Documents/Sefaria/data.Sefaria/lexicon/jastrow ref titles'
 with codecs.open(filepath, 'r', encoding='utf-8') as fr:
-    for line in fr:
-        abrv, longform = line.split('=')
-        abrv = abrv.strip()
-        longform = longform
-        if Ref().is_ref(abrv):
-            print u"{} is a ref".format(abrv)
-        if Ref().is_ref(longform):
-            print u"{} its a ref".format(longform)
+    with codecs.open('./jastrow_refs.txt', 'w', 'utf-8') as fw:
+        for line in fr:
+            values = line.split('=')
+            abrv = values[0].strip()
+            idx = find_insert_idx(values[1])
+            longform = values[1][:idx].strip()
+            ref = ''
+            if Ref().is_ref(abrv):
+                ref = Ref(abrv)
+                print u"{} is a short ref for {}".format(abrv, Ref(abrv)._normal)
+            if Ref().is_ref(longform):
+                print u"{} is a long ref for {}".format(longform, Ref(longform)._normal)
+                ref = Ref(longform)
+            
+            if ref:
+                fw.write(u"{} = {} = {}\n".format(abrv, longform, ref._normal))
+            else:
+                fw.write(u"{} = {} = \n".format(abrv, longform))
+            # else:
+            #     
+                
