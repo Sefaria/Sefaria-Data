e4ed1d4e01a01f0513bf7077762ae4a4b4e2e82d
Jonathan <jonathan@sefaria.org>
2018-06-12 13:30:24 +0300

    Finished tag resolution code - first draft; good time to commit

--- sources/Shulchan_Arukh/scripts/YD_scripts/fix_indistinguishable_tags.py
+++ sources/Shulchan_Arukh/scripts/YD_scripts/fix_indistinguishable_tags.py
@@ -72,28 +72,29 @@ class TagResolver:
         self.unknown_pattern = unknown_pattern
         self.filename = filename
         self.document = StructuredDocument(filename, section_pattern)
+        self.resolution_mapping = {}
+        self.commentary_a = None
+        self.commentary_b = None
 
-    def resolve_chains(self, chain_list, commentary_a, commentary_b, section):
+    def _resolve_chains(self, chain_list, section):
         """
         :param chain_list:
-        :param Commentary commentary_a:
-        :param Commentary commentary_b:
         :param section
         :return:
         """
         resolution = {
-            commentary_a.name: [],
-            commentary_b.name: [],
+            self.commentary_a.name: [],
+            self.commentary_b.name: [],
             u'tbd': []
         }
 
         def a_to_a(x):
-            resolution[commentary_a.name].append(x[0])
-            resolution[commentary_b.name].append(x[1])
+            resolution[self.commentary_a.name].extend(x[0])
+            resolution[self.commentary_b.name].extend(x[1])
 
         def a_to_b(x):
-            resolution[commentary_a.name].append(x[1])
-            resolution[commentary_b.name].append(x[0])
+            resolution[self.commentary_a.name].extend(x[1])
+            resolution[self.commentary_b.name].extend(x[0])
 
         def bad_bad_bad(x):
             raise AssertionError(u"Chain marked positively for two commentaries at section {}".format(section))
@@ -113,18 +114,18 @@ class TagResolver:
         # the last chains have the ability to match to number of comments. Solve those first, then work backwards
         chain_list = list(reversed(chain_list))
 
-        comments_per_section = (commentary_a.comments_per_section[section], commentary_b.comments_per_section[section])
+        comments_per_section = (self.commentary_a.comments_per_section[section], self.commentary_b.comments_per_section[section])
         if comments_per_section[0] != comments_per_section[1]:
             last_chains = chain_list[0]
             num_tags = [getGematria(i.group(1)) for i in last_chains]
 
             if num_tags == comments_per_section:
-                resolution[commentary_a.name].append(last_chains[0])
-                resolution[commentary_b.name].append(last_chains[1])
+                resolution[self.commentary_a.name].extend(last_chains[0])
+                resolution[self.commentary_b.name].extend(last_chains[1])
                 chain_list = chain_list[1:]
             elif (num_tags[1], num_tags[0]) == comments_per_section:
-                resolution[commentary_b.name].append(last_chains[0])
-                resolution[commentary_a.name].append(last_chains[1])
+                resolution[self.commentary_b.name].extend(last_chains[0])
+                resolution[self.commentary_a.name].extend(last_chains[1])
                 chain_list = chain_list[1:]
             else:
                 print u"Comment number mismatch in section {}".format(section)
@@ -132,71 +133,93 @@ class TagResolver:
         for chain_pair in chain_list:
             # test against each commentary's unique regex
             # check each chain for any appearance of unique tags
-            com_a_test = (any(re.search(commentary_a.tag_pattern, i.group()) for i in c) for c in chain_pair)
-            com_b_test = (any(re.search(commentary_b.tag_pattern, i.group()) for i in c) for c in chain_pair)
+            com_a_test = (any(re.search(self.commentary_a.tag_pattern, i.group()) for i in c) for c in chain_pair)
+            com_b_test = (any(re.search(self.commentary_b.tag_pattern, i.group()) for i in c) for c in chain_pair)
 
             # ensure that we don't have cases where both chains have been positively marked for both commentaries
             if all(com_a_test) or all(com_b_test):
                 raise AssertionError(u"Chain marked positively for two commentaries at section {}".format(section))
             resolution_map[(com_a_test, com_b_test)](chain_pair)
-        return resolution
 
+        self.resolution_mapping[section] = resolution
 
-def build_chains(text, pattern, group=1):
-    def digitify(x):
-        return int(x)
+    @staticmethod
+    def build_chains(text, pattern, group=1):
+        def digitify(x):
+            return getGematria(x)
 
-    chains = []
-    current_chain = ([], [])
+        chains = []
+        current_chain = ([], [])
 
-    all_tags = re.finditer(pattern, text)
-    for tag in all_tags:
+        all_tags = re.finditer(pattern, text)
+        for tag in all_tags:
 
-        # both chains are empty
-        if all([len(i) == 0 for i in current_chain]):
-            current_chain[0].append(tag)
-
-        # one chain is empty -> no need to worry about broken chains
-        elif len(current_chain[1]) == 0:
-            if digitify(tag.group(group)) - 1 == digitify(current_chain[0][-1].group(group)):
+            # both chains are empty
+            if all([len(i) == 0 for i in current_chain]):
                 current_chain[0].append(tag)
+
+            # one chain is empty -> no need to worry about broken chains
+            elif len(current_chain[1]) == 0:
+                if digitify(tag.group(group)) - 1 == digitify(current_chain[0][-1].group(group)):
+                    current_chain[0].append(tag)
+                else:
+                    current_chain[1].append(tag)
+
             else:
-                current_chain[1].append(tag)
+                # both chains are full. Start by testing that the ends of both chains aren't equal (broken chain)
+                if digitify(current_chain[0][-1].group(group)) == digitify(current_chain[1][-1].group(group)):
+                    chains.append(current_chain)
+                    current_chain = ([], [])
+                    current_chain[0].append(tag)
 
-        else:
-            # both chains are full. Start by testing that the ends of both chains aren't equal (broken chain)
-            if digitify(current_chain[0][-1].group(group)) == digitify(current_chain[1][-1].group(group)):
-                chains.append(current_chain)
-                current_chain = ([], [])
-                current_chain[0].append(tag)
+                elif digitify(tag.group(group)) - digitify(current_chain[0][-1].group(group)) == 1:
+                    current_chain[0].append(tag)
+                elif digitify(tag.group(group)) - digitify(current_chain[1][-1].group(group)) == 1:
+                    current_chain[1].append(tag)
+                else:
+                    raise AssertionError("Tag does not match either chain")
+        chains.append(current_chain)
+        return chains
 
-            elif digitify(tag.group(group)) - digitify(current_chain[0][-1].group(group)) == 1:
-                current_chain[0].append(tag)
-            elif digitify(tag.group(group)) - digitify(current_chain[1][-1].group(group)) == 1:
-                current_chain[1].append(tag)
+    def resolve_sections(self, commentary_a, commentary_b, group=1):
+        self.resolution_mapping.clear()
+        self.commentary_a = commentary_a
+        self.commentary_b = commentary_b
+        sections = self.document.get_chapter_values()
+        for section in sections:
+            chain_list = self.build_chains(self.document.get_section(section), self.unknown_pattern, group)
+            self._resolve_chains(chain_list, section)
+
+    @staticmethod
+    def fix_tag(text, match_obj_list, repl):
+        char_list = list(text)
+        for match_obj in match_obj_list:
+            char_list[match_obj.start()+1] = repl
+            char_list[match_obj.start()+2] = repl
+        fixed = u''.join(char_list)
+        assert len(fixed) == len(text)
+        return fixed
+
+    def fix_all_tags(self, test_mode=True):
+        if not self.resolution_mapping:
+            raise AssertionError("Please run TagResolver.resolve_sections")
+        for section in self.document.get_chapter_values():
+            resolution = self.resolution_mapping[section]
+            if resolution['tbd']:
+                print u"unresolved chains at section {}".format(section)
+                continue
+            self.document.edit_section(section, self.fix_tag,
+                                       resolution[self.commentary_a.name], self.commentary_a.tag_pattern)
+            self.document.edit_section(section, self.fix_tag,
+                                       resolution[self.commentary_b.name], self.commentary_b.tag_pattern)
+            if test_mode:
+                filename = re.sub(ur'\.txt$', u'_test.txt', self.filename)
             else:
-                raise AssertionError("Tag does not match either chain")
-    chains.append(current_chain)
-    return chains
+                filename = self.filename
+            self.document.write_to_file(filename)
 
 
-def fix_tag(text, match_obj, repl):
-    char_list = list(text)
-    char_list[match_obj.start()+1] = repl
-    char_list[match_obj.start()+2] = repl
-    fixed = u''.join(char_list)
-    assert len(fixed) == len(text)
-    return fixed
 
-
-my_text = u'''Hello @771 there @772 @771 bob @773 what's @772 up'''
-the_chains = build_chains(my_text, ur'@77(\d)')
-for chain in the_chains:
-    for thing in chain[0]:
-        my_text = fix_tag(my_text, thing, '8')
-    for thing in chain[1]:
-        my_text = fix_tag(my_text, thing, '6')
-print my_text
 
 
 
