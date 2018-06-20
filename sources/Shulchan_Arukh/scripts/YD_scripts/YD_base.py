ef306217f84c6d16d55d0e9c6811a5e02b918150
Jonathan <jonathan@sefaria.org>
2018-06-20 14:00:33 +0300

    moved some variables so they can be accessed by external scripts

--- sources/Shulchan_Arukh/scripts/YD_scripts/YD_base.py
+++ sources/Shulchan_Arukh/scripts/YD_scripts/YD_base.py
@@ -4,42 +4,44 @@ import os
 from os.path import dirname as loc
 from sources.Shulchan_Arukh.ShulchanArukh import *
 
+root_dir = loc(loc(loc(os.path.abspath(__file__))))
+xml_loc = os.path.join(root_dir, 'Yoreh_Deah.xml')
+
+filenames = [
+    u"txt_files/Yoreh_Deah/part_1/שולחן ערוך יורה דעה חלק א מחבר.txt",
+    u"txt_files/Yoreh_Deah/part_2/שולחן ערוך יורה דעה חלק ב מחבר.txt",
+    u"txt_files/Yoreh_Deah/part_3/שולחן ערוך יורה דעה חלק ג מחבר.txt",
+    u"txt_files/Yoreh_Deah/part_4/שולחן ערוך יורה דעה חלק ד מחבר.txt",
+]
+filenames = dict(zip(range(1, 5), [os.path.join(root_dir, f) for f in filenames]))
+
+codes = [
+    u'@55 -Shach',
+    u"@66 -Ba'er Hetev",
+    u'@71 -Turei Zahav',
+    u'@74 -Pithei Teshuva',
+    u'@99 -Gra',
+    u"@44 -Be'er HaGolah",
+    u"&   -Torat HaShlamim"
+]
+patterns = [
+    ur'@55{}',
+    ur'@66\({}\)',
+    ur'@71\({}\)',
+    ur'@74\({}\)',
+    ur'@99\[{}\]',
+    ur'@44{}',
+    ur'\&\[{}\]'
+]
+patterns = [pattern.format(ur'([\u05d0-\u05ea]{1,3})') for pattern in patterns]
+
 if __name__ == "__main__":
-    root_dir = loc(loc(loc(os.path.abspath(__file__))))
-    xml_loc = os.path.join(root_dir, 'Yoreh_Deah.xml')
+
     if not os.path.exists(xml_loc):
         raise IOError("xml file does not exist. Please run startup.py")
 
     root = Root(xml_loc)
     base = root.get_base_text()
-
-    filenames = [
-        u"txt_files/Yoreh_Deah/part_1/שולחן ערוך יורה דעה חלק א מחבר.txt",
-        u"txt_files/Yoreh_Deah/part_2/שולחן ערוך יורה דעה חלק ב מחבר.txt",
-        u"txt_files/Yoreh_Deah/part_3/שולחן ערוך יורה דעה חלק ג מחבר.txt",
-        u"txt_files/Yoreh_Deah/part_4/שולחן ערוך יורה דעה חלק ד מחבר.txt",
-    ]
-    filenames = dict(zip(range(1, 5), [os.path.join(root_dir, f) for f in filenames]))
-
-    codes = [
-        u'@55 -Shach',
-        u"@66 -Ba'er Hetev",
-        u'@71 -Turei Zahav',
-        u'@74 -Pithei Teshuva',
-        u'@99 -Gra',
-        u"@44 -Be'er HaGolah",
-        u"&   -Torat HaShlamim"
-    ]
-    patterns = [
-        ur'@55{}',
-        ur'@66\({}\)',
-        ur'@71\({}\)',
-        ur'@74\({}\)',
-        ur'@99\[{}\]',
-        ur'@44{}',
-        ur'\&\[{}\]'
-    ]
-    patterns = [pattern.format(ur'([\u05d0-\u05ea]{1,3})') for pattern in patterns]
 
     for vol_num in range(1, 5):
         print u"Working on vol. {}".format(vol_num)
