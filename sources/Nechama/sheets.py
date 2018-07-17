6a265148819880353bc37e0c429caece1496b569
Shanee <shanee@sefaria.org>
2018-07-17 13:35:50 +0300

    some new classes

--- sources/Nechama/sheets.py
+++ sources/Nechama/sheets.py
@@ -13,10 +13,10 @@ from sefaria.model.text import *
 
 class Sheet(object):
 
-    def __init__(self, title, year, ref):
+    def __init__(self, title, year, ref=None):
         self.title = title
-        self.year = getGematria(year)+1240
         self.he_year = year
+        self.year = getGematria(year)+1240
         self.sections = []
         self.pesukim = Ref(ref)
         self.header_text = u""
@@ -55,7 +55,7 @@ class Parshan(object):
         return ref
 
 
-class Tanakh_extract(object):
+class Bible(object):
 
     def __init__(self, pasuk_ref):
         self.ref = Ref(pasuk_ref)
@@ -71,13 +71,75 @@ class Qustion(object):
         self.text = u""
 
 
-def bs4_reader(self):
+class Table(object):
+
+    def __init__(self):
+        pass
+
+class RT_Rashi(object):
+
+    """an object representing an RT_Rashi table for parsing purposes"""
+
+
+class Nechama_comment(object):
+
+    def __init__(self, text):
+        self.text = text
+
+
+def bs4_reader(file_list):
     """
     The main BeautifulSoup reader function, that etrates on all sheets and creates the obj, probably should be in it's own file
     :param self:
     :return:
     """
-    pass
 
+    for html_sheet in file_list:
+        content = BeautifulSoup(open("{}".format(html_sheet)), "lxml")
+        sheet = Sheet(content.find('div', {'id': 'contentTop'}).find("h1").text, content.find("div", {"id": "year"}).text.replace(u"שנת", u""))
+
+        pass
+
+    # for which_sheet, i in enumerate(self.bereshit_parshiot):
+    #     i += ".html"
+    #     self.sheet_num = which_sheet + 1
+    #     content = BeautifulSoup(open("{}".format(i)), "lxml")
+    #     header = content.find('div', {'id': 'contentTop'})
+    #     if page_missing in header.text:
+    #         continue
+    #     sheet_title = header.find("h1").text
+    #     hebrew_year = content.find("div", {"id": "year"}).text.replace(u"שנת", u"")
+    #     roman_year = getGematria(hebrew_year) + 1240
+    #     self.current_en_year = roman_year
+    #
+    #     parsha = content.find("div", {"id": "paging"}).text
+    #     self.current_parsha = parsha
+    #     print i
+    #     self.current_sefer, self.current_perakim, self.current_pasukim = self.extract_perek_info(content)
+    #     self.current_sefer = library.get_index(self.current_sefer)
+    #     self.current_alt_titles = self.current_sefer.nodes.get_titles('en')
+    #     self.current_sefer = self.current_sefer.title
+    #     text = content.find("div", {"id": "contentBody"})
+    #     if parsha not in self.sheets:
+    #         self.sheets[parsha] = {}
+    #     assert roman_year not in self.sheets[parsha].keys()
+    #     self.year_to_url[roman_year] = i
+    #     self.year_to_sheet[roman_year] = sheet_title
+    #     self.current_url = i
+    #     self.current_perek = self.current_perakim[0]
+    #     self.current_pasuk = None
+    #     self.quotations = []
+    #     self.current_pos_in_quotation_stack = 0
+    #     self.quotation_stack = []
+    #     self.current_section = 0
+    #     self.quotation_stack = []
+    #     self.current_parsha_ref = ["bible", u"{} {}".format(self.current_sefer, self.current_perek)]
+    #     self.add_to_quotation_stack(self.current_parsha_ref)
+    #     self.sheets[parsha][self.current_en_year] = (
+    #     self.current_url, hebrew_year, self.current_sefer, self.current_perakim, self.parse_as_text(text))
+    #     self.post_text(parsha, self.current_en_year, self.sheets[parsha][self.current_en_year])
+
+if __name__ == "__main__":
+    bs4_reader(['html_sheets/{}.html'.format(x) for x in ["1", "2", "30", "62", "84", "148","212","274","302","378","451","488","527","563","570","581","750","787","820","844","894","929","1021","1034","1125","1183","1229","1291","1351","1420"]])
 
 
