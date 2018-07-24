f381b83a8d3a1395cad312d6eed4446bf8ba916b
Shanee <shanee@sefaria.org>
2018-07-24 15:37:51 +0300

    debuging prallel matcher

--- sources/Nechama/nechama_tests.py
+++ sources/Nechama/nechama_tests.py
@@ -14,20 +14,20 @@ class Test_prallel_matcher():
 
     def test_matching(self):
         np = Nechama_parser()
-        ref = u'Haamek Davar on Genesis 4:17'
-        comment = u" שהבין קין עתה רצון ה\\', שטוב להיות מרבה בצרכיו ולא לחיות כחיה ובהמה על ידי עבודת האדמה לבדה, אלא לבקש חיי אנושי בייחוד, על כן בנה לו עיר."
-        # new_ref = refine_ref_by_text(ref, "", comment, 20, alwaysCheck=True, truncateSheet=False, daf_skips=2, rashi_skips=2, overall=2)
-        matched = np.check_reduce_sources(comment, Ref(ref))
-        new_ref = matched[0].a.ref
-        assert new_ref == Ref(u'Haamek Davar on Genesis 4:17:2')
-
-        comment = u''' והוא היה לוטש וחורש כל נחושת וברזל. יאמר, כי הוא היה מחדד וחורש כל מלאכת נחושת וברזל. וכמוהו (הושע י"ד ג\\') "כל תשא עוון וקח טוב".'''
-        ref = u'Ramban on Genesis 4:22'
-        matched = np.check_reduce_sources(comment, Ref(ref))
+        # ref = u'Haamek Davar on Genesis 4:17'
+        # comment = u" שהבין קין עתה רצון ה\\', שטוב להיות מרבה בצרכיו ולא לחיות כחיה ובהמה על ידי עבודת האדמה לבדה, אלא לבקש חיי אנושי בייחוד, על כן בנה לו עיר."
+        # # new_ref = refine_ref_by_text(ref, "", comment, 20, alwaysCheck=True, truncateSheet=False, daf_skips=2, rashi_skips=2, overall=2)
+        # matched = np.check_reduce_sources(comment, Ref(ref))
         # new_ref = matched[0].a.ref
-        # assert new_ref == Ref(u'Ramban on Genesis 4:22:1')
+        # assert new_ref == Ref(u'Haamek Davar on Genesis 4:17:2')
+        #
+        # comment = u''' והוא היה לוטש וחורש כל נחושת וברזל. יאמר, כי הוא היה מחדד וחורש כל מלאכת נחושת וברזל. וכמוהו (הושע י"ד ג\\') "כל תשא עוון וקח טוב".'''
+        # ref = u'Ramban on Genesis 4:22'
+        # matched = np.check_reduce_sources(comment, Ref(ref))
+        # # new_ref = matched[0].a.ref
+        # # assert new_ref == Ref(u'Ramban on Genesis 4:22:1')
 
-        comment = u'''ונקרא כך שנתערבו בו הצורות ובקר הפך ערב שיוכל אדם לבקר '''
-        ref = u'Ibn Ezra on Genesis.1.5'
+        comment = u'''ויעש אלהים את־הרקיע ויבדל בין המים אשר מתחת לרקיע ובין המים אשר מעל לרקיע ויהי־כן'''
+        ref = u'Genesis.1.7'
         matched = np.check_reduce_sources(comment, Ref(ref))
-        new_ref = matched[0].a.ref == Ref(u'Ibn Ezra on Genesis.1.5.2')
+        new_ref = matched[0].a.ref == Ref(u'Genesis.1.7')
--- sources/Nechama/parse_sheets.py
+++ sources/Nechama/parse_sheets.py
@@ -9,6 +9,8 @@ import unicodedata
 from sefaria.utils.hebrew import strip_cantillation
 from collections import OrderedDict
 from segments import *
+from data_utilities.util import WeightedLevenshtein
+
 
 
 class Nechama_parser(object):
@@ -16,6 +18,8 @@ class Nechama_parser(object):
     def __init__(self):
         self.sheets = OrderedDict()
         self.sheet = None
+        self.levenshtein = WeightedLevenshtein()
+
 
 
     def bs4_reader(self, file_list_names):
@@ -34,9 +38,8 @@ class Nechama_parser(object):
             body_dict = self.dict_from_html_attrs(content.find('div', {'id': "contentBody"}))
             self.sheet.sections.extend([v for k, v in body_dict.items() if re.search(u'ContentSection_\d', k)]) # check that these come in in the right order
             self.sheet.sheet_remark = body_dict['sheetRemark'].text
-
             pass
-        return sheets
+        return self.sheets
 
     def dict_from_html_attrs(self, contents):
         d = OrderedDict()
@@ -46,7 +49,6 @@ class Nechama_parser(object):
             else:
                 d[e.name] = e
         return d
-
 
     def get_score(self, words_a, words_b):
         normalizingFactor = 100
@@ -74,25 +76,22 @@ class Nechama_parser(object):
     def tokenizer(self, s):
         return self.clean(s).split()
 
-    def filter_matches_by_score_and_duplicates(self, matches, min_score=0):
-        '''
-        :param matches: List of Mesorah_Matches
-        :param min_score: The minimum score found by the callback function, calculate_score
+    def get_score(self, words_a, words_b):
+        normalizingFactor = 100
+        smoothingFactor = 1
+        ImaginaryContenderPerWord = 22
+        str_a = u" ".join(words_a)
+        str_b = u" ".join(words_b)
+        dist = self.levenshtein.calculate(str_a, str_b,normalize=False)
+        score = 1.0 * (dist + smoothingFactor) / (len(str_a) + smoothingFactor) * normalizingFactor
 
-        It removes anything with a score less than min_score and also removes duplicate matches.
-
-        :return:
-        '''
-        matches = [x.b.ref for x in matches if x.score >= min_score]
-        match_set = set()
-        for match in matches:
-            match_set.add(match)
-        return list(match_set)
+        dumb_score = (ImaginaryContenderPerWord * len(words_a)) - score
+        return dumb_score
 
     def check_reduce_sources(self, comment, ref):
         n = len(re.split(u'\s+', comment))
         pm = ParallelMatcher(self.tokenizer, dh_extract_method = None, ngram_size=3, max_words_between=4, min_words_in_match =int(round(n*0.9)),
-        min_distance_between_matches=0, all_to_all=True, parallelize = False, verbose = True)#, calculate_score = self.filter_matches_by_score_and_duplicates)
+        min_distance_between_matches=0, all_to_all=False, parallelize = False, verbose = True, calculate_score = self.get_score)
         new_ref = pm.match(tc_list=[ref.text('he'), (comment, 1)], return_obj=True)
         return new_ref
 
@@ -101,10 +100,6 @@ if __name__ == "__main__":
     # Ref(u"בראשית פרק ג פסוק ד - פרק ה פסוק י")
     # Ref(u"u'דברים פרק ט, ז-כט - פרק י, א-י'")
     np = Nechama_parser()
-    ref = u'Haamek Davar on Genesis 4:17'
-    comment = u" שהבין קין עתה רצון ה\\', שטוב להיות מרבה בצרכיו ולא לחיות כחיה ובהמה על ידי עבודת האדמה לבדה, אלא לבקש חיי אנושי בייחוד, על כן בנה לו עיר."
-    new_ref = np.check_reduce_sources(comment, Ref(ref))
-    assert new_ref[0].a.ref == Ref(u'Haamek Davar on Genesis 4:17:2')
 
     # sheets = bs4_reader(['html_sheets/{}.html'.format(x) for x in ["1", "2", "30", "62", "84", "148","212","274","302","378","451","488","527","563","570","581","750","787","820","844","894","929","1021","1034","1125","1183","1229","1291","1351","1420"]])
     # sheets = np.bs4_reader(["html_sheets/{}".format(fn) for fn in os.listdir("html_sheets") if fn != 'errors.html'])
