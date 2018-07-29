910c7d645b3aa90fc56c77c13abd27b5a0a5c63c
Jonathan <jonathan@sefaria.org>
2018-07-29 14:12:24 +0300

    bulk Yoreh Deah parse script

---
+++ sources/Shulchan_Arukh/scripts/YD_scripts/run_yoreh_deah.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+rm ././../../Yoreh_Deah.xml
+python ./startup.py
+python ./YD_base.py
+echo "Shach"
+python ./YD_Shach.py
+echo "Taz"
+python ./YD_Taz.py
+echo "Pithei Teshuva"
+python ./YD_Pithei.py
+echo "Beur HaGra"
+python ./YD_gra.py
+echo "Torat HaShalmim"
+python ./YD_torat_hashalmim.py
+echo "Beer HaGolah"
+python ./beer_hagolah.py
+echo "Nekudat HaKesef"
+python ./nekudat_haKesef.py
