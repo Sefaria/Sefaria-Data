0a50141fdbe895807989b9ef6c6e261ec2cf5873
Jonathan <jonathan@sefaria.org>
2018-10-29 14:45:45 +0200

    startup method

--- sources/Shulchan_Arukh/txt_files/Choshen_Mishpat/Urim_VeTumim/urim_vetumim.py
+++ sources/Shulchan_Arukh/txt_files/Choshen_Mishpat/Urim_VeTumim/urim_vetumim.py
@@ -1,2 +1,16 @@
 # encoding=utf-8
 
+import os
+from sources.Shulchan_Arukh.ShulchanArukh import *
+
+
+def startup(datafile):
+    Root.create_skeleton(datafile)
+    data_root = Root(datafile)
+    data_base = data_root.get_base_text()
+    data_base.add_titles(u"Shulchan Arukh, Choshen Mishpat", u"שולחן ערוך חושן משפט")
+    data_commentaries = data_root.get_commentaries()
+    data_commentaries.add_commentary(u"Urim", u"אורים")
+    data_commentaries.add_commentary(u"Turim", u"תומים")
+    return data_root
+
