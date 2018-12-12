b5e97a28f71289719e8eebaf3ace445f500b38a6
Jonathan <jonathan@sefaria.org>
2018-12-12 14:53:20 +0200

    Pri Migaddim Siftei Daat vol. 2 parsing

--- sources/Shulchan_Arukh/txt_files/Yoreh_Deah/pri_migadim.py
+++ sources/Shulchan_Arukh/txt_files/Yoreh_Deah/pri_migadim.py
@@ -28,7 +28,7 @@ for line in my_lines:
 
 del seif
 
-mishbetzot = list()
+mishbetzot = []
 chapter, seif = None, None
 chap_num, seif_num = -1, -1
 
@@ -39,13 +39,13 @@ for line in my_lines:
             if seif is not None:
                 chapter[seif_num - 1] = seif
             mishbetzot.append(convert_dict_to_array(chapter, default_value=list))
-        chapter, chap_num = dict(), getGematria(holder.group(1))
+        chapter, chap_num = {}, getGematria(holder.group(1))
         seif, seif_num = None, -1
 
     elif holder(re.search(u'@44\(([\u05d0-\u05ea]{1,2})\)', line)):
         if seif is not None:
             chapter[seif_num - 1] = seif
-        seif, seif_num = list(), getGematria(holder.group(1))
+        seif, seif_num = [], getGematria(holder.group(1))
 
     else:
         assert re.match(u'^@11', line) is not None
@@ -83,13 +83,13 @@ for line in my_lines:
             if seif is not None:
                 chapter[seif_num - 1] = seif
             mishbetzot.append(convert_dict_to_array(chapter, default_value=list))
-        chapter, chap_num = dict(), getGematria(holder.group(1))
+        chapter, chap_num = {}, getGematria(holder.group(1))
         seif, seif_num = None, -1
 
     elif holder(re.search(u'@11\(([\u05d0-\u05ea]{1,2})\)', line)):
         if seif is not None:
             chapter[seif_num - 1] = seif
-        seif, seif_num = list(), getGematria(holder.group(1))
+        seif, seif_num = [], getGematria(holder.group(1))
 
     if re.match(u'^@11', line):
         fixed_line = re.sub(u'@11\(([\u05d0-\u05ea]{1,2})\)', u'@11', line)
@@ -107,7 +107,7 @@ with codecs.open(u'./part_3/‏‏פרי מגדים משבצות זהב שולח
 
 for line in my_lines:
     if holder(re.search(u'@22([\u05d0-\u05ea]{3})', line)):
-        chapter, chap_num = list(), getGematria(holder.group(1))
+        chapter, chap_num = [], getGematria(holder.group(1))
 
     elif re.match(u'^@11', line):
         fixed_line = re.sub(u'^@11[^@]+@33\s*', u'', line)
@@ -143,7 +143,7 @@ def itag_repl(match):
     return u'<i data-commentator="Maharsha" data-order="{}"></i> '.format(getGematria(match.group(1)))
 
 
-siftei = list()
+siftei = []
 with codecs.open(u'./part_1/שולחן ערוך יורה דעה חלק א פרי מגדים שפתי דעת.txt', 'r', 'utf-8') as fp:
     my_lines = fp.readlines()
 
@@ -161,7 +161,7 @@ for line in my_lines:
             print "Siman {}: Jump from seif {} to {}".format(siman, seif, new_seif)
         seif = new_seif
 
-is_opening, current_opening, opening_title, opening_list = False, None, None, list()
+is_opening, current_opening, opening_title, opening_list = False, None, None, []
 del chapter, chap_num, seif_num, seif
 chapter, seif = None, None
 chap_num, seif_num = -1, -1
@@ -169,7 +169,12 @@ chap_num, seif_num = -1, -1
 for line in my_lines:
     if is_opening:
         if re.search(u'^@55', line):
-            is_opening = False
+            opening_list.append({
+                'title': opening_title,
+                'text': current_opening,
+                'type': 'opening'
+            })
+            is_opening, current_opening = False, None
         elif holder(re.search(u'^@77(.*)', line)):
             opening_title = holder.group(1)
         else:
@@ -181,30 +186,20 @@ for line in my_lines:
 
     elif re.search(u'^@44', line):
         is_opening = True
-        current_opening = list()
+        current_opening = []
 
     elif holder(re.search(u'@00([\u05d0-\u05ea]{1,2})', line)):
         if chapter is not None:
             if seif is not None:
                 chapter[seif_num - 1] = seif
             siftei.append(convert_dict_to_array(chapter))
-        chapter, chap_num = dict(), getGematria(holder.group(1))
+        chapter, chap_num = {}, getGematria(holder.group(1))
         seif, seif_num = None, -1
 
     elif holder(re.search(u'@22([\u05d0-\u05ea]{1,2})', line)):
         if seif is not None:
             chapter[seif_num - 1] = seif
-        if current_opening:
-            opening_list.append({
-                'start': (chap_num, 1, 1),
-                'end': (chap_num, 1, len(current_opening)),
-                'title': opening_title
-            })
-            seif = current_opening
-            current_opening, opening_title = None, None
-        else:
-            seif = list()
-        seif_num = getGematria(holder.group(1))
+        seif, seif_num = [], getGematria(holder.group(1))
 
     else:
         assert re.match(u'^@11', line) is not None
@@ -217,4 +212,115 @@ for line in my_lines:
 chapter[seif_num - 1] = seif
 siftei.append(convert_dict_to_array(chapter))
 
-ja_to_xml(siftei, ['Siman', 'Seif', 'Paragraph'])
+with codecs.open(u'./part_2/שפתי דעת פרי מגדים יורה דעה חלק ב מוכן.txt', 'r', 'utf-8') as fp:
+    my_lines = fp.readlines()
+
+seif_num = 0
+is_opening = False
+for line in my_lines:
+    if is_opening:
+        if re.search(u'@55', line):
+            is_opening = False
+    else:
+        continue
+    if holder(re.search(u'@00([\u05d0-\u05ea]{1,3})', line)):
+        new_chap_num, seif_num = getGematria(holder.group(1)), 0
+        if new_chap_num - chap_num != 1:
+            print "Missing Siman! Jump from Siman {} to {}".format(chap_num, new_chap_num)
+        chap_num = new_chap_num
+
+    elif holder(re.search(u'@22([\u05d0-\u05ea]{1,2})', line)):
+        new_seif_num = getGematria(holder.group(1))
+        if new_seif_num - seif_num != 1:
+            print "Siman {}: Jump from seif {} to {}".format(chap_num, seif_num, new_seif_num)
+        seif_num = new_seif_num
+
+    elif re.search(u'@44', line):
+        is_opening = True
+del seif_num
+
+is_opening, current_opening, opening_title, opening_type = False, None, None, None
+chapter, seif = None, None
+chap_num, seif_num = -1, -1
+sfek_sfeka, segment = False, None
+
+for line in my_lines:
+    if is_opening:
+        if re.search(u'@55', line):
+            opening_list.append({
+                'title': opening_title,
+                'text': current_opening,
+                'type': opening_type
+            })
+            is_opening, current_opening = False, None
+        elif holder(re.search(u'^@77(.*)', line)):
+            opening_title = holder.group(1)
+
+        else:
+            assert re.match(u'^@(11|00)', line) is not None
+            fixed_line = re.sub(u'@11([^@]+)@33', u'<b>\g<1></b>', line)
+            fixed_line = re.sub(u'@\d{2}', u'', fixed_line)
+            fixed_line = clean_whitespace(fixed_line)
+            current_opening.append(fixed_line)
+
+    elif holder(re.search(u'@4([45])', line)):
+        is_opening, current_opening = True, []
+        if holder.group(1) == '4':
+            opening_type = 'opening'
+        else:
+            opening_type = 'appendix'
+        current_opening = []
+
+    elif re.search(u'@41', line):
+        sfek_sfeka = True
+        segment = []
+
+    elif holder(re.search(u'@00([\u05d0-\u05ea]{1,3})', line)):
+        if sfek_sfeka:
+            if seif and segment:
+                seif.append(u'<br>'.join(segment))
+            sfek_sfeka = False
+        if chapter is not None:
+            if seif is not None:
+                chapter[seif_num - 1] = seif
+            siftei.append(convert_dict_to_array(chapter))
+        chapter, chap_num = {}, getGematria(holder.group(1))
+        seif, seif_num = None, -1
+
+    elif holder(re.search(u'@22([\u05d0-\u05ea]{1,2})', line)):
+        if sfek_sfeka and segment:
+            seif.append(u'<br>'.join(segment))
+            segment = []
+        if seif is not None:
+            chapter[seif_num - 1] = seif
+        seif, seif_num = [], getGematria(holder.group(1))
+
+    else:
+        if sfek_sfeka:
+            if holder(re.search(u'^@12(.*)', line)):
+                if segment:
+                    seif.append(u'<br>'.join(segment))
+                    segment = []
+                segment.append(u'{})'.format(holder.group(1)))
+            else:
+                assert re.match(u'^@11', line) is not None
+                fixed_line = re.sub(u'@11([^@]+)@33', u'<b>\g<1></b>', line)
+                fixed_line = re.sub(u'\s?\(#\)\s?', u' \u270d', fixed_line)
+                fixed_line = re.sub(u'@\d{2}', u'', fixed_line)
+                fixed_line = clean_whitespace(fixed_line)
+                segment.append(fixed_line)
+
+        else:
+            assert re.match(u'^@11', line) is not None
+            fixed_line = re.sub(u'@11([^@]+)@33', u'<b>\g<1></b>', line)
+            fixed_line = re.sub(u'@77([^@]+)@88', u'<big>\g<1></big>', fixed_line)
+            fixed_line = re.sub(u'\s?\(#\)\s?', u' \u270d', fixed_line)
+            fixed_line = re.sub(u'@\d{2}', u'', fixed_line)
+            fixed_line = clean_whitespace(fixed_line)
+            seif.append(fixed_line)
+chapter[seif_num - 1] = seif
+siftei.append(convert_dict_to_array(chapter))
+
+# ja_to_xml(siftei, ['Siman', 'Seif', 'Paragraph'])
+for o in opening_list:
+    print o['title'], o['type']
