814edf0ab296b632fea1d684119e5ad0ba245cab
Jonathan <jonathan@sefaria.org>
2018-12-18 13:35:05 +0200

    Parse for CM and Urim

--- sources/Shulchan_Arukh/txt_files/Choshen_Mishpat/Urim_VeTumim/urim_vetumim.py
+++ sources/Shulchan_Arukh/txt_files/Choshen_Mishpat/Urim_VeTumim/urim_vetumim.py
@@ -1,7 +1,19 @@
 # encoding=utf-8
 
 import os
+import re
+import codecs
+from collections import Counter
 from sources.Shulchan_Arukh.ShulchanArukh import *
+
+txt_files = [
+    u'Choshen_Mishpat_1.txt',
+    u'Choshen_Mishpat_2.txt',
+    u'urim_1.txt',
+    u'urim_2.txt',
+    u'tumim_1.txt',
+    u'tumim_2.txt',
+]
 
 
 def startup(datafile):
@@ -11,6 +23,68 @@ def startup(datafile):
     data_base.add_titles(u"Shulchan Arukh, Choshen Mishpat", u"שולחן ערוך חושן משפט")
     data_commentaries = data_root.get_commentaries()
     data_commentaries.add_commentary(u"Urim", u"אורים")
-    data_commentaries.add_commentary(u"Turim", u"תומים")
+    data_commentaries.add_commentary(u"Tumim", u"תומים")
     return data_root
 
+
+def check_markers():
+    for filename in txt_files:
+        with codecs.open(filename, 'r', 'utf-8') as fp:
+            txt = fp.read()
+        counts = Counter(re.findall(u'@\d{2}', txt))
+        print filename
+        for code, counts in sorted(counts.items(), key=lambda x: x[1]):
+            print u'{}: {}'.format(code, counts)
+        print u'\n'
+
+
+datafilename = u'urim_vetumim.xml'
+if not os.path.exists(datafilename):
+    root = startup(datafilename)
+else:
+    root = Root(datafilename)
+
+base, commentaries = root.get_base_text(), root.get_commentaries()
+for vol_num, txt_file in zip([1, 2], txt_files[:2]):
+    print u"\nWorking on {}".format(txt_file)
+    base.remove_volume(vol_num)
+    with codecs.open(txt_file, 'r', 'utf-8') as fp:
+        volume = base.add_volume(fp.read(), vol_num)
+    assert isinstance(volume, Volume)
+    volume.mark_simanim(u'@00([\u05d0-\u05ea]{1,3})')
+    print u"Validating Simanim"
+    volume.validate_simanim()
+
+    errors = volume.mark_seifim(u'@11([\u05d0-\u05ea]{1,3})')
+    print "Validating Seifim"
+    for e in errors:
+        print e
+    volume.validate_seifim()
+
+    errors = volume.format_text(u'.^', u'$.', u'ramah')
+    for e in errors:
+        print e
+
+    volume.mark_references(commentaries.commentary_ids[u"Urim"], u"@55")
+    volume.mark_references(commentaries.commentary_ids[u"Tumim"], u"@66")
+
+urim = commentaries.get_commentary_by_title(u"Urim")
+for vol_num, txt_file in zip([1, 2], txt_files[2:4]):
+    print u"\nWorking on {}".format(txt_file)
+    urim.remove_volume(vol_num)
+    with codecs.open(txt_file, 'r', 'utf-8') as fp:
+        volume = urim.add_volume(fp.read(), vol_num)
+    assert isinstance(volume, Volume)
+
+    volume.mark_simanim(u'@00([\u05d0-\u05ea]{1,3})')
+    volume.validate_simanim(complete=False)
+    print "Validating Seifim"
+    errors = volume.mark_seifim(u'@22\(([\u05d0-\u05ea]{1,3})\)')
+    for e in errors:
+        print e
+    volume.validate_seifim()
+
+    errors = volume.format_text(u'@11', u'@33', u'dh')
+    for e in errors:
+        print e
+root.export()
