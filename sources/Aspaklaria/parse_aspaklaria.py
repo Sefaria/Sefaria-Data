6f34e494bf752ae7483978aa6dfd55bdbf4ac042
shaneerosen <shanee@sefaria.org>
2018-12-20 22:32:50 +0200

    a base for extracting index/terms to the Source.index and using it. code for deleting wrongly caught refs

--- sources/Aspaklaria/parse_aspaklaria.py
+++ sources/Aspaklaria/parse_aspaklaria.py
@@ -18,7 +18,7 @@ def bs_read(fileName):
     content = BeautifulSoup(file_content, "lxml")
     headWord = clean_txt(content.findAll('h1')[0].text)
     t = Topic(headWord)
-    b = BookCit(u'before')
+    b = AoutherCit(u'before')
     wait = True
     for tag in content.findAll():
         if tag.name != 'h1' and wait:
@@ -26,16 +26,16 @@ def bs_read(fileName):
             continue
         wait = False
         if tag.name == 'h2':
-            t.citationsBb.append(b)
-            b = BookCit(clean_txt(tag.text))
+            t.all_a_citations.append(b)
+            b = AoutherCit(clean_txt(tag.text))
         else:
             txt = clean_txt(tag.text)
             b.cit_list.append(txt)
-    t.citationsBb.append(b)
+    t.all_a_citations.append(b)
 
     tanakh = []
-    if t.citationsBb[0].author == 'before':
-        for ic, c in enumerate(t.citationsBb[0].cit_list):
+    if t.all_a_citations[0].author == 'before':
+        for ic, c in enumerate(t.all_a_citations[0].cit_list):
             if c == t.headWord:
                 pass
             elif re.search(u'ראה', c) and re.search(u':', c):
@@ -45,10 +45,10 @@ def bs_read(fileName):
                 # t.see = [re.sub(, u'', s) for s in see]
             else:
                 tanakh.append(c)
-    t.citationsBb.pop(0)
-    if tanakh and t.citationsBb:
-        t.citationsBb[0] = BookCit(u'Tanakh')
-        t.citationsBb[0].cit_list = tanakh
+    t.all_a_citations.pop(0)
+    if tanakh and t.all_a_citations:
+        t.all_a_citations[0] = AoutherCit(u'תנך')
+        t.all_a_citations[0].cit_list = tanakh
 
     return t
 
@@ -70,15 +70,15 @@ def clean_txt(text):
 
 def parse_refs(topic):
 
-    if topic.citationsBb:
-        for citbook in topic.citationsBb:
+    if topic.all_a_citations:
+        for citbook in topic.all_a_citations:
             citbook.create_sources()
             pass
         pass
 
 
 
-class BookCit(object):
+class AoutherCit(object):
 
     def __init__(self, author):
         self.author = author
@@ -96,17 +96,21 @@ class Topic(object):
 
     def __init__(self, headWord):
         self.headWord = headWord
-        self.citationsBb = [] # list of BookCits
+        self.all_a_citations = [] # list of BookCits
         self.see = None
         self.altTitles = None
 
 
 class Source(object):
+    cnt = 0
+    cnt_sham = 0
+    cnt_resolved = 0
+    cnt_not_found = 0
 
     def __init__(self, text, author):
         self.text = text
         self.author = author
-        # self.index = library.get_index(self.author) if library.get_index(self.author) else None # needs to go into a try catch "sefaria.system.exceptions.BookNameError"
+        self.index = None
         self.raw_ref = None
         self.ref = None
         self.extract_raw_ref()
@@ -117,15 +121,28 @@ class Source(object):
             self.raw_ref = RawRef(re.search(u'(\([^)]*?\)$)',self.text).group(1), self.author)
         else:
             pass
+    def extract_index(self):
+        # library.get_index(self.author) if library.get_index(self.author) else None # needs to go into a try catch "sefaria.system.exceptions.BookNameError"
+        try:
+            self.index = library.get_index(self.author)
+        except sefaria.system.exceptions.BookNameError as e:
+            #todo: check if it is part of an index. maybe a category?
+            self.author in library.get_text_categories()
+            library.get_indexes_in_category(self.author, include_dependant=True, full_records=True)
+            #might need to use Terms to make lookup the category in english
+            Term().load({"name": self.author})
+            filter(lambda x: x['lang'] == 'he', t.contents()['titles'])[0]['text']
+
 
     def get_ref_from_api(self):
 
         def clean_raw(st):
+            st = re.sub(u'[)(]', u'', st) # incase we put those parantheses on before to try and catch it with get_ref_in_string
             he_parashah_array = [u"בראשית", u"נח", u"לך לך", u"וירא", u"חיי שרה", u"תולדות", u"ויצא", u"וישלח", u"וישב", u"מקץ", u"ויגש", u"ויחי", u"שמות", u"וארא", u"בא", u"בשלח", u"יתרו", u"משפטים", u"תרומה", u"תצוה", u"כי תשא", u"ויקהל", u"פקודי", u"ויקרא", u"צו", u"שמיני", u"תזריע", u"מצורע", u"אחרי מות", u"קדושים", u"אמור", u"בהר", u"בחוקתי", u"במדבר", u"נשא", u"בהעלותך", u"שלח", u"קרח", u"חקת", u"בלק", u"פנחס", u"מטות", u"מסעי", u"דברים", u"ואתחנן", u"עקב", u"ראה", u"שופטים", u"כי תצא", u"כי תבוא", u"נצבים", u"וילך", u"האזינו", u"וזאת הברכה"]
             for s in [u"מאמר", u"פרק"]:
                 st = re.sub(s, u"", st)
             if self.author == u"משנה תורה":
-                st = u" הלכות" + st
+                st = u"הלכות " + st
             elif self.author == u"ספר החינוך":
                 for s in he_parashah_array:
                     st = re.sub(s, u"", st)
@@ -133,20 +150,51 @@ class Source(object):
             return st
 
         if self.raw_ref:
-            new_ref = u"(" + self.author + u", " + re.sub(u"[)(]", u"", self.raw_ref.rawText) + u")"
-            print self.raw_ref.rawText,'\n', new_ref
-            refs = library.get_refs_in_string(new_ref)
+            ref_w_author = u"(" + self.author + u", " + re.sub(u"[)(]", u"", self.raw_ref.rawText) + u")"
+            refs = library.get_refs_in_string(ref_w_author)
             if refs:
                 assert len(refs) == 1
                 self.ref = refs[0]
                 print self.ref
             else:
                 if re.search(u"^\(שם", self.raw_ref.rawText):
-                    print u"*** is a Sham"
+                    self.raw_ref.is_sham = True
+                    Source.cnt_sham +=1
                 else:
-                    cleaned = clean_raw(new_ref)
-                    print u"*** try new ref, new_ref = {}".format(cleaned)
-                    print library.get_refs_in_string(new_ref)
+                    cleaned = clean_raw(self.raw_ref.rawText)
+                    fixed_ref = u"(" + self.author + u", " + re.sub(u"[)(]", u"", cleaned) + u")"
+                    print u"*** try new ref, fixed_ref = {}".format(fixed_ref)
+                    print library.get_refs_in_string(fixed_ref)
+            print self.raw_ref.rawText, u'\n', ref_w_author
+            Source.cnt+=1
+        else: # there isn't a self.ref means we couldn't find parenthises at the end, probably a conyinuation of the prev source/ cit (in cit_list
+            pass
+
+        self.get_ref_step2()
+
+
+
+    def get_ref_step2(self):
+        """
+        after get_ref_from_api() we want to call this function.
+        It should discard/change wrongly caught refs, like ones caught bavli instead of Yerushalmi or mishanah instead of Tosefta.
+        :return: doesn't return but changes self.ref
+        """
+        if self.ref and self.ref.index.title in library.get_indexes_in_category(u'Tanakh'): #  probabaly should be calculated once
+            if self.author != u"תנך":
+                self.ref = None
+                print u"deleted wrong {}".format(self.ref)
+                if self.index:
+                    pass
+                    # than try to get the true title from the index
+                # self.try_from_ls()
+
+    def try_from_ls(self):
+        """
+        try to look for a link to the pasuk from it's link set, since you have a pasuk that is related and an author's name. the aouthers name need to be exctracted via Term
+        :return: will return the new guess
+        """
+        return None
 
 class RawRef(object):
 
@@ -156,6 +204,7 @@ class RawRef(object):
         self.book = None
         self.section_level = None
         self.segment_level = None
+        self.is_sham = False
 
 
 def parse2pickle():
@@ -195,7 +244,12 @@ def parse2pickle():
 
 if __name__ == "__main__":
     # parse2pickle()
-    for file in os.listdir(u"/home/shanee/www/Sefaria-Data/sources/Aspaklaria/pickle_files/"):
+
+    # a = dict()
+    # a[u'שלום'] = 1
+    # pass
+    # for file in os.listdir(u"/home/shanee/www/Sefaria-Data/sources/Aspaklaria/pickle_files/"):
+    for file in [u'KAF.pickle']:
         with codecs.open(u"/home/shanee/www/Sefaria-Data/sources/Aspaklaria/pickle_files/{}".format(file), "rb") as fp:
             topics = pickle.load(fp)
             for t in topics.values():
