1337a5d5e6c1dd9db9a455bddb5bb1e7f20df944
Jonathan <jonathan@sefaria.org>
2018-12-26 17:47:31 +0200

    merge itags and upload logic

--- sources/Shulchan_Arukh/txt_files/Choshen_Mishpat/Urim_VeTumim/urim_vetumim.py
+++ sources/Shulchan_Arukh/txt_files/Choshen_Mishpat/Urim_VeTumim/urim_vetumim.py
@@ -2,14 +2,18 @@
 
 import os
 import re
+import sys
+import math
 import json
 import codecs
 import bleach
 import Levenshtein
 from functools import partial
 from collections import Counter
+from tag_merge import merge_tags
 from data_utilities.util import traverse_ja
 from sources.Shulchan_Arukh.ShulchanArukh import *
+from sources.functions import post_text, post_index, post_link, add_category
 import django
 django.setup()
 from sefaria.model import *  # noqa
@@ -46,8 +50,8 @@ def startup(datafile):
     data_base = data_root.get_base_text()
     data_base.add_titles(u"Shulchan Arukh, Choshen Mishpat", u"שולחן ערוך חושן משפט")
     data_commentaries = data_root.get_commentaries()
-    data_commentaries.add_commentary(u"Urim", u"אורים")
-    data_commentaries.add_commentary(u"Tumim", u"תומים")
+    data_commentaries.add_commentary(u"Urim VeTumim, Urim", u"אורים ותומים, אורים")
+    data_commentaries.add_commentary(u"Urim VeTumim, Tumim", u"אורים ותומים, תומים")
     return data_root
 
 
@@ -91,10 +95,10 @@ def parse():
         for e in errors:
             print e
 
-        volume.mark_references(commentaries.commentary_ids[u"Urim"], u"@55\(([\u05d0-\u05ea]{1,3})\)", group=1)
-        volume.mark_references(commentaries.commentary_ids[u"Tumim"], u"@66\(([\u05d0-\u05ea]{1,3})\)", group=1)
+        volume.mark_references(commentaries.commentary_ids[u"Urim VeTumim, Urim"], u"@55\(([\u05d0-\u05ea]{1,3})\)", group=1)
+        volume.mark_references(commentaries.commentary_ids[u"Urim VeTumim, Tumim"], u"@66\(([\u05d0-\u05ea]{1,3})\)", group=1)
 
-    urim = commentaries.get_commentary_by_title(u"Urim")
+    urim = commentaries.get_commentary_by_title(u"Urim VeTumim, Urim")
     for vol_num, txt_file in zip([1, 2], txt_files[2:4]):
         print u"\nWorking on {}".format(txt_file)
         urim.remove_volume(vol_num)
@@ -122,7 +126,7 @@ def parse():
         for e in errors:
             print e
 
-    tumim = commentaries.get_commentary_by_title(u"Tumim")
+    tumim = commentaries.get_commentary_by_title(u"Urim VeTumim, Tumim")
     for vol_num, txt_file in zip([1, 2], txt_files[4:]):
         print u"\nWorking on {}".format(txt_file)
         tumim.remove_volume(vol_num)
@@ -138,7 +142,7 @@ def parse():
             print e
         volume.validate_seifim()
 
-        errors = volume.format_text(u'@11', u'@33', u'df')
+        errors = volume.format_text(u'@11', u'@33', u'dh')
         for e in errors:
             print e
 
@@ -157,37 +161,116 @@ def parse():
 root.populate_comment_store()
 base_text = root.get_base_text().render()
 
-with open('Shulchan Arukh, Choshen Mishpat - he - Shulhan Arukh, Hoshen ha-Mishpat - Lemberg, 1898.json') as fp:
-    sef_base_text = json.load(fp, encoding='utf-8')['text'][:153]
-# sef_base_text = Ref(u"Choshen Mishpat 1-153").text(u"he", u"Shulhan Arukh, Hoshen ha-Mishpat - Lemberg, 1898").text
+# with open('Shulchan Arukh, Choshen Mishpat - he - Shulhan Arukh, Hoshen ha-Mishpat - Lemberg, 1898.json') as fp:
+#     sef_base_text = json.load(fp, encoding='utf-8')['text'][:153]
+sef_tc = Ref(u"Choshen Mishpat").text(u"he", u"Shulhan Arukh, Hoshen ha-Mishpat - Lemberg, 1898")
+sef_base_text = sef_tc.text
+sef_ja = sef_tc.ja()
 
 base_segments, sef_segments = list(traverse_ja(base_text)), list(traverse_ja(sef_base_text))
-assert len(base_segments) == len(sef_segments)
+# assert len(base_segments) == len(sef_segments)
 
 problems = 0
 for x_seg, sef_seg in zip(base_segments, sef_segments):
     if x_seg['indices'] != sef_seg['indices']:
         print "Weird!"
 
-    # if x_seg['indices'] == [152, 5]:
-    #     print 'foo'
-
     x_text, sef_text = standardize_tag_spacing(x_seg['data']), standardize_tag_spacing(sef_seg['data'])
-    # if re.search(u'\s</small>:$', sef_text):
-    #     x_text = re.sub(u'(?<! ):$', u' :', x_text)
 
-    if remove_html(x_text) != remove_html(sef_text):
+    try:
+        sef_ja.set_element(sef_seg['indices'], merge_tags(sef_text, x_text))
+    except AssertionError:
         problems += 1
         print map(lambda x: x+1, x_seg['indices'])
-        # if problems % 10 == 0:
-        #     print ''
+        if problems % 10 == 0:
+            print ''
 
         print remove_html(x_text), u'\n'
         print remove_html(sef_text)
         print Levenshtein.distance(remove_html(x_text), remove_html(sef_text))
 
         print '\n\n'
-    if problems > 6:
-        break
-print problems
-# todo: try to mark where tags go (word_num, char_num). Do a basic verification that the word (and maybe some surrounding context) matches in both cases
+
+if problems:
+    print problems
+    print "Problems, so exiting early"
+    sys.exit(0)
+
+
+def build_index(title):
+    he_title = {
+        u"Urim": u"אורים",
+        u"Tumim": u",תומים"
+    }[title]
+    title = u"Urim VeTumim, {}".format(title)
+    he_title = u"אורים ותומים, {}".format(he_title)
+    j_node = JaggedArrayNode()
+    j_node.add_structure([u'Siman', u'Seif'])
+    j_node.add_primary_titles(title, he_title)
+    return {
+        u'title': title,
+        u'categories': [u"Halakhah", u"Shulchan Arukh", u"Commentary", u"Urim VeTumim"],
+        u"dependence": u"Commentary",
+        u"collective_title": title,
+        u"schema": j_node.serialize(),
+        u"base_text_titles": [u"Shulchan Arukh, Choshen Mishpat"]
+    }
+
+
+urim_vetumim = root.get_commentaries()
+server = 'http://urim.sandbox.sefaria.org'
+add_category(u"Urim VeTumim", [u"Halakhah", u"Shulchan Arukh", u"Commentary", u"Urim VeTumim"], server=server)
+
+
+version_json = {
+    u"versionTitle": u"Urim veTumim, Warsaw 1881",
+    u"versionSource": u"http://primo.nli.org.il/primo_library/libweb/action/dlDisplay.do?vid=NLI&docId=NNL_ALEPH001167224",
+    u"language": u"he"
+}
+
+
+def split_version(version_dict, splits):
+    def edges(length, num_splits):
+        ratio = float(length) / float(num_splits)
+        indices = [math.trunc(ratio*i) for i in range(num_splits+1)]
+        return zip(indices[:-1], indices[1:])
+
+    volumes = []
+    for vol_num, (start, end) in enumerate(edges(len(version_dict['text']), splits), 1):
+        new_version = {
+            u'versionTitle': u'{}, Vol {}'.format(version_dict[u'versionTitle'], vol_num),
+            u'versionSource': version_dict[u'versionSource'],
+            u'language': version_dict[u'language'],
+            u'text': [t if start <= ind < end else [] for ind, t in enumerate(version_dict[u'text'])]
+        }
+        volumes.append(new_version)
+    return volumes
+
+
+def upload_commentary(title):
+    c_index = build_index(title)
+    c_xml = urim_vetumim.get_commentary_by_title(u"Urim VeTumim, {}".format(title))
+    c_text = c_xml.render()
+    c_links = c_xml.collect_links()
+    for l in c_links:
+        l['refs'] = [re.sub(u' on Shulchan Arukh, Choshen Mishpat', u'', lr) for lr in l['refs']]
+    post_index(c_index, server)
+    version_json[u"text"] = c_text
+    if title == u"Tumim":
+        for v in split_version(version_json, 2):
+            post_text(u"Urim VeTumim, {}".format(title), v, index_count=u"on", server=server)
+    else:
+        post_text(u"Urim VeTumim, {}".format(title), version_json, index_count=u"on", server=server)
+    post_link(c_links, weak_network=True)
+
+
+upload_commentary(u"Urim")
+upload_commentary(u"Tumim")
+
+choshen_mishpat_version = {
+    u"versionTitle": sef_tc.vtitle,
+    u"versionSource": sef_tc.version().versionSource,
+    u"language": u"he",
+    u"text": sef_tc.text
+}
+post_text(u"Shulchan Arukh, Choshen Mishpat", choshen_mishpat_version, index_count="on", server=server)
