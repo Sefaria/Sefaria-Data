b224c59e9ea099a0e37f8cdcc3f2fc559b71d104
shaneerosen <shanee@sefaria.org>
2019-02-10 12:35:07 +0200

    scripts for posting and reposting btween servers and linking sheets to sheets after that

--- sources/Nechama/link_sheets_to_sheets.py
+++ sources/Nechama/link_sheets_to_sheets.py
@@ -3,19 +3,63 @@ django.setup()
 from sefaria.model import *
 from sefaria.system.database import db
 import re
+
+def get_ssn(read_id):
+    """
+    gets a sheet id from a server and maps it to the correct ssn (using tags)
+    :param read_id: Sheet id
+    :return: ssn
+    """
+    map_get_id_ssn = {}  # this is a map id -> ssn in the server db we are copying from
+    ssn = map_get_id_ssn.get(read_id, 0)
+    return ssn
+
+def get_post_id(ssn):
+    """
+    for the post server gets a ssn and returns the id to which the sheet should be posted. based on the ssn<->id maping
+    on the post server which we learn from the ssn tags
+    :param ssn:
+    :return:
+    """
+    map_post_ssn_id = {}
+    post_id = map_post_ssn_id.get(ssn, 0)
+    return post_id
+
 if __name__ == "__main__":
     #use list of which sheets have links which has sheet title and sheet year and tags
     #for each sheet, get each segment and check if it has
     sheet_data = []
-    sheets = db.sheets.find()
-    for title_year_tags in sheet_data:
-        title, year, tags = title_year_tags
-        sheet = db.sheets.find({"title": title, "summary": year, "tags": tags})
-        assert sheet, u"Couldn't find sheet {}".format(title)
-        for segment in sheet["sources"]:
-            text = segment["text"]["he"] if "text" in segment.keys() else segment["outsideText"]
-            for match in re.findall("^<a href.*?nechama.org.il/pages/.*?</a>", text):
-                text = text.replace(match, )
+    sheets = db.sheets.find({"tags": "Bilingual"})
+    compile = re.compile(u'/sheets/(?P<id>\d+)')  # (?:\.(?P<node>\d+))?
+    for sheet_json in sheets:
+        for i, s in enumerate(sheet_json['sources']):
+            if 'outsideText' in s.keys():
+                uni_outsidetext = unicode(s['outsideText'], encoding='utf8') if isinstance(s['outsideText'],str) else s['outsideText']
+                compile = re.compile(u'/sheets/(?P<id>\d+)') #(?:\.(?P<node>\d+))?
+                matched = re.search(compile, s['outsideText'])
+                if matched:
+                    ssn = get_ssn(matched.group("id"))
+                    to_post_id = get_post_id(ssn)
+                    new_link = re.sub(compile, u'/sheets/\g<id>')
+                    s['outsideText'] = re.sub(compile, u'/sheets/\g<id>', uni_outsidetext) #todo: check and then test this line
+            elif 'outsideBiText' in s.keys():
+                uni_outsidetext = unicode(s['outsideBiText']['he'], encoding='utf8') if isinstance(s['outsideBiText']['he'], str) else \
+                s['outsideBiText']['he']
+                matched = re.search(compile, s['outsideBiText'])
+                if matched:
+                    ssn = get_ssn(matched.group("id"))
+                    to_post_id = get_post_id(ssn)
+                    new_link = re.sub(compile, u'/sheets/\g<id>')
+                    s['outsideBiText'] = re.sub(compile, u'/sheets/\g<id>',uni_outsidetext)  # todo: check and then test this line
+        pass
+    # for title_year_tags in sheet_data:
+    #     title, year, tags = title_year_tags
+    #     sheet = db.sheets.find({"title": title, "summary": year, "tags": tags})
+    #     assert sheet, u"Couldn't find sheet {}".format(title)
+    #     for segment in sheet["sources"]:
+    #         text = segment["text"]["he"] if "text" in segment.keys() else segment["outsideText"]
+    #         for match in re.findall("^<a href.*?nechama.org.il/pages/.*?</a>", text):
+    #             text = text.replace(match, )
 
 
 
--- sources/Nechama/map_ssn_url.py
+++ sources/Nechama/map_ssn_url.py
@@ -4,15 +4,18 @@ import django
 django.setup()
 from sefaria.system.database import db
 
-#NOTE: this code is based on ssn tag numbers. if we choose to take those tags off while postinf to production we muxt put this info in in a diffrent space
-#one space it is already hidden in is in the pdf html link
+# NOTE: this code is based on ssn tag numbers. if we choose to take those tags off while posting to production
+# we must put this info in in a different space
+# one space it is already hidden in is in the pdf html link
 map_ssn_url = dict()
 for ssn in (range(1, 1479)):
+    # sheets = db.sheets.find({"$and": [{"$or": [{"tags": "Bilingual"}, {"tags": "Hebrew Sheet"}]}, {"tags": "{}".format(ssn)}]})
     sheets = db.sheets.find({"$and": [{"tags": "Bilingual"}, {"tags": "{}".format(ssn)}]})
+    sheets.sort("dateCreated")
     for s in sheets:
+        # if sheets.count_documents() >1:
+        #     print s['dateCreated']
         map_ssn_url[ssn]=s['id']
-        print ssn, u":", s['id'], u','
-        break
 print map_ssn_url
 print len(map_ssn_url.items())
 
