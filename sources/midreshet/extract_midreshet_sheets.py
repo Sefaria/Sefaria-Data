a2490bab66ed5d5b17193f26a85d3dc0c8610cc6
Jonathan <jonathan@sefaria.org>
2019-03-28 16:52:03 +0200

    disambiguate sheet refs

--- sources/midreshet/extract_midreshet_sheets.py
+++ sources/midreshet/extract_midreshet_sheets.py
@@ -3,12 +3,14 @@ from __future__ import print_function
 
 import re
 import json
+from tqdm import tqdm
 import codecs
 import bleach
 import pyodbc
 import requests
-from data_utilities.util import Singleton, getGematria
 from sources.functions import post_sheet
+from data_utilities.util import Singleton, getGematria
+from research.source_sheet_disambiguator.main import refine_ref_by_text
 
 import django
 django.setup()
@@ -220,26 +222,27 @@ def find_books_and_categories():
     my_cursor.commit()
 
 
-def disambiguate_simple_tanakh_ref(map_row):
+def disambiguate_simple_tanakh_ref(raw_ref, book_name):
     # strip all non-hebrew characters, except the dash (maybe the em-dash?)
-    clean_ref = re.sub(u'[^\u05d0-\u05ea\s\-\u2013]', u'', map_row.MidreshetRef)
+    clean_ref = re.sub(u'[^\u05d0-\u05ea\s\-\u2013]', u'', raw_ref)
     clean_ref = u' '.join(clean_ref.split())
 
-    he_book_title = Ref(map_row.Book).he_book()
+    # he_book_title = Ref(book_name).he_book()
+    he_book_title = u'|'.join(Ref(book_name).index.all_titles('he'))
     perek = u'פרק'
     prefix = u'ספר|מגילת'
 
     # ^(<book-name>) <perek> (<1-3 letters>) <pasuk(im)> (<chars>-?<chars>?)
-    pattern = ur'^(?:(?:%s) )?(%s) %s ([\u05d0-\u05ea]{1,3}) \u05e4\u05e1\u05d5\u05e7(?:\u05d9\u05dd)? ([\u05d0-\u05ea]{1,3}(-[\u05d0-\u05ea]{1,3})?)' % (prefix, he_book_title, perek)
+    pattern = ur'(?:(?:%s) )?(%s)(?: %s)? ([\u05d0-\u05ea]{1,3}) \u05e4\u05e1\u05d5\u05e7(?:\u05d9\u05dd)? ([\u05d0-\u05ea]{1,3}(-[\u05d0-\u05ea]{1,3})?)' % (prefix, he_book_title, perek)
     match = re.search(pattern, clean_ref)
     if match is None:
         return None
     else:
         sefaria_ref = u'{} {} {}'.format(match.group(1), match.group(2), match.group(3))
         try:
-            return Ref(sefaria_ref).normal()
+            return Ref(sefaria_ref)
         except InputError:
-            print(u'Could not create Ref at id = {}'.format(map_row.id))
+            print(u'Could not create Ref:')
             print(sefaria_ref)
             return None
 
@@ -274,12 +277,13 @@ def disambiguate_simple_talmud_ref(midreshet_ref, expected_book):
     cleaned_ref = re.sub(u'\s*-\s*', u' - ', cleaned_ref)
     cleaned_ref = u' '.join(cleaned_ref.split())
 
-    if re.search(u'רשי', cleaned_ref):
-        return None
+    # if re.search(u'רשי', cleaned_ref):
+    #     return None
 
     # he_book_title = Ref(expected_book).he_book()
     he_book_title = get_he_talmud_titles(expected_book)
-    masechet_pattern = u'{} (?:{} )?(?P<book>{})'.format(u'תלמוד ה?בבלי', u'מסכת', he_book_title)
+    # masechet_pattern = u'{} (?:{} )?(?P<book>{})'.format(u'תלמוד ה?בבלי', u'מסכת', he_book_title)
+    masechet_pattern = u'(?:{} )?(?P<book>{})'.format(u'מסכת', he_book_title)
 
     start_daf_pattern = u'(?:%s )?(?P<start_daf>[\u05d0-\u05ea]{1,3})' % (u'דף',)
     end_daf_pattern = start_daf_pattern.replace(u'start_daf', u'end_daf')
@@ -317,14 +321,51 @@ def disambiguate_simple_talmud_ref(midreshet_ref, expected_book):
         raw_ref = u'{} {}{}'.format(
             expected_book, getGematria(match.group('start_daf')), translate_ammud(match.group('start_ammud')))
 
-    return Ref(raw_ref).normal()
+    return Ref(raw_ref)
 
 
 class RefBuilder(object):
 
     def __init__(self):
-        self._category_maps = {}
+        self._category_maps = {
+            u'משנה': u'Mishnah',
+            u'תוספתא': u'Tosefta',
+            u'ירושלמי': u'Jerusalem Talmud',
+        }
         self._commentary_maps = {}
+
+    def redirect_category(self, raw_ref, base_ref):
+
+        def redirected_sections(new_book, sections):
+            book_node = Ref(new_book).index_node
+            return u':'.join([book_node.address_class(i).toStr('en', section) for i, section in enumerate(sections)])
+
+        if base_ref.book == u'Pirkei Avot':  # special case
+            return base_ref
+
+        if base_ref.is_talmud():
+            base_book = base_ref.book
+        elif base_ref.primary_category == u'Mishnah':
+            base_book = re.sub(u'Mishnah ', u'', base_ref.book)
+        else:
+            return base_ref
+
+        cat_regex = re.compile(u'|'.join(self._category_maps.keys()))
+        category_match = cat_regex.search(raw_ref)
+
+        if not category_match:
+            return base_ref
+
+        ref_prefix = self._category_maps[category_match.group()]
+        book_title = u"{} {}".format(ref_prefix, base_book)
+        try:
+            new_ref = Ref(u"{} {}".format(book_title, redirected_sections(book_title, base_ref.sections)))
+        except InputError:
+            return base_ref
+        if base_ref.is_range():
+            range_end = Ref(u"{} {}".format(book_title, redirected_sections(book_title, base_ref.toSections)))
+            new_ref = new_ref.to(range_end)
+        return new_ref
 
     def _get_commentary_map_for_book(self, book_title):
         if book_title in self._category_maps:
@@ -373,39 +414,111 @@ class RefBuilder(object):
             else:
                 return base_ref
 
-        commentator_ref = Ref(u"{} {}".format(commentator_book, u':'.join((unicode(i) for i in base_ref.sections))))
+        commentator_ref = Ref(u"{} {}".format(commentator_book, u':'.join(base_ref.normal_sections())))
         if base_ref.is_range():
-            range_end = Ref(u"{} {}".format(commentator_book, u':'.join((unicode(i) for i in base_ref.toSections))))
+            range_end = Ref(u"{} {}".format(commentator_book, u':'.join(base_ref.normal_toSections())))
             commentator_ref = commentator_ref.to(range_end)
 
         return commentator_ref
 
+    @staticmethod
+    def refine_ref(raw_ref, base_ref):
+        """
+        :param raw_ref:
+        :param Ref base_ref:
+        :return:
+        """
+        fixed_ref = None
 
+        if base_ref.is_talmud():
+            fixed_ref = disambiguate_simple_talmud_ref(raw_ref, base_ref.book)
+        elif u'Tanakh' in base_ref.index.categories:
+            if base_ref.is_dependant():
+                fixed_ref = disambiguate_simple_tanakh_ref(raw_ref, base_ref.index.base_text_titles[0])
+            else:
+                fixed_ref = disambiguate_simple_tanakh_ref(raw_ref, base_ref.book)
+
+        if fixed_ref is None:
+            return base_ref
+        else:
+            return fixed_ref
+
+    def build_difficult_ref(self, raw_ref, suspected_base_ref):
+        refined_ref = self.refine_ref(raw_ref, suspected_base_ref)
+        refined_ref = self.redirect_category(raw_ref, refined_ref)
+        return self.apply_commentator(raw_ref, refined_ref)
+
+
+builder = RefBuilder()
 cu = MidreshetCursor()
-cu.execute("SELECT id, MidreshetRef, Book FROM RefMap WHERE SefariaRef IS NULL")
+cu.execute("SELECT RefMap.id, MidreshetRef, Book, body FROM RefMap JOIN Resources ON RefMap.id = Resources.id WHERE RefMap.SefariaRef IS NULL")
 easy, weird = [], []
 
 for q in cu.fetchall():
     possible_refs = library.get_refs_in_string(u'({})'.format(q.MidreshetRef), 'he', citing_only=True)
     if len(possible_refs) == 1:
-        easy.append((possible_refs[0], q))
+        better_ref = builder.build_difficult_ref(q.MidreshetRef, possible_refs[0])
+        easy.append((possible_refs[0], q, better_ref))
     elif len(possible_refs) > 1:
         weird.append(q)
 
-builder = RefBuilder()
 print(len(easy))
 print(len(weird))
-for e in easy[:100]:
-    print(e[0].normal())
-    print(e[1].MidreshetRef)
-    print(builder.apply_commentator(e[1].MidreshetRef, e[0]))
-    print('')
+# for e in easy[100:200]:
+#     print(e[0].normal())
+#     print(e[1].MidreshetRef)
+#     print(builder.build_difficult_ref(e[1].MidreshetRef, e[0]))
+#     print('')
 
-for q in weird:
-    print(q.MidreshetRef)
+# for q in weird:
+#     print(q.MidreshetRef)
+
+
+def bleach_clean(some_text):
+    return bleach.clean(some_text, tags=[], attributes={}, strip=True)
+
+import unicodecsv
+rows = []
+for e in tqdm(easy):
+    my_row = {
+        'id': e[1].id,
+        'MidreshetRef': e[1].MidreshetRef,
+        'full_text': bleach_clean(e[1].body),
+        'Sefaria Ref': e[0].normal(),
+        "Yoni's attempt": e[2],
+    }
+    try:
+        my_row['Disambiguated'] = refine_ref_by_text(e[2], '', bleach_clean(e[1].body))
+    except InputError:
+        my_row['Disambiguated'] = e[2]
+    rows.append(my_row)
+
+with open('Ref_Report.csv', 'w') as fp:
+    writer = unicodecsv.DictWriter(fp, ['id', 'MidreshetRef', 'Sefaria Ref', "Yoni's attempt", "Disambiguated", 'full_text'])
+    writer.writeheader()
+    writer.writerows(rows)
+
+
+# blork = RefBuilder()
+# blip = u'רש"י על התלמוד הבבלי, בבא בתרא דף ז, עמוד ב'
+# bloop = Ref(u'Bava Batra 7a')
+# porg = blork.build_difficult_ref(blip, bloop)
+# print(porg.normal())
+
 
 # cu.executemany("UPDATE RefMap SET Category = 'Mishnah' WHERE id = ?", mishnah)
-
-
-
-
+# build_thingy = RefBuilder()
+# my_cursor = MidreshetCursor()
+# my_id_list = [111, 124, 543]
+# for thing in my_id_list:
+#     my_cursor.execute('SELECT exactLocation, body FROM Resources WHERE id = ?', (thing,))
+#     q_row = my_cursor.fetchone()
+#     basic_ref = library.get_refs_in_string(u'({})'.format(q_row.exactLocation), 'he', citing_only=True)[0]
+#     better_ref = build_thingy.build_difficult_ref(q_row.exactLocation, basic_ref)
+#     even_better = refine_ref_by_text(better_ref, '', bleach.clean(q_row.body, tags=[], attributes={}, strip=True))
+#
+#     print(q_row.exactLocation)
+#     print(basic_ref.normal())
+#     print(better_ref.normal())
+#     print(even_better.normal())
+#     print(u'')
