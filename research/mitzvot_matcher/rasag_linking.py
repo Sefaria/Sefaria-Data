fb08d6f9abfebeaa3255e429607fd5235cb3f787
shaneerosen <shanee@sefaria.org>
2019-04-10 18:57:55 +0300

    clustering was overkill, changed script to link only rasag

--- research/mitzvot_matcher/rasag_linking.py
+++ research/mitzvot_matcher/rasag_linking.py
@@ -7,6 +7,7 @@ from sefaria.model import *
 from sefaria.helper.link import *
 from sources.functions import post_link
 from sefaria.system.database import db
+import regex as re
 
 
 def get_links(sources):
@@ -30,28 +31,34 @@ def get_links(sources):
 
 def derive_halakhah_links():
     new_links = []
-    tanakh_links = get_links(['Tanakh'])
+    tanakh_links = get_links(['Mishneh Torah'])
     for link in tanakh_links:
-        rsg, pasuk = link['refs']
+        rsg, pasuk = link['refs'] if re.search("Sefer Hamitzvot", link['refs'][0]) else [link['refs'][1], link['refs'][0]] # check that this is correct !!! not correct for Mishneh Torah need to diffrentiate
         ls_pasuk = LinkSet(Ref(pasuk)).filter('Halakhah')
         print pasuk
         c = set([l.refs[0] for l in ls_pasuk] + [l.refs[1] for l in ls_pasuk])
-        cluster_refs = [Ref(r) for r in c]
-        create_link_cluster(cluster_refs, 30044, "Sifrei Mitzvot",
-                            attrs={"generated_by": "rsg_sfm_linker"}, exception_pairs=[("Tur", "Shulchan Arukh")])
-        link=({
-            "refs":[
-                "",
-                ""
-            ],
-            "type": "Sifrei Mitzvot",
-            "auto": True,
-            "generated_by":"rsg_sfm_linker"
-        })
-        # break
-    return
+        # cluster_refs = [Ref(r) for r in c]
+        # create_link_cluster(cluster_refs, 30044, "Sifrei Mitzvot",
+        #                     attrs={"generated_by": "rsg_sfm_linker"}, exception_pairs=[("Tur", "Shulchan Arukh")])
+        c = c.difference({pasuk, rsg})
+        for ref_string in list(c):
+            if re.search("Sefer Hamitzvot", ref_string) or not re.search("Sefer Hamitzvot", rsg):
+                continue
+            link=({
+                "refs":[
+                    rsg,
+                    ref_string
+                ],
+                "type": "Sifrei Mitzvot",
+                "auto": True,
+                "generated_by":"rsg_sfm_linker"
+            })
+            print link['refs']
+            new_links.append(link)
+    print len(new_links)
+    return new_links
 
 if __name__ == "__main__":
     derive_halakhah_links()
-    # links = get_links()
+    links = get_links(['Tanakh', 'Mishneh Torah'])
     # post_link(links)
