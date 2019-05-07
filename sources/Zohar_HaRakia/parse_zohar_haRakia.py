827c95f34cfece52157146fcc95653db07fd46cf
Jonathan <jonathan@sefaria.org>
2019-05-07 17:03:05 +0300

    Full Azharot parsing logic

--- sources/Zohar_HaRakia/parse_zohar_haRakia.py
+++ sources/Zohar_HaRakia/parse_zohar_haRakia.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals, print_function
 
 import re
 import codecs
+from collections import OrderedDict
 from data_utilities.util import getGematria
 
 
@@ -31,7 +32,7 @@ class Azharot(object):
 
     def __init__(self):
         self.positive_data, self.negative_data = self._load_data()
-        
+
         self.positive_segments = self._make_initial_parse('positive')
         self.negative_segments = self._make_initial_parse('negative')
 
@@ -41,11 +42,11 @@ class Azharot(object):
             doc_lines = fp.readlines()
 
         negative_start = find_line_with_pattern(u'^@00', doc_lines)
-        return doc_lines[:negative_start], doc_lines[negative_start:]
+        return doc_lines[:negative_start], doc_lines[negative_start+1:]
 
     def _make_initial_parse(self, commandment_type):
         def prepare_segment(string_list):
-            new_segment = ''.join(string_list)
+            new_segment = ' '.join(string_list)
             new_segment = re.sub('^@22([\u05d0-\u05ea]{1,3})\)', '', new_segment)
             new_segment = ' '.join(new_segment.split())
             return new_segment
@@ -58,19 +59,89 @@ class Azharot(object):
             raise ValueError("commandment_type must be 'positive' or 'negative'")
 
         current_segment, parsed_data = [], []
+        report_errors = True
         for row in raw_data:
             new_segment_match = re.match('^@22([\u05d0-\u05ea]{1,3})\)', row)
 
             if new_segment_match:
-                if getGematria(new_segment_match.group(1)) - 1 != len(parsed_data):
-                    print("Length mismatch found at {} for {} commandments".format(new_segment_match.group(1),
-                                                                                   commandment_type))
+
                 if current_segment:
                     parsed_data.append(prepare_segment(current_segment))
                     current_segment = []
+
+                if getGematria(new_segment_match.group(1)) - 1 != len(parsed_data):
+                    if report_errors:
+                        print("Length mismatch found at {} for {} commandments".format(new_segment_match.group(1),
+                                                                                       commandment_type))
+                        report_errors = False
+
+                elif not report_errors:
+                    report_errors = True
+
+            current_segment.append(row)
         else:
             parsed_data.append(prepare_segment(current_segment))
 
         return parsed_data
+
+    def get_commandment_map(self, commandment_type):
+        """
+        Iterate through segments.
+        Collect commandment numbers.
+        Map each commandment number to current line
+        Make sure no duplicates show up
+        Check that the numbers are in order
+        :param commandment_type:
+        :return:
+        """
+        if commandment_type == 'positive':
+            segments = self.positive_segments
+        elif commandment_type == 'negative':
+            segments = self.negative_segments
+        else:
+            raise ValueError("commandment_type must be 'positive' or 'negative'")
+
+        mapping = OrderedDict()
+        for seg_num, segment in enumerate(segments, 1):
+            map_match = re.search('@44\((.*)\)@55', segment)
+            if not map_match:
+                continue
+
+            values = map_match.group(1).split()
+            for value in values:
+                value = getGematria(re.sub(u'[^\u05d0-\u05ea]', '', value))
+                if value in mapping:
+                    print('{} appears twice in {} commandments'.format(value, commandment_type))
+
+                mapping[value] = seg_num
+
+        commandment_values = mapping.keys()
+        previous_value = 0
+        for value in commandment_values:
+            if value - previous_value != 1:
+                print('{} followed by {} in {} commandments'.format(previous_value, value, commandment_type))
+            previous_value = value
+
+        return dict(mapping)
+
+    def get_formatted_segments(self, commandment_type):
+        def format_segment(segment):
+            segment = re.sub('@44([^@]+)@55', '<small>\g<1></small>', segment)
+            segment = re.sub('@\d{2}', '', segment)
+            return segment
+
+        if commandment_type == 'positive':
+            return [format_segment(seg) for seg in self.positive_segments]
+        elif commandment_type == 'negative':
+            return [format_segment(seg) for seg in self.negative_segments]
+        else:
+            raise ValueError("commandment_type must be 'positive' or 'negative'")
+
+
+a = Azharot()
+p = a.get_commandment_map('positive')
+n = a.get_commandment_map('negative')
+
+
 
 
