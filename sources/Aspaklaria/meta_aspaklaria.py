2687b7e8accf7e1a80698fe9301964bc54623add
shaneerosen <shanee@sefaria.org>
2019-05-20 18:03:45 +0300

    stats on the ref catching success rates script

--- sources/Aspaklaria/meta_aspaklaria.py
+++ sources/Aspaklaria/meta_aspaklaria.py
@@ -18,19 +18,43 @@ def list_authors():
     return authors
 
 
-def prct_shams():
-    return
+def stats(sham=None, author=None, caught=None):
+    query = {}
+    if author:
+        query['author'] = u'{}'.format(author)
+    if caught is not None:
+        query['ref'] = {'$exists': caught}
+    if sham is not None:
+        query['is_sham']= sham
+    query_cnt = db.aspaklaria_source.count_documents(query)
+    return query_cnt
 
 
-def prct_caught_refs():
+def percentages(author=None):
+    print u"per {}".format(author)
+    # caught
+    caught = stats(author=author, caught=True)*100.0/stats(author=author)*1.0
+    print "caught/all: {}%".format(caught)
+    # caught without Shams
+    caught_ns = stats(sham=False, author=author, caught=True)*100.0/stats(sham=False, author=author)*1.0
+    print "ignore shams, caught/all: {}%".format(caught_ns)
+    # shams = shams/all (per Author)
+    shams = stats(sham=True, author=author, caught=None)*100.0/stats(author=author)*1.0
+    print "shams/all: {}%".format(shams)
+    # caught shams out of all shams
+    all_shams = stats(author=author, sham=True)*1.0
+    if all_shams:
+        caught_shams = stats(sham=True, author=author, caught=True)*100.0/all_shams*1.0
+        print "caught_shams/all_shams: {}%".format(caught_shams)
+
     return
 
 
 if __name__ == "__main__":
 
     authors = list_authors()
-
-    print len(authors)
+    percentages()
+    print '_________________________'
     for auth in authors:
-        print auth
+        percentages(auth)
 
