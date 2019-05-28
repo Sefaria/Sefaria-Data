9b1ff75ce3889c23b1f27243af4384c7f42c2ae3
steven kaplan <stevenkaplan@stevens-mbp.mynet>
2019-05-28 15:09:43 +0300

    Rosh on Tamid and Yad Ephraim reports ready

---
+++ sources/Rosh on Yoma/tamid.py
@@ -0,0 +1,57 @@
+import django
+django.setup()
+from sefaria.model import *
+from data_utilities.dibur_hamatchil_matcher import *
+from sources.functions import *
+import re
+def dher(str):
+    if "." in str:
+        return str.split(".", 1)[0]
+    else:
+        return ""
+
+if __name__ == "__main__":
+    daf = 0
+    text = {}
+    with open("rosh_on_tamid.txt") as f:
+        lines = list(f)
+        for i, line in enumerate(lines):
+            line = line.replace("\r\n", "")
+            if not line:
+                continue
+            line = line.decode('utf-8')
+            if len(line.split()) is 1:
+                new_daf = getGematria(line)*2
+                if not ":" in line:
+                    new_daf -= 1
+                    assert "." in line
+                assert new_daf > daf
+                daf = new_daf
+                text[daf] = []
+            elif "." in line:
+                dh, comment = line.split(".", 1)
+                text[daf].append(line)
+            else:
+                text[daf].append(line)
+
+
+
+    links = []
+    for daf, comments in text.items():
+        base = TextChunk(Ref("Tamid {}".format(AddressTalmud.toStr('en', daf))), lang='he')
+        results = match_ref(base, comments, base_tokenizer=lambda x: x.split(), dh_extract_method=dher)
+        for match_n, match in enumerate(results["matches"]):
+            if match:
+                rosh = "Rosh on Tamid {}:{}".format(AddressTalmud.toStr("en", daf), match_n+1)
+                link = {"refs": [match.normal(), rosh], "generated_by": "rosh_on_tamid", "auto": True, "type": "Commentary"}
+                links.append(link)
+
+    post_link(links, server=SEFARIA_SERVER)
+
+    send_text = {
+        "language": "he",
+        "versionTitle": "Vilna Edition",
+        "versionSource": "http://primo.nli.org.il/primo_library/libweb/action/dlDisplay.do?vid=NLI&docId=NNL_ALEPH001300957",
+        "text": convertDictToArray(text)
+    }
+    post_text("Rosh on Tamid", send_text, server=SEFARIA_SERVER)
--- sources/Yad Ephraim/parse helek bet.py
+++ sources/Yad Ephraim/parse helek bet.py
@@ -22,14 +22,17 @@ def replacements(book, prev_book):
 if __name__ == "__main__":
     file = "Levushei Serad.txt"
     simanim_f = open("{}_simanim.csv".format(file.split()[0]), 'w')
+    with open("Levushei Serad/list of simanim.csv") as f:
+        actual_simanim = [el.replace("\r\n", "") for el in list(f)]
+        actual_simanim_gematrias = [getGematria(el) for el in actual_simanim]
     simanim = csv.writer(simanim_f)
-    found = []
+    found = set()
     book_set = set()
     known_books = ["""ט"ז""", """ש"ע""",  """מג"א"""]
     prev_book = ""
     prev = 1
     prev_line = ""
-    with open(file) as f:
+    with open("extra_books.txt", 'w') as extra_books, open("simanim_in_our_text_but_not_list.txt", 'w') as simanim_in_our_text, open(file) as f:
         lines = list(f)
         for n, line in enumerate(lines):
             if line == "\r\n":
@@ -50,10 +53,10 @@ if __name__ == "__main__":
             #     siman = getGematria(words[-1])
             # elif (line.startswith("@") or line[0].isdigit()) and has_siman > -1:
             #     siman = getGematria(words[has_siman+1])
-            if len(words) <= 2 and not seif_k:
-                siman = getGematria(words[-1])
+            if "@22" in line and len(words) <= 2 and not seif_k_link:
+                siman = words[-1]
             elif "@22" in line and has_siman > -1:
-                siman = getGematria(words[has_siman+1])
+                siman = words[has_siman+1]
             if not siman and getGematria(words[0]) == prev + 1:
                 pass
             if siman:
@@ -61,7 +64,10 @@ if __name__ == "__main__":
                 third = ""
                 if siman <= prev:
                     third = "X"
-                found.append((siman, line, third))
+                siman = getGematria(siman)
+                found.add(siman)
+                if siman not in actual_simanim_gematrias:
+                    simanim_in_our_text.write(line)
                 simanim.writerow([siman, line, third])
                 prev = siman
                 prev_line = n
@@ -85,7 +91,7 @@ if __name__ == "__main__":
                             book = known_book
                             break
                     if book not in known_books:
-                        print seif_k_link
+                         extra_books.write(re.search("@11(.*?)@33", line).group(1)+"\n")
                     prev_book = book
                 elif len(seif_k_link.split()) == 2:
                     book = prev_book
@@ -93,10 +99,11 @@ if __name__ == "__main__":
                 else:
                     pass
 
-        print "EXTRAS"
-        for extra in book_set - set(known_books):
-            print extra
 
+        with open("simanim_in_list_but_in_our_text.txt", 'w') as simanim_in_list:
+            for siman, siman_gematria in zip(actual_simanim, actual_simanim_gematrias):
+                if siman_gematria not in found:
+                    simanim_in_list.write(siman+"\n")
 
 
 
