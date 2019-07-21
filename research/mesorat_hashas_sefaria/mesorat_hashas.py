c4028cf3554964951fcf95db1db0353b78265bc5
Noah Santacruz <noahssantacruz@gmail.com>
2019-07-21 12:56:33 +0300

    fix indices in edge case

--- research/mesorat_hashas_sefaria/mesorat_hashas.py
+++ research/mesorat_hashas_sefaria/mesorat_hashas.py
@@ -566,9 +566,11 @@ class ParallelMatcher:
                     pickle.dump((unit_il, unit_rl, total_len), my_pickle, -1)
 
             for i_word in xrange(len(unit_wl) - self.skip_gram_size):
-                skip_gram_list = self.ght.get_skip_grams(unit_wl[i_word:i_word + self.skip_gram_size + 1], is_end=i_word == (len(unit_wl) - self.skip_gram_size - 1))
-                for skip_gram in skip_gram_list:
-                    start_index = i_word
+                is_unit_end = i_word == (len(unit_wl) - self.skip_gram_size - 1)
+                skip_gram_list = self.ght.get_skip_grams(unit_wl[i_word:i_word + self.skip_gram_size + 1], is_end=is_unit_end)
+                for iskip_gram, skip_gram in enumerate(skip_gram_list):
+                    # if you're at the last skip gram in the unit, you skipped the first word of the skip gram
+                    start_index = i_word - 1 if is_unit_end and iskip_gram == len(skip_gram_list) - 1 else i_word
                     end_index = i_word + self.skip_gram_size
 
                     start_ref = unit_rl[bisect.bisect_right(unit_il, start_index) - 1]
