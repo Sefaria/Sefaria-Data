29277450d20783cf31a2806da7116a6de4927399
Ephraim Damboritz <ephraim@sefaria.org>
2019-11-12 12:28:47 +0200

    Add files via upload

---
+++ lexicon/primary-headwords/common_words_adjust.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+
+import django
+django.setup()
+import unicodecsv as csv
+from sefaria.system.database import db
+from sefaria.model import *
+
+def create_Word_form(word_form_str, headword, lexicon, primaryH):
+    wordform_obj = WordForm({
+        "form": word_form_str,
+        "c_form": word_form_str,
+        "generated_by": "adjust_most_common_talmud_words",
+        "lookups": [
+            {
+                "headword": headword,
+                "parent_lexicon": lexicon
+            }
+        ]
+    })
+    if primaryH:
+        wordform_obj.lookups[0]["primary"] = True
+    return wordform_obj.save()
+
+with open("/home/ephraim/Downloads/most_common_words_and_lexicon_entries_revised.csv") as csvfile:
+    reader = csv.DictReader(csvfile)
+    word_form_str = ""
+    for i, row in enumerate(reader, 2):
+        headword, lexicon = [row[k] for k in ('Headword', 'Lexicon')]
+        primaryH, addH, deleteH = [bool(row[k]) for k in ('Primary', 'For Addition', 'For Deletion')]
+        if len(row["Common Word"]):
+            word_form_str = row["Common Word"]
+        print u"{}) {}-{}-{} [p{}, a{}, d{}]".format(i, word_form_str, headword, lexicon, primaryH, addH, deleteH)
+        query = { "$or": [{"c_form": word_form_str}, {"form": word_form_str}],
+                  "lookups": { "$elemMatch": { "headword": headword, "parent_lexicon": lexicon } } }
+        if addH or primaryH:
+            print u"{}-{}-{} being added ({} {})".format(word_form_str, headword, lexicon, "New" if addH else "", "Primary" if primaryH else "")
+            form = create_Word_form(word_form_str, headword, lexicon, primaryH)
+        elif deleteH:
+            print u"{}-{}-{} being deleted:".format(word_form_str, headword, lexicon)
+            wfset = WordFormSet(query)
+            for wf in wfset:
+                len_lookups = len(wf.lookups)
+                wf.lookups = filter(lambda x: x["headword"] != headword or x["parent_lexicon"] != lexicon, wf.lookups)
+                len_lookups_adj = len(wf.lookups)
+                print u"*     Original lookups: {} | Lookups removed {}".format(len_lookups, len_lookups-len_lookups_adj)
+                if len_lookups_adj > 0:
+                    print u"*     saving word form"
+                    try:
+                        wf.save()
+                    except:
+                        pass
+                else:
+                    print u"*     deleting word form"
+                    try:
+                        wf.delete()
+                    except:
+                        pass
+
+
