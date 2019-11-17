052a28476f1e3d705f8d8c4dd04a9570a7be739a
Jonathan <jonathan@sefaria.org>
2019-11-17 13:18:43 +0200

    punctuated Talmud does not wreck the Talmud (finally)

--- sources/puncutation_project/lift_punctuation.py
+++ sources/puncutation_project/lift_punctuation.py
@@ -6,11 +6,14 @@ import re
 
 import sefaria_classes as sef
 import bleach
+from sefaria.utils.hebrew import strip_nikkud
 from data_utilities.dibur_hamatchil_matcher import match_text
 
 
-base_vtitle = ''
-elucidated_vtitle = ''
+# base_vtitle = 'William Davidson Edition - Aramaic'
+base_vtitle = 'William Davidson Edition - Vocalized Aramaic'
+elucidated_vtitle = 'William Davidson Edition - Hebrew'
+punctuated_vtitle = 'William Davidson Edition - Aramaic Punctuated'
 
 
 def extract_talmud_text(text_segment):
@@ -35,6 +38,7 @@ class ModeledSegment(object):
         self._raw_segment = segment
         self._ts_pairs = []
         self._load_pairs()
+        self._connect_talmud()
 
     def _load_pairs(self):
         matches = list(re.finditer(r'<b>([^<]+)</b>', self._raw_segment))
@@ -43,19 +47,39 @@ class ModeledSegment(object):
             # handle segment introductions
             if i == 0 and match.start() != 0:
                 self._ts_pairs.append(SteinsaltzIntro.load_talmud_steinsaltz(self._raw_segment, match.start()))
-            self._ts_pairs.append(TalmudSteinsaltz.load_talmud_steinsaltz(
-                self._raw_segment, match.start(), match.end(), next_match.start()))
+
+            ts = TalmudSteinsaltz.load_talmud_steinsaltz(
+                self._raw_segment, match.start(), match.end(), next_match.start())
+            if ConnectedTalmud.is_connected(ts.talmud):
+                self._ts_pairs.append(ConnectedTalmud(ts.talmud, ts.steinsaltz))
+            else:
+                self._ts_pairs.append(ts)
 
         # last one
-        self._ts_pairs.append(TalmudSteinsaltz.load_talmud_steinsaltz(
+        ts = TalmudSteinsaltz.load_talmud_steinsaltz(
             self._raw_segment, matches[-1].start(), matches[-1].end(), len(self._raw_segment)
-        ))
+        )
+        if ConnectedTalmud.is_connected(ts.talmud):
+            self._ts_pairs.append(ConnectedTalmud(ts.talmud, ts.steinsaltz))
+        else:
+            self._ts_pairs.append(ts)
 
     def reform_segment(self):
         return u''.join([ts.reform_pair() for ts in self._ts_pairs])
 
     def get_ts_objects(self):
         return self._ts_pairs[:]
+
+    def _connect_talmud(self):
+        """
+        Connect talmud where Steinsaltz introduced elucidation within a word.
+        :return:
+        """
+        for current, next_obj in zip(self._ts_pairs[:-1], self._ts_pairs[1:]):
+            if isinstance(current, ConnectedTalmud):
+                next_obj.make_connection(current)
+
+        self._ts_pairs = [ts for ts in self._ts_pairs if not isinstance(ts, ConnectedTalmud)]
 
 
 class TalmudSteinsaltz(object):
@@ -66,8 +90,31 @@ class TalmudSteinsaltz(object):
         self.talmud = talmud
         self.steinsaltz = steinsaltz
 
-    def lift_punctuaion(self):
-        return '!' if self.talmud else ''
+    @staticmethod
+    def punctuation_list():
+        return ['?!', '?', '!', ':', '.', ',']
+
+    @classmethod
+    def punctuation_regex(cls):
+        marks = [re.escape(s) for s in cls.punctuation_list()]
+        return u'|'.join(marks)
+
+    def lift_punctuation(self):
+        if not self.talmud:
+            return ''
+        priority_map = {
+            punc: i
+            for i, punc in enumerate(self.punctuation_list())
+        }
+        punc_regex = re.compile(self.punctuation_regex())
+        punc_marks = [match.group() for match in punc_regex.finditer(self.steinsaltz)]
+        talmud_mark = re.search('r{}$'.format(punc_regex.pattern), self.talmud)
+        if talmud_mark:
+            punc_marks.append(talmud_mark.group())
+        if punc_marks:
+            return min(punc_marks, key=lambda x: priority_map[x])
+        else:
+            return ''
 
     def reform_pair(self):
         return '{}{}'.format(self.talmud, self.steinsaltz)
@@ -77,6 +124,16 @@ class TalmudSteinsaltz(object):
 
     def get_steinsaltz(self):
         return self.steinsaltz
+
+    def talmud_word_count(self):
+        return len(self.talmud.split())
+
+    def make_connection(self, other):
+        """
+        :param ConnectedTalmud other:
+        :return:
+        """
+        self.talmud = u'{}{}'.format(other.talmud, self.talmud)
 
     @classmethod
     def load_talmud_steinsaltz(cls, segment, current_start, current_end, next_start):
@@ -99,6 +156,25 @@ class SteinsaltzIntro(TalmudSteinsaltz):
     def load_talmud_steinsaltz(cls, segment, next_start, *args):
         return cls(segment[:next_start])
 
+    def lift_punctuation(self):
+        return ''
+
+
+class ConnectedTalmud(TalmudSteinsaltz):
+
+    @staticmethod
+    def is_connected(talmud_piece):
+        # option 1: prefix letter (בהוכלמש)
+        if re.match(r'^"?[\u05d1\u05d4\u05d5\u05db\u05dc\u05de\u05e9]$', talmud_piece):
+            return True
+        # option 2: no hebrew
+        if re.match(r'^[^\u05d0-\u05ea]+$', talmud_piece):
+            return True
+        return False
+
+    def lift_punctuation(self):
+        return ''
+
 
 class TSMap(object):
     """
@@ -113,13 +189,21 @@ class TSMap(object):
         self.talmud_steinsaltz = talmud_steinsaltz
 
     def get_punctuation_for_talmud(self):
-        return '{}{}'.format(self.reg_talmud, self.talmud_steinsaltz.lift_punctuaion())
+        cleaned_talmud = re.sub('{}$'.format(TalmudSteinsaltz.punctuation_regex()), '', self.reg_talmud)
+        return '{}{}'.format(cleaned_talmud, self.talmud_steinsaltz.lift_punctuation())
 
     @classmethod
     def build_from_map(cls, segment, ts, mapping):
         if mapping[0] == -1 and mapping[1] == -1:
             return cls(u'', ts)
-        return cls(u' '.join(segment.split()[mapping[0]:mapping[1]+1]), ts)
+        return cls(u' '.join(segment.split()[mapping[0]:mapping[1]]), ts)
+
+    def actually_has_talmud(self):
+        """
+        Indidcates if there is actually Talmud associated with this Steinsaltz.
+        :return: bool
+        """
+        return bool(self.reg_talmud)
 
 
 class TSSuite(object):
@@ -127,19 +211,45 @@ class TSSuite(object):
         self.suite = tsmap_list
 
     def get_punctuated_talmud(self):
-        return u' '.join([tsmap.get_punctuation_for_talmud() for tsmap in self.suite])
+        return u' '.join([tsmap.get_punctuation_for_talmud() for tsmap in self.suite if tsmap.actually_has_talmud()])
 
 
 def build_maps(simple_segment, ellucidated_segment):
     modeled = ModeledSegment(ellucidated_segment)
-    words = actual_bleach(simple_segment).split()
+    words = actual_bleach(strip_nikkud(simple_segment)).split()
     ts_objects = modeled.get_ts_objects()
-    mapping = match_text(words, [ts.get_talmud() for ts in ts_objects])
+    elucidated_words = reduce(lambda x, y: x + y.talmud_word_count(), ts_objects, 0)
+
+    if len(words) == elucidated_words:
+        mapping, current_loc = [], 0
+        for ts in ts_objects:
+            end = current_loc + ts.talmud_word_count()
+            mapping.append((current_loc, end))
+            current_loc = end
+    else:
+        mapping = match_text(words, [ts.get_talmud() for ts in ts_objects], overall=0)['matches']
+        mapping = map(lambda x: (x[0], x[1]+1), mapping)
+
+        last_word = 0
+        for i in range(len(mapping)):
+            """
+            Start at 0.
+            For each item, make sure the end is higher than the beginning
+            if smaller:
+                if the next item has a valid start value, set the end to that value
+                otherwise, make this a 0 length segment
+
+            """
+            start, end = last_word, mapping[i][-1]
+            if end < start:
+                end = start
+            mapping[i] = (start, end)
+            last_word = end
+
+        mapping[-1] = (mapping[-1][0], len(words))  # make sure mapping goes to the end
 
     return TSSuite([TSMap.build_from_map(simple_segment, ts, single_map)
-                    for ts, single_map in zip(ts_objects, mapping['matches'])])
-
-
+                    for ts, single_map in zip(ts_objects, mapping)])
 
 
 # base_text = actual_bleach(sef.Ref("Berakhot 2a.1").text('he', base_vtitle).text)
@@ -167,10 +277,24 @@ We'll want to have pairs strings - Talmud, ellucidation. This will be a class. I
 logic can be here. The elucidation will always follow the Talmud. We'll want to have a special "intro" subclass. 
 """
 
-my_r = sef.Ref("Steinsaltz on Berakhot 2a.2")
-base_r = sef.Ref("Berakhot 2a.2")
+my_r = sef.Ref("Steinsaltz on Berakhot 47b.12")
+base_r = sef.Ref("Berakhot 47b.12")
 my_t = my_r.text('he', elucidated_vtitle).text
 base_t = base_r.text('he', base_vtitle).text
 maps = build_maps(base_t, my_t)
 print(base_t, my_t, maps.get_punctuated_talmud(), sep='\n\n')
-print(*[ts.reg_talmud for ts in maps.suite], sep='\n')
+
+simple, elucidated = sef.Ref("Berakhot"), sef.Ref("Steinsaltz on Berakhot")
+for simple_seg in simple.all_segment_refs():
+    print(simple_seg.normal())
+    elucidated_seg = sef.Ref("Steinsaltz on {}".format(simple_seg.normal()))
+    assert simple_seg.sections == elucidated_seg.sections
+    base_t, eluc_t = simple_seg.text('he', base_vtitle).text, elucidated_seg.text('he', elucidated_vtitle).text
+    new_tc = simple_seg.text('he', punctuated_vtitle)
+    if not eluc_t:
+        new_tc.text = base_t
+    else:
+        maps = build_maps(base_t, eluc_t)
+        punctuated_text = maps.get_punctuated_talmud()
+        new_tc.text = punctuated_text
+    new_tc.save()
