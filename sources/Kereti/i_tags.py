3a65ebc8c1432ff5bd4624f32139fd21625d896c
Steven Kaplan <steve@sefaria.org>
2019-11-25 17:37:07 +0200

    Kereti itags inserted but Kereti text not up yet

--- sources/Kereti/i_tags.py
+++ sources/Kereti/i_tags.py
@@ -10,189 +10,178 @@ from difflib import context_diff
 from data_utilities.dibur_hamatchil_matcher import *
 import bleach
 
+class WordObj:
+    def __init__(self, word, info={}, before=False):
+        self.word = word
+        self.before = before
+        self.info = info
 
-class PieceOfText:
-    def __init__(self, text):
-        self.text = text
-        self.tag = None
+    def toStr(self):
+        html = ""
+        if self.info:
+            html += "<i "
+            html += u'{}="{}"'.format("data-commentator", self.info["data-commentator"])
+            if "data-label" in self.info:
+                html += u' {}="{}"'.format("data-label", self.info["data-label"])
+            html += u' {}="{}"'.format("data-order", self.info["data-order"])
+            html += "></i>"
+        return html
 
-
-def get_closest_hebrew_word(line, dir=1):
-    words = line.split()
-    # dir == 1: start at first word and keep going forward until there's a Hebrew word
-    # dir == -1: start at last word and keep going backward until there's a Hebrew word
-    curr_word = len(words) - 1 if dir == -1 else 0
-    while len(words) > curr_word >= 0:
-        m = re.search(u"[<>a-z)(א-ת]+", words[curr_word])
-        if m:
-            assert " "
-            return m.group(0)
+    def beforeStr(self):
+        if self.before:
+            return self.toStr()
         else:
-            curr_word += dir
+            return ""
 
-#
-#
-# def extract_i_tags(line):
-#     i_tags = {}
-#     altered_line, i_tag_dicts = remove_i_tags(line)
-#     for words_before_tag in line.split("$"):
-
-    #altered_line + tag_dicts
-    # for n, m in enumerate(re.finditer("""<i data.*?></i>""", line)):
-    #     pos = m.regs[0]
-    #     i_tag = line[pos[0]:pos[1]]
-    #     tag_dict = BeautifulSoup(i_tag).find("i").attrs
-    #     word_before = get_closest_hebrew_word(altered_line[0:pos[0]], dir=-1)
-    #     word_after = get_closest_hebrew_word(altered_line[pos[0]:], dir=1)
-    #     assert "<" not in word_before
-    #     assert "<" not in word_after
-    #     p = u"{}.*?{}".format(word_before, word_after).replace(")", "\)").replace("(", "\(")
-    #     matches = re.finditer(p, line)
-    #     for i, m in enumerate(matches):
-    #         start = m.regs[0][0]
-    #         end = m.regs[0][1]
-    #         if pos[0] >= start and end >= pos[1]:
-    #             found = i
-    #             tag_dict["pos"] = i
-    #     curr_tag_dicts = i_tags.get((word_before, word_after), [])
-    #     i_tags[(word_before, word_after)] = curr_tag_dicts + [tag_dict]
-    # return i_tags
-
+    def afterStr(self):
+        if not self.before:
+            return self.toStr()
+        else:
+            return ""
 
 def remove_i_tags(line):
     assert not "$" in line
+    assert not "^" in line
     i_tags = re.findall(u"<i data.*?></i>", line)
-    i_tag_dicts = []
-    for i_tag in i_tags:
+    i_tag_dicts = {}
+    for n, i_tag in enumerate(i_tags):
         i_tag_dict = BeautifulSoup(i_tag).find("i").attrs
-        start_pos = line.find(i_tag)
-        line = line.replace(i_tag, "$")
-        i_tag_dicts.append(i_tag_dict)
+        marker = "${}".format(n+1)
+        line = line.replace(i_tag, marker, 1)
+        line = line.replace(" {} ".format(marker), "{} ".format(marker))
+        start_pos = line.split("${}".format(n+1))[0].count(" ")
+        before = False
+        if line.split(" ")[start_pos].startswith(marker):
+            before = True
+        i_tag_dicts[start_pos] = (i_tag_dict, before)
 
-    words_before_tag_list = line.split("$")[:-1]
+    words = []
+    line = re.sub("\$\d+", "", line)
+    for n, word in enumerate(line.split()):
+        if n in i_tag_dicts.keys():
+            word_obj = WordObj(word, i_tag_dicts[n][0], i_tag_dicts[n][1])
+        else:
+            word_obj = WordObj(word)
+
+        words.append(word_obj)
+    return words
+
+def get_kereti_dhs_in_mechaber():
+    siman = 0
+    all_dhs = {}
+    for file in ["Mechaber 1.txt", "Mechaber 2.txt"]:
+        with open(file) as f:
+            for n, line in enumerate(f):
+                line = line.decode('utf-8')
+                if line.startswith("@22"):
+                    siman = getGematria(line.split()[0])
+                    all_dhs[siman] = {}
+                    all_dhs[siman][1] = []
+                    seif = 1
+                elif line.startswith("@11"):
+                    seif = getGematria(line.split()[0])
+                    all_dhs[siman][seif] = []
+
+                if line.count("&") > 0:
+                    line = re.sub(u"\[\S{1,3}\]", u"", line)
+                    line = re.sub(u"[!*]+\S+", u"", line)
+                    line = re.sub(u"@\d+\S{1,5}", u"", line)
+                    line = re.sub(u"\(#\)", u"", line)
+                    line = re.sub(u" \*", u"", line)
+
+                    line = line.replace("  ", " ").strip()
+                    dhs = [u" ".join(dh.split()[0:3]) for dh in re.split("\s?&\s?", line)]
+                    dhs = dhs[1:]
+                    all_dhs[siman][seif] += dhs
+    return all_dhs
+
+
+def create_new_pieces(results, section):
+    data_order = 0
     pieces = []
     prev = 0
-    for i, words_before in enumerate(words_before_tag_list):
-        tc = PieceOfText((prev, len(words_before.split())))
-        tc.tag = i_tag_dicts[i]
-        pieces.append(tc)
-        prev += len(words_before)
-
-    tc = PieceOfText((prev, -1))
-    pieces.append(tc)
-    return pieces
-
-def resolve_ambiguity(i_tag_dict):
-    words_before_prev = i_tag_dict["words_before_prev"]
-    curr_matches = [m.regs[0][0] for m in re.finditer(i_tag_dict["words_before_curr"], line)]
-    prev_matches = [m.regs[0][0] + len(words_before_prev) for m in re.finditer(words_before_prev, line)]
-    for curr_match in curr_matches:
-        for prev_match in prev_matches:
-            if curr_match == prev_match:
-                return curr_match
-    return -1
-
-def insert_i_tags(i_tags):
-    for i_tag_dict in i_tags:
-        html = ""
-        words_before = i_tag_dict["words_before_curr"]
-        if line.find(words_before) != line.rfind(words_before): #found many possible locations
-            start_pos = resolve_ambiguity(i_tag_dict)
+    word_dict = {}
+    words = section.text('he').text.split()
+    for m in results:
+        data_order += 1
+        if m[0] != -1:
+            tag = {}
+            tag["data-commentator"] = "Kereti"
+            tag["data-order"] = data_order
+            pos = m[0]
+            word_dict[pos] = tag
         else:
-            start_pos = line.find(words_before)
-        i_tag_pos = start_pos + len(words_before)
-        html += "<i "
-        html += u'{}="{}"'.format("data-commentator", i_tag_dict["data-commentator"])
-        if "data-label" in i_tag_dict:
-            html += u' {}="{}"'.format("data-label", i_tag_dict["data-label"])
-        html += u' {}="{}"'.format("data-order", i_tag_dict["data-order"])
-        html += "></i>"
-        line = line[0:i_tag_pos] + html + line[i_tag_pos:]
+            print "Didnt find match in {}".format(section)
 
-    return line
+    word_objs = []
+    for n, word in enumerate(words):
+        if n in word_dict.keys():
+            word_obj = WordObj(word, word_dict[n])
+        else:
+            word_obj = WordObj(word)
+        word_objs.append(word_obj)
+    return word_objs
+
+def get_kereti_tags():
+    sections = Ref("Shulchan Arukh, Yoreh Deah").all_segment_refs()
+    i_tags_kereti = {}
+    for sec in sections:
+        if sec.sections[0] == 112:
+            break
+        base_text = TextChunk(sec, lang='he').text
+        base_words = re.sub(u"<.*?>", u" ", base_text)
+        while u"  " in base_words:
+            base_words = base_words.replace(u"  ", u" ")
+        try:
+            results = match_text(base_words.split(), all_dhs[sec.sections[0]][sec.sections[1]])
+        except:
+            print "Problem with {}".format(sec)
+            continue
+        if sec.sections[0]-1 not in i_tags_kereti:
+            i_tags_kereti[sec.sections[0]-1] = {}
+        i_tags_kereti[sec.sections[0]-1][sec.sections[1]-1] = create_new_pieces(results["matches"], sec)
+    return i_tags_kereti
 
 
-i_tags = {}
+SA_word_objs = {}
 tc = TextChunk(Ref("Shulchan Arukh, Yoreh Deah"), lang='he', vtitle="Ashlei Ravrevei: Shulchan Aruch Yoreh Deah, Lemberg, 1888")
 for i, section in enumerate(tc.text):
-    i_tags[i] = {}
+    SA_word_objs[i] = {}
     for j, line in enumerate(section):
         #i_tags[i][j] = extract_i_tags(line)
-        i_tags[i][j] = remove_i_tags(line)
+        SA_word_objs[i][j] = remove_i_tags(line)
         # altered_line = insert_i_tags(i_tags[i][j])
         # for txt in context_diff(altered_line.split(), section[j].split()):
         #     print txt
 
-
 #now take out all tags in file and then create data structure of all & and two words after
 #dh_matcher can take all these two word strings and TextChunk(Ref("Shulchan Arukh, Yoreh Deah")
-all_dhs = {}
-siman = 0
-for file in ["Mechaber 1.txt", "Mechaber 2.txt"]:
-    with open(file) as f:
-        for n, line in enumerate(f):
-            line = line.decode('utf-8')
-            if line.startswith("@22"):
-                siman = getGematria(line.split()[0])
-                all_dhs[siman] = {}
-                all_dhs[siman][1] = []
-                seif = 1
-            elif line.startswith("@11"):
-                seif = getGematria(line.split()[0])
-                all_dhs[siman][seif] = []
+all_dhs = get_kereti_dhs_in_mechaber()
+kereti_word_objs = get_kereti_tags()
+tc = TextChunk(Ref("Shulchan Arukh, Yoreh Deah"), lang='he')
+base_text = tc.text
 
-            if line.count("&") > 0:
-                line = re.sub(u"\[\S{1,3}\]", u"", line)
-                line = re.sub(u"[!*]+\S+", u"", line)
-                line = re.sub(u"@\d+\S{1,5}", u"", line)
-                line = re.sub(u"\(#\)", u"", line)
-                line = re.sub(u" \*", u"", line)
-
-                line = line.replace("  ", " ").strip()
-                dhs = [u" ".join(dh.split()[0:3]) for dh in re.split("\s?&\s?", line)]
-                dhs = dhs[1:]
-                all_dhs[siman][seif] += dhs
-
-
-sections = Ref("Shulchan Arukh, Yoreh Deah").all_segment_refs()
-total = missed = 0
-print "Reference,Sefaria's Text,Mechaber Text"
-for sec in sections:
-    if sec.sections[0] == 112:
+for i in range(len(SA_word_objs)):
+    if i > 110:
         break
-    tcs = [tc.text for tc in i_tags[sec.sections[0]-1][sec.sections[1]-1]]
-    base_text = u" ".join(tcs)
-    while u"  " in base_text:
-        base_text = base_text.replace(u"  ", u" ")
-    base_words = base_text.split()
-    results = match_text(base_words, all_dhs[sec.sections[0]][sec.sections[1]])
-    total += len(results["matches"])
-    missed += results["matches"].count((-1, -1))
-    match_index = 0
-    prev = 0
-    pieces = []
-    data_order = 0
-    for n, line in enumerate(tc.text):
-        for m in results["matches"]:
-            data_order += 1
-            if m[0] != -1:
-                start_pos = m[0]
-                end_pos = m[1]
-                tc = PieceOfText((prev, start_pos))
-                tag = {}
-                tag["data-commentator"] = "Kereti"
-                tag["data-order"] = data_order
-                tc.tag = tag
-                prev = end_pos
-                pieces.append(tc)
-            else:
-                print "didnt find"
+    for j in range(len(SA_word_objs[i])):
+        new_base_text = []
+        try:
+            for n, word_obj in enumerate(SA_word_objs[i][j]):
+                SA_before = word_obj.beforeStr()
+                kereti_before = kereti_word_objs[i][j][n].beforeStr()
+                SA_after = word_obj.afterStr()
+                kereti_after = kereti_word_objs[i][j][n].afterStr()
+                full_word = SA_before + kereti_before + word_obj.word + SA_after + kereti_after
+                new_base_text.append(full_word)
+            base_text[i][j] = u" ".join(new_base_text)
+        except Exception as e:
+            print (i, j)
 
-
-# for i, section in enumerate(tc.text):
-#     i_tags[i] = {}
-#     for j, line in enumerate(section):
-#         i_tags[i][j] = extract_i_tags(line)
-#         altered_line = remove_i_tags(line)
-#         altered_line
-#         altered_line = insert_i_tags(altered_line, i_tags[i][j])
+text_version = {
+    "text": base_text,
+    "versionTitle": "Kereti",
+    "language": "he",
+    "versionSource": "http://www.sefaria.org",
+}
+post_text("Shulchan Arukh, Yoreh Deah", text_version)
