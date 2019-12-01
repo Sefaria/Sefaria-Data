4a7075629df2ea1b7f4e22bc4a0ee5b2c03d08eb
Steven Kaplan <steve@sefaria.org>
2019-12-01 12:30:19 +0200

    kereti text up and itags up

--- sources/Kereti/i_tags.py
+++ sources/Kereti/i_tags.py
@@ -11,19 +11,19 @@ from data_utilities.dibur_hamatchil_matcher import *
 import bleach
 
 class WordObj:
-    def __init__(self, word, info={}, before=False):
+    def __init__(self, word, info=[], before=False):
         self.word = word
         self.before = before
         self.info = info
 
     def toStr(self):
         html = ""
-        if self.info:
+        for i_tag in self.info:
             html += "<i "
-            html += u'{}="{}"'.format("data-commentator", self.info["data-commentator"])
-            if "data-label" in self.info:
-                html += u' {}="{}"'.format("data-label", self.info["data-label"])
-            html += u' {}="{}"'.format("data-order", self.info["data-order"])
+            html += u'{}="{}"'.format("data-commentator", i_tag["data-commentator"])
+            if "data-label" in i_tag:
+                html += u' {}="{}"'.format("data-label", i_tag["data-label"])
+            html += u' {}="{}"'.format("data-order", i_tag["data-order"])
             html += "></i>"
         return html
 
@@ -42,29 +42,29 @@ class WordObj:
 def remove_i_tags(line):
     assert not "$" in line
     assert not "^" in line
+    line = re.sub(u" <i data.*?></i> ", u" \1", line)
     i_tags = re.findall(u"<i data.*?></i>", line)
     i_tag_dicts = {}
+    word_objs = []
+    word_dict = {}
     for n, i_tag in enumerate(i_tags):
         i_tag_dict = BeautifulSoup(i_tag).find("i").attrs
-        marker = "${}".format(n+1)
-        line = line.replace(i_tag, marker, 1)
-        line = line.replace(" {} ".format(marker), "{} ".format(marker))
-        start_pos = line.split("${}".format(n+1))[0].count(" ")
-        before = False
-        if line.split(" ")[start_pos].startswith(marker):
-            before = True
-        i_tag_dicts[start_pos] = (i_tag_dict, before)
+        i_tag_start_pos = line.find(i_tag)
+        # get i_tag positions
+        # then take line from 0 until there, remove all i tags, and count words
+        word_pos = re.sub("<i data.*?></i>", u"", line[0:i_tag_start_pos]).count(" ")
+        if word_pos not in word_dict:
+            word_dict[word_pos] = []
+        word_dict[word_pos] += [i_tag_dict]
 
-    words = []
-    line = re.sub("\$\d+", "", line)
-    for n, word in enumerate(line.split()):
-        if n in i_tag_dicts.keys():
-            word_obj = WordObj(word, i_tag_dicts[n][0], i_tag_dicts[n][1])
+    # now go through all words
+    for n, word in enumerate(re.sub("<i data.*?></i>", u"", line).split()):
+        if n not in word_dict.keys():
+            word_objs.append(WordObj(word))
         else:
-            word_obj = WordObj(word)
+            word_objs.append(WordObj(word, word_dict[n], True))
 
-        words.append(word_obj)
-    return words
+    return word_objs
 
 def get_kereti_dhs_in_mechaber():
     siman = 0
@@ -96,50 +96,58 @@ def get_kereti_dhs_in_mechaber():
     return all_dhs
 
 
-def create_new_pieces(results, section):
+def create_new_pieces(results, section, data_order_running_count):
     data_order = 0
-    pieces = []
-    prev = 0
     word_dict = {}
-    words = section.text('he').text.split()
+    words = re.sub(u"<i data.*?></i>", u" ", section.text('he').text)
+    while u"  " in words:
+        words = words.replace(u"  ", u" ")
+    words = words.split()
     for m in results:
         data_order += 1
         if m[0] != -1:
             tag = {}
             tag["data-commentator"] = "Kereti"
-            tag["data-order"] = data_order
+            tag["data-order"] = data_order + data_order_running_count
             pos = m[0]
-            word_dict[pos] = tag
+            if pos in word_dict:
+                word_dict[pos] += [tag]
+            else:
+                word_dict[pos] = tag
         else:
             print "Didnt find match in {}".format(section)
 
     word_objs = []
     for n, word in enumerate(words):
         if n in word_dict.keys():
-            word_obj = WordObj(word, word_dict[n])
+            word_obj = WordObj(word, [word_dict[n]], before=True)
         else:
-            word_obj = WordObj(word)
+            word_obj = WordObj(word, before=True)
         word_objs.append(word_obj)
     return word_objs
 
 def get_kereti_tags():
-    sections = Ref("Shulchan Arukh, Yoreh Deah").all_segment_refs()
+    sections = Ref("Shulchan Arukh, Yoreh Deah").all_subrefs()
     i_tags_kereti = {}
     for sec in sections:
         if sec.sections[0] == 112:
             break
-        base_text = TextChunk(sec, lang='he').text
-        base_words = re.sub(u"<.*?>", u" ", base_text)
-        while u"  " in base_words:
-            base_words = base_words.replace(u"  ", u" ")
-        try:
-            results = match_text(base_words.split(), all_dhs[sec.sections[0]][sec.sections[1]])
-        except:
-            print "Problem with {}".format(sec)
-            continue
-        if sec.sections[0]-1 not in i_tags_kereti:
-            i_tags_kereti[sec.sections[0]-1] = {}
-        i_tags_kereti[sec.sections[0]-1][sec.sections[1]-1] = create_new_pieces(results["matches"], sec)
+        data_order = 0
+        if sec.sections[0] - 1 not in i_tags_kereti:
+            i_tags_kereti[sec.sections[0] - 1] = {}
+        for seg in sec.all_segment_refs():
+            base_text = TextChunk(seg, lang='he').text
+            base_words = re.sub(u"<.*?>", u" ", base_text)
+            while u"  " in base_words:
+                base_words = base_words.replace(u"  ", u" ")
+            try:
+                results = match_text(base_words.split(), all_dhs[seg.sections[0]][seg.sections[1]])
+            except Exception as e:
+                print seg
+                continue
+
+            i_tags_kereti[sec.sections[0]-1][seg.sections[1]-1] = create_new_pieces(results["matches"], seg, data_order)
+            data_order += len(results["matches"])
     return i_tags_kereti
 
 
@@ -174,13 +182,14 @@ for i in range(len(SA_word_objs)):
                 kereti_after = kereti_word_objs[i][j][n].afterStr()
                 full_word = SA_before + kereti_before + word_obj.word + SA_after + kereti_after
                 new_base_text.append(full_word)
-            base_text[i][j] = u" ".join(new_base_text)
-        except Exception as e:
+        except KeyError as e:
             print (i, j)
+
+        base_text[i][j] = u" ".join(new_base_text)
 
 text_version = {
     "text": base_text,
-    "versionTitle": "Kereti",
+    "versionTitle": "NEW Ashlei Ravrevei: Shulchan Aruch Yoreh Deah, Lemberg, 1888",
     "language": "he",
     "versionSource": "http://www.sefaria.org",
 }
