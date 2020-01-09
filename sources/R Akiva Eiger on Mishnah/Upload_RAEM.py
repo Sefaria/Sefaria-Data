577a9f4ac8115634c38c6c28140482e4680f5964
Ezradweck <edweck278@gmail.com>
2020-01-09 14:18:24 +0200

    debugged linking code and fixed. now need to check if the matches are good...

--- sources/R Akiva Eiger on Mishnah/Upload_RAEM.py
+++ sources/R Akiva Eiger on Mishnah/Upload_RAEM.py
@@ -14,12 +14,17 @@ try:
 except ImportError:  # python 3.x
     import pickle
 
+# make a list of all the comments in a perek on a specific perush (bartenura or toyt)
 def extract_specific_comments(perek, perek_index, regex, index_list):
     list_of_comments = []
     for mishna_index, mishna in enumerate(perek):
         for comment_index, comment in enumerate(mishna):
             # take the beginning of the comment
-            beginning = re.search(ur'<b>(.*)</b>', comment)
+            if re.search(ur'<b>(.*?)</b>', comment):
+                beginning = re.search(ur'<b>(.*?)</b>', comment).group(1)
+            # if no bold just take the whole comment
+            else:
+                beginning = comment
             # if the beginning of this comment references what we are looking for (bartenura or toyt)
             if re.search(regex, beginning):
                 list_of_comments.append(comment)
@@ -31,7 +36,7 @@ def extract_specific_comments(perek, perek_index, regex, index_list):
                 temp_comment_index = comment_index
                 while base_not_found:
                     temp_comment_index = temp_comment_index - 1
-                    base_beginning = re.search(ur'<b>(.*)</b>', mishna[temp_comment_index])
+                    base_beginning = re.search(ur'<b>(.*?)</b>', mishna[temp_comment_index]).group(1)
                     # if no שם or בא״ד in the comment before then we found the base
                     if not re.search(ur'(?:\u05e9\u05dd \u05d3(?:\"|\u05f4)\u05d4|\u05d1\u05d0(?:\"|\u05f4)\u05d3)', base_beginning):
                         base_not_found = False
@@ -42,24 +47,38 @@ def extract_specific_comments(perek, perek_index, regex, index_list):
                 if belongs:
                     while temp_comment_index < comment_index:
                         temp_comment_index = temp_comment_index + 1
-                        current_comment_beginning = re.search(ur'<b>(.*)</b>', mishna[temp_comment_index])
+                        current_comment_beginning = re.search(ur'<b>(.*?)</b>', mishna[temp_comment_index]).group(1)
                         if re.search(ur'\u05e9\u05dd \u05d3(?:\"|\u05f4)\u05d4', current_comment_beginning):
                             list_of_comments.append(mishna[temp_comment_index])
                             index_list.append(u'{}:{}:{}'.format(perek_index, mishna_index, temp_comment_index))
                 # else, doesnt belong, do nothing and go on to next comment
     return list_of_comments
 
-# base tokenizer
+# base tokenizer (for bartenura or toyt)
 def extract_and_split_dh(comment):
-    dh = re.search(ur'<b>(.*)\.</b>', comment)
+    # if theres bold take everything bolded
+    if re.search(ur'<b>(.*?)</b>', comment):
+        dh = re.search(ur'<b>(.*?)</b>', comment).group(1)
+    # if there isn't take until the first period
+    elif re.search(ur'(.*?)\.', comment):
+        dh = re.search(ur'(.*?)\.', comment).group(1)
+    # if theres no period take the whole thing
+    else:
+        dh = comment
     dh_split = re.split(ur' ', dh)
+    # remove וכו׳?
     return dh_split
 
+# extract dh from RAEM comment
 def dh_extract_method(comment):
     # capture the dh between ד״ה and .
-    dh = re.search(ur'\u05d3(?:\"|\u05f4)\u05d4 (.*)\.', comment)
-    # remove the וכו׳
-    dh = re.sub(ur' \u05d5\u05db\u05d5(?:\'|\u05f3)', u'', dh)
+    if re.search(ur'\u05d3(?:\"|\u05f4)\u05d4 (.*?)\.', comment):
+        dh = re.search(ur'\u05d3(?:\"|\u05f4)\u05d4 (.*?)\.', comment).group(1)
+    # if no ד״ה take until the first period
+    else:
+        dh = re.search(ur'(.*?)\.', comment).group(1)
+    # remove the וכו׳ (if there is one...)
+    dh = re.sub(ur' \u05d5?\u05db\u05d5(?:\'|\u05f3)', u'', dh)
     return dh
 
 
@@ -71,12 +90,16 @@ if __name__ == "__main__":
     for masechet in raem:
         #link perek by perek
         for perek_index, perek in enumerate(raem[masechet]):
+            #list for specific index of comments in original perek mishna breakdown (i.e. p:m:c)
             bartenura_index_list = []
             bartenura_comments_list = extract_specific_comments(perek, perek_index, ur'\u05e8\u05e2?(?:\"|\u05f4)\u05d1', bartenura_index_list)
-            match_ref(Ref(u'Bartenura on {} {}'.format(masechet, perek_index+1)), bartenura_comments_list, extract_and_split_dh(), dh_extract_method=dh_extract_method())
+            match_ref(Ref(u'Bartenura on {} {}'.format(masechet, perek_index+1)).text('he'), bartenura_comments_list, extract_and_split_dh, dh_extract_method=dh_extract_method)
             toyt_index_list = []
             toyt_comments_list = extract_specific_comments(perek, perek_index, ur'\u05ea\u05d5?\u05d9(?:\"|\u05f4)\u05d8', toyt_index_list)
-            match_ref(Ref(u'Tosafot Yom Tov on {} {}'.format(masechet, perek_index+1)), bartenura_comments_list, extract_and_split_dh(), dh_extract_method=dh_extract_method())
+            match_ref(Ref(u'Tosafot Yom Tov on {} {}'.format(masechet, perek_index+1)).text('he'), toyt_comments_list, extract_and_split_dh, dh_extract_method=dh_extract_method)
+
+    #extract_and_split_dh(Ref(u'Tosafot Yom Tov on Mishnah Tamid 1:1:1').text('he'))
+    #dh_extract_method(raem['Mishnah Tamid'][0][0][0])
 
     """
     # no yadayim and kinnim, no commentary on it
