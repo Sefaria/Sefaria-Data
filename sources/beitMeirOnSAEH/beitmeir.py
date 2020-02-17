16e28ffdeb85c4011d56ca7a7450659687023069
YishaiGlasner <ishaigla@gmail.com>
2020-02-17 19:19:27 +0200

    3 texts parsed

---
+++ sources/beitMeirOnSAEH/beitmeir.py
@@ -0,0 +1,162 @@
+import django
+django.setup()
+import urllib
+import json
+from sources import functions
+from sefaria.model.schema import *
+from sefaria.model import *
+import re
+from data_utilities.util import getGematria as gematria
+
+server = 'http://draft.sandbox.sefaria.org'
+#server = 'http://localhost:8000'
+
+functions.add_term('Beit Meir', 'בית מאיר', server=server)
+
+record = SchemaNode()
+record.add_primary_titles("Beit Meir on Shulchan Arukh, Even HaEzer", 'בית מאיר על שולחן ערוך אבן העזר')
+
+node = JaggedArrayNode()
+node.add_primary_titles('Introduction', 'הקדמה')
+node.add_structure(['Pararaph'])
+record.append(node)
+
+node = JaggedArrayNode()
+node.key = 'default'
+node.default = True
+node.add_structure(['Siman', 'Pararaph'])
+record.append(node)
+
+node = JaggedArrayNode()
+node.add_primary_titles("Seder HaGet", 'סדר הגט')
+node.add_structure(['Pararaph'])
+record.append(node)
+
+node = JaggedArrayNode()
+node.add_primary_titles("Seder Halitzah", 'סדר חליצה')
+node.add_structure(['Pararaph'])
+record.append(node)
+
+node = JaggedArrayNode()
+node.add_primary_titles("Tzal'ot HaBayit", 'צלעות הבית')
+node.add_structure(['Siman', 'Pararaph'])
+record.append(node)
+
+record.validate()
+
+index_dict = {
+    'collective_title': 'Beit Meir',
+    'title': "Beit Meir on Shulchan Arukh, Even HaEzer",
+    'categories': ["Halakhah", "Shulchan Arukh", "Commentary"],
+    'schema': record.serialize(),
+    'dependence' : 'Commentary',
+    'base_text_titles': ["Shulchan Arukh, Even HaEzer"]
+}
+
+functions.post_index(index_dict, server = server)
+
+with open('beitmeir.txt', encoding = 'utf-8') as file:
+    data = file.readlines()
+
+introduction = []
+textsim = []
+simanim = []
+get = []
+halitza = []
+tzalot = []
+main = False
+links = []
+par = 1
+preSiman = 0
+first = False
+
+for line in data:
+    line = line.replace('\n', '').strip()
+
+    if line == '':
+        continue
+
+    elif 'בית מאיר אבן העזר ' in line:
+        first = not first
+        if first:
+            continue
+        seif = ''
+        par = 1
+        if 'הקדמה' in line:
+            siman = ''
+        elif 'העזר סימן' in line:
+            if  main:
+                simanim.append(textsim)
+                textsim = []
+            siman = gematria(line.split()[-1])
+            par = 1
+            main = True
+            if siman - preSiman != 1:
+                for n in range(1, siman - preSiman):
+                    simanim.append([])
+            preSiman = siman
+        elif 'הגט' in line:
+            siman = "Seder HaGet"
+            simanim.append(textsim)
+        elif 'חליצה' in line:
+            siman = "Seder Halitzah"
+        elif 'צלעות הבית' in line:
+            siman = 'tzalot'
+            if main:
+                main = False
+                print(tzalot)
+            else:
+                tzalot.append(textsim)
+            textsim = []
+        else:
+            print('error', line)
+        continue
+
+    elif line.split()[0] == 'סעיף' and len(line.split()) == 2:
+        seif = gematria(line.split()[-1])
+        continue
+
+    else:
+        if siman == '':
+            introduction.append(line)
+        elif siman == 'Seder HaGet':
+            get.append(line)
+        elif siman == 'Seder Halitzah':
+            halitza.append(line)
+        else:
+            textsim.append(line)
+
+        if seif != '':
+            if type(siman) == str:
+                ref1 = 'Shulchan Arukh, Even HaEzer, {} {}'.format(siman, seif)
+                ref2 = 'Beit Meir on Shulchan Arukh, Even HaEzer, {} {}'.format(siman, par)
+            else:
+                ref1 = 'Shulchan Arukh, Even HaEzer {}:{}'.format(siman, seif)
+                ref2 = 'Beit Meir on Shulchan Arukh, Even HaEzer {}:{}'.format(siman, par)
+            ref = Ref(ref1).text('he').text
+            if ref == '' or ref == []:
+                print('no such seif', siman, seif)
+            else:
+                links.append({
+                "refs": [ref1, ref2],
+                "type": "Commentary",
+                "auto": True,
+                "generated_by": 'beit meir'
+                })
+        par += 1
+
+tzalot.append(textsim)
+
+
+text = [introduction, simanim, get, halitza, tzalot]
+conts = [', Introduction', '', ', Seder HaGet', ', Seder Halitzah', ", Tzal'ot HaBayit"]
+for i, j in zip(text, conts):
+    text_version = {
+        'versionTitle': "Apei Ravrevei: Shulchan Aruch Even HaEzer, Lemberg, 1886",
+        'versionSource': "http://primo.nli.org.il/primo_library/libweb/action/dlDisplay.do?vid=NLI&docId=NNL_ALEPH002097680",
+        'language': 'he',
+        'text': i
+    }
+    functions.post_text('Beit Meir on Shulchan Arukh, Even HaEzer{}'.format(j), text_version, server = server)
+
+functions.post_link(links, server = server)
---
+++ sources/tazCM/tazCM.py
@@ -0,0 +1,87 @@
+import django
+django.setup()
+import urllib
+import json
+from sources import functions
+from sefaria.model.schema import *
+from sefaria.model import *
+import csv
+
+schema = JaggedArrayNode()
+schema.add_primary_titles("Turei Zahav on Shulchan Arukh, Choshen Mishpat", "טורי זהב על שולחן ערוך חושן משפט")
+schema.add_structure(["Siman", "Paragraph"])
+schema.validate()
+
+index_dict = {
+    'collective_title': 'Turei Zahav',
+    'title': "Turei Zahav on Shulchan Arukh, Choshen Mishpat",
+    'categories': ["Halakhah", "Shulchan Arukh", "Commentary", "Turei Zahav"],
+    'schema': schema.serialize(),
+    'dependence' : 'Commentary',
+    'base_text_titles': ["Shulchan Arukh, Choshen Mishpat"]
+}
+server = 'http://draft.sandbox.sefaria.org'
+functions.post_index(index_dict, server = server)
+
+
+text = []
+links = []
+with open('taz cm.csv', newline='', encoding='utf-8') as file:
+    dictin = csv.DictReader(file)
+
+    preSiman = 0
+    simantext = []
+
+    for row in dictin:
+        if 'Zahav' not in row['Index Title']:
+            pass
+        else:
+            siman = row['Index Title'].split()[-1].split(':')[0]
+            if siman == preSiman:
+                simantext.append(row['Turei Zahav on Shulchan Arukh, Choshen Mishpat'])
+            else:
+                text.append(simantext)
+                if int(siman) - int(preSiman) != 1:
+                    for n in range(0, int(siman) - int(preSiman) - 1):
+                        text.append([])
+                simantext = [row['Turei Zahav on Shulchan Arukh, Choshen Mishpat']]
+                preSiman = siman
+
+            if row['link'] == '':
+                continue
+            if Ref(row['link']).text('he').text == [] or Ref(row['link']).text('he').text == '':
+                print('link to null, ', row['link'])
+            elif 'Beit' in row['link']:
+                links.append({
+                "refs": [row['link'] + ':1', row['Index Title']],
+                "type": "Commentary",
+                "auto": True,
+                "generated_by": 'taz cm'
+                })
+                links.append({
+                "refs": [row['link'].replace('Beit Yosef', 'Tur'), row['Index Title']],
+                "type": "Commentary",
+                "auto": True,
+                "generated_by": 'taz cm'
+                })
+            else:
+                links.append({
+                "refs": [row['link'], row['Index Title']],
+                "type": "Commentary",
+                "auto": True,
+                "generated_by": 'taz cm'
+                })
+
+
+    text.append(simantext)
+    text = text[1:]
+
+text_version = {
+    'versionTitle': "Shulhan Arukh, Hoshen ha-Mishpat, Lemberg, 1898",
+    'versionSource': "http://primo.nli.org.il/primo_library/libweb/action/dlDisplay.do?vid=NLI&docId=NNL_ALEPH002097680",
+    'language': 'he',
+    'text': text
+}
+
+functions.post_text('Turei Zahav on Shulchan Arukh, Choshen Mishpat', text_version, server = server)
+functions.post_link(links, server = server)
