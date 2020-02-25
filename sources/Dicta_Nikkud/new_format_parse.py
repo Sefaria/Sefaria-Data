9b774c5d2e5ac0b5865a0898b978276431d66e54
Jonathan <jonathan@sefaria.org>
2020-02-25 13:17:39 +0200

    Dicta Nikkud p3 adaptations

--- sources/Dicta_Nikkud/new_format_parse.py
+++ sources/Dicta_Nikkud/new_format_parse.py
@@ -5,8 +5,8 @@ Had to rewrite the parsing code to account for a new data format that was sent i
 """
 
 import re
-import codecs
-import argparse
+import sys
+from functools import partial
 from sefaria.utils.talmud import section_to_daf
 from parse_nikkud import create_section, prettify_text
 from data_utilities.ParseUtil import directed_run_on_list
@@ -16,27 +16,24 @@ django.setup()
 from sefaria.model import *
 
 
-def identify_daf(daf_line):
-    """
-    :param unicode daf_line:
-    :return: int
-    """
-    match = re.search(ur'\u05d3\u05e3 [\u05d0-\u05ea]{1,3}[.:]', daf_line)
+def identify_daf(book: str, daf_line: str) -> int:
+    he_book = Ref(book).he_book()
+    match = re.search(r'\u05d3\u05e3 [\u05d0-\u05ea]{1,3}[.:]', daf_line)
     if match:
         daf_data = match.group(0)
-        return Ref(u'{} {}'.format(u'ברכות', daf_data)).sections[0]
+        return Ref(f'{he_book} {daf_data}').sections[0]
     else:
         return False
 
 
 def prepare_file(filename):
     current_section, sections = [], []
-    with codecs.open(filename, 'r', 'utf-8') as fp:
+    with open(filename) as fp:
         for line in fp:
-            line = line.rstrip()
-            if re.search(ur'^\*', line):  # start of new section
+            line = line.rstrip().lstrip()
+            if re.search(r'^\*', line):  # start of new section
                 if current_section:
-                    sections.append(u' '.join(current_section))
+                    sections.append(' '.join(current_section))
                     current_section = []
                 sections.append(line)
             else:
@@ -44,24 +41,31 @@ def prepare_file(filename):
 
         # final section
         if current_section:
-            sections.append(u' '.join(current_section))
+            sections.append(' '.join(current_section))
 
     return sections
 
 
-def parse_file(filename):
+def parse_file(tractate):
+    filename = f'{tractate}.txt'
+    identify_daf_by_book = partial(identify_daf, tractate)
     preparsed = prepare_file(filename)
-    parser = directed_run_on_list(identify_daf, include_matches=False, one_indexed=False)
-    section_list = [item[0] if item else u'' for item in parser(preparsed)]
+    parser = directed_run_on_list(identify_daf_by_book, include_matches=False, one_indexed=False)
+    section_list = [item[0] if item else '' for item in parser(preparsed)]
     for section_index, section in enumerate(section_list):
         if not section:
             continue
-        tref = u'Berakhot {}'.format(section_to_daf(section_index))
-        print tref
-        dicta_vtitle = 'William Davidson Edition - Vocalized Aramaic2'
+        tref = f'{tractate} {section_to_daf(section_index)}'
+        print(tref)
+        dicta_vtitle = 'William Davidson Edition - Vocalized Aramaic'
         create_section(Ref(tref), section, dicta_vtitle)
         prettify_text(tref, dicta_vtitle)
 
 
-
-parse_file('Berakhot.txt')
+if __name__ == '__main__':
+    try:
+        book_title = sys.argv[1]
+    except IndexError:
+        print("Please add name of Tractate")
+        sys.exit(0)
+    parse_file(book_title)
