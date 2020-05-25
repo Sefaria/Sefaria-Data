3a1179029e318981b46d9ca48ca9b9c33aaa0298
shaneerosen <shanee@sefaria.org>
2020-05-25 15:23:26 +0300

    script to change SA and MB citation in the Peninei Halakhah

---
+++ sources/Peninei Halacha/citation_testing.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+import django
+django.setup()
+from sefaria.model import *
+import requests
+import regex as re
+import csv
+import codecs
+from sources.functions import post_text
+
+SEFARIA_SERVER = "https://topicside.cauldron.sefaria.org"
+
+# Shulchan Arukh
+sa_re = re.compile('\([^()]*?(שו"ע)(?!\s*(או"ח|יו"ד|אב"ה|חו"מ|אה"ע))[^()]*?\)')
+
+def retrieve_segments(title, server=None):
+    ind = library.get_index(title)
+    sections = ind.all_section_refs()
+    segments = []
+    for sec in sections:
+        if server:
+            sec_text = requests.get("{}/api/texts/{}?context=0"
+                                    .format(server, sec.normal())).json()["he"]
+
+        else:
+            sec_text = sec.text('he').text
+        segments.append((sec, sec_text))
+    return segments
+
+def add_to_SA(section_tuples, string_to_add_before = "", post=False, server='local'):
+    rows = []
+    for (sec, sec_text) in section_tuples:
+        new_sec_text = []
+        for jseg, text in enumerate(sec_text):
+            subs = []
+            row = {}
+            # matchs = re.findall('(שו"ע) (ורמ"א)? (^)', text)
+            matchs = re.finditer(sa_re, text)
+            for match in matchs:
+                to_change = match.group()
+                new_subd = False
+                if re.search('רמ"א', match.group()):
+                    to_change = re.sub('שו"ע\s*?(?P<vav>ו?)(רמ"א)', 'רמ"א \g<vav>שו"ע', match.group())
+                    if not re.search(sa_re, to_change):
+                        new_sub = to_change
+                        new_subd=True
+                if not new_subd:
+                    new_sub = re.sub('שו"ע', 'שו"ע {}'.format(string_to_add_before), to_change)
+                subs.append((match.group(), new_sub))
+                row['ref'] = Ref("{}:{}".format(sec.normal(), jseg+1)).normal()
+                row['old'] = match.group()
+                row['new'] = new_sub
+                rows.append(row.copy())
+            new_text = text
+            for sub in subs:
+                new_text = re.sub(sub[0], sub[1], new_text)
+                print("{}:{}".format(sec.normal(), jseg+1))
+                print(new_text)  # post back to server here
+            new_sec_text.append(new_text)
+        if post:
+            post_to_server(sec, -1, new_sec_text, server)
+        # post_to_server(sec, jseg, new_text, 'server')
+    return rows
+
+
+def write_to_csv(rows, filename):
+    with codecs.open('{}.csv'.format(filename), 'w') as csv_file:
+        writer = csv.DictWriter(csv_file, ['ref', 'old', 'new'])  # fieldnames = obj_list[0].keys())
+        writer.writeheader()
+        writer.writerows(rows)
+
+def post_to_server(sec, jseg, new_text, post = 'local', text_version='', vtitle="Peninei Halakhah, Yeshivat Har Bracha"):
+    if post == 'server':  # from local settings
+        topost = {
+            'versionTitle': "Peninei Halakhah, Yeshivat Har Bracha",
+            'versionSource': "https://ph.yhb.org.il",
+            'language': 'he',
+            'text': new_text
+        }
+        post_ref = "{}.{}".format(sec.normal(), jseg + 1) if jseg + 1 else "{}".format(sec.normal())
+        post_text(post_ref, topost) #  server=SEFARIA_SERVER
+    elif post == 'local':
+        tc = TextChunk(Ref("{}.{}".format(sec.normal(), jseg + 1)), lang='he',
+                       vtitle=vtitle)
+        tc.text = new_text
+        tc.save(force_save=True)
+
+if __name__ == "__main__":
+    section_tuples = retrieve_segments('Peninei Halakhah, Prayer')
+    rows = add_to_SA(section_tuples, string_to_add_before='או"ח')
+    write_to_csv(rows, '/home/shanee/www/Sefaria-Data/sources/Peninei Halacha/Prayer_och')
+    # post_to_server(Ref(rows[0]['ref']), rows[0]['new'], post='local')
