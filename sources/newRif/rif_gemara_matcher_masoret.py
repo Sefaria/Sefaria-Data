0297d990b18d5b9d5edabc28117784f889aa0dd7
YishaiGlasner <ishaigla@gmail.com>
2020-07-06 17:17:55 +0300

    new matcher

--- sources/newRif/rif_gemara_matcher_masoret.py
+++ sources/newRif/rif_gemara_matcher_masoret.py
@@ -1,6 +1,7 @@
 import re
 import csv
 import copy
+import json
 import django
 django.setup()
 from functools import partial
@@ -12,55 +13,101 @@ from sefaria.system.exceptions import InputError
 def base_tokenizer(string, masechet):
     return remove_metadata(string, masechet).split()
 
+def check_ref(string, masechet):
+    string = re.sub('במכילתין|לקמן|לעיל|ע"ש|מכלתין|מכילתין|כאן|[\(\)\[\]]', '', string)
+    try:
+        Ref(string)
+        if string.strip() != 'דף': return string, 'just ref'
+        else: return None
+    except (InputError, AttributeError):
+        try:
+            Ref(masechet+' '+string)
+            return masechet + ' ' + string, 'just ref'
+        except InputError:
+            return None
+
+def check_note_and_ref(string, masechet):
+    try: #maybe first 2 words are ref and the other is note
+        Ref(' '.join([masechet]+string.split()[:2]))
+        return ' '.join([masechet] + string.split()[:2]), 'ref and note'
+    except InputError:
+        return None
+
+ref_and_note_tag = '%@%'
 for masechet in list(tags_map):
     heb_masechet = get_hebrew_masechet(masechet)
     data = []
-    report = []
+    links = []
+    new_data = []
     current_ref = 'דף ב.'
     splitted_data = {current_ref: []}
     matcher = ParallelMatcher(partial(base_tokenizer, masechet=masechet), verbose=False)
+    ref_tag = tags_map[masechet]['gemara_refs']
+    note_tag = tags_map[masechet]['notes']
+    end_tag = tags_map[masechet]['end_tag']
 
     with open(path+'/rif_csv/rif_'+masechet+'.csv', encoding='utf-8', newline='') as file:
         data = list(csv.DictReader(file))
 
-    ref_tag = tags_map[masechet]['gemara_refs']
     for row in data:
-        row['content'] = row['content'].replace('\ufeff', '')
-        if row['content'] == '': continue
-        while ref_tag in row['content']:
-            if row['content'].index(ref_tag) != 0:
-                newrow = copy.copy(row)
-                newrow['content'], row['content'] = newrow['content'].split(ref_tag, 1)
-                if remove_metadata(newrow['content'], masechet) != '':
-                    splitted_data[current_ref].append(newrow)
-            else:
-                row['content'] = row['content'][3:]
-            print(row['content'])
-            current_ref, row['content'] = row['content'].split(tags_map[masechet]['end_tag'], 1)
-            if current_ref not in splitted_data: splitted_data[current_ref] = []
-        print(masechet, row['content'])
-        splitted_data[current_ref].append(row)
+        row['content'] = re.sub(r'[\ufeff\n]', '', row['content'])
+        if row['content'] == '':
+            print('empty row in', row['page.section'])
+            continue
 
-    for page in splitted_data:
-        try:
-            Ref(heb_masechet+' '+page).text('he')
-            gemara_text = heb_masechet + ' ' + page
-        except InputError:
-            print(heb_masechet+' '+page+' didnt find')
-        for section in splitted_data[page]:
+        if note_tag in row['content']: #notes tag can be refs to another masechet. in that case, this replaceing the tag
+            for note in re.findall(note_tag+'([^@<]*)'+end_tag, row['content']):
+                if check_ref(note, heb_masechet):
+                    row['content'] = row['content'].replace(note_tag+note, ref_tag+note)
+                elif check_note_and_ref(note, heb_masechet):
+                    row['content'] = row['content'].replace(note_tag+note, ref_and_note_tag+note)
+                    print('note and ref', masechet, note)
+
+        newrow = copy.copy(row)
+        row['content'] = re.sub(ref_tag+'([^@<]*)'+end_tag, r' (\1) ', row['content'])
+        row['content'] = re.sub(note_tag+'|'+ref_and_note_tag+'([^@<]*)'+end_tag, r'<sup>*</sup><i class="footnote">\1</i>', row['content'])
+        row['content'] = row['content'].replace(' </i>', '</i> ')
+        new_data.append(row)
+
+        with_ref_tag = ref_tag + '|' + ref_and_note_tag
+        while re.search(with_ref_tag, newrow['content']):
+            if re.search(with_ref_tag, newrow['content']).start() != 0:
+                newrow0 = copy.copy(newrow)
+                newrow0['content'], newrow['content'] = re.split(with_ref_tag, newrow['content'], 1)
+                if remove_metadata(newrow0['content'], masechet) != '':
+                    splitted_data[current_ref].append(newrow0)
+            else:
+                newrow['content'] = re.sub(with_ref_tag, '', newrow['content'], 1)
+            current_ref, newrow['content'] = newrow['content'].split(end_tag, 1)
+            if current_ref not in splitted_data: splitted_data[current_ref] = []
+        splitted_data[current_ref].append(newrow)
+
+    for ref in splitted_data:
+        if check_ref(ref, heb_masechet):
+            gemara_text = check_ref(ref, heb_masechet)
+        elif check_note_and_ref(ref, heb_masechet):
+            gemara_text = check_note_and_ref(ref, heb_masechet)
+        else:
+            print(heb_masechet, ref, 'didnt find')
+
+        for section in splitted_data[ref]:
             if netlen2(section['content'], masechet) > 4:
                 tref_list = [gemara_text, (section['content'], 'rif')]
                 match_list = matcher.match(tref_list, return_obj=True)
-                if match_list == []:
-                    report.append({'ref': '', 'gemara': '', 'rif': section['content']})
-                else:
-                    match_list = [item.a.ref for item in match_list]
-                    match_list = unite_ref(match_list)
-                    for ref in match_list:
-                        report.append({'ref': ref, 'gemara': Ref(ref).text('he').text, 'rif': section['content']})
+                match_list = [item.a.ref if item.a.mesechta!='rif' else item.b.ref for item in match_list]
+                match_list = unite_ref(match_list)
+                for item in match_list:
+                    links.append([{
+                    "refs": ["Rif {} {}".format(masechet, section['page.section']), item],
+                    "type": "Commentary",
+                    "auto": True,
+                    "generated_by": 'rif gemara matchet'
+                    }])
 
-    with open(path+'/gemara_links/{}-masoret.csv'.format(masechet), 'w', newline='', encoding='utf-8') as fp:
-        awriter = csv.DictWriter(fp, fieldnames=['ref', 'gemara', 'rif'])
+    with open(path+'/gemara_links/{}.json'.format(masechet), 'w') as fp:
+        json.dump(links, fp)
+
+    with open(path+'/rif_gemara_refs/rif_{}.csv'.format(masechet), 'w', encoding='utf-8', newline = '') as file:
+        awriter = csv.DictWriter(file, fieldnames=['page.section', 'content'])
         awriter.writeheader()
-        for item in report:
-            awriter.writerow(item)
+        for item in new_data: awriter.writerow(item)
