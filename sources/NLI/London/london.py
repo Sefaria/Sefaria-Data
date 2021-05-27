5ac5243deca3b275a0f7d428d91a4404d5192318
Jonathan <jonathan@sefaria.org>
2021-05-27 11:02:21 +0300

    london changes

--- sources/NLI/London/london.py
+++ sources/NLI/London/london.py
@@ -1,8 +1,19 @@
+# encoding=utf-8
+
+# from concurrent.futures.thread import ThreadPoolExecutor
 from PIL import Image
 import requests
 import time
-import io
 import sys
+import io
+import os
+import re
+
+COMPLETED = os.environ['COMPLETED']
+EXPORT = os.environ['EXPORT']
+STOP = os.environ['STOP']
+
+# COMPLETED, EXPORT, STOP = ['foo', 'bar', 'baz']
 
 def combine_tiles_1d(images, dimension, id_name=None):
     widths, heights = zip(*(i.size for i in images))
@@ -34,22 +45,60 @@ def combine_tiles_2d(tiles_array, id_name=None):
     return combine_tiles_1d(rows, 'height', id_name)
 
 def download_london_tiles(page):
+    def download(url):
+        r = requests.request('get', url)
+        while r.status_code == 429:
+            time.sleep(1)
+            print('got error, retrying')
+            r = requests.request('get', url)
+        return r.content
+
     tiles = [[] for _ in range(25)]
+    # tiles = []
     for m in range(25):
+        # print('downloading row', m)
+        # url_list = [f'http://www.bl.uk/manuscripts/Proxy.ashx?view=add_ms_27296_f{page}_files/13/{n}_{m}.jpg' for n in range(19)]
+        # with ThreadPoolExecutor() as executor:
+        #     responses = executor.map(download, url_list)
+        # tiles.append([Image.open(io.BytesIO(response.content)) for response in responses])
         for n in range(19):
+            print('downloading tile', page, n, m)
             r = requests.request('get', f'http://www.bl.uk/manuscripts/Proxy.ashx?view=add_ms_27296_f{page}_files/13/{n}_{m}.jpg')
             while r.status_code == 429:
                 time.sleep(1)
+                print('got error, retrying')
                 r = requests.request('get', f'http://www.bl.uk/manuscripts/Proxy.ashx?view=add_ms_27296_f{page}_files/13/{n}_{m}.jpg')
             tiles[m].append(Image.open(io.BytesIO(r.content)))
     return tiles
 
 if __name__ == '__main__':
+    if os.path.exists(f'{COMPLETED}/completed.txt'):
+        with open(f'{COMPLETED}/completed.txt') as fp:
+            completed = (re.search(r'[^/]+$', r.rstrip()) for r in fp)
+            completed = set(r.group() for r in completed if r)
+            print(*completed, sep='\n')
+    else:
+        completed = set()
+    if len(sys.argv) > 1:
+        page_limit = sys.argv[1]
+    else:
+        page_limit = None
     for i in range(3, 74):
+        if page_limit and i != page_limit:
+            print('skipping page', i)
         for side in ['v', 'r']:
+            file_name = f'tosefata_london_{i}{side}.jpg'
+            if file_name in completed:
+                print(file_name, 'already downloaded, skipping')
+                continue
+            else:
+                print('downloading', file_name)
+            thumbnail_name = file_name.replace('.jpg', '_thumbnail.jpg')
             page = f'{str(i).zfill(3)}{side}'
             tiles = download_london_tiles(page)
             page_image = combine_tiles_2d(tiles, page)
-            page_image.save(f'./london_images/tosefata_london_{i}{side}.jpg')
+            page_image.save(f'{EXPORT}/{file_name}')
             page_image.thumbnail((256, 278))
-            page_image.save(f'./london_images/tosefata_london_{i}{side}_thumbnail.jpg')
+            page_image.save(f'{EXPORT}/{thumbnail_name}')
+    with open(f'{STOP}/stop', 'w') as fp:
+        fp.write('')
