7bdca581565026974ee26360d6f6e7106af0fa88
shaneerosen <shanee@sefaria.org>
2021-07-11 00:42:15 -0400

    added citation embed based on prefix quotation words option

--- research/quotation_finder/citation_inserts.py
+++ research/quotation_finder/citation_inserts.py
@@ -13,7 +13,7 @@ db_qf = client.quotations
 # paste in the pasukRef in normal hebrew.
 
 dummy_char = "█"
-
+only_prefixed = True
 
 def get_links_for_citation_insert(ref, score, link_source):
     if isinstance(link_source, dict):  # link_source == dict
@@ -45,26 +45,41 @@ def get_tc(tref, from_file=None):
     return seg_text
 
 
-def get_segment(ref, score=22, link_source=None):
-    seg_text = get_tc(ref.normal())
+def get_segment(ref, score=22, link_source=None, from_file = None, char_range=None):
+    seg_text = get_tc(ref.normal(), from_file=from_file)
     seg_text_list = list(re.sub('\s+', dummy_char, seg_text))
+    # pat = re.compile('^(<.*?>)(.*?)(<.*?>)')
+    # html_dh = re.match(pat, seg_text)
+    move=0
+    # if html_dh:
+    #     seg_text_list = list(re.sub('\s+', dummy_char, re.sub(pat, html_dh.group(2), seg_text)))
+    #     move = len(html_dh.groups(1))+len(html_dh.groups(3))
     lls = get_links_for_citation_insert(ref, score, link_source)
     if lls:
-        text_w_citations = add_citations(lls, seg_text_list, ref.normal())
+        text_w_citations = add_citations(lls, seg_text_list, ref.normal(), char_range=char_range)
         # print(f"check {link2url(lls[0], sandbox='quotations')}")
     else:
         return {}
     return {ref.normal(): text_w_citations}
 
+
 def get_place(link, seg_text_list):
     place = link.charLevelData[1]['endChar']
     search_here = ''.join(seg_text_list[place:min(place+10, len(seg_text_list))]).replace(dummy_char, ' ')
-    match_gomer = re.match(""".*וגו('|")?""", search_here)
+    match_gomer = re.match(""".*?(וגו|וכו)('|"|מר)?.*?""", search_here)
     if match_gomer:
         move = len(match_gomer.group())
         place = place + move
     return place
 
+
+def sheneemar(link, seg_text_list, char_range=10):
+    place = link.charLevelData[1]['startChar']
+    search_here = ''.join(seg_text_list[max(place-char_range, 0):place+1]).replace(dummy_char, ' ')
+    match_sheneemar = re.match(""".*?(וזהו|ש?נ?אמר|(ד|ו)?כתיב|ו?כתוב|ו?הדר).*?""", search_here)
+    if match_sheneemar:
+        return True
+    return False
 
 def test_word_char_level(link, citation):
     pasuk_chars = list(re.sub('\s+', dummy_char, Ref(link.refs[0]).text('he').text))
@@ -74,6 +89,13 @@ def test_word_char_level(link, citation):
     words = ''.join(words_list)
     words = words.replace(dummy_char, ' ')
     citation = f'{citation}<sup>{words}</sup>'
+
+    #test words in base text
+    #
+    # start = l.charLevelData[1]['startChar']
+    # end = l.charLevelData[1]['endChar']
+    # words = ''.join(seg_text_list[start:end]).replace(dummy_char, " ")
+    # citation = f'{citation}<sup>{words}</sup>'
     return citation
 
 
@@ -90,9 +112,9 @@ def get_citation(link, color_score=None):
     #     elif color_score[2] <= link.score:
     #         color = 3  # blue
     #     else:
-    #         return []
+    #         return ''
     #     citation = f'<span class="ref-link-color-{color}">{citation}</span>'
-    #     # citation = f'<sup><span class="ref-link-color-{color}">*</span></sup><i class="footnote">{citation}</i>'
+        # citation = f'<sup><span class="ref-link-color-{color}">*</span></sup><i class="footnote">{citation}</i>'
     # citation = test_word_char_level(link, citation)
     return citation
 
@@ -146,7 +168,7 @@ def to_be_embedded(ls, lls, seg_text_list, book_ref):
     return left_links, text_to_delete
 
 
-def add_citations(lls, seg_text_list, book_ref):
+def add_citations(lls, seg_text_list, book_ref, char_range=None):
     """
 
     :param lls: list of the links to be added in inline citations. (according to links that were created by quotation_finder)
@@ -161,6 +183,7 @@ def add_citations(lls, seg_text_list, book_ref):
     for l in trimmed_lls:
         # if l.charLevelData[1]['startChar'] <= 10 or (hasattr(l, 'dh') and l.dh):  # check for DH
         if l.type == 'dibur_hamatchil':
+            post_link(l.contents())
             continue
         if Ref(l.refs[1]).book != Ref(book_ref).book:
             print("needed reverse")
@@ -171,9 +194,19 @@ def add_citations(lls, seg_text_list, book_ref):
         # seg_citation_list = get_place_citation(l, color_score=[22, 30, 50])
         place = get_place(l, seg_text_list)
         citation = get_citation(l, color_score=[22,30,50])
+
+        prefixed = sheneemar(l, seg_text_list, char_range=char_range)
+        if prefixed:
+            db_qf.quotations.update_one({"charLevelData": f"{l.charLevelData}", "refs": f"{l.refs}"},
+                                    {"$set": {"prefixed": True}})
+        if only_prefixed and not prefixed and not hasattr(l, "prefixed"):
+            continue
         seg_citation_list = [(place, citation)]
         citation_list.extend(seg_citation_list)
     citation_list.sort()
+    # if move and citation_list:
+    #     citation_list_n = [(cl[0]+move, cl[1]) for cl in citation_list]
+    #     citation_list = citation_list_n
     for p, c in citation_list:
         place = p+cnt
         seg_text_list.insert(place, c)
@@ -212,10 +245,10 @@ def order_links_by_segments(links): #, base_book_title):
 
 if __name__ == '__main__':
     new_texts_dict = dict()
-    range_ref = Ref('Yalkut Shimoni on Torah 907:5')  # 'Yalkut_Shimoni_on_Torah.783.3'
+    range_ref = Ref('Tzror HaMor on Torah, Genesis 1:26')  # 'Yalkut_Shimoni_on_Torah.783.3'
     # links = get_links_from_file('Ramban_on_Numbers.31.23.json')
     # link_dict = order_links_by_segments(links)  #,"Selichot_Nusach_Ashkenaz_Lita")
     for r in range_ref.all_segment_refs():
-        new_texts_dict.update(get_segment(r, score=25, link_source='quotation_DB'))
+        new_texts_dict.update(get_segment(r, score=20, link_source='quotation_DB', char_range=30)) #from_file=get_links_from_file("Tzror_HaMor_on_Torah.json"), char_range=30))
         # new_texts_dict.update(get_local_seg(r))
     push_text_w_citations(range_ref.text('he').version(), new_texts_dict)
