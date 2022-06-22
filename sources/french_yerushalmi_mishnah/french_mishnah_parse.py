23425f07ff71f99e947ca818db9cc11273f8534b
saengel <sarah@sefaria.org>
2022-06-22 16:45:07 +0300

    Add manager class

--- sources/french_yerushalmi_mishnah/french_mishnah_parse.py
+++ sources/french_yerushalmi_mishnah/french_mishnah_parse.py
@@ -32,142 +32,51 @@ class MishnahRow:
             "yerushalmi_mishnah_text": self.yerushalmi_mishnah_text
         }
 
-    # Accessors and Mutators
-    def set_mishnah_tref(self, new_tref):
-        self.mishnah_tref = new_tref
-
-    def set_talmud_tref(self, new_tref):
-        self.talmud_tref = new_tref
-
-    def set_mishnah_mishnah_text(self, new_text):
-        self.mishnah_mishnah_text = new_text
-
-    def set_yerushalmi_mishnah_text(self, new_text):
-        self.yerushalmi_mishnah_text = new_text
-
-    def get_mishnah_tref(self):
-        return self.mishnah_tref
-
-    def get_talmud_tref(self):
-        return self.talmud_tref
-
-    def get_mishnah_mishnah_text(self):
-        return self.mishnah_mishnah_text
-
-    def get_yerushalmi_mishnah_text(self):
-        return self.yerushalmi_mishnah_text
-
 
 class FrenchMishnahManager:
 
     def __init__(self):
-        pass
+        self.mishnah_list = []
+        self.cleaned_mishnah_list = []  # for processing
+        self.ym_list = []  # list of Yerushalmi mishnahs
 
+    def run(self):
+        print("Parsing French Mishnah")
+        self.parse_french_mishnah()
+        print("Post Processing the Data")
+        self.post_processing_data()
+        print("Sorting the Data")
+        self.mishnah_list.sort(key=lambda x: Ref(x["mishnah_tref"]).order_id())
+        print("Writing to a CSV")
+        self.generate_csv()
 
-# Todo - create a utility file?
+    def parse_french_mishnah(self):
+        ls = LinkSet({"generated_by": "yerushalmi-mishnah linker"})
 
-# This function generates a CSV given a list of dicts
-def generate_csv(dict_list, headers, file_name):
-    with open(f'{file_name}.csv', 'w+') as file:
-        c = csv.DictWriter(file, fieldnames=headers)
-        c.writeheader()
-        c.writerows(dict_list)
-    print(f"File writing of {file_name} complete")
+        for link in ls:
+            refs = link.refs
+            mishnah_ref, talmud_ref = refs if "Mishnah" in refs[0] else reversed(refs)
+            french_text = get_french_text(Ref(talmud_ref))
+            mm_text = get_hebrew_text(Ref(mishnah_ref), type="mishnah-mishnah")
+            ym_text = get_hebrew_text(Ref(talmud_ref), type="yerushalmi-mishnah")
+            self.mishnah_list.append(MishnahRow(mishnah_ref, talmud_ref, mm_text, ym_text).get_row())
 
+    def post_processing_data(self):
+        print("parsing single refs")
+        self.mishnah_list = handle_duplicated_single_ref(self.mishnah_list)
+        print("parsing ranged refs")
+        self.mishnah_list = process_ranged_ref(self.mishnah_list)
 
-def clean_text(raw_text):
-    raw_text = str(raw_text)
-    text_array = re.sub(r"\[|\]|\{|\}", "", raw_text)
-    return text_array
-
-
-def get_numbers_from_yerushalmi_tref(y_tref):
-    result = re.findall(r".* (\d*:\d*:\d*)$", y_tref)
-    if result:
-        return result[0]
-    else:
-        return y_tref
-
-
-def create_list_yerushalmi_masechtot():
-    yerushalmi_masechtot = []
-    talmud_indices = library.get_indexes_in_category("Yerushalmi")
-    for index in talmud_indices:
-        masechet = re.findall(r"Jerusalem Talmud (.*?)$", index)
-        yerushalmi_masechtot.append(masechet[0])
-    return yerushalmi_masechtot
-
-
-# This function creates a list of mishnah trefs, and extracts any masechtot
-# that are not also found in the Yerushalmi
-def create_list_of_yerushalmi_mishnah_trefs():
-    full_tref_list = []
-
-    # Clean out non-Yerushalmi references
-    talmud_indices = library.get_indexes_in_category("Yerushalmi", full_records=True)
-    for index in talmud_indices:
-        mishnah_refs = index.all_segment_refs()
-        for mishnah in mishnah_refs:
-            mishnah_tref = mishnah.normal()
-            first_segment = re.match(r".*:1$", mishnah_tref)
-
-            # First segment is usually the Mishnah text
-            if first_segment:
-                full_tref_list.append(mishnah_tref)
-    return full_tref_list
-
-
-# This function creates a list of mishnah trefs, and extracts any masechtot
-# that are not Yerushalmi
-def create_list_of_mishnah_mishnah_trefs():
-    mishnah_indices = library.get_indexes_in_category("Mishnah", full_records=True)
-    full_tref_list = []
-    yerushalmi_mishnah_mishnah_refs = []
-    for index in mishnah_indices:
-        mishnah_refs = index.all_segment_refs()
-        for mishnah in mishnah_refs:
-            full_tref_list.append(mishnah.normal())
-    yerushalmi_masechtot = create_list_yerushalmi_masechtot()
-
-    for mishnah in full_tref_list:
-        mishnah_masechet = Ref(mishnah).index.title
-        if mishnah_masechet in yerushalmi_masechtot:
-            yerushalmi_mishnah_mishnah_refs.append(mishnah)
-    return yerushalmi_mishnah_mishnah_refs
-
-
-def get_french_text(talmud_ref):
-    french_text = talmud_ref.text('en', vtitle='Le Talmud de Jérusalem, traduit par Moise Schwab, 1878-1890 [fr]')
-    french_text = french_text.text
-    french_text = clean_text(french_text)
-    return french_text
-
-
-def get_hebrew_text(ref, type='yerushalmi-mishnah'):
-    if type == 'yerushalmi-mishnah':
-        heb_text = ref.text('he', vtitle='Mechon-Mamre')
-        heb_text = heb_text.text
-    elif type == 'mishnah-mishnah':
-        heb_text = ref.text('he', vtitle='Torat Emet 357')
-        heb_text = heb_text.text
-        heb_text = heb_text[:-1]
-
-    return heb_text
-
-
-def parse_french_mishnah():
-    mishnah_list = []
-
-    ls = LinkSet({"generated_by": "yerushalmi-mishnah linker"})
-
-    for link in ls:
-        refs = link.refs
-        mishnah_ref, talmud_ref = refs if "Mishnah" in refs[0] else reversed(refs)
-        french_text = get_french_text(Ref(talmud_ref))
-        mm_text = get_hebrew_text(Ref(mishnah_ref), type="mishnah-mishnah")
-        ym_text = get_hebrew_text(Ref(talmud_ref), type="yerushalmi-mishnah")
-        mishnah_list.append(MishnahRow(mishnah_ref, talmud_ref, mm_text, ym_text).get_row())
-    return mishnah_list
+    def generate_csv(self):
+        headers = ['mishnah_tref',
+                   'talmud_tref',
+                   'mishnah_mishnah_text',
+                   'yerushalmi_mishnah_text']
+        with open('french_mishnah.csv', 'w+') as file:
+            c = csv.DictWriter(file, fieldnames=headers)
+            c.writeheader()
+            c.writerows(self.mishnah_list)
+        print(f"File writing of french mishnah complete")
 
 
 def get_yerushalmi_linkset_mishnahs():
@@ -282,26 +191,95 @@ def process_ranged_ref(mishnah_list):
     return cleaned_mishnah_list
 
 
-# Todo - processors - part of the manager class
-def post_processing_data(mishnah_list):
-    print("parsing single refs")
-    mishnah_list = handle_duplicated_single_ref(mishnah_list)
-    print("parsing ranged refs")
-    mishnah_list = process_ranged_ref(mishnah_list)
-    return mishnah_list
+# # # # Utility Functions # # # #
+def create_normalized_ref_list(mishnah):
+    refs = Ref(mishnah['mishnah_tref']).range_list()
+    refs_normal = []
+    for ref in refs:
+        refs_normal.append(ref.normal())
+    return refs_normal
 
 
-def generate_linkset_validation_csv():
-    mishnah_list = parse_french_mishnah()
-    print("parsed mishnah")
-    print("beginning to parse the data")
-    mishnah_list = post_processing_data(mishnah_list)
-    mishnah_list.sort(key=lambda x: Ref(x["mishnah_tref"]).order_id())
-    generate_csv(mishnah_list, ['mishnah_tref',
-                                'talmud_tref',
-                                'mishnah_mishnah_text',
-                                'yerushalmi_mishnah_text'], 'french_mishnah_list')
+def clean_text(raw_text):
+    raw_text = str(raw_text)
+    text_array = re.sub(r"\[|\]|\{|\}", "", raw_text)
+    return text_array
+
+
+def get_numbers_from_yerushalmi_tref(y_tref):
+    result = re.findall(r".* (\d*:\d*:\d*)$", y_tref)
+    if result:
+        return result[0]
+    else:
+        return y_tref
+
+
+def create_list_yerushalmi_masechtot():
+    yerushalmi_masechtot = []
+    talmud_indices = library.get_indexes_in_category("Yerushalmi")
+    for index in talmud_indices:
+        masechet = re.findall(r"Jerusalem Talmud (.*?)$", index)
+        yerushalmi_masechtot.append(masechet[0])
+    return yerushalmi_masechtot
+
+
+# This function creates a list of mishnah trefs, and extracts any masechtot
+# that are not also found in the Yerushalmi
+def create_list_of_yerushalmi_mishnah_trefs():
+    full_tref_list = []
+
+    # Clean out non-Yerushalmi references
+    talmud_indices = library.get_indexes_in_category("Yerushalmi", full_records=True)
+    for index in talmud_indices:
+        mishnah_refs = index.all_segment_refs()
+        for mishnah in mishnah_refs:
+            mishnah_tref = mishnah.normal()
+            first_segment = re.match(r".*:1$", mishnah_tref)
+
+            # First segment is usually the Mishnah text
+            if first_segment:
+                full_tref_list.append(mishnah_tref)
+    return full_tref_list
+
+
+# This function creates a list of mishnah trefs, and extracts any masechtot
+# that are not Yerushalmi
+def create_list_of_mishnah_mishnah_trefs():
+    mishnah_indices = library.get_indexes_in_category("Mishnah", full_records=True)
+    full_tref_list = []
+    yerushalmi_mishnah_mishnah_refs = []
+    for index in mishnah_indices:
+        mishnah_refs = index.all_segment_refs()
+        for mishnah in mishnah_refs:
+            full_tref_list.append(mishnah.normal())
+    yerushalmi_masechtot = create_list_yerushalmi_masechtot()
+
+    for mishnah in full_tref_list:
+        mishnah_masechet = Ref(mishnah).index.title
+        if mishnah_masechet in yerushalmi_masechtot:
+            yerushalmi_mishnah_mishnah_refs.append(mishnah)
+    return yerushalmi_mishnah_mishnah_refs
+
+
+def get_french_text(talmud_ref):
+    french_text = talmud_ref.text('en', vtitle='Le Talmud de Jérusalem, traduit par Moise Schwab, 1878-1890 [fr]')
+    french_text = french_text.text
+    french_text = clean_text(french_text)
+    return french_text
+
+
+def get_hebrew_text(ref, type='yerushalmi-mishnah'):
+    if type == 'yerushalmi-mishnah':
+        heb_text = ref.text('he', vtitle='Mechon-Mamre')
+        heb_text = heb_text.text
+    elif type == 'mishnah-mishnah':
+        heb_text = ref.text('he', vtitle='Torat Emet 357')
+        heb_text = heb_text.text
+        heb_text = heb_text[:-1]
+
+    return heb_text
 
 
 if __name__ == "__main__":
-    generate_linkset_validation_csv()
+    manager = FrenchMishnahManager()
+    manager.run()
