470a347b90365060cda1a210084f5d7da7092699
saengel <sarah@sefaria.org>
2022-06-28 10:34:12 +0300

    Minor clean up - renumbering footnotes by chapter, and removing superscript roman numerals

--- sources/Goldschmidt German Mishnah/clean_up_for_prod.py
+++ sources/Goldschmidt German Mishnah/clean_up_for_prod.py
@@ -12,12 +12,15 @@ from sefaria.model import *
 
 
 def remove_roman_numerals():
+    dict_list = []
     with open('german_mishnah_data.csv', newline='') as csvfile:
         german_mishnah_csv = csv.DictReader(csvfile)
         for row in german_mishnah_csv:
-            # Todo - fix where it's being saved based on implementation (i.e.
-            # to the CSV, in the ingest script etc?)
-            row['de_text'] = re.sub(r"<sup>([xvilcdm]*)<\/sup>", "", row['de_text'])
+            cleaned_text = re.sub(r"<sup>([xvilcdm]*)<\/sup>", "", row['de_text'])
+            cur_row_dict = {"mishnah_tref": row['mishnah_tref'], "de_text": cleaned_text}
+            dict_list.append(cur_row_dict)
+    return dict_list
+
 
 
 def identify_chapter(tref):
@@ -25,12 +28,12 @@ def identify_chapter(tref):
     return int(chapter)
 
 
-def renumber_footnotes_by_chapter():
-    with open('german_mishnah_data.csv', newline='') as csvfile:
-        german_mishnah_csv = csv.DictReader(csvfile)
+def renumber_footnotes_by_chapter(list_of_rows):
         chapter = 1
         count = 1
-        for row in german_mishnah_csv:
+        renumbered_list = []
+        for row in list_of_rows:
+            renumbered_text = ""
             cur_chapter = identify_chapter(row['mishnah_tref'])
             if chapter != cur_chapter:
                 # Reset chapter
@@ -38,10 +41,36 @@ def renumber_footnotes_by_chapter():
                 # Reset the count
                 count = 1
 
-            # Todo - again based on what Noah says decide where to save
             if 'footnote' in row['de_text']:
-                row['de_text'] = re.sub(r"<sup>(\d*)<\/sup><i class=""footnote"">", str(count), row['de_text'])
+                renumbered_text = re.sub(r"<sup>(\d*)<\/sup><i class=""footnote"">", str(count), row['de_text'])
                 count += 1
 
+            if len(renumbered_text) > 1:
+                renumbered_list.append({'mishnah_tref': row['mishnah_tref'], 'de_text': renumbered_text})
+            else:
+                renumbered_list.append(row)
+        return renumbered_list
 
-renumber_footnotes_by_chapter()
+
+# This function generates the CSV of the Mishnayot
+def generate_csv(mishnah_list):
+    headers = ['mishnah_tref',
+               'de_text']
+    with open(f'german_mishnah_data_cleaned.csv', 'w+') as file:
+        mishnah_list.sort(key=lambda x: Ref(x["mishnah_tref"]).order_id())
+        c = csv.DictWriter(file, fieldnames=headers)
+        c.writeheader()
+        c.writerows(mishnah_list)
+
+    print("File writing complete")
+
+
+# Updates the CSV to renumber the footnotes and delete the roman numerals
+def run():
+    mishnah_list = remove_roman_numerals()
+    mishnah_list = renumber_footnotes_by_chapter(mishnah_list)
+    generate_csv(mishnah_list)
+
+
+if __name__ == "__main__":
+    run()
