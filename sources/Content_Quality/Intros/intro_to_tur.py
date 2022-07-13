543659b1faf5d10adb48945b608d1839d49d3342
saengel <sarah@sefaria.org>
2022-07-13 14:57:28 +0300

    feat(intros): Convert JA nodes to schema nodes, and add introductions for each

--- sources/Content_Quality/Intros/intro_to_tur.py
+++ sources/Content_Quality/Intros/intro_to_tur.py
@@ -6,11 +6,19 @@ def create_intro():
     intro = JaggedArrayNode()
     intro.add_structure(["Paragraph"])
     intro.add_shared_term("Introduction")
-    intro.key = "Introduction"
+    intro.key = f"Introduction"
     intro.validate()
     return intro
 
-# i = library.get_index("Tur")
-# intro = create_intro()
-# insert_first_child(intro, i.nodes)
+i = library.get_index("Tur")
+# # Convert to Schema nodes
+# for ja_node in i.nodes.children:
+#     convert_jagged_array_to_schema_with_default(ja_node)
+
+for schema_node in i.nodes.children:
+    intro = create_intro()
+    insert_first_child(intro, schema_node)
+
+
+
 
