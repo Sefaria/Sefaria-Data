b580294307ce69829dbeb004deb755b69fc2b71c
saengel <sarah@sefaria.org>
2022-08-09 13:18:14 +0300

    chore(intros): Minor edits in progress

--- sources/intros/intro_script.py
+++ sources/intros/intro_script.py
@@ -7,20 +7,28 @@ import re
 from sefaria.model import *
 from sefaria.helper.schema import convert_simple_index_to_complex, convert_jagged_array_to_schema_with_default, \
     insert_first_child
+from sources.functions import add_term
+
+# Todo - only run first time through
+# add_term("An Introduction by the Author's Son", 'הקדמת בן המחבר')
 
 
-def create_intro():
+def create_intro(en_title=None):
     intro = JaggedArrayNode()
     intro.add_structure(["Paragraph"])
-    intro.add_shared_term("Introduction")
-    intro.key = "Introduction"
+    if en_title:
+        intro.add_shared_term(en_title)
+        intro.key = en_title
+    else:
+        intro.add_shared_term("Introduction")
+        intro.key = "Introduction"
     intro.validate()
     return intro
 
 
-def create_intro_complex_text(tref):
+def create_intro_complex_text(tref, en_title=None):
     node = Ref(tref).index_node
-    intro = create_intro()
+    intro = create_intro(en_title)
     insert_first_child(intro, node)
 
 
@@ -33,15 +41,20 @@ def convert_children_to_schema_nodes(tref):
 
 
 def create_index_dict():
-    with open('sample_intros_7_25_22.csv', newline='') as csvfile:
+    with open('intros_08_09_2022.csv', newline='') as csvfile:
         german_mishnah_csv = csv.DictReader(csvfile)
         index_dict = {}
         for row in german_mishnah_csv:
             parent_tref = row['Parent Ref']
 
             if row["Intro Address Types"] or row["Intro Section Names"] or row["Intro Position"]:
-                print("Contains a specific address, section or position type")
-                raise Exception()
+                print("Error: Contains a specific address, section or position type")
+                print(f"ref: {parent_tref}, "
+                      f"addr_types: {row['Intro Address Types']}, "
+                      f"sec_names: {row['Intro Section Names']}, "
+                      f"intro_pos: {row['Intro Position']}")
+                # TODO - figure out how to use Intro position to get it right
+                # raise Exception()
 
             i = library.get_index(parent_tref)
             idx_title = re.findall(r"Index: (.*)", str(i))[0]
@@ -56,10 +69,15 @@ def create_index_dict():
 def run(index_dict):
     for node_title in index_dict:
 
+        seen = False
+
         node = Ref(node_title).index_node
+
+        print(f"Attempting to handle {node_title}")
 
         if len(index_dict[node_title]) > 1:  # Multiple indices for the children
             print("Converting children from JA to schema nodes")
+            seen = True
             for child in node.children:
                 child_tref = str(child)
                 print(f"Adding intro to {child_tref}")
@@ -68,15 +86,23 @@ def run(index_dict):
 
         elif isinstance(node, JaggedArrayNode):  # Simple index
             if not node.parent and not node.children:
+                seen = True
                 print("Converting simple index to complex")
+                row = index_dict[node_title]
+                en_title = row[0]['Intro English Title']
+                print(en_title)
                 convert_simple_index_to_complex(library.get_index(node_title))
-                create_intro_complex_text(node_title)
+                create_intro_complex_text(node_title, en_title=en_title)
         else:  # Schema node (BY case)
+            seen = True
             print("Adding intro to an existing schema node")
             print(f"for {node_title}")
             i = library.get_index(node_title)
             intro = create_intro()
             insert_first_child(intro, i.nodes)
+
+        if not seen:
+            print(f"ERROR: {node_title} was not addressed by this script")
 
 
 if __name__ == '__main__':
