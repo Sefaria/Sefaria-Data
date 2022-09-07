998d22cb7535368bb3f31e798a309b6a6a0b4393
saengel <sarah@sefaria.org>
2022-09-07 11:51:02 +0300

    fix(English Mishneh Torah): Account for alt versions for some books in validation

--- sources/english_mishneh_torah_touger/segment_validation.py
+++ sources/english_mishneh_torah_touger/segment_validation.py
@@ -1,13 +1,21 @@
 import django
+
 django.setup()
 from sefaria.model import *
 from utilities import sefaria_book_names
 
 if __name__ == '__main__':
+    alt_version_books = ['Reading the Shema',
+                         'Prayer and the Priestly Blessing',
+                         'Tefillin, Mezuzah and the Torah Scroll',
+                         'Fringes',
+                         'Blessings',
+                         'Circumcision',
+                         'The Order of Prayer']
     for book in sefaria_book_names:
         index = library.get_index(f"Mishneh Torah, {book}")
         en_version_title = "Mishneh Torah, trans. by Eliyahu Touger. Jerusalem, Moznaim Pub. c1986-c2007"
-        he_version_title = "Torat Emet 363"
+        he_version_title = "Torat Emet 363" if book not in alt_version_books else 'Torat Emet 370'
         for section_ref in index.all_section_refs():
             en_text = section_ref.text("en", vtitle=en_version_title).text
             he_text = section_ref.text("he", vtitle=he_version_title).text
