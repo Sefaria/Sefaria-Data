07cd977fec241be50453b3e4da03e0d1d86978b6
yonadavGit <92536571+yonadavGit@users.noreply.github.com>
2022-10-30 23:43:02 +0200

    pushed script for adding intro nodes

---
+++ sources/steinsaltz_intros_talmud/steinsaltz_intros_add_nodes.py
@@ -0,0 +1,183 @@
+import django
+django.setup()
+
+# import statistics
+import csv
+from sefaria.model import *
+from sefaria.helper.schema import insert_last_child
+
+
+def latin_numeral_to_hebrew_numeral(latin_numeral):
+    if latin_numeral == "I":
+        return("א'")
+    if latin_numeral ==  "II":
+        return("ב'")
+    if latin_numeral ==  "III":
+        return("ג'")
+    if latin_numeral == "IV":
+        return("ד'")
+    if latin_numeral == "V":
+        return("ה'")
+    if latin_numeral == "VI":
+        return("ו'")
+    if latin_numeral == "VII":
+        return("ז'")
+    if latin_numeral == "VIII":
+        return("ח'")
+    if latin_numeral ==  "IX":
+        return("ט'")
+    if latin_numeral == "X":
+        return("י'")
+    if latin_numeral == "XI":
+        return('י"א')
+    if latin_numeral ==  "XII":
+        return('י"ב')
+    if latin_numeral ==  "XIII":
+        return('י"ג')
+    if latin_numeral == "XIV":
+        return('י"ד')
+    if latin_numeral == "XV":
+        return('ט"ו')
+    if latin_numeral == "XVI":
+        return('ט"ז')
+    if latin_numeral == "XVII":
+        return('י"ז')
+    if latin_numeral == "XVIII":
+        return('י"ח')
+    if latin_numeral ==  "XIX":
+        return('י"ט')
+    if latin_numeral == "XX":
+        return("כ'")
+    if latin_numeral == "XXI":
+        return('כ"א')
+    if latin_numeral == "XXII":
+        return('כ"ב')
+    if latin_numeral == "XXIII":
+        return('כ"ג')
+    if latin_numeral == "XXIV":
+        return('כ"ד')
+    if latin_numeral == "XXV":
+        return('כ"ה')
+    if latin_numeral == "XXVI":
+        return('כ"ו')
+    if latin_numeral == "XXVII":
+        return('כ"ז')
+    if latin_numeral == "XXVIII":
+        return('כ"ח')
+    if latin_numeral ==  "XXIX":
+        return('כ"ט')
+    if latin_numeral == "XXX":
+        return("ל'")
+
+
+
+def translate_title_to_hebrew(title, masechet_hebrew):
+    hebrew_title = ''
+    if 'Introduction to' in title:
+        hebrew_title += 'הקדמה ל'
+
+        if " Perek " in title:
+            hebrew_title += 'פרק '
+            hebrew_title += latin_numeral_to_hebrew_numeral(title.split(' ')[3].strip())
+            return hebrew_title
+
+        else:
+            hebrew_title += "מסכת "
+            hebrew_title += masechet_hebrew
+            return hebrew_title
+
+    if 'Summary of' in title:
+        hebrew_title += 'סיכום לפרק '
+        numeral = title.split(' ')[3].strip()
+        hebrew_title += latin_numeral_to_hebrew_numeral(numeral)
+        return hebrew_title
+
+def parse_csv_to_object():
+    list_of_dicts = []
+    #structure:
+    [{'ref': ...,
+    'english_text': [],
+    'hebrew_text': [],
+    'masechet': ...,
+    'masechet_hebrew': ...,
+    'title': 'Introduction to Berakhot',
+    'title_hebrew': ''}]
+    with open('introductions.csv', newline='') as csvfile:
+        r = csv.reader(csvfile, delimiter=',')
+
+        current_ref_dict = {}
+        for row in r:
+            ref = row[0]
+            if ref != '':
+                current_ref_dict = {}
+                current_ref_dict["ref"] = ref
+                current_ref_dict['english_text'] = []
+                current_ref_dict['english_text'].append(row[1])
+                current_ref_dict['hebrew_text'] = []
+                current_ref_dict['hebrew_text'].append(row[2])
+
+                ##########################
+                # getting titles and mesechtot:
+
+                ref_parts = ref.split(',')
+
+                masechet = ref_parts[1].strip()
+                masechet_hebrew = Ref(masechet).he_normal()
+
+                title = ref_parts[2].strip()
+                title_hebrew = translate_title_to_hebrew(title, masechet_hebrew)
+
+                current_ref_dict["masechet"] = masechet
+                current_ref_dict["masechet_hebrew"] = masechet_hebrew
+                current_ref_dict["title"] = title
+                current_ref_dict["title_hebrew"] = title_hebrew
+                if len(current_ref_dict["english_text"]) == 1:
+                    list_of_dicts.append(current_ref_dict)
+            else:
+                current_ref_dict['english_text'].append(row[1])
+                current_ref_dict['hebrew_text'].append(row[2])
+
+    return (list_of_dicts)
+
+def get_list_of_masechtot_nodes(list_of_masechtot, starting_masechet_name):
+
+    found_starting_masechet_flag = False
+    #index = library.get_index("Introductions to the Babylonian Talmud")
+    #parent = index.nodes
+    nodes_list = []
+    current_masechet = "no masechet yet"
+    for segment_dict in list_of_masechtot:
+        if(segment_dict['masechet'] == starting_masechet_name) or found_starting_masechet_flag:
+            found_starting_masechet_flag = True
+            if segment_dict['masechet'] != current_masechet:
+
+                masechet_node = SchemaNode()
+                masechet_node.key = segment_dict["masechet"]  # should be equal to primary title
+                masechet_node.add_primary_titles(segment_dict["masechet"], segment_dict["masechet_hebrew"])
+
+            leaf_node = JaggedArrayNode()
+            leaf_node.add_primary_titles(segment_dict["title"], segment_dict["title_hebrew"])
+            leaf_node.add_structure(["Paragraph"])
+            masechet_node.append(leaf_node)
+            if segment_dict['masechet'] != current_masechet:
+                nodes_list.append(masechet_node)
+            current_masechet = segment_dict['masechet']
+            #insert_last_child(masechet_node, parent)
+    return nodes_list
+
+def list_of_masechtot_to_db(nodes_list):
+    index = library.get_index("Introductions to the Babylonian Talmud")
+    parent = index.nodes
+    for node in nodes_list:
+        insert_last_child(node, parent)
+
+
+if __name__ == '__main__':
+    print("hello")
+    #index = library.get_index("Introductions to the Babylonian Talmud")
+    # m = library._index_map
+    #print(index);
+    parsed = parse_csv_to_object()
+    nodes = get_list_of_masechtot_nodes(parsed, "Sanhedrin")
+    list_of_masechtot_to_db(nodes)
+    print("finished updating db")
