bacf1e6eee69012705706050d3d6b427446c41f0
yonadavGit <92536571+yonadavGit@users.noreply.github.com>
2022-11-23 00:04:18 +0200

    Update kuzri_injest_from_csv.py

--- sources/kuzari_arabic/kuzri_injest_from_csv.py
+++ sources/kuzari_arabic/kuzri_injest_from_csv.py
@@ -1,10 +1,23 @@
+import django
+django.setup()
+
 import csv
+from sefaria.model import *
+from sefaria.tracker import modify_bulk_text
 
 if __name__ == '__main__':
 
-
+    superuser_id = 1
     ref_dict = {}
 
+    #delete version if already exits:
+    cur_version = VersionSet({'title': 'Kuzari',
+                              'versionTitle': "Kuzari in Arabic, trans. Nabih Bashir, Al-Kamel Verlag, 2012 [ar]"})
+    if cur_version.count() > 0:
+        cur_version.delete()
+
+
+    #pase csv into fictionary of refs:
     with open('kuzari aligned.csv', newline='') as csvfile:
         r = csv.reader(csvfile, delimiter=',')
 
@@ -12,18 +25,34 @@ if __name__ == '__main__':
         paragraph_num = 1
         for row in r:
             if row[0] == '':
-                ref = title + ", " + str(paragraph_num)
+                ref = title + ":" + str(paragraph_num)
                 ref_dict[ref] = row[2]
                 paragraph_num += 1
             else:
-                title = row[0]
                 paragraph_num = 1
-                ref = title + ", " + str(paragraph_num)
+                title = row[0]
+                ref = title + ":" + str(paragraph_num)
                 ref_dict[ref] = row[2]
+                paragraph_num += 1
 
 
-    print(ref_dict)
 
+    #create new version:
+    index = library.get_index("Kuzari")
+
+    chapter = index.nodes.create_skeleton()
+    new_version = Version({"versionTitle": "Kuzari in Arabic, trans. Nabih Bashir, Al-Kamel Verlag, 2012 [ar]",
+                               "versionSource": "'https://korenpub.com/collections/the-noe-edition-koren-talmud-bavli-1'",
+                               "title": "Kuzari",
+                               "chapter": chapter,
+                               "language": "he",
+                               "digitizedBySefaria": True,
+                               "license": "CC-BY",
+                               "status": "locked"
+                               })
+
+modify_bulk_text(superuser_id, new_version, ref_dict)
+print("finished update")
 
 
 
