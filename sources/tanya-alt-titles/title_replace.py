c92045ccf8bc77cbed874fdbd2b943035a9374b5
saengel <sarah@sefaria.org>
2022-11-23 08:47:36 +0200

    fix(Tanya Alt Titles): Account for Chinukh Katan

--- sources/tanya-alt-titles/title_replace.py
+++ sources/tanya-alt-titles/title_replace.py
@@ -6,7 +6,6 @@ from sefaria.model import *
 from sefaria.helper.schema import change_node_title
 import csv
 
-
 def tanya_title_replace_run():
     title_dict = {}
 
@@ -27,6 +26,17 @@ def tanya_title_replace_run():
         new_he_title = info['new heb']
         retire_he_title = info['current heb']
 
+        if len(node.children) > 0:
+            children = node.children
+            for child in children:
+                child_title = child.get_primary_title()
+                print(child_title)
+                if child_title == "The Education of the Child":
+                    change_node_title(child, old_title="The Education of the Child", lang="en", new_title="Chinukh Katan")
+                    child.add_title("The Education of the Child", "en")
+                print(child.get_primary_title())
+
+
         # Create a new primary title
         if new_en_title:
             change_node_title(node, old_title=retire_en_title, lang="en", new_title=new_en_title)
@@ -38,7 +48,7 @@ def tanya_title_replace_run():
             node.add_title(retire_en_title, "en")
         if retire_he_title:
             node.add_title(retire_he_title, "he")
-
+        print()
     tanya_index.save()
 
 
