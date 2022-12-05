72377c205b055b8a15dbc35cb7a9174a62955ac2
saengel <sarah@sefaria.org>
2022-12-05 13:40:27 +0200

    fix(Kuntres Acharon): Resolve missing letter issue

--- sources/kuntres_acharon_linking/linking.py
+++ sources/kuntres_acharon_linking/linking.py
@@ -8,6 +8,8 @@ from sefaria.model import *
 from sefaria.utils.hebrew import gematria
 
 
+# Todo - w a fresh DB, check why some links already exist? Is it handling multiple markers within the same segment (w/o overwriting)?
+
 def get_all_segments():
     all_text = []
 
@@ -41,9 +43,11 @@ def create_inline_ref_link(main_text_ref, ka_ref, data_order, save=True):
             'data-order': data_order
         }}
     if save:
-        Link(link_creation_dict).save()
-    # Todo - indent this line once testing done
-    print(f"Created inline-ref link between {main_text_ref} and {ka_ref}")
+        try:
+            Link(link_creation_dict).save()
+            print(f"Created inline-ref link between {main_text_ref} and {ka_ref}")
+        except Exception as e:
+            print(e)
 
 
 def create_html_tags(ka_ref, data_order, data_label):
@@ -51,7 +55,7 @@ def create_html_tags(ka_ref, data_order, data_label):
     data-order: the order it appears in
     data-label: the gematria tag we want to use
     """
-    return f"<i data-commentator=\"{ka_ref}\" data-order=\"{data_order}\" data-label=\"{data_label}\"></i>"
+    return f" <i data-commentator=\"Shulchan Arukh HaRav\" data-order=\"{data_order}\" data-label=\"{data_label}\"></i> "
 
 
 def find_gematria_markers(text):
@@ -82,18 +86,19 @@ def get_siman(tref, chelek):
         return siman[0].strip()
     return None
 
+
 def clean_marker(marker):
     cleaned_marker = re.findall(r"[אבגדהוזחטיכלמנסעפצקרשת]{1,2}", marker)
     if cleaned_marker:
         return cleaned_marker[0]
     return None
 
-def generate_csv(dict_list, headers):
-    with open('SAR_text_with_KA_inline_refs.csv', 'w+') as file:
-        c = csv.DictWriter(file, fieldnames=headers)
-        c.writeheader()
-        c.writerows(dict_list)
-    print(f"File writing of Shulchan Arukh HaRav with Kuntres Acharon as inline-refs complete")
+
+def update_text(tref, text):
+    oref = Ref(tref)
+    tc = TextChunk(oref, vtitle="Kehot Publication Society", lang='he')
+    tc.text = text
+    tc.save()
 
 
 def match_link_html():
@@ -124,20 +129,16 @@ def match_link_html():
                 create_inline_ref_link(main_text_ref=cur_main_text_tref,
                                        ka_ref=ka_ref,
                                        data_order=gematria_marker_value,
-                                       save=False)  # Todo toggle this switch when ready
+                                       save=True)  # Todo toggle this switch when ready
 
                 # create HTML
                 html_tag = create_html_tags(ka_ref=ka_ref,
-                                             data_order=gematria_marker_value,
-                                             data_label=data_label_cleaned)
+                                            data_order=gematria_marker_value,
+                                            data_label=data_label_cleaned)
 
                 # Insert HTML tag
                 main_text_with_html = cur_main_text.replace(marker, html_tag)
-                html_csv_dict.append({'ref': cur_main_text_tref,
-                                      'text': main_text_with_html})
-
-    # Save to new CSV
-    generate_csv(html_csv_dict, headers=['ref', 'text'])
+                update_text(tref=cur_main_text_tref, text=main_text_with_html)
 
 
 if __name__ == '__main__':
