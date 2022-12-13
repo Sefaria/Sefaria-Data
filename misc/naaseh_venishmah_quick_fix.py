ff12fa3d04bf6d7a6e11679b20ad41510efd7f1b
saengel <sarah@sefaria.org>
2022-12-13 18:01:32 +0200

    fix(Naaseh V'Nishmah Topic): Fix error with Naaseh v'Nishmah topic

--- misc/naaseh_venishmah_quick_fix.py
+++ misc/naaseh_venishmah_quick_fix.py
@@ -7,8 +7,11 @@ from sefaria.model import *
 
 def fix_nvn():
     nvn_topic_stub = Topic({"slug": "naaseh-venishmah",
-                            "titles": {"lang": "en", "primary": True, "text": "naaseh-venishmah"}})
+                            "titles": [{"lang": "en", "primary": True, "text": "naaseh-venishmah"}]}).save()
     existing_nvn_topic = Topic().load({"slug": "naaseh-venishma"})
+    # Delete broken link which interferes with merge (the Ref is invalid and does not exist)
+    broken_link = RefTopicLink().load({"toTopic": "naaseh-venishmah", "ref": "Ner Mitzvah, The Kingdom of Greece wishes to uproot the bond between Israel and God 11"})
+    broken_link.delete()
     existing_nvn_topic.merge(nvn_topic_stub)
 
 
