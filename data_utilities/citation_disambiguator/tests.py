f446346a457b6d4d3e048bf4f6b6dc486287188e
nsantacruz <noahssantacruz@gmail.com>
2023-01-10 12:58:32 +0200

    refactor(link disambiguator): use fixture for citation disambiguator

--- data_utilities/citation_disambiguator/tests.py
+++ data_utilities/citation_disambiguator/tests.py
@@ -6,6 +6,11 @@ from .main import CitationDisambiguator
 
 
 @pytest.fixture(scope="module")
+def citation_disambiguator():
+    return CitationDisambiguator("he")
+
+
+@pytest.fixture(scope="module")
 def input_output(request):
     main_tref, quote_tref, is_good = request.param
     main_oref = Ref(main_tref)
@@ -26,11 +31,10 @@ def input_output(request):
     ("Shemirat HaLashon, Book I, The Gate of Torah 4:25", "Shevuot 39a:22", True),
     ("Commentary on Sefer Hamitzvot of Rasag, Positive Commandments 136:4", "Horayot 5a:1", False),
 ], indirect=True)
-def test_citation_disambiguator(input_output):
+def test_citation_disambiguator(input_output, citation_disambiguator):
     input, output = input_output
 
-    ld = CitationDisambiguator("he")
-    good, bad = ld.disambiguate_one(input["main"], input["main"].text('he'), input["quote"])
+    good, bad = citation_disambiguator.disambiguate_one(input["main"], input["main"].text('he'), input["quote"])
     if output["quote"] is None:
         # output should be bad
         assert good == []
