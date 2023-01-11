b0fd190d933d7f3e62a60c03924dd21d04a0a63d
nsantacruz <noahssantacruz@gmail.com>
2023-01-11 17:00:11 +0200

    fix(link disambiguator): revive modify_tanakh_links.py to work again

--- data_utilities/citation_disambiguator/modify_tanakh_links.py
+++ data_utilities/citation_disambiguator/modify_tanakh_links.py
@@ -1,121 +1,36 @@
-# -*- coding: utf-8 -*-
-import re, json, codecs, csv, heapq, random, regex, math, cProfile, pstats, time
+import re, csv, time
 from collections import defaultdict
 from pymongo.errors import AutoReconnect
 from tqdm import tqdm
 import django
 django.setup()
 from sefaria.model import *
-from sefaria.system.exceptions import PartialRefInputError, InputError, NoVersionFoundError
+from sefaria.system.exceptions import InputError
 from sefaria.utils.hebrew import strip_cantillation
 import sefaria.tracker as tracker
-from sefaria import settings
-from sefaria.system.database import db
+from data_utilities.citation_disambiguator.main import get_snippet_by_seg_ref
 
-ROOT = "data"
-# ROOT = "research/link_disambiguator"
+DATA_DIR = "data"
 
-def get_tc(tref, just_ref=False, tries=0):
+
+def get_tc(tref, vtitle=None, just_ref=False, tries=0):
     try:
         main_oref = Ref(tref)
         if just_ref:
             return main_oref
-        vset = VersionSet(main_oref.condition_query("he"), proj=main_oref.part_projection())
-        if len(vset) < 1:
-            raise InputError("VSET not equal to 1")
-        main_version = vset[0]
-        main_tc = main_oref.text("he", vtitle=main_version.versionTitle)
-        return main_tc, main_oref, main_version
+        if vtitle is None:
+            vset = main_oref.versionset()
+            if len(vset) < 1:
+                raise InputError("VSET not equal to 1")
+            vtitle = vset[0].versionTitle
+        main_tc = main_oref.text("he", vtitle=vtitle)
+        return main_tc, main_oref, main_tc._versions[0]
     except AutoReconnect:
         if tries < 200:
             time.sleep(0.24)
-            return get_tc(tref, just_ref=just_ref, tries=tries+1)
+            return get_tc(tref, vtitle=vtitle, just_ref=just_ref, tries=tries+1)
         else:
             raise AutoReconnect("Tried so hard but got so many autoreconnects...")
-
-
-"""
-Copied and pasted from main.py to make this file independently runnable. not a great idea...
-"""
-def get_snippet_by_seg_ref(source_text, source_ref, found, must_find_snippet=False, snip_size=100, use_indicator_words=False, return_matches=False):
-    """
-    based off of library.get_wrapped_refs_string
-    :param source:
-    :param found:
-    :param must_find_snippet: bool, True if you only want to return a str if you found a snippet
-    :param snip_size int number of chars in snippet on each side
-    :param use_indicator_words bool, True if you want to use hard-coded indicator words to determine which side of the ref the quote is on
-    :return:
-    """
-    after_indicators = ["דכתיב", "ודכתיב", "וכתיב", "וכתוב", "שכתוב", "כשכתוב", "כדכתיב", "זל", "ז״ל", "ז''ל",
-                       "ז\"ל", "אומרם", "כאמור", "ואומר", "אמר", "שנאמר", "בגמ'", "בגמ׳", "בפסוק", "לעיל", "ולעיל", "לקמן", "ולקמן", "בירושלמי",
-                       "בבבלי", "שדרשו", "ששנינו", "שנינו", "ושנינו", "דשנינו", "כמש״כ", "כמש\"כ", "כמ״ש", "כמ\"ש",
-                       "וכמש״כ", "וכמ\"ש", "וכמ״ש", "וכמש\"כ", "ע״ה", "ע\"ה", "מבואר", "כמבואר", "במתני׳",
-                       "במתנ\'", "דתנן", "זכרונם לברכה", "זכר לברכה"]
-    after_reg = r"(?:^|\s)(?:{})\s*[(\[]?$".format("|".join(after_indicators))
-    after_indicators_far = ["דבפרק", "בפרק", "שבפרק", "פרק"]
-    after_far_reg = r"(?:^|\s)(?{}:)(?=\s|$)".format("|".join(after_indicators_far))
-    after_indicators_after = ["בד״ה", "בד\"ה", "ד״ה", "ד\"ה"]
-    after_after_reg = r"^\s*(?:{})\s".format("|".join(after_indicators_after))
-    punctuation = [",", ".", ":", "?", "!", "׃"]
-    punctuation_after_reg = r"^\s*(?:{})\s".format("|".join(punctuation))
-    punctuation_before_reg = r"(?:{})\s*$".format("|".join(punctuation))
-    after_indicators_after_far = ["וגו׳", "וגו'", "וגו", "וכו׳", "וכו'", "וכו"]
-    after_after_far_reg = r"(?:^|\s)(?{}:)(?=\s|$)".format("|".join(after_indicators_after_far))
-    found_title = found.index.get_title("he")
-    found_node = library.get_schema_node(found_title, "he")
-    title_nodes = {t: found_node for t in found.index.all_titles("he")}
-    all_reg = library.get_multi_title_regex_string(set(found.index.all_titles("he")), "he")
-    reg = regex.compile(all_reg, regex.VERBOSE)
-    if len(source_text) == 0 or not isinstance(source_text, str):
-        print(source_ref)
-    source_text_norm = re.sub(r"<[^>]+>", "", strip_cantillation(source_text, strip_vowels=True))
-    linkified = library._wrap_all_refs_in_string(title_nodes, reg, source_text_norm, "he")
-
-    snippets = []
-    found_normal = found.normal()
-    found_section_normal = re.match(r"^[^:]+", found_normal).group()
-    for match in re.finditer("(<a [^>]+>)([^<]+)(</a>)", linkified):
-        ref = Ref(match.group(2))
-        # use split_spanning_ref in case where talmud ref is amudless. It's essentially a ranged ref across two amudim
-        if len({found_section_normal, found_normal} & {temp_ref.normal() for temp_ref in ref.split_spanning_ref()}) > 0 or ref.normal() == found_normal or ref.normal() == found_section_normal:
-            if return_matches:
-                snippets += [(match, linkified)]
-            else:
-                start_snip_naive = match.start(1) - snip_size if match.start(1) >= snip_size else 0
-                start_snip_space = linkified.rfind(" ", 0, start_snip_naive)
-                start_snip_link = linkified.rfind("</a>", 0, match.start(1))
-                start_snip = max(start_snip_space, start_snip_link)
-                if start_snip == -1:
-                    start_snip = start_snip_naive
-                end_snip_naive = match.end(3) + snip_size if match.end(3) + snip_size <= len(linkified) else len(linkified)
-                end_snip_space = linkified.find(" ", end_snip_naive)
-                end_snip_link = linkified.find("<a ", match.end(3))
-                end_snip = min(end_snip_space, end_snip_link)
-                if end_snip == -1:
-                    end_snip = end_snip_naive
-
-                if use_indicator_words:
-                    before_snippet = linkified[start_snip:match.start(1)]
-                    if "ירושלמי" in before_snippet[-20:] and (len(ref.index.categories) < 2 or ref.index.categories[1] != 'Yerushalmi'):
-                        # this guys not a yerushalmi but very likely should be
-                        continue
-                    after_snippet = linkified[match.end(3):end_snip]
-                    if re.search(after_reg, before_snippet) is not None:
-                        temp_snip = after_snippet
-                        # print before_snippet
-                    else:
-                        temp_snip = linkified[start_snip:end_snip]
-                else:
-                    temp_snip = linkified[start_snip:end_snip]
-                snippets += [re.sub(r"<[^>]+>", "", temp_snip)]
-
-    if len(snippets) == 0:
-        if must_find_snippet:
-            return None
-        return [source_text_norm]
-
-    return snippets
 
 
 def modify_text(user, oref, versionTitle, language, text, versionSource, tries=0):
@@ -172,6 +87,7 @@ def get_mapping_after_normalization(text, find_text_to_remove=None, removal_list
         removal_map[normalized_text_index] = total_removed
     return removal_map
 
+
 def convert_normalized_indices_to_unnormalized_indices(normalized_indices, removal_map):
     from bisect import bisect_right, bisect_left
     removal_keys = sorted(removal_map.keys())
@@ -184,7 +100,10 @@ def convert_normalized_indices_to_unnormalized_indices(normalized_indices, remov
         unnormalized_indices += [(unnorm_start, unnorm_end)]
     return unnormalized_indices
 
+
 _version_cache = {}
+
+
 def get_full_version(v):
     global _version_cache
     key = (v.title, v.versionTitle, v.language)
@@ -193,24 +112,26 @@ def get_full_version(v):
         _version_cache[key] = full_v
     return _version_cache[key]
 
-def modify_tanakh_links_one(main_ref, section_map, error_file_csv):
+
+def modify_tanakh_links_one(main_ref, section_map, error_file_csv, vtitle):
     try:
-        main_tc, main_oref, main_version = get_tc(main_ref)
+        main_tc, main_oref, main_version = get_tc(main_ref, vtitle=vtitle)
         if main_tc.is_merged:
             # not really possible but whatevs
             return
         # to make it _savable
         main_tc._saveable = True
         new_main_text = main_tc.text
+
         def find_text_to_remove(s):
             for m in re.finditer(r"(<[^>]+>|[\u0591-\u05bd\u05bf-\u05c5\u05c7])", s):
                 yield m, ''
         for section_tref, segment_ref_dict in list(section_map.items()):
             removal_map = get_mapping_after_normalization(new_main_text, find_text_to_remove)
-            section_oref = get_tc(section_tref, True)
+            section_oref = get_tc(section_tref, just_ref=True)
             quoted_list_temp = sorted(list(segment_ref_dict.items()), key=lambda x: x[0])
             segment_ref_list = [segment_ref_dict.get(i, None) for i in range(quoted_list_temp[-1][0]+1)]
-            match_list = get_snippet_by_seg_ref(new_main_text, main_ref, section_oref, must_find_snippet=True, snip_size=65, return_matches=True)
+            match_list = get_snippet_by_seg_ref(new_main_text, section_oref, must_find_snippet=True, snip_size=65, return_matches=True)
             if match_list:
                 if len(match_list) == len(segment_ref_list):
                     chars_to_wrap = []
@@ -241,34 +162,39 @@ def modify_tanakh_links_one(main_ref, section_map, error_file_csv):
 
 
 def modify_tanakh_links_all(start=0, end=None, min_num_citation=0):
-    error_file = open(ROOT + "/link_disambiguatore_errors.csv", "w")
+    error_file = open(DATA_DIR + "/modify_tanakh_errors.csv", "w")
     error_file_csv = csv.DictWriter(error_file, ["Quoting Ref", "Error"])
     total = 0
     user = 5842
     all_modify_bulk_text_input = {}  # key = (title, vtitle, lang). value = {"version", "user", "text_map"}
-    with open(ROOT + "/unambiguous_links.csv", "r") as fin:
+    with open(DATA_DIR + "/unambiguous_links.csv", "r") as fin:
         cin = csv.DictReader(fin)
+        vtitle_mapping = {}
         mapping = defaultdict(lambda: defaultdict(dict))
         for row in cin:
             total += 1
-            quoted_ref = get_tc(row["Quoted Ref"], True)
+            quoting_tref = row["Quoting Ref"]
+            quoting_vtitle = row["Quoting Version Title"]
+            quoted_tref = row["Quoted Ref"]
+            quoted_ref = get_tc(quoted_tref, just_ref=True)
             if quoted_ref.primary_category != "Tanakh":
                 continue
+            vtitle_mapping[quoting_tref] = quoting_vtitle
             section_ref = quoted_ref.section_ref().normal()
-            curr_dict = mapping[row["Quoting Ref"]][section_ref]
-            if curr_dict.get(int(row["Quote Num"]), None) is not None and curr_dict.get(int(row["Quote Num"]), None) != quoted_ref:
-                print("{} - {}==={} - {}".format(row["Quoting Ref"], row["Quoted Ref"], curr_dict.get(int(row["Quote Num"]), None),row["Quote Num"]))
+            curr_dict = mapping[quoting_tref][section_ref]
+            if curr_dict.get(int(row["Quote Num"]), None) is not None and curr_dict[int(row["Quote Num"])] != quoted_ref:
+                print("{} - {}==={} - {}".format(quoting_tref, quoted_tref, curr_dict.get(int(row["Quote Num"]), None),row["Quote Num"]))
             curr_dict[int(row["Quote Num"])] = quoted_ref
         print("Total {}".format(total))
 
-        for i, (k, v) in enumerate(tqdm(mapping.items(), total=len(mapping))):
+        for i, (quoting_tref, v) in enumerate(tqdm(mapping.items(), total=len(mapping))):
             if i < start:
                 continue
             if end is not None and i > end:
                 continue
             if len(v) < min_num_citation:
                 continue
-            single_edit = modify_tanakh_links_one(k, v, error_file_csv)
+            single_edit = modify_tanakh_links_one(quoting_tref, v, error_file_csv, vtitle_mapping[quoting_tref])
             if single_edit is None: continue
             vers = single_edit['version']
             key = (vers.title, vers.versionTitle, vers.language)
@@ -290,11 +216,11 @@ def modify_tanakh_links_all(start=0, end=None, min_num_citation=0):
 # Deletion damage control
 ###
 def get_bad_refs():
-    with open(ROOT + "/unambiguous_links.csv", "r") as fin:
+    with open(DATA_DIR + "/unambiguous_links.csv", "r") as fin:
         cin = csv.DictReader(fin)
         mapping = defaultdict(lambda: defaultdict(dict))
         for row in cin:
-            quoted_ref = get_tc(row["Quoted Ref"], True)
+            quoted_ref = get_tc(row["Quoted Ref"], just_ref=True)
             if quoted_ref.primary_category != "Tanakh":
                 continue
             section_ref = quoted_ref.section_ref().normal()
@@ -335,13 +261,13 @@ def dump_backed_up_text():
             "Language": main_version.language,
             "Text": backup_text
         }]
-    with open(ROOT + '/backup_text.csv', 'w') as fout:
+    with open(DATA_DIR + '/backup_text.csv', 'w') as fout:
         c = csv.DictWriter(fout, ['Ref', 'Title', 'Version Title', 'Language', 'Text'])
         c.writeheader()
         c.writerows(rows)
 
 def restore_text_from_backup():
-    with open(ROOT + '/backup_text.csv', 'r') as fin:
+    with open(DATA_DIR + '/backup_text.csv', 'r') as fin:
         c = list(csv.DictReader(fin))
         for row in tqdm(c):
             main_tc, main_oref, main_version = get_tc(row['Ref'])
@@ -351,8 +277,9 @@ def restore_text_from_backup():
 # End deletion damage control
 ###
 
+
 if __name__ == "__main__":
-    modify_tanakh_links_all(start=0, min_num_citation=2)
+    modify_tanakh_links_all(start=0)
     # verify_refs_werent_edited()
     # dump_backed_up_text()
     # restore_text_from_backup()
