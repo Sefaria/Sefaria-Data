394d5b9b85f0b6f7d782c4b621f2d879c84ad7ec
yon <yonadav.le@gmail.com>
2023-01-24 02:15:19 +0200

    script posts efodi version

--- sources/moreh_nevuchim_commentaries/ingest_commentary.py
+++ sources/moreh_nevuchim_commentaries/ingest_commentary.py
@@ -3,13 +3,14 @@ import django
 django.setup()
 
 django.setup()
-
+superuser_id = 171118
 # import statistics
 import csv
 from sefaria.model import *
 from sefaria.helper.schema import insert_last_child, reorder_children
 from sefaria.helper.schema import remove_branch
 from sefaria.tracker import modify_bulk_text
+# from sources.functions import *
 from sefaria.system.database import db
 import time
 from docx import Document
@@ -357,7 +358,7 @@ def delete_existing_automated_links(query={"type": "", "refs": {"$regex" : "Intr
 def delete_existing_correct_essay_links(query={"type":"essay", "refs": {"$regex" : "Introductions to the Babylonian Talmud"}}):
     list_of_links = LinkSet(query).array()
     for l in list_of_links:
-            l.delete()
+        l.delete()
 
 def insert_links_to_db(list_of_links):
     for l in list_of_links:
@@ -378,12 +379,23 @@ def insert_links_to_db(list_of_links):
     # db.index.insert_one(new_index)
     # index = library.get_index("Introductions to the Babylonian Talmud")
 
+def create_fake_schema(en, he):
+    root = JaggedArrayNode()
+    comm_en = "Chomat Anakh on {}".format(en)
+    comm_he = u"חומת אנך על {}".format(he)
+    root.add_primary_titles(comm_en, comm_he)
+    root.add_structure(["Chapter", "Paragraph"])
+    index = {
+        "title": comm_en,
+        "schema": root.serialize(),
+        "categories": ["Tanakh", "Commentary"]
+    }
+    post_index(index, server="http://localhost:8000")
 
-if __name__ == '__main__':
-    print("hello world")
-    # "Guide for the Perplexed, Part 1 2:7"
+def create_text_object():
     ref_prefix = "Efodi on Guide for the Perplexed, "
-    sections = ["Introduction, Letter to R Joseph son of Judah", "Introduction, Prefatory Remarks", "Introduction, Introduction",
+    sections = ["Introduction, Letter to R Joseph son of Judah", "Introduction, Prefatory Remarks",
+                "Introduction, Introduction",
                 "Part 1", "Part 2 Introduction", "Part 2", "Part 3 Introduction", "Part 3"]
     ref = ''
     ref_chapter = 0
@@ -400,15 +412,14 @@ if __name__ == '__main__':
     part_2 = {}
     part_3_introduction = {}
     part_3 = {}
-    comm_list = [letter_to_R_Joseph_son_of_Judah, prefatory_Remarks, introduction, part_1, part_2_introduction, part_2, part_3_introduction, part_3]
-
-
+    comm_list = [letter_to_R_Joseph_son_of_Judah, prefatory_Remarks, introduction, part_1, part_2_introduction, part_2,
+                 part_3_introduction, part_3]
 
     # Print the text of the docx file
     section_index = -1
     for para in document.paragraphs:
         print(para.text)
-        if para.text == '' or '@22' in para.text:
+        if para.text == '' or para.text == ' ' or para.text == '  ' or '@22' in para.text:
             continue
         if '@00' in para.text and not para.text.startswith("@00פרק"):
             section_index += 1
@@ -421,50 +432,130 @@ if __name__ == '__main__':
 
         else:
             ref_paragraph += 1
-            if section_index in {0,1,2,4,6}:
+            if section_index in {0, 1, 2, 4, 6}:
                 ref = str(ref_prefix) + sections[section_index] + ' ' + str(ref_paragraph)
 
             else:
-                ref = str(ref_prefix) + sections[section_index] + ' ' + str(ref_chapter) + ' ' + ":" + str(ref_paragraph)
+                ref = str(ref_prefix) + sections[section_index] + ' ' + str(ref_chapter) + ":" + str(
+                    ref_paragraph)
             comm_list[section_index][ref] = para.text.translate(str.maketrans("", "", "0123456789@"))
         # cleaned_string = para.text.translate(str.maketrans("", "", "0123456789@"))
         # print(cleaned_string)
-    he_title, en_title = """רב פנינים על משלי / Rav Peninim on Proverbs""".split(" / ")
+    # return [list(comm_list[0].values()), list(comm_list[1].values()),list(comm_list[2].values()), list(comm_list[3].values()), list(comm_list[4].values()), list(comm_list[5].values()), list(comm_list[6].values())]
+    return {**comm_list[0], **comm_list[1], **comm_list[2], **comm_list[3], **comm_list[4], **comm_list[5], **comm_list[6], **comm_list[7]}
+
+
+def ingest_version(map_text):
+    # vs = VersionState(index=library.get_index("Introductions to the Babylonian Talmud"))
+    # vs.delete()
+    # print("deleted version state")
+    index = library.get_index("Efodi on Guide for the Perplexed")
+    cur_version = VersionSet({'title': 'fodi on Guide for the Perplexed'})
+    if cur_version.count() > 0:
+        cur_version.delete()
+        print("deleted existing version")
+    chapter = index.nodes.create_skeleton()
+    version = Version({"versionTitle": "Warsaw, 1872",
+                       "versionSource": "https://www.nli.org.il/he/books/NNL_ALEPH990017717720205171/NLI",
+                       "title": "Efodi on Guide for the Perplexed",
+                       "language": "he",
+                       "chapter": chapter,
+                       "digitizedBySefaria": True,
+                       "license": "PD",
+                       "status": "locked"
+                       })
+
+
+    modify_bulk_text(superuser_id, version, map_text)
+def ingest_nodes():
     en_title = 'Efodi on Guide for the Perplexed'
     he_title = 'אפודי על מורה נבוכים'
     root = SchemaNode()
     root.add_primary_titles(en_title, he_title)
-    intro = SchemaNode()
-    intro.add_shared_term("Introduction")
-    letter = JaggedArrayNode()
-    letter.add_structure(["Paragraph"])
-    prefatory = JaggedArrayNode()
-    prefatory.add_structure(["Paragraph"])
-    introduction = JaggedArrayNode()
-    introduction.add_structure(["Paragraph"])
-    intro.key = "Introduction"
-    letter.ket = "Letter to R Joseph son of Judah"
-    prefatory.key = "Prefatory Remarks"
-    introduction.key = "Introduction"
+    introduction_node = SchemaNode()
+    introduction_node.key = "Introduction"
+    introduction_node.add_shared_term("Introduction")
+    introduction_node.add_primary_titles("Introduction", 'הקדמה')
 
-    part2_intro = JaggedArrayNode()
-    part2_intro.add_structure(["Paragraph"])
-    part2_intro.key = "Part 2 Introduction"
+    letter_to_R_Joseph_son_of_Judah_node = JaggedArrayNode()
+    letter_to_R_Joseph_son_of_Judah_node.add_structure(["Paragraph"])
+    letter_to_R_Joseph_son_of_Judah_node.key = "Letter to R Joseph son of Judah"
+    letter_to_R_Joseph_son_of_Judah_node.add_primary_titles("Letter to R Joseph son of Judah", 'איגרת')
+    prefatory_Remarks_node = JaggedArrayNode()
+    prefatory_Remarks_node.add_structure(["Paragraph"])
+    prefatory_Remarks_node.key = "Prefatory Remarks"
+    prefatory_Remarks_node.add_primary_titles("Prefatory Remarks", 'פתיחת הרמב"ם')
+    inner_introduction_node = JaggedArrayNode()
+    inner_introduction_node.add_structure(["Paragraph"])
+    inner_introduction_node.key = "Introduction"
+    inner_introduction_node.add_primary_titles("Introduction", 'הקדמה')
 
-    part2 = JaggedArrayNode()
 
-    # default = JaggedArrayNode()
-    # default.default = True
-    # default.key = "default"
-    # default.add_structure(["Chapter", "Verse", "Comment"])
-    root.append(intro)
-    # root.append(default)
+    # insert_last_child(introduction_node, letter_to_R_Joseph_son_of_Judah_node)
+    # insert_last_child(introduction_node, prefatory_Remarks_node)
+    # insert_last_child(introduction_node, inner_introduction_node)
+    introduction_node.append(letter_to_R_Joseph_son_of_Judah_node)
+    introduction_node.append(prefatory_Remarks_node)
+    introduction_node.append(inner_introduction_node)
+
+    part1_node = JaggedArrayNode()
+    part1_node.add_structure(["Chapter", "Paragraph"])
+    part1_node.key = "Part 1"
+    part1_node.add_primary_titles("Part 1", 'חלק א')
+
+
+    part2_introduction_node = JaggedArrayNode()
+    part2_introduction_node.add_structure(["Paragraph"])
+    part2_introduction_node.key = "Part 2 Introduction"
+    part2_introduction_node.add_primary_titles("Part 2 Introduction", "חלק ב' הקדמה")
+
+    part2_node = JaggedArrayNode()
+    part2_node.add_structure(["Chapter", "Paragraph"])
+    part2_node.key = "Part 2"
+    part2_node.add_primary_titles("Part 2", 'חלק ב')
+
+    part3_introduction_node = JaggedArrayNode()
+    part3_introduction_node.add_structure(["Paragraph"])
+    part3_introduction_node.key = "Part 3 Introduction"
+    part3_introduction_node.add_primary_titles("Part 3 Introduction", "חלק ג' הקדמה")
+
+    part3_node = JaggedArrayNode()
+    part3_node.add_structure(["Chapter", "Paragraph"])
+    part3_node.key = "Part 3"
+    part3_node.add_primary_titles("Part 3", 'חלק ג')
+
+    root.append(introduction_node)
+    root.append(part1_node)
+    root.append(part2_introduction_node)
+    root.append(part2_node)
+    root.append(part3_introduction_node)
+    root.append(part3_node)
     root.validate()
+
+    index = {
+        "title": en_title,
+        "schema": root.serialize(),
+        "categories": ["Jewish Thought"]
+    }
+    post_index(index, server="https://guide-commentaries.cauldron.sefaria.org")
+    # post_index(index)
+
     # post_index({"title": en_title,
     #             "base_text_titles": ["Proverbs"],
     #             "base_text_mapping": "many_to_one_default_only",
     #             "dependence": "Commentary", "collective_title": "Alshich",
     #             "categories": ["Tanakh", "Commentary", "Alshich", "Writings"], "schema": root.serialize()})
 
-    superuser_id = 171118
+if __name__ == '__main__':
+    print("hello world")
+    # "Guide for the Perplexed, Part 1 2:7"
+    # ingest_nodes()
+    obj = create_text_object()
+    print(obj)
+    ingest_version(obj)
+
+
+
+
+
 
