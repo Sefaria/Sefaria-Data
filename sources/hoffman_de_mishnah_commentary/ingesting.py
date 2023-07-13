194faad68e8ece22341bb18755aaac6f48638d89
saengel <sarah@sefaria.org>
2023-07-13 10:36:50 +0300

    feat(Hoffman Mishnah): In-progress, error with ingest for intro

--- sources/hoffman_de_mishnah_commentary/ingesting.py
+++ sources/hoffman_de_mishnah_commentary/ingesting.py
@@ -7,6 +7,7 @@ import csv
 from collections import defaultdict
 from sefaria.model import *
 from sefaria.tracker import modify_bulk_text, modify_text
+from sources.functions import post_text
 from sources.hoffman_de_mishnah_commentary.extract_commentary import create_text_data_dict
 from sources.hoffman_de_mishnah_commentary.parse_intro_xml import process_xml
 
@@ -35,36 +36,34 @@ def create_version_from_scratch(title, versionTitle):
                        "language": "en",
                        "digitizedBySefaria": True,
                        "license": "Public Domain",
-                       # "status": "locked",
+                       "status": "unlocked",
                        })
-    return version
+    version.save()
 
-def upload_intro():
-    # TODO automate which tref it's using
 
-    intro_dict = process_xml()
-    intro_oref = Ref("German Commentary on Mishnah Berakhot, Introduction")
-    modify_text(user=142625,
-                oref=intro_oref,
-                vtitle="Mischnajot mit deutscher Übersetzung und Erklärung. Berlin 1887-1933 [de]",
-                lang="en",
-                text=intro_dict["Traktat Berachot"])
+def generate_text_post_format(text):
+    return {
+        "text": [text],
+        "versionTitle": "Mischnajot mit deutscher Übersetzung und Erklärung. Berlin 1887-1933 [de]",
+        "versionSource": "talmud.de",
+        "language": "en"
+    }
 
 
 def upload_text(mappings):
     for book, book_map in mappings.items():
-        version = create_version_from_scratch(book, "Mischnajot mit deutscher Übersetzung und Erklärung. Berlin 1887-1933 [de]")
+        create_version_from_scratch(book, "Mischnajot mit deutscher Übersetzung und Erklärung. Berlin 1887-1933 [de]")
         print(f"Uploading text for {book}")
-        # Ingest Introduction:
 
-        modify_bulk_text(user=142625,
-                         version=version,
-                         text_map=book_map,
-                         skip_links=True,
-                         count_after=False)
+        intro_dict = process_xml()
+        tref = "German Commentary on Mishnah Berakhot, Introduction"
+        text = generate_text_post_format(intro_dict["Traktat Berachot"])
+        post_text(ref=tref, text=text, server="http://localhost:8000")
+        # for tref in book_map:
+        #     text = generate_text_post_format(book_map[tref])
+        #     post_text(ref=tref, text=text, server="http://localhost:8000")
 
 
 if __name__ == '__main__':
     map = create_mappings()
-    upload_intro()
     upload_text(map)
