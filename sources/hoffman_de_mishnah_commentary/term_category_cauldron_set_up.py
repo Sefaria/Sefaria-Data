44f3681ebe09978fada8b1a37e74250bb5384a7b
saengel <sarah@sefaria.org>
2023-08-06 09:12:32 +0300

    fix(Hoffman Mishnah): Fix indent to always run, debug

--- sources/hoffman_de_mishnah_commentary/term_category_cauldron_set_up.py
+++ sources/hoffman_de_mishnah_commentary/term_category_cauldron_set_up.py
@@ -4,6 +4,7 @@ import django
 django.setup()
 
 from sefaria.model import *
+from sefaria.helper import category
 
 
 def create_term_and_category():
@@ -16,12 +17,13 @@ def create_term_and_category():
         t.save()
 
     # Create a Category, if it doesn't yet exist
-    cs = CategorySet({'sharedTitle': 'German Commentary'})
-    if cs.count() == 0:
-        c = Category()
-        c.path = ["Mishnah", "Modern Commentary on Mishnah", "German Commentary"]
-        c.add_shared_term("German Commentary")
-        c.save()
+    print("Creating general German Commentary category")
+    category.create_category(["Mishnah", "Modern Commentary on Mishnah", "German Commentary"])
+    print("Creating seder-specific German Commentary category")
+    sedarim = ["Seder Zeraim", "Seder Moed", "Seder Nashim", "Seder Nezikin", "Seder Kodashim", "Seder Tahorot"]
+    for seder in sedarim:
+        category.create_category(["Mishnah", "Modern Commentary on Mishnah", "German Commentary", seder])
+
 
 if __name__ == '__main__':
     create_term_and_category()
