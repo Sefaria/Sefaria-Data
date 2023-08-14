b3768072dbbe4f08a6c9dc588aca2a1aae5147ee
saengel <sarah@sefaria.org>
2023-08-14 13:01:03 +0300

    fix(MDTI Clean up): Refactor to pull API key from local_settings

--- sources/Content_Quality/move_draft_text_interface.py
+++ sources/Content_Quality/move_draft_text_interface.py
@@ -1,6 +1,7 @@
 import django
 django.setup()
 from sefaria.model import *
+from sources.local_settings import API_KEY, SEFARIA_SERVER
 
 import time
 import os
@@ -11,8 +12,8 @@ indices = []
 
 
 # Change the following according to your specific needs
-SEFARIA_SERVER = "https://www.sefaria.org"
-api_key = ""
+sefaria_server = SEFARIA_SERVER
+api_key = API_KEY
 
 # Specify which indices you need, and which version.
 indices = ["Torah Ohr", "Likkutei Torah"]
@@ -22,6 +23,6 @@ lang = "he"
 
 for m in indices:
      print(m)
-     cmd = f"./run scripts/move_draft_text.py '{m}' --noindex -v '{lang}:{vtitle}' -k '{api_key}' -d '{SEFARIA_SERVER}'"
+     cmd = f"./run scripts/move_draft_text.py '{m}' --noindex -v '{lang}:{vtitle}' -k '{api_key}' -d '{sefaria_server}'"
      results.append(os.popen(cmd).read())
      time.sleep(5)
