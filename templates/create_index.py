eeb36a45a807b0f0587d11570cda7768dfe459b1
saengel <sarah@sefaria.org>
2023-08-29 13:51:40 +0300

    feat(templates): Working create index, no intros

--- templates/create_index.py
+++ templates/create_index.py
@@ -10,35 +10,36 @@ from sources import functions
 
 def create_term_and_category():
     # Create Term, if it doesn't yet exist
-    ts = TermSet({'name': '/German Commentary/'})
+    ts = TermSet({'name': 'My Sample Commentary'})
     if ts.count() == 0:
-        # Term for the Collective Title and the Category
+        # Term for the Collective Title
         t = Term()
-        t.name = "German Commentary"
-        t.add_primary_titles("German Commentary", "פירוש גרמני")
+        t.name = "My Sample Commentary"
+        t.add_primary_titles("My Sample Commentary", "פירוש דוגמא")
         t.save()
 
     # Create a Category, if it doesn't yet exist
-    cs = CategorySet({'sharedTitle': 'German Commentary'})
+    cs = CategorySet({'sharedTitle': 'My Sample Commentary'})
     if cs.count() == 0:
-        category.create_category(["Mishnah", "Modern Commentary on Mishnah", "German Commentary"])
-        sedarim = ["Seder Zeraim", "Seder Moed", "Seder Nashim", "Seder Nezikin", "Seder Kodashim", "Seder Tahorot"]
-        for seder in sedarim:
-            category.create_category(["Mishnah", "Modern Commentary on Mishnah", "German Commentary", seder])
+        category.create_category(["Mishnah", "Modern Commentary on Mishnah", "My Sample Commentary"])
 
 
-def create_index_record(en_title, he_title):
+def create_index_record(masechet, he_masechet):
     record = SchemaNode()
+    he_title = "פירוש דוגמא על " + he_masechet  # f-String was not working well with RTL text
+    en_title = f"My Sample Commentary on {masechet}"
     record.add_title(en_title, 'en', primary=True, )
     record.add_title(he_title, "he", primary=True, )
-    record.key = f"Commentary on {en_title}"
+    record.key = f"My Sample Commentary on {masechet}" if masechet == "Pirkei Avot" else f"My Sample Commentary on {masechet}"
+    record.collective_title = "My Sample Commentary"  # Must be a term
     return record
 
 
 # Text node:
 def add_text_node(record):
     text_node = JaggedArrayNode()
-    text_node.key = record.primary_title()
+    text_node.key = "default"
+    text_node.default = record.primary_title()
     text_node.depth = 3
     text_node.addressTypes = ['Integer', 'Integer', 'Integer']
     text_node.sectionNames = ['Chapter', 'Mishnah', 'Paragraph']
@@ -46,28 +47,30 @@ def add_text_node(record):
 
 
 def create_index_main():
-    en_title = "My Book"
-    he_title = "הספר שלי"
+    mishnayot = library.get_indexes_in_category("Mishnah", full_records=True)
 
-    record = create_index_record(en_title, he_title)
-    add_text_node(record)
-    record.validate()
+    for mishnah_index in mishnayot:
+        en_title = mishnah_index.get_title("en")
+        he_title = mishnah_index.get_title("he")
 
-    index = {
-        "title": record.primary_title(),
-        "categories": ["Mishnah", "Modern Commentary on Mishnah", "German Commentary"],
-        "schema": record.serialize(),
-        "base_text_titles": [en_title],
-        "base_text_mapping": "many_to_one",
-        "is_dependant": True,
-        "dependence": "Commentary",
-        "collective_title": "German Commentary"
-    }
+        record = create_index_record(en_title, he_title)
+        add_text_node(record)
 
-    functions.post_index(index)
-    print(index)
+        record.validate()
+        index = {
+            "title": record.primary_title(),
+            "categories": ["Mishnah", "Modern Commentary on Mishnah", "My Sample Commentary"],
+            "schema": record.serialize(),
+            "base_text_titles": [en_title],
+            "base_text_mapping": "many_to_one",
+            "is_dependant": True,
+            "dependence": "Commentary"
+        }
+
+        functions.post_index(index)
+        print(index)
 
 
 if __name__ == '__main__':
-    # create_term_and_category()
+    create_term_and_category()
     create_index_main()
