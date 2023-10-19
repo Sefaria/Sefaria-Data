d2d0aedba6642f9d490180a3f8af1c8ecad0e3f2
saengel <sarah@sefaria.org>
2023-10-19 16:34:59 +0300

    chore(Beeri Mekhilta): Added notes

--- sources/Content_Quality/beeri-mekhilta/implement_cascade.py
+++ sources/Content_Quality/beeri-mekhilta/implement_cascade.py
@@ -4,7 +4,9 @@ django.setup()
 
 from sefaria.model import *
 from sefaria.helper.schema import cascade
+from sources.functions import post_text, post_index, post_link, add_category
 import csv
+import re
 
 
 def ingest_map():
@@ -12,18 +14,43 @@ def ingest_map():
         reader = csv.DictReader(f)
         map = {}
         for row in reader:
-            map[row["prod_ref"]] = row["beeri_ref"]
+            if row["prod_ref"] in map:
+                map[row["prod_ref"]].append(row["beeri_ref"])
+            else:
+                map[row["prod_ref"]] = [row["beeri_ref"]]
     return map
 
 
 def rewriter_function(prod_ref):
     mapper = ingest_map()
+    ranged_ref_list = []
+    # print(prod_ref)
     if prod_ref in mapper:
-        return mapper[prod_ref]
-    else:  # Handle ranged refs
+        cur_beeri_ref_list = mapper[prod_ref]
+        if len(cur_beeri_ref_list) > 1:
+            cleaned_end_ref = re.sub(r'[a-zA-Z\s\',]', '', cur_beeri_ref_list[len(cur_beeri_ref_list)-1])
+            return f"{cur_beeri_ref_list[0]}-{cleaned_end_ref}"
+        else:
+            return cur_beeri_ref_list[0]
+    elif "Mekhilta d'Rabbi Yishmael" in prod_ref:  # Handle ranged refs
         for ref_key in mapper:
             if Ref(ref_key).contains(Ref(prod_ref)):
-                return mapper[ref_key]
+                if len(mapper[ref_key]) > 1:
+                    cleaned_end_ref = re.sub(r'[a-zA-Z\s\',]', '', mapper[ref_key][len(mapper[ref_key])-1])
+                    return f"{mapper[ref_key][0]}-{cleaned_end_ref}"
+                else:
+                    return mapper[ref_key][0]
+            elif Ref(prod_ref).contains(Ref(ref_key)):
+                ranged_ref_list += mapper[ref_key]
+                # print(f"Since {prod_ref} contains {ref_key}, appending {mapper[ref_key]}")
+        rr_str = ""
+        if ranged_ref_list and len(ranged_ref_list) > 1:
+            cleaned_end_ref = re.sub(r'[a-zA-Z\s\',]', '', ranged_ref_list[len(ranged_ref_list)-1])
+            rr_str = f"{ranged_ref_list[0]}-{cleaned_end_ref}"
+        elif ranged_ref_list:
+            rr_str = ranged_ref_list[0]
+        return rr_str
+
 
 
 def action(segment_str, tref, he_tref, version):
@@ -55,15 +82,50 @@ def rename_books():
     print(f"Saved and renamed {index.title}")
 
 
+def order_links(refs):
+    if "Mekhilta d'Rabbi Yishmael Old" in refs[0]:
+        return refs[1], refs[0], True
+    return refs[0], refs[1], False
+
+
 if __name__ == '__main__':
 
     # TODO - Run this first
     # rename_books()
 
-    prod_refs = []
-    retrieve_version_text()
+    # prod_refs = []
+    # retrieve_version_text()
 
-    mapper = ingest_map()
-    for prod_ref in prod_refs:
-        print(f"Working on {prod_ref}")
-        cascade(prod_ref, rewriter=lambda beeri_ref: rewriter_function(prod_ref), skip_history=False)
+    # mapper = ingest_map()
+    # for prod_ref in prod_refs:
+    #     print(f"Working on {prod_ref}")
+    #     cascade(prod_ref, rewriter=lambda beeri_ref: rewriter_function(prod_ref), skip_history=False)
+
+    query = {"refs": {"$regex": "Mekhilta d'Rabbi Yishmael Old"}}
+    ls = LinkSet(query)
+
+    generated_by_list_set = []
+
+    print(ls.count())
+
+    # TODO - read in CSV here. 
+
+    old_ref_list = []
+    print("linked_ref, old_mekhilta_ref, beeri_ref, is_auto_linked, from_mekhilta_to_external")
+    for link in ls:
+        other_ref, mekhilta_old_ref, from_mekhilta = order_links(link.refs)
+        beeri_mekhilta_ref = rewriter_function(mekhilta_old_ref)
+
+        # post_link({'refs': [other_ref, beeri_mekhilta_ref]})
+        generated_by_list_set.append(link.generated_by)
+        # exclude automatic
+        if link.generated_by == "add_links_from_text":
+            print(f"\"{other_ref}\", \"{mekhilta_old_ref}\", \"{beeri_mekhilta_ref}\", True, {from_mekhilta}")
+        elif beeri_mekhilta_ref:
+            print(f"\"{other_ref}\", \"{mekhilta_old_ref}\", \"{beeri_mekhilta_ref}\", False, {from_mekhilta}")
+        if not beeri_mekhilta_ref:
+            old_ref_list.append(mekhilta_old_ref)
+    print(f"{len(old_ref_list)} refs need to be dealt with")
+    print(old_ref_list)
+    print(set(generated_by_list_set))
+
