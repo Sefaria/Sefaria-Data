9f6edbe939917c9287badad7548a9a5311267c7a
saengel <sarah@sefaria.org>
2023-12-05 12:13:38 +0200

    chore(Beeri Mekhilta): Fix bugs in source sheet script

--- sources/Content_Quality/beeri-mekhilta/source_sheet_mapper_generator.py
+++ sources/Content_Quality/beeri-mekhilta/source_sheet_mapper_generator.py
@@ -6,7 +6,9 @@ django.setup()
 from sefaria.model import *
 import csv
 
+
 # TODO: Fix rewriter function to handle errors
+# TODO: Also need to handle 'sources' in DB
 # TODO: conditional debug on uuid, why the rewriter function is failing, and rewrite to fix
 
 def ingest_map():
@@ -20,6 +22,7 @@ def ingest_map():
                 map[row["prod_ref"]] = [row["beeri_ref"]]
     return map
 
+
 def make_prod_ref_old(prod_ref):
     idx = prod_ref.find("Yishmael")
     idx = idx + len("Yishmael")
@@ -27,8 +30,9 @@ def make_prod_ref_old(prod_ref):
     return new_prod_ref
 
 
-def rewriter_function(prod_ref, mapper, uuid=None):
-    if "old" not in prod_ref: # i.e. prod ref hasn't been affected by name change
+def rewriter_function(prod_ref, mapper, uuid=None, id=None):
+    set_list = []
+    if "old" not in prod_ref:  # i.e. prod ref hasn't been affected by name change
         prod_ref = make_prod_ref_old(prod_ref)
     # If a segment-level ref
     if prod_ref and prod_ref in mapper:
@@ -46,12 +50,18 @@ def rewriter_function(prod_ref, mapper, uuid=None):
             return cur_beeri_ref_list[0]
 
     ## TODO - maybe take this out so we can catch segment levels (trying to catch upper section, but only one instance)
-    elif prod_ref and not Ref(prod_ref).is_segment_level():
-        print(f"{prod_ref} needs to be handled manually for UUID {uuid}")
+    elif prod_ref in ["Mekhilta d'Rabbi Yishmael Old 22",
+                      "Mekhilta d'Rabbi Yishmael Old 19",
+                      "Mekhilta d'Rabbi Yishmael Old 20",
+                      "Mekhilta d'Rabbi Yishmael Old 17",
+                      "Mekhilta d'Rabbi Yishmael Old 1",
+                      "Mekhilta d'Rabbi Yishmael Old 3",
+                      "Mekhilta d'Rabbi Yishmael Old 2"]:
+        print(f"Sheet ID: {sheet_id} -- {prod_ref} needs to be handled manually for (user: {uuid})")
         return ""
 
     # If a section or book level ref, determine the mapping based on the segment refs within
-    elif prod_ref and Ref(prod_ref).is_range():
+    elif prod_ref and (not Ref(prod_ref).is_segment_level() or Ref(prod_ref).is_range()):
         segment_ranged_ref = Ref(prod_ref).as_ranged_segment_ref()
         start_tref = segment_ranged_ref.starting_ref().normal()
         end_tref = segment_ranged_ref.ending_ref().normal()
@@ -60,10 +70,10 @@ def rewriter_function(prod_ref, mapper, uuid=None):
             last_oref = Ref(mapper[end_tref][0])
             ranged_ref = first_oref.to(last_oref)
             return ranged_ref.normal()
-        print(f"ERROR for {prod_ref}")
+        print(f"Sheet ID: {sheet_id} -- {prod_ref} not a valid ref (user: {uuid})")
         return ""
 
-    print(f"ERROR for {prod_ref}")
+    print(f"Sheet ID: {sheet_id} -- {prod_ref} not a valid ref (user: {uuid})")
     return ""
 
 
@@ -71,11 +81,9 @@ if __name__ == '__main__':
 
     mapping = ingest_map()
 
-
     # For each source sheet quoting mekhilta
     # capture the UUID, the old_ref and the new_ref
     # Generate a CSV that can be used on prod with a script to replace.
-
 
     local_uri = "mongodb://localhost:27017"
 
@@ -97,21 +105,22 @@ if __name__ == '__main__':
     # Print or process the result
     for document in result:
         uuid = document["owner"]
-        mapper_dict = {"uuid": uuid, "includedRefsMap": [], "expandedRefsMap": []}
+        sheet_id = document["id"]
+        mapper_dict = {"uuid": uuid, "sheet_id": sheet_id, "includedRefsMap": [], "expandedRefsMap": []}
 
         for ref in document["includedRefs"]:
             if "Mekhilta d'Rabbi Yishmael" in ref:
-                mapper_dict["includedRefsMap"].append({"old_ref": ref, "new_ref": rewriter_function(ref, mapping, uuid)})
+                mapper_dict["includedRefsMap"].append(
+                    {"old_ref": ref, "new_ref": rewriter_function(ref, mapping, uuid, sheet_id)})
 
         for ref in document["expandedRefs"]:
             if "Mekhilta d'Rabbi Yishmael" in ref:
-                mapped_ref = rewriter_function(ref, mapping, uuid)
+                mapped_ref = rewriter_function(ref, mapping, uuid, sheet_id)
                 if mapped_ref:
                     ref_list = Ref(mapped_ref).all_segment_refs()
                     tref_list = []
                     for r in ref_list:
                         tref_list.append(r.normal())
-
 
                 mapper_dict["expandedRefsMap"].append({"old_ref": ref, "new_refs": ref_list})
 
@@ -119,5 +128,5 @@ if __name__ == '__main__':
 
     # Close the MongoDB connection
     client.close()
-
-    print(results)
+    print("FINISHED")
+    # print(results)
