3c6fe7246879a6e33419f1f884987692d886a433
saengel <sarah@sefaria.org>
2023-12-06 13:04:50 +0200

    chore(Beeri Mekhilta): Add Hebrew to rename books script

--- sources/Content_Quality/beeri-mekhilta/rename_books.py
+++ sources/Content_Quality/beeri-mekhilta/rename_books.py
@@ -6,12 +6,12 @@ from sefaria.model import *
 import time
 
 
-
 def rename_old():
     index_query = {"title": "Mekhilta DeRabbi Yishmael"}
     index = Index().load(index_query)
     print(f"Retrieved {index.title}")
-    index.set_title("Mekhilta d'Rabbi Yishmael Old")
+    index.set_title("Mekhilta d'Rabbi Yishmael Old", lang="en")
+    index.set_title("מכילתא דרבי ישמעאל ישן", lang="he")
     index.save()
     print(f"Saved and renamed {index.title}")
 
@@ -20,7 +20,8 @@ def rename_new():
     index_query = {"title": "Mekhilta DeRabbi Yishmael Beeri"}
     index = Index().load(index_query)
     print(f"Retrieved {index.title}")
-    index.set_title("Mekhilta DeRabbi Yishmael")
+    index.set_title("Mekhilta DeRabbi Yishmael", lang="en")
+    index.set_title("מכילתא דרבי ישמעאל", lang="he")
     index.save()
     print(f"Saved and renamed {index.title}")
 
