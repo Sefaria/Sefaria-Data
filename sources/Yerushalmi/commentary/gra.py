8c1f4c6f273028e85db9cf6ae90eb5a417131deb
YishaiGlasner <ishaigla@gmail.com>
2024-01-17 11:35:20 +0200

    gra on yerushalmi

--- sources/Yerushalmi/commentary/gra.py
+++ sources/Yerushalmi/commentary/gra.py
@@ -1,16 +1,15 @@
-import json
 import django
 django.setup()
 from sefaria.model import *
-import csv
+from docx import Document
 import os
 import re
 from sources.functions import getGematria
-# from sources.Yerushalmi.yutil import OverlayBuilder
+import json
+from sefaria.utils.talmud import section_to_daf, daf_to_section
 from sources.tosefta_scans.zuckermandel import unite_refs
 from linking_utilities.dibur_hamatchil_matcher import match_ref
-from sefaria.utils.talmud import section_to_daf, daf_to_section
-from docx import Document
+import csv
 
 YERUSHALMI = 'Jerusalem Talmud'
 HEB_YER = 'תלמוד ירושלמי'
@@ -65,97 +64,14 @@ VILNA_MAPPING['Jerusalem Talmud Yoma 5, 24a'] = 'Jerusalem Talmud Yoma 5:1:1'
 VILNA_MAPPING['Jerusalem Talmud Kilayim 3, 12b'] = 'Jerusalem Talmud Kilayim 3:1:1'
 VILNA_MAPPING['Jerusalem Talmud Niddah, 13a'] = 'Jerusalem Talmud Kilayim 4:6-7'
 
-def find_coms():
-    with open('masechtot.csv', encoding='utf-8', newline='') as fp:
-        data = csv.DictReader(fp)
-        fields = data.fieldnames
-        return {row['ירושלמי']: [f for f in fields if row[f].isdigit()] for row in data}
-
-def get_details():
-    with open('commentaries.csv', encoding='utf-8', newline='') as fp:
-        return {row['he']: row for row in csv.DictReader(fp)}
-
-def get_com_data(mas, com):
-    with open('trans.csv', encoding='utf-8', newline='') as fp:
-        trans = {row['he']: row['en'] for row in csv.DictReader(fp)}
-    def transliter(letter):
-        try:
-            return trans[letter]
-        except KeyError:
-            if letter == ' ':
-                return letter
-            else:
-                return ''
-
-    # encom = ''.join([transliter(l) for l in com])
-    # for root, dirs, files in os.walk('data/'):
-    #     files = [file for file in files if mas in file]
-    #     exact = [file for file in files if encom in file
-    #              and (com!='עמודי ירושלים' or all(w not in file for w in ['btra', 'tnyna']))
-    #              and (com!='ביאור הגר"א' or 'ktb' not in file)]
-    #     if len(exact) == 0:
-    #         regexes = ['h(fnym)', 'ry(dbz)', '(tws)fwt ryd', 'syy(ry )']
-    #         for reg in regexes:
-    #             encom = re.sub(reg, r'\1', encom)
-    #         if encom == 'byawr hgra ktb yd':
-    #             encom = 'ktb yd'
-    #         encom = encom.replace('hghwt yfm', 'mhrm yfm')
-    #         encom = encom.replace('hghwt rda', 'rda')
-    #         encom = encom.replace('mhrm dy lwnzanw', ' lw')
-    #         encom = encom.replace('chydwsy rag', 'ra_g')
-    #         encom = encom.replace('mhra grydts', 'gry')
-    #         exact = [file for file in files if encom in file]
-    #         if len(exact) == 0:
-    #             encom = encom.replace('mhrm yfm', 'mhrm yfym')
-    #             encom = encom.replace('rda', 'rd_a')
-    #             encom = encom.replace('ra_g', 'rag')
-    #             encom = encom.replace(' lw', 'dylw')
-    #             exact = [file for file in files if encom in file]
-    # if len(exact) == 1:
-    #     return open(f'data/{exact[0]}', encoding='utf-8').read()
-    # elif len(exact) == 0:
-    #     print(f'doesnt find com {com}-{encom} on {mas}. all files on masechet:', files)
-    # else:
-    #     print(f'more than on file for commentary {com} on {mas}:', exact)
-    for root, dirs, files in os.walk('data/'):
-        file = [file for file in files if mas in file][0]
-        if '.doc' in file:
-            doc = Document(f'data/{file}')
-            return '\n'.join([paragraph.text for paragraph in doc.paragraphs])
-        else:
-            return open(f'data/{file}').read()
-
-def find_tags(text):
-    base_tag = r'^(?:@\d\d|\d|\$|~|&)'
-    return {'page': '^(?:@\d\d|\d|\$|~|&)? ?\(? ?(?:דף|שם ע"ב)|^(?:@\d\d|\d|\$|~|&)\(?שם', 'perek': base_tag + '(?:פרק |פ")([א-כ]"?[א-ט]?)(?: |\'|$)',
-            'halakha': base_tag + '(?:הלכה |ה")([א-בד-וח-כ]"?[א-ט]?|[גז](?!.{10,}))(?: |\'|$)', 'hadran': base_tag + '(?:הדרן|סליק)'}
-    '''reg = r'[\d@#$%^&\*!\?\{\}]+'
-    tags = set(re.findall(reg, text))
-    for tag in tags:
-        tag_re = re.escape(tag)
-        if text.count(f'{tag}פרק') + text.count(f'{tag}פ"') > 3:
-            perek_tag = tag
-        elif text.count(f'{tag}הלכה') > 10:
-            halakha_tag = tag
-        elif text.count(f'{tag}דף') > 10:
-            page_tag = tag
-        elif len(re.findall(f'{tag_re}(?:הדרן|סליק)', text)) > 3:
-            hadran_tag = tag
-        else:
-            if text.count(f'{tag}פרק')'''
-
 def del_trash(text):
     return text.replace('\ufeff', '').strip()
 
-def split_pages(text, tags):
-    text = re.split(f'({tags["page"]})', text)
-    '''if text[0].strip():
-        print(text[0])'''
-    text[1] = text[0] + text[1]
-    text = [text[i] + text[i+1] for i in range(len(text)) if i % 2 != 0]
-
 def parse_text(text, mas, com_name):
-    tags = find_tags(text)
+    tags = {'page': r'^(?:@\d\d|\d|\$|~|&)? ?\(? ?(?:דף|שם ע"ב|ע"ב)|^(?:@\d\d|\d|\$|~|&)\(?שם',
+         'perek': r'^(?:@\d\d|\d|\$|~|&)(?:פרק |פ")([א-כ]"?[א-ט]?)(?: |\'|$)',
+         'halakha': r'^(?:@\d\d|\d|\$|~|&)(?:הלכה |ה")([א-בד-וח-כ]"?[א-ט]?|[גז](?!.{10,}))(?: |\'|$)',
+         'hadran': '^(?:@\\d\\d|\\d|\\$|~|&)(?:הדרן|סליק)'}
     text = del_trash(text)
     arrayed = [[[]]]
     comments = []
@@ -165,6 +81,10 @@ def parse_text(text, mas, com_name):
     amud = 0
     prev = False
     mid = False
+    ordinals = ['ראשון', 'שני', 'שלישי', 'רביעי', 'חמישי', 'ששי', 'שביעי', 'שמיני', 'תשיעי', 'עשירי', 'אחד עשר']
+    ore_reg = '|'.join(ordinals)
+    ordinals = {k: v for v, k in enumerate(ordinals, 1)}
+
     for line in text.split('\n'):
         line = ' '.join(line.split())
         line = re.sub("(.{,3}מתני'|גמ'):", r'\1', line)
@@ -172,13 +92,12 @@ def parse_text(text, mas, com_name):
             continue
 
         #perek
-        if re.findall(tags['perek'], line):
-            perek = getGematria(re.findall(tags['perek'], line)[0])
+        perek_search = re.search(f'@\d\d.*?פרק ({ore_reg})$', line)
+        if perek_search:
+            perek = ordinals[perek_search.group(1)]
             if len(arrayed) < perek:
                 arrayed += [[[]] for _ in range(perek-len(arrayed))]
-            line = re.split(tags['perek'], line)[-1].strip()
-            if len(line) < 4:
-                continue
+            continue
 
         #halakha
         if re.findall(tags['halakha'], line):
@@ -211,7 +130,7 @@ def parse_text(text, mas, com_name):
                     daf = getGematria(page[0])
                     amud = 'a' if page[1] == 'א' else 'b'
                 else:
-                    page = re.findall('דף ([א-צ]"?\'?[א-ט]?) ב?(מתני|גמ)', line)[0]
+                    page = re.findall('דף ([א-צ]"?\'?[א-ט]?) ב?(מתני|גמ)', line)
                     if page:
                         temp_daf = getGematria(page[0][0])
                         if temp_daf != daf:
@@ -219,27 +138,9 @@ def parse_text(text, mas, com_name):
                         daf = temp_daf
                     else:
                         print('cant find page in page tag')
-                if '(' in tag:
-                    if ')' in line[:25]:
-                        line = line.split(')', 1)[1].strip()
-                    else:
-                        if len(line) < 20:
-                            prev = line.strip()
-                            continue
-                        else:
-                            print("problem with splitting line", line)
-                else:
-                    line = re.split(f'{tag} [א-צ]"?\'?[א-ט]? [^ ]*(?: |$)', line, 1)[1].strip()
-            elif 'שם' in tag:
-                if 'שם ע"ב' in line[:13]:
-                    amud = 'b'
-                if '(' in tag:
-                    line = line.split(')', 1)[1].strip()
-                else:
-                    line = re.split(f'{tag}(?: [^ ]*(?: |$))?', line, 1)[1].strip()
-            if len(line) < 10:
-                prev = line.strip()
-                continue
+            elif 'ע"ב' in tag:
+                amud = 'b'
+            continue
 
         if re.search("^.{0,5}(?:גמ'|מתני')\.?$", line):
             prev = line.strip()
@@ -268,6 +169,18 @@ def parse_text(text, mas, com_name):
                 mid = True if not comment.strip().endswith(':') and dh else False
 
     return comments
+
+def is_halakhot_nember_equal(comments):
+    pereks = {com['perek'] for com in comments}
+    fixed_halakhot = {}
+    for perek in pereks:
+        halakhot = {com['halakha'] for com in comments if com['perek'] == perek}
+        last_halkha = max(halakhot)
+        yer_halakhot = len(Ref(f'{YERUSHALMI} {comments[0]["masechet"]} {perek}').all_subrefs())
+        fixed_halakhot[perek] = True if last_halkha == yer_halakhot else False
+        if last_halkha != yer_halakhot:
+            print('not equal halakhot', comments[0]["masechet"], perek, halakhot, yer_halakhot)
+    return fixed_halakhot
 
 def find_vilna_page(ref, page):
     perek = 0
@@ -315,18 +228,6 @@ def find_ref(mas, perek, halakha, page):
     else: #page q/o halakha
         return page_ref
 
-def is_halakhot_nember_equal(comments):
-    pereks = {com['perek'] for com in comments}
-    fixed_halakhot = {}
-    for perek in pereks:
-        halakhot = {com['halakha'] for com in comments if com['perek'] == perek}
-        last_halkha = max(halakhot)
-        yer_halakhot = len(Ref(f'{YERUSHALMI} {comments[0]["masechet"]} {perek}').all_subrefs())
-        fixed_halakhot[perek] = True if last_halkha == yer_halakhot else False
-        if last_halkha != yer_halakhot:
-            print('not equal halakhot', comments[0]["masechet"], perek, halakhot, yer_halakhot)
-    return fixed_halakhot
-
 def match_base(comments):
     has_halakha = True if 2 in [com['halakha'] for com in comments] else False
     has_perek = True if 2 in [com['perek'] for com in comments] or has_halakha else False
@@ -355,6 +256,9 @@ def match_base(comments):
         gen_ref = gen_ref.replace('JTmock', YERUSHALMI)
         text = Ref(gen_ref).text('he', 'Mechon-Mamre')
         dhs = [com['dh'] for com in pack if com['dh']]
+        if not text.text:
+            print(f'no text for {gen_ref}')
+            continue
         matches = match_ref(text, dhs, lambda x: x.split())['matches']
         for com in pack:
             if com['dh']:
@@ -365,13 +269,13 @@ def match_base(comments):
 def match_between_refs(comments):
     prev, next = '', ''
     for i, com in enumerate(comments):
-        if com['base text ref']:
+        if com.get('base text ref', None):
             prev = com['base text ref']
             next = ''
         else:
             if not next:
                 for c in comments[i+1:]:
-                    if c['base text ref']:
+                    if c.get('base text ref', None):
                         next = c['base text ref']
                         break
             if prev == next:
@@ -379,59 +283,39 @@ def match_between_refs(comments):
             elif re.search("^.{0,5}(?:גמ')\.?$", com['comment']) and next[-2:] == (':2'):
                 com['base text ref'] = next
                 prev = com['base text ref']
-                print(111, com)
             elif re.search("^.{0,5}(?:מתני')\.?$", com['comment']) and next[-2:] in [':2', ':1']:
                 com['base text ref'] = next[:-2] + ':1'
                 prev = com['base text ref']
-                print(222, com)
-        if com['base text ref']:
+        if com.get('base text ref', None):
             com['base text'] = com['base text ref'].text('he', 'Mechon-Mamre').text
         else:
             com['base text'] = None
     return comments
 
-def parse_com(com_name, mas, he_mas):
-    com = coms_details[com_name]
-    text = get_com_data(he_mas, com_name)
-    comments = parse_text(text, mas, com_name)
-    comments = match_base(comments)
-    comments = match_between_refs(comments)
-    return comments
+all = []
+for root, dirs, files in os.walk('data/'):
+    for file in files:
+        if 'docx' not in file:
+            continue
+        doc = Document(f'data/{file}')
+        print(file)
+        data = '\n'.join([paragraph.text for paragraph in doc.paragraphs])
+        he_mas = file.split('הגרא')[1].split('.')[0].split('כתב יד')[0].strip()
+        mas = Ref(he_mas).normal().split('Mishnah ')[1]
+        if he_mas == 'פאה':
+            data = data.replace('@22(דל"ד ע"ב)', '@22(דף ל"ד ע"ב)')
+            data = re.sub('(@22\(דף \S*)\)', r'\1 ע"ב)', data)
+        comments = parse_text(data, mas, 'Beur HaGra')
+        comments = match_base(comments)
+        all += match_between_refs(comments)
 
-if __name__ == '__main__':
-    coms_map = find_coms()
-    coms_details = get_details()
-    indexes = [ind for ind in library.get_indexes_in_category('Yerushalmi') if 'Jerusalem Talmud' in ind]
-    for ind in indexes:
-        library.get_index(ind).versionState().refresh()
-        mas = ind.replace(f'{YERUSHALMI} ', '')
-        he_mas = library.get_index(ind).get_title('he').replace(f'{HEB_YER} ', '')
-        print(he_mas)
-        coms = coms_map[he_mas]
-        for com_name in coms:
-            if com_name not in coms_details:
-                continue
-            en_com = coms_details[com_name]['en']
-            fname = f'csvs/{en_com}/{mas}.csv'
-            '''if os.path.isfile(fname):
-                continue'''
-            if 'ביאור' not in com_name:
-                continue
-            print(com_name)
-            comments = parse_com(com_name, mas, he_mas)
-
-            try:
-                os.mkdir(f'{os.getcwd()}/csvs/{en_com}')
-            except FileExistsError:
-                pass
-
-            with open(fname, 'w', encoding='utf-8', newline='') as fp:
-                writer = csv.DictWriter(fp, fieldnames=['masechet', 'commentary', 'perek', 'halakha', 'page', 'comment', 'dh', 'base text ref', 'base text'])
-                writer.writeheader()
-                for row in comments:
-                    writer.writerow(row)
-
-        with open('vilna_mapping.json', 'w') as fp:
-            json.dump(VILNA_MAPPING, fp)
-
+with open('gra.csv', 'w', encoding='utf-8', newline='') as fp:
+    writer = csv.DictWriter(fp, fieldnames=['file', 'masechet', 'commentary', 'perek', 'halakha', 'page', 'comment', 'dh',
+                                            'base text ref', 'base text'])
+    writer.writeheader()
+    for row in all:
+        row['file'] = file
+        writer.writerow(row)
+with open('vilna_mapping.json', 'w') as fp:
+    json.dump(VILNA_MAPPING, fp)
 
