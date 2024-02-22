ec2b2970c24e785ffffe8fcfbd49f40563ae771f
saengel <sarah@sefaria.org>
2024-02-22 12:05:43 +0200

    feat(Megaleh Amukot): First pass at matching

--- sources/Content_Quality/megaleh_amukot/linking_ma.py
+++ sources/Content_Quality/megaleh_amukot/linking_ma.py
@@ -2,7 +2,9 @@ import django
 
 django.setup()
 
+import re
 from sefaria.model import *
+from linking_utilities.dibur_hamatchil_matcher import match_text
 
 
 # Run DH on each parasha
@@ -16,6 +18,20 @@ def get_parasha_text_ref(section_title):
     elif parasha == "Matot Masei":
         return Ref("Matot-Masei")
 
+def extract_dibbur_hamatchil(txt):
+    dhm = re.findall(r"<b>(.*?)</b>", txt, re.DOTALL)
+    if len(dhm) < 1:
+        return ""
+    return dhm[0]
+
+def attempt_to_match(base_words, comment_list):
+    results = match_text(base_words,
+                         comment_list,
+                         dh_extract_method=extract_dibbur_hamatchil,
+                         lang='en')
+    print(results)
+    return results['matches']
+
 
 if __name__ == '__main__':
     ma = Index().load({'title': 'Megaleh Amukot on Torah'})
@@ -24,5 +40,17 @@ if __name__ == '__main__':
     for section_title in sections:
         parasha_text_ref = get_parasha_text_ref(section_title)
 
-        # TODO - match goes here.
-        print(f"{section_title} <<>> {parasha_text_ref}")
+        if not parasha_text_ref:
+            # Skip holidays
+            continue
+
+        parasha_text = parasha_text_ref.text(lang="he").text
+        parasha_text_words = []
+        for perek in parasha_text:
+            for pasuk in perek:
+                pasuk_cleaned = pasuk.replace("-", " ")
+                parasha_text_words += pasuk_cleaned.split(" ")
+
+        commentary_text = section_title.text(lang="he").text
+        attempt_to_match(base_words=parasha_text_words, comment_list=commentary_text)
+        # print(f"{section_title} <<>> {parasha_text_ref}")
