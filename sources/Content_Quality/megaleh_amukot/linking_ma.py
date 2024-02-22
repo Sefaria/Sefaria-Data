b3dd821bf8d6e1a2f41807fc9ef41df6caff3fbf
saengel <sarah@sefaria.org>
2024-02-22 13:40:30 +0200

    feat(Megaleh Amukot): Working preliminary matches, returning valid matches

--- sources/Content_Quality/megaleh_amukot/linking_ma.py
+++ sources/Content_Quality/megaleh_amukot/linking_ma.py
@@ -18,19 +18,23 @@ def get_parasha_text_ref(section_title):
     elif parasha == "Matot Masei":
         return Ref("Matot-Masei")
 
+
 def extract_dibbur_hamatchil(txt):
     dhm = re.findall(r"<b>(.*?)</b>", txt, re.DOTALL)
     if len(dhm) < 1:
         return ""
-    return dhm[0]
+    dhm = re.sub(r'[^\w\s]', '', dhm[0])
+    return dhm
+
 
 def attempt_to_match(base_words, comment_list):
     results = match_text(base_words,
                          comment_list,
                          dh_extract_method=extract_dibbur_hamatchil,
-                         lang='en')
-    print(results)
-    return results['matches']
+                         word_threshold=0,
+                         char_threshold=0,
+                         lang='he')
+    return results
 
 
 if __name__ == '__main__':
@@ -44,13 +48,19 @@ if __name__ == '__main__':
             # Skip holidays
             continue
 
-        parasha_text = parasha_text_ref.text(lang="he").text
+        parasha_text = parasha_text_ref.text(lang="he", vtitle="Tanach with Text Only").text
         parasha_text_words = []
+        # TODO - Get the specific ref
         for perek in parasha_text:
             for pasuk in perek:
                 pasuk_cleaned = pasuk.replace("-", " ")
                 parasha_text_words += pasuk_cleaned.split(" ")
 
         commentary_text = section_title.text(lang="he").text
-        attempt_to_match(base_words=parasha_text_words, comment_list=commentary_text)
-        # print(f"{section_title} <<>> {parasha_text_ref}")
+        print(f"{section_title} <<>> {parasha_text_ref}")
+        match = attempt_to_match(base_words=parasha_text_words, comment_list=commentary_text)
+
+        for i in range(len(match["matches"])):
+            if match["matches"][i] != (-1, -1):
+                print(f"Score: {match['matches'][i]} for {match['match_text'][i]}")
+
