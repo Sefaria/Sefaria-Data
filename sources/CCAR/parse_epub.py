6375ca27fd5e01fd7161e6153c8f58b15c066fbf
stevekaplan123 <stevek004@gmail.com>
2024-06-30 14:43:46 +0300

    started CCAR

---
+++ sources/CCAR/parse_epub.py
@@ -0,0 +1,113 @@
+from ebooklib import epub
+import ebooklib
+from bs4 import BeautifulSoup
+from bs4 import Tag, NavigableString
+import re
+
+def remove_unwanted_tags(soup_element):
+    # List of allowed tags
+    allowed_tags = {'i', 'b', 'u', 'small', 'img', 'a'}
+
+    # Iterate through all descendants recursively
+    for tag in soup_element.find_all(recursive=True):
+        if tag.name not in allowed_tags:
+            # Decompose removes the tag and its children from the tree
+            tag.decompose()
+def parse(children):
+    elements = list(children)
+
+    # Initialize variables to keep track of the current chapter
+    current_chap = None
+    parent_chap = None
+    parents = []
+    for elem in elements:
+        # Check for 'chap' elements
+        if not isinstance(elem, Tag):
+            continue
+        classes = str(elem.get('class', []))
+        ignore_words = ['new-head', 'page-break-before:always;', 'box1', 'sidebar', 'boxv']
+        ignoring = False
+        for ignore in ignore_words:
+            if elem.name == 'div' and ignore in classes:
+                ignoring = True
+        if elem.name == "table":
+            ignoring = True
+        if ignoring:
+            continue
+        if elem.name == 'div' and ('chap' in classes or 'note' in classes):
+            if parent_chap and len(parent_chap) == 1:
+                parent_chap.append(elem.contents[0])
+            elif parent_chap:
+                parents.append(parent_chap)
+                parent_chap = elem
+            else:
+                remove_unwanted_tags(elem)
+                parent_chap = elem
+        elif parent_chap:
+            parent_chap.append(elem)
+
+    parents.append(parent_chap)
+    return parents
+
+def identify_chapters(parents):
+    for parent in parents:
+        classes = str(parent.get('class', []))
+        chapter = -1
+        if 'note' in classes:
+            for child in parent.children:
+                if isinstance(child, NavigableString):
+                    continue
+                child_classes = str(child.get('class', []))
+                if 'head' in child_classes:
+                    match = re.search(r'\((\d+):', child.text)
+
+                    # Extract and print the chapter number if found
+                    if match:
+                        chapter = int(match.group(1))
+                        print(f"Chapter: {chapter}")
+                    else:
+                        print("Chapter not found")
+                elif re.search(r'^(\d+)\.', child.text) and chapter >= 1:
+                    for text_node in child.find_all(text=True):
+                        match = re.search(r'^(\d+)\.', child.text)
+                        if match and match.group(0) in text_node:
+                            new_text = text_node.replace(match.group(0), f"{chapter}:{match.group(0)}")
+                            text_node.replace_with(new_text)
+
+
+    return parents
+
+
+
+# Read the ePUB file
+book = epub.read_epub('ISBN_9780881232837.epub')
+
+# Store extracted text
+text_content = []
+import os
+output_dir = 'images'
+if not os.path.exists(output_dir):
+    os.mkdir(output_dir)
+# Iterate over each item in the book
+for item in book.get_items():
+    if item.get_type() == ebooklib.ITEM_DOCUMENT:
+        # Use BeautifulSoup to parse HTML
+        soup = BeautifulSoup(item.get_content(), 'html.parser')
+        # with open(f"{item.id}.html", 'w', encoding='utf-8') as f:
+        #     f.write(soup.prettify())
+        if 'chap' in item.id:
+            with open(f"parsed_HTML/{item.id}.html", 'w', encoding='utf-8') as f:
+                parents = parse(soup.find('body').children)
+                parents = identify_chapters(parents)
+                combined_soup = BeautifulSoup('', 'html.parser')
+
+                # Append each element to the container
+                for element in parents:
+                    combined_soup.append(element)
+                f.write(combined_soup.prettify())
+    if item.get_type() == ebooklib.ITEM_IMAGE:
+        # Get the image name and content
+        image_name = os.path.basename(item.file_name)
+        image_data = item.content
+        with open(os.path.join(output_dir, image_name), 'wb') as image_file:
+            image_file.write(image_data)
