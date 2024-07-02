e2e99e50b0727be70577519fa4ee9afc3ff46405
stevekaplan123 <stevek004@gmail.com>
2024-07-02 09:47:41 +0300

    CCAR parsed HTML includes ref and orig_ref in data

---
+++ sources/CCAR/create_index.py
@@ -0,0 +1,35 @@
+from sources.functions import *
+nodes = """Post-Biblical Interpretations"
+Contemporary Reflection
+Another View""".splitlines()
+
+root = SchemaNode()
+root.key = "The Torah; A Women's Commentary"
+root.add_primary_titles(root.key, "")
+for book in ["Genesis", "Exodus", "Leviticus", "Numbers", "Deuteronomy"]:
+    book_node = JaggedArrayNode()
+    book_node.key = book
+    book_node.add_primary_titles(book, library.get_index(book).get_primary_title('he'))
+    book_node.add_structure(["Chapter", "Verse", "Paragraph"])
+    book_node.depth = 3
+    book_node.validate()
+    root.append(book_node)
+
+for node in nodes:
+    child = SchemaNode()
+    child.key = node
+    child.add_primary_titles(child.key, "")
+    parshiot = []
+    for book in ["Genesis", "Exodus", "Leviticus", "Numbers", "Deuteronomy"]:
+        parshiot += [Term().load({"name": x["sharedTitle"]}) for x in library.get_index(book).alt_structs["Parasha"]["nodes"]]
+    parshiot_nodes = []
+    for parsha in parshiot:
+        parsha_node = JaggedArrayNode()
+        parsha_node.key = parsha.name
+        parsha_node.add_primary_titles(parsha.get_primary_title('en'), parsha.get_primary_title('he'))
+        parsha_node.add_structure(["Paragraph"])
+        parsha_node.depth = 1
+        parsha_node.validate()
+        child.append(parsha_node)
+    child.validate()
+    root.append(child)
--- sources/CCAR/parse_epub.py
+++ sources/CCAR/parse_epub.py
@@ -3,10 +3,29 @@ import ebooklib
 from bs4 import BeautifulSoup
 from bs4 import Tag, NavigableString
 import re
+from sources.functions import *
+import difflib
 
+def get_parasha(name):
+    url = "http://localhost:8000/api/name/"+name
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an error for bad status codes
+    except requests.RequestException as e:
+        print(f"An error occurred: {e}")
+        return
+
+    try:
+        data = response.json()
+    except ValueError:
+        print("Unable to parse JSON response.")
+        return
+
+    return [x for x in data["completion_objects"] if x['type'] == "Topic"][0]
 def remove_unwanted_tags(soup_element):
     # List of allowed tags
-    allowed_tags = {'i', 'b', 'u', 'small', 'img', 'a'}
+    allowed_tags = {'i', 'b', 'u', 'small', 'a'}
 
     # Iterate through all descendants recursively
     for tag in soup_element.find_all(recursive=True):
@@ -49,11 +68,16 @@ def parse(children):
     parents.append(parent_chap)
     return parents
 
+def extract_text(element):
+   if isinstance(element, NavigableString):
+       return str(element)
+   elif element is not None and hasattr(element, 'get_text'):
+       return element.get_text()
+   return ''
 def identify_chapters(parents):
     for parent in parents:
-        classes = str(parent.get('class', []))
         chapter = -1
-        if 'note' in classes:
+        if parent.get('class', []) == ['note']:
             for child in parent.children:
                 if isinstance(child, NavigableString):
                     continue
@@ -65,54 +89,124 @@ def identify_chapters(parents):
                     if match:
                         chapter = int(match.group(1))
                 elif re.search(r'^(\d+)', child.text) and chapter >= 1:
-                    for node in child:
-                        if isinstance(node, Tag) and node.name in ['b', 'i']:
-                            match = re.search(r'^(\d+)', node.text)
-                            already_pasuk = re.search(r'^\d+:\d+', node.text)
-                            if match and already_pasuk is None:
-                                node.string = node.text.replace(match.group(0), f"{chapter}:{match.group(0)}", 1)
+                    node = child.contents[0]
+                    match = re.search(r'^(\d+)', extract_text(node))
+                    already_pasuk = re.search(r'^\d+:\d+', extract_text(node))
+                    new_text = extract_text(node)
+                    if match and already_pasuk is None:
+                        new_text = new_text.replace(match.group(0), f"{chapter}:{match.group(0)}", 1)
+                    new_node = Tag(name="b")
+                    new_node.string = new_text
+                    node.replace_with(new_node)
+                    if '–' in new_text:
+                        child.attrs["orig_ref"] = new_text.replace(".", "")
+                        new_node.string = new_text.split('–')[0]+"."
+
+
                 # elif re.search("<b><i>\D+</i></b>", str(child)):
             found_verse = False
             prepend = []
-            for c, child in enumerate(parent.children):
+            diff = 0
+            for c in range(len(parent.contents)):
+                child = parent.contents[c+diff]
+                child_classes = str(child.get('class', []))
                 if re.search(r'^(\d+)', child.text) is None:
-                    if found_verse:
+                    if found_verse and 'head' not in child_classes:
                         br_tag = Tag(name='br')
                         prev_child_num = 1
-                        prev_child = parent.contents[c - prev_child_num]
+                        prev_child = parent.contents[c - prev_child_num + diff]
                         while prev_child.text == "":
                             prev_child_num += 1
-                            prev_child = parent.contents[c - prev_child_num]
-                        parent.contents[c - prev_child_num].append(br_tag)
+                            prev_child = parent.contents[c - prev_child_num + diff]
+                        if isinstance(prev_child.contents[-1], NavigableString) or prev_child.contents[-1].name != 'br':
+                            prev_child.append(br_tag)
                         for x in list(child.children):
-                            parent.contents[c - prev_child_num].append(x)
+                            prev_child.append(x)
                         child.string = ""
                     else:
                         prepend.append(child)
                 else:
+                    verse_and_chapter = re.search(r'^(\d+:\d+)', child.text).group(0)
+                    child.contents[0].string = child.contents[0].text.replace(verse_and_chapter+'. ', '', 1)
+                    child.contents[0].string = child.contents[0].text.replace(verse_and_chapter+'.', '', 1)
+                    child.contents[0].string = child.contents[0].text.replace(verse_and_chapter, '', 1).strip()
+
+                    child.attrs["ref"] = verse_and_chapter
                     if len(prepend) > 0:
                         for i, x in enumerate(prepend):
                             child.insert(i, x)
+                            diff -= 1
                         prepend = []
                     found_verse = True
 
-
-
+            for x in parent.find_all("div", {"class": "tab-en1"}):
+                if x.text.strip() == "":
+                    x.decompose()
     return parents
 
+def extract_chapters(parents, book_dict):
+    for parent in parents:
+        chapter = -1
+        if parent.get('class', []) == ['note']:
+            for child in parent.children:
+                if 'ref' in child.attrs:
+                    chapter, verse = child.attrs["ref"].split(':')
+                    chapter = int(chapter)
+                    verse = int(verse)
+                    if verse in book_dict[chapter]:
+                        book_dict[chapter][verse].append(Tag(name="br"))
+                        book_dict[chapter][verse].append(child)
+                    else:
+                        book_dict[chapter][verse] = child
+                    for grandchild in child:
+                        if grandchild.name == "div" and 'head' in str(grandchild.get('class', [])):
+                            grandchild.append(Tag(name="br"))
+                        if grandchild.name == "div" and 'head' not in str(grandchild.get('class', [])):
+                            grandchild.append(Tag(name="br"))
+                    for grandchild in child:
+                        if grandchild.text == "Commentary":
+                            grandchild.decompose()
+                        if grandchild.text.strip == "":
+                            grandchild.decompose()
+    return book_dict
+
+def extract_book(parents):
+    #     for p in parshiot:
+    #         if p in parents[0].text:
+    #             found.append(book)
+    print(parents[0].contents[-1].contents[-1].text)
+    found = []
+    parshiot = []
+    for book in ["Genesis", "Exodus", "Leviticus", "Numbers", "Deuteronomy"]:
+        parshiot += [Term().load({"name": x["sharedTitle"]}).get_primary_title('en') for x in library.get_index(book).alt_structs["Parasha"]["nodes"]]
+    closest_matches = difflib.get_close_matches(parents[0].contents[-1].contents[-1].text, parshiot, n=1, cutoff=0.0)
+    if closest_matches:
+        closest_match = closest_matches[0]
+        for book in ["Genesis", "Exodus", "Leviticus", "Numbers", "Deuteronomy"]:
+            parshiot = [Term().load({"name": x["sharedTitle"]}).get_primary_title('en') for x in
+                         library.get_index(book).alt_structs["Parasha"]["nodes"]]
+            if closest_match in parshiot:
+                return book
+    return None
 
 
 # Read the ePUB file
-book = epub.read_epub('ISBN_9780881232837.epub')
+book_file = epub.read_epub('ISBN_9780881232837.epub')
 
 # Store extracted text
 text_content = []
+books = {}
+for book in ["Genesis", "Exodus", "Leviticus", "Numbers", "Deuteronomy"]:
+    books[book] = defaultdict(dict)
+    chapters = library.get_index(book).schema['lengths'][0]
+    for i in range(chapters):
+        books[book][i+1] = {}
 import os
 output_dir = 'images'
 if not os.path.exists(output_dir):
     os.mkdir(output_dir)
 # Iterate over each item in the book
-for item in book.get_items():
+for item in book_file.get_items():
     if item.get_type() == ebooklib.ITEM_DOCUMENT:
         # Use BeautifulSoup to parse HTML
         soup = BeautifulSoup(item.get_content(), 'html.parser')
@@ -128,15 +222,23 @@ for item in book.get_items():
             with open(f"parsed_HTML/{item.id}.html", 'w', encoding='utf-8') as f:
                 parents = parse(soup.find('body').children)
                 parents = identify_chapters(parents)
+                book_title = extract_book(parents)
+                extract_chapters(parents, books[book_title])
                 combined_soup = BeautifulSoup('', 'html.parser')
 
                 # Append each element to the container
                 for element in parents:
                     combined_soup.append(element)
-                f.write(combined_soup.prettify())
+                f.write(str(combined_soup))
     if item.get_type() == ebooklib.ITEM_IMAGE:
         # Get the image name and content
         image_name = os.path.basename(item.file_name)
         image_data = item.content
         with open(os.path.join(output_dir, image_name), 'wb') as image_file:
             image_file.write(image_data)
+
+for book in ["Genesis", "Exodus", "Leviticus", "Numbers", "Deuteronomy"]:
+    book_dict = books[book]
+    for chapter in book_dict:
+        for verse in book_dict[chapter]:
+            book_dict[chapter][verse] = bleach.clean(str(book_dict[chapter][verse]), strip=True, tags=["i", "b", 'br']).replace("<br><br>", "<br>")
