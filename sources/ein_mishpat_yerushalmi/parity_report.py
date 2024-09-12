3ded450b9a6b450f12ac78d1463fa1b7b2e37427
yonadavGit <yonadav.le@gmail.com>
2024-09-12 13:38:34 +0300

    (ein mishpat): groundworks for parity report

--- sources/ein_mishpat_yerushalmi/parity_report.py
+++ sources/ein_mishpat_yerushalmi/parity_report.py
@@ -36,7 +36,6 @@ def concatenate_lines(input_string):
 def infer_footnotes_links(html_content, filename=""):
     divs_text = get_divs_starting_with_text(html_content, 'עין משפט ונר מצוה')
     markers_footnotes = []
-    # linker = library.get_linker("he")
     for div_text in divs_text:
         lines = concatenate_lines(div_text)
         lines_starting_with_number = [
@@ -47,12 +46,7 @@ def infer_footnotes_links(html_content, filename=""):
             marker = match.group() if match else None
             if match:
                 print(match.string)
-                # print(filename)
-# def get_html_files(directory):
-#     for root, dirs, files in os.walk(directory):
-#         for file in files:
-#             if file.endswith(".html"):
-#                 yield os.path.join(root, file)
+
 def get_html_files(directory):
     html_files = []
     for root, dirs, files in os.walk(directory):
@@ -69,36 +63,31 @@ def get_html_files(directory):
 def extract_first_number(string):
     match = re.search(r'\d+', string)
     return int(match.group()) if match else None
-def simple_validation():
-    for wikifile in get_html_files('wiki_data'):
-        # print(wikifile)
-        move_to_next_chapter = False
-        comments_index_for_perek = 0
-        with open(wikifile, 'r', encoding='utf-8') as file:
-            html_content = file.read()
-        divs_text = get_divs_starting_with_text(html_content, 'עין משפט ונר מצוה')
-        markers_footnotes = []
-        for div_text in divs_text:
-            lines = concatenate_lines(div_text)
-            lines_starting_with_number = [
-                line for line in lines if
-                line.lstrip() and line.lstrip().split() and line.lstrip().split()[0][0].isdigit()
-            ]
-            for line in lines_starting_with_number:
-                comments_index_for_perek += 1
-                match = re.search(r'[\u0590-\u05FF]+_[\u0590-\u05FF]+\b', line)
-                if not match:
-                    continue
-                marker = match.group() if match else None
-                serial_number = extract_first_number(match.string)
-                if serial_number != comments_index_for_perek:
-                    print(wikifile)
-                    print(f"{match.string}")
-                    move_to_next_chapter = True
-                    break
-            if move_to_next_chapter:
-                break
 
+def infer_sefaria_segment_for_markers(html_content, masechet_name, chapter_num):
+    pattern = r'\b[\u0590-\u05FF]+_[\u0590-\u05FF]+\b'
+    html_content = re.sub(pattern, lambda m: f"${m.group()}$", html_content)
+    html_content = remove_divs_starting_with_text(html_content, 'עין משפט ונר מצוה')
+    html_content = remove_paragraphs_starting_with_text(html_content, "קישורים")
+    html_content = remove_elements_by_tag(html_content, ['h3', 'figure'])
+    plain_text = html_to_text(html_content)
+    #remove
+    #מפרשים:
+    #   ^[דף ג עמוד ב]
+    #from page
+    plain_text = re.sub(r"מפרשים:\s*\^\[.*?\]", '', plain_text)
+    # Using re.findall to find all matches
+    matches = re.compile(r'\$.+?\$').finditer(plain_text)
+    comments = []
+    for match in matches:
+        match_text = plain_text[match.regs[0][0]:match.regs[0][1]]
+        if all(term not in match_text for term in ['מסכת', 'פרק', 'ירושלמי']):
+            extraction = extract_with_context(plain_text, (match.regs[0][0], match.regs[0][1]), 0, 5)
+            comments.append(extraction)
+
+    for index, c in enumerate(comments):
+        print(str(index) + ": " + c)
+    link_markers_to_sefaria_segments(comments, masechet_name, chapter_num)
 
 if __name__ == '__main__':
     for wikifile in get_html_files('wiki_data'):
