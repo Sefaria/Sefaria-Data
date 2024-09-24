5f3c3ab18258231b3db3d8c353aa5461ab84ca4f
yonadavGit <yonadav.le@gmail.com>
2024-09-24 17:27:42 +0300

    (Kehot CHP Chumash): many more fixes

--- sources/kehot_chp_chumash/handle_chp.py
+++ sources/kehot_chp_chumash/handle_chp.py
@@ -3,11 +3,46 @@ import django
 
 django.setup()
 from sefaria.model import *
+from sefaria.tracker import modify_bulk_text
 import os
 from bs4 import BeautifulSoup
 import re
 import csv
+superuser_id = 171118
 
+
+def content_fixes(html_content):
+    html_content = html_content.replace("Rashi on Leviticus 24:1", "Rashi on Leviticus 24:2")
+    return html_content
+
+def ingest_version(book, map_text):
+    index = library.get_index(f"{book}")
+    cur_version = VersionSet({'title': f'{book}', 'versionTitle': "The Torah; Chabad House Publications, Los Angeles, 2015-2023"})
+    if cur_version.count() > 0:
+        cur_version.delete()
+        print("deleted existing version")
+    chapter = index.nodes.create_skeleton()
+    version = Version({"versionTitle": "The Torah; Chabad House Publications, Los Angeles, 2015-2023",
+                       "versionSource": "https://www.chabadhousepublications.org/books",
+                       "title": f"{book}",
+                       "language": "en",
+                       "chapter": chapter,
+                       "digitizedBySefaria": True,
+                       "license": "PD",
+                       "status": "locked"
+                       })
+
+    modify_bulk_text(superuser_id, version, map_text)
+
+
+def partition_dict_by_first_word(d):
+    partitioned_dicts = {}
+    for key, value in d.items():
+        first_word = key.split()[0]
+        if first_word not in partitioned_dicts:
+            partitioned_dicts[first_word] = {}
+        partitioned_dicts[first_word][key] = value
+    return partitioned_dicts
 
 def extract_elements_with_class(html_content, *class_names):
     soup = BeautifulSoup(html_content, 'html.parser')
@@ -35,6 +70,7 @@ def file_name_to_book(file_name):
 def replace_bold_and_italic_span(text):
     # pattern = r'<span class="bold">\s*(.*?)\s*</span>'
 
+    small_patterns = [r'<span class="bold CharOverride-6">(.*?)</span>',]
     bold_patterns = [r'<span class="bold">(.*?)</span>',
                 r'<span class="Peshat-PN">(.*?)</span>',
                 r'<span class="bd">(.*?)</span>']
@@ -45,10 +81,15 @@ def replace_bold_and_italic_span(text):
     def replace_italic(match):
         content = match.group(1)
         return f'@@@{content}/@@@'
+    def replace_small(match):
+        content = match.group(1)
+        return f'$SMALL&BOLD{content}/$SMALL&BOLD'
     for pattern in bold_patterns:
         text = re.sub(pattern, replace_bold, text)
     for pattern in italic_patterns:
         text = re.sub(pattern, replace_italic, text)
+    for pattern in small_patterns:
+        text = re.sub(pattern, replace_small, text)
     return text
 
 current_chapter_num = None
@@ -80,6 +121,8 @@ def extract_verse_address(verse_text):
 
     verse_text = verse_text.replace('$', '')
     verse_text = verse_text.replace('#', '')
+    verse_text = re.sub(r"FOOTNOTE_MARK(.*?)/FOOTNOTE_MARK", "", verse_text)
+    verse_text = re.sub(r"FOOTNOTE(.*?)/FOOTNOTE", "", verse_text )
     address = extract_number_pair(verse_text)
     if address:
         c_num, v_num = address
@@ -89,7 +132,10 @@ def extract_verse_address(verse_text):
     if not begins_with_number(verse_text):
         return current_chapter_num, current_verse_num
     if extract_first_number(verse_text):
-        v_num = int(extract_first_number(verse_text))
+        first_num = int(extract_first_number(verse_text))
+        if first_num == current_chapter_num:
+            return current_chapter_num, current_verse_num
+        v_num = first_num
         current_verse_num = v_num
         return current_chapter_num, v_num
     else:
@@ -117,6 +163,8 @@ def find_and_remove_b_num_b(s: str, n: int):
 def format_text_map(text_map):
     for key in text_map:
         text = text_map[key]
+        text = text.replace('/$SMALL&BOLD', '</b></small>')
+        text = text.replace('$SMALL&BOLD', '<b><small>')
         text = replace_symbols_with_bold_tags(text)
         text = find_and_remove_b_num_b(text, 40)
         match = re.search(r"<b>\d+", text)
@@ -127,13 +175,18 @@ def format_text_map(text_map):
         text = text.replace('/FOOTNOTE_MARKER', "</sup>")
         text = text.replace('FOOTNOTE_MARKER', '<sup class="footnote-marker">')
         text = text.replace('/FOOTNOTE', '</i>')
-        text = text.replace('FOOTNOTE', '<i class="footnote" style="display: inline;">')
+        text = text.replace('FOOTNOTE', '<i class="footnote">')
         text_map[key] = text
     return text_map
 
 def clean_for_footnotes_extraction(html_content):
+    def replace_italicized_footnote(match):
+        content = match.group(1)
+        return f'@@@{content}/@@@'
     html_content = html_content.replace('<span class="FNN-Peshat">&nbsp;</span>', ' ')
     html_content = html_content.replace('<span class="FNN-Peshat">&#160;</span>', ' ')
+    html_content = re.sub('<span class="O-it-text-in-footnotes">(.*?)</span>', replace_italicized_footnote, html_content)
+    html_content = re.sub('<span class="it-caption">(.*?)</span>', replace_italicized_footnote, html_content)
     # itcaption_pattern = r'<span class="it-caption">(.*?)</span>'
     # def replace_itcaption(match):
     #     content = match.group(1)
@@ -167,7 +220,9 @@ def extract_footnotes(html_content):
 def insert_footnotes(html_content, footnotes_map):
     soup = BeautifulSoup(html_content, 'html.parser')
     # Find all spans with the specific class and replace the text
-    for span in soup.find_all('span', class_='FNR-Peshat _idGenCharOverride-2'):
+    # for span in soup.find_all('span', class_='FNR-Peshat _idGenCharOverride-2'):
+    for span in soup.find_all('span', class_='FNR-Peshat'):
+
         if span.text in footnotes_map:
             span.string = f'FOOTNOTE_MARKER{span.text}/FOOTNOTE_MARKER FOOTNOTE{footnotes_map[span.string]}/FOOTNOTE'
     modified_html = str(soup)
@@ -189,6 +244,7 @@ if __name__ == '__main__':
         print(current_book)
         with open(file_path, 'r', encoding='utf-8', errors='ignore') as file:
             html_content = file.read()
+            html_content = content_fixes(html_content)
             footnotes_map = extract_footnotes(html_content)
             html_content = insert_footnotes(html_content, footnotes_map)
             html_content = replace_bold_and_italic_span(html_content)
@@ -196,10 +252,11 @@ if __name__ == '__main__':
             elements = extract_elements_with_class(html_content, 'Peshat')
             for element in elements:
 
-                if "Upon emerging from the ark" in element.text:
-                    halt = True
+
 
                 address = extract_verse_address(element.text)
+                if "to affix the onyx stones to the upper ends of the Ephod’s shoulder straps," in element.text:
+                    address = (12, 13)
                 if not contains_english_letters(element.text):
                     continue
                 ref = f"{current_book} {address[0]}:{address[1]}"
@@ -215,4 +272,7 @@ if __name__ == '__main__':
     text_map = format_text_map(text_map)
     with open('output.csv', mode='w', newline='') as file:
         csv.writer(file).writerows([['Ref', 'Text']] + list(text_map.items()))
+    partitioned_map = partition_dict_by_first_word(text_map)
+    for book, map in partitioned_map.items():
+        ingest_version(book, map)
     print('hi')
