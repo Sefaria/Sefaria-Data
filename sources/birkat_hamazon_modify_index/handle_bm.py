7e350e822b4ffd97790d86950e52014a8f923637
yonadavGit <92536571+yonadavGit@users.noreply.github.com>
2025-03-02 13:16:21 +0200

    create fake node and adjust child hoisting logic in handle_bm.py

--- sources/birkat_hamazon_modify_index/handle_bm.py
+++ sources/birkat_hamazon_modify_index/handle_bm.py
@@ -8,19 +8,19 @@ if __name__ == '__main__':
     bm_index = library.get_index("Birkat Hamazon")
     main_parent = bm_index.nodes
     children_to_hoist = bm_index.nodes.children[0].children
-    # print("Creating fake node")
-    # fake_node = JaggedArrayNode()
-    # fake_node.key = "fake"
-    # fake_node.add_primary_titles("fake", "fake hebrew")
-    # fake_node.add_structure(["Paragraph"])
-    # fake_node.validate()
-    # attach_branch(fake_node, bm_index.nodes.children[0], 6)
+    print("Creating fake node")
+    fake_node = JaggedArrayNode()
+    fake_node.key = "fake"
+    fake_node.add_primary_titles("fake", "fake hebrew")
+    fake_node.add_structure(["Paragraph"])
+    fake_node.validate()
+    attach_branch(fake_node, bm_index.nodes.children[0], 6)
     for i, child in enumerate(children_to_hoist):
         if i == 6:
             break
-        bm_index = library.get_index("Birkat Hamazon")
+        bm_index = Index().load({"title":"Birkat Hamazon"})
         main_parent = bm_index.nodes
-        children_to_hoist = bm_index.nodes.children[i].children
-        change_parent(child, main_parent)
+        # children_to_hoist = bm_index.nodes.children[i].children
+        change_parent(child, main_parent, place=i)
     remove_branch(bm_index.nodes.children[0])
     print("bye")
