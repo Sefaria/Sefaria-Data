125b3d2448a946d65bd72a4d6dc107ca3b28f873
yonadavGit <92536571+yonadavGit@users.noreply.github.com>
2025-08-18 14:36:42 +0300

    Add validate_empty_footnotes.py to check for empty footnotes in Chasidic Commentary

--- sources/tanakh_passages/validate_empty_footnotes.py
+++ sources/tanakh_passages/validate_empty_footnotes.py
@@ -5,8 +5,14 @@ import re
 
 if __name__ == '__main__':
     all_seg_ref = Index().load({"title": "The Kehot Chumash; A Chasidic Commentary"}).all_segment_refs()
+    # for ref in all_seg_ref:
+    #     text = ref.text('en').text
+    #     matches = re.findall(r'<i\s+class="footnote"\s*>\s*</i>', text)
+    #     if matches:
+    #         print(f"{ref.normal()}")
+
     for ref in all_seg_ref:
         text = ref.text('en').text
-        matches = re.findall(r'<i\s+class="footnote"\s*>\s*</i>', text)
+        matches = re.findall(r'<i\s+class="O-it-text-in-footnotes"\s*>', text)
         if matches:
             print(f"{ref.normal()}")
